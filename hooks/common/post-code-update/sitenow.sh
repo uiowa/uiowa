954d29f64951dd4b465425c9148af7534a805ad1
Richard B. Porter <rbp@richardbporter.com>
2019-03-11 13:50:36 -0500

    add sitenow cloud hook

---
+++ hooks/common/post-code-update/sitenow.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Cloud Hook: post-code-update
+#
+# The post-code-update hook runs in response to code commits.
+# When you push commits to a Git branch, the post-code-update hooks runs for
+# each environment that is currently running that branch. See
+# ../README.md for details.
+#
+# Usage: post-code-update site target-env source-branch deployed-tag repo-url
+#                         repo-type
+
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+
+# Configuration workflow for sitenow profile.
+/var/www/html/$site.$target_env/vendor/bin/drush mse 'csex database, updb, cim' --profile sitenow --yes;
