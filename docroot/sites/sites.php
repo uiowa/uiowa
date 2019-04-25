ce92ca2386ae3b5d4df38227649ce0ee4af38bdb
Richard B. Porter <richard-b-porter@uiowa.edu>
2019-04-25 13:49:53 -0500

    doc sites.php

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -2,13 +2,17 @@
 
 /**
  * @file
- * Directory aliases.
+ * Multisite directory aliases.
+ *
+ * These may need to be more restrictive depending on how acquia_purge
+ * determines the purge domains for multisites.
+ *
+ * @see https://www.drupal.org/project/acquia_purge/issues/3050554
  * @see example.sites.php
  */
 
-// Directory aliases for sitenow.uiowa.edu (default);
+// Directory aliases for sitenow.uiowa.edu (default).
 $sites['sitenow.uiowa.lndo.site'] = 'default';
 $sites['sitenow.dev.drupal.uiowa.edu'] = 'default';
 $sites['sitenow.test.drupal.uiowa.edu'] = 'default';
 $sites['sitenow.prod.drupal.uiowa.edu'] = 'default';
-
