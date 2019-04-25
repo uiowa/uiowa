bf12a8d8dc99563d5a32cb7d96b937f0fb671aa7
Richard B. Porter <richard-b-porter@uiowa.edu>
2019-04-25 11:53:39 -0500

    add sites.php

---
+++ docroot/sites/sites.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Directory aliases.
+ * @see example.sites.php
+ */
+
+ // Default - sitenow.uiowa.edu.
+$sites['sitenow.uiowa.lndo.site'] = 'default';
+$sites['sitenow.dev.drupal.uiowa.edu'] = 'default';
+$sites['sitenow.test.drupal.uiowa.edu'] = 'default';
+$sites['sitenow.prod.drupal.uiowa.edu'] = 'default';
