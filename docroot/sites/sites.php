b3cbcaa877ac30e703414fd1b4e1b2fbb0d2054a
Richard B. Porter <richard-b-porter@uiowa.edu>
2019-05-06 09:51:20 -0500

    use stage URIs for now

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -19,19 +19,19 @@ if (file_exists(__DIR__ . '/sites.local.php')) {
 // Directory aliases for the default site.
 $sites['uiowa.lndo.site'] = 'default';
 $sites['demo.dev.drupal.uiowa.edu'] = 'default';
-$sites['demo.test.drupal.uiowa.edu'] = 'default';
+$sites['demo.stage.drupal.uiowa.edu'] = 'default';
 $sites['demo.prod.drupal.uiowa.edu'] = 'default';
 
 // Directory aliases for hr.uiowa.edu.
 $sites['hr.uiowa.lndo.site'] = 'hr.uiowa.edu';
 $sites['hr-d8.dev.drupal.uiowa.edu'] = 'hr.uiowa.edu';
-$sites['hr-d8.test.drupal.uiowa.edu'] = 'hr.uiowa.edu';
+$sites['hr-d8.stage.drupal.uiowa.edu'] = 'hr.uiowa.edu';
 $sites['hr-d8.prod.drupal.uiowa.edu'] = 'hr.uiowa.edu';
 $sites['hr.uiowa.edu'] = 'hr.uiowa.edu';
 
 // Directory aliases for callctr.dentistry.uiowa.edu.
 $sites['dentistrycallctr.uiowa.lndo.site'] = 'callctr.dentistry.uiowa.edu';
 $sites['dentistrycallctr.dev.drupal.uiowa.edu'] = 'callctr.dentistry.uiowa.edu';
-$sites['dentistrycallctr.test.drupal.uiowa.edu'] = 'callctr.dentistry.uiowa.edu';
+$sites['dentistrycallctr.stage.drupal.uiowa.edu'] = 'callctr.dentistry.uiowa.edu';
 $sites['dentistrycallctr.prod.drupal.uiowa.edu'] = 'callctr.dentistry.uiowa.edu';
 $sites['callctr.dentistry.uiowa.edu'] = 'callctr.dentistry.uiowa.edu';
