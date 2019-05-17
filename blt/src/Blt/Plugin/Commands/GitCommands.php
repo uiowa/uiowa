3ca49668b5e9a8bdad63d0b70de40e18e749feb6
Richard B. Porter <richard-b-porter@uiowa.edu>
2019-05-17 14:25:57 -0500

    implement git command to clean acquia remote

---
+++ blt/src/Blt/Plugin/Commands/GitCommands.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Sitenow\Blt\Plugin\Commands;
+
+use Acquia\Blt\Robo\BltTasks;
+use Consolidation\AnnotatedCommand\CommandData;
+use Consolidation\AnnotatedCommand\CommandError;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Defines commands in the Sitenow namespace.
+ */
+class GitCommands extends BltTasks {
+
+  /**
+   * Delete all remote branches except master and develop.
+   *
+   * @command clean:acquia
+   */
+  public function cleanAcquiaRemote() {
+    $result = $this->taskExecStack()
+      ->exec('git ls-remote --heads acquia')
+      ->silent(TRUE)
+      ->run();
+
+    $output = $result->getMessage();
+    $heads = explode(PHP_EOL, $output);
+
+    $keep = [
+      'refs/heads/master',
+      'refs/heads/pipelines-build-master',
+      'refs/heads/pipelines-build-develop',
+    ];
+
+    $delete = [];
+
+    foreach ($heads as $head) {
+      list($sha, $ref) = explode("\t", $head);
+
+      if (!in_array($ref, $keep)) {
+        $delete[] = $ref;
+      }
+    }
+
+    $this->io()->section('Refs');
+    $this->io()->listing($delete);
+
+    if (!$this->confirm('You will delete all the branches above from the Acquia remote. Are you sure?')) {
+      throw new \Exception('Aborted.');
+    }
+    else {
+      foreach ($delete as $ref) {
+        $this->taskExecStack()
+          ->exec("git push --delete acquia {$ref}")
+          ->run();
+      }
+    }
+  }
+
+}
