24c55503a670e8eff5eeccf4163c9a0974204534
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2019-05-21 11:06:38 -0500

    Cleaning up iowasuperfund theme

--- docroot/sites/iowasuperfund/themes/iowasuperfund/gulpfile.js
+++ docroot/sites/iowasuperfund/themes/iowasuperfund/gulpfile.js
@@ -1,232 +1,80 @@
-// -------------------------------------
-//
-//   Gulpfile
-//
-// -------------------------------------
-//
-// Available tasks:
-//   `gulp`
-//   'gulp browser-sync'
-//   `gulp build`
-//   `gulp build:dev`
-//   `gulp clean`
-//   `gulp clean:css`
-//   `gulp clean:styleguide`
-//   `gulp compile:sass`
-//   `gulp compile:styleguide`
-//   `gulp lint:js`
-//   `gulp lint:css`
-//   `gulp minify:css`
-//   `gulp serve`
-//   `gulp test:css`
-//   `gulp test:pa11y`
-//   `gulp watch`
-//   `gulp watch:js`
-//   `gulp watch:sass`
-//   `gulp watch:styleguide`
-//
-// -------------------------------------
+// Include gulp.
+const { src, dest, parallel, series, watch } = require('gulp');
+const config = require('./config.json');
 
-// -------------------------------------
-//   Modules
-// -------------------------------------
-//
-// gulp              : The streaming build system
-// gulp-autoprefixer : Prefix CSS
-// gulp-concat       : Concatenate files
-// gulp-clean-css    : Minify CSS
-// gulp-load-plugins : Automatically load Gulp plugins
-// gulp-parker       : Stylesheet analysis tool
-// gulp-plumber      : Prevent pipe breaking from errors
-// gulp-rename       : Rename files
-// gulp-sass         : Compile Sass
-// gulp-sass-glob    : Provide Sass Globbing
-// gulp-sass-lint    : Lint Sass
-// gulp-size         : Print file sizes
-// gulp-sourcemaps   : Generate sourcemaps
-// gulp-uglify       : Minify JavaScript with UglifyJS
-// gulp-util         : Utility functions
-// gulp-watch        : Watch stream
-// browser-sync      : Device and browser testing tool
-// del               : delete
-// eslint            : JavaScript code quality tool
-// run-sequence      : Run a series of dependent Gulp tasks in order
-// -------------------------------------
+// Include plugins.
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const prefix = require('gulp-autoprefixer');
+const glob = require('gulp-sass-glob');
+const sourcemaps = require('gulp-sourcemaps');
+const browsersync = require('browser-sync');
 
-// -------------------------------------
-//   Front-End Dependencies
-// -------------------------------------
-// breakpoint-sass       : Really Simple Media Queries with Sass
-// kss                   : A methodology for documenting CSS and building style guides
-// node-sass             : Wrapper around libsass
-// node-sass-import-once : Custom importer for node-sass that only allows a file to be imported once
-// susy                  : Sass power-tools for web layout
-// typey                 : A complete framework for working with typography in sass
-// -------------------------------------
-
-/* global require */
-
-var gulp = require('gulp');
-// Setting pattern this way allows non gulp- plugins to be loaded as well.
-var plugins = require('gulp-load-plugins')({
-  pattern: '*',
-  rename: {
-    'node-sass-import-once': 'importOnce',
-    'gulp-sass-glob': 'sassGlob',
-    'run-sequence': 'runSequence',
-    'gulp-clean-css': 'cleanCSS',
-    'gulp-stylelint': 'gulpStylelint',
-    'gulp-eslint': 'gulpEslint',
-    'gulp-babel': 'babel',
-    'gulp-util': 'gutil'
-  }
-});
-
-// Used to generate relative paths for style guide output.
-var path = require('path');
-
-// These are used in the options below.
+/*
+ * Directories here
+ */
 var paths = {
-  styles: {
-    source: 'patterns/',
-    destination: 'css/'
-  },
-  scripts: {
-    source: 'js/src',
-    destination: 'js/dist'
-  },
-  images: 'img/',
-  styleGuide: 'styleguide'
+  build: './assets/',
+  scss: './scss/'
 };
 
-// These are passed to each task.
-var options = {
-
-  // ----- Browsersync ----- //
-
-  browserSync: {
-    // Put your local site URL here to prevent Browsersync
-    // from prompting you to add additional scripts to your page.
-    proxy: {
-      target: 'https://iowasuperfund.uiowa.local.site'
-    },
-    open: 'external',
-    xip: true,
-    logConnections: true
-  },
-
-  // ----- CSS ----- //
-
-  css: {
-    files: path.join(paths.styles.destination, '**/*.css'),
-    file: path.join(paths.styles.destination, '/styles.css'),
-    destination: path.join(paths.styles.destination)
-  },
-
-  // ----- Sass ----- //
-
-  sass: {
-    files: path.join(paths.styles.source, '**/*.scss'),
-    file: path.join(paths.styles.source, 'styles.scss'),
-    destination: path.join(paths.styles.destination)
-  },
-
-  // ----- JS ----- //
-  js: {
-    files: path.join(paths.scripts.source, '**/*.js'),
-    destination: path.join(paths.scripts.destination)
-  },
-
-  // ----- Images ----- //
-  images: {
-    files: paths.images + '**/*.{png,gif,jpg,svg}',
-    destination: paths.images
-  },
-
-  // ----- eslint ----- //
-  jsLinting: {
-    files: {
-      theme: [
-        paths.scripts + '**/*.js',
-        '!' + paths.scripts + '**/*.min.js'
-      ],
-      gulp: [
-        'gulpfile.js',
-        'gulp-tasks/**/*'
-      ]
-    }
-
-  },
-
-  // ------ pa11y ----- //
-  pa11y: {
-    urls: [ // An array of urls to test.
-      // For testing in a travis environment:
-      // '/',
-      // '/themes/custom/iowasuperfund/styleguide'
-      // '/themes/custom/iowasuperfund/styleguide/section-components.html'
-    ],
-    ignore: [
-        'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Abs',
-        'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.BgImage',
-        'WCAG2AA.Principle1.Guideline1_4.1_4_3.G145.BgImage',
-        'WCAG2AA.Principle1.Guideline1_4.1_4_3.G145.Abs',
-        'WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.A.Empty'
-    ],
-    failOnError: true, // fail the build on error
-    showFailedOnly: true, // show errors only and override reporter
-    reporter: 'console',
-    includeWarnings: true, // including warnings by default. - set it to false to disable
-    includeNotices: true, // including notices by default. - set it to false to disable
-    hideElements: '.kss-markup, .kss-toolbar', // Prevents kss markup from triggering false positives.
-    log: {
-      debug: console.log.bind(console),
-      error: console.error.bind(console),
-      info: console.info.bind(console)
-    },
-    standard: 'WCAG2AA', // choose from Section508, WCAG2A, WCAG2AA, and WCAG2AAA
-    page: {
-      settings: {
-        loadImages: false,
-        userName: '', // .htacess username
-        password: '' // .htaccess password
+// SCSS bundled into CSS task
+function css() {
+  return src(config.css.src)
+    .pipe(sourcemaps.init())
+    .pipe(glob())
+    // Stay live and reload on error
+    .pipe(plumber({
+      handleError: function (err) {
+        console.log(err);
+        this.emit('end');
       }
-    },
-    threshold: { // Set to -1 for no threshold.
-      errors: 1,
-      warnings: 10,
-      notices: -1
-    },
-    chromeLaunchConfig: {
-      executablePath: '/usr/bin/google-chrome',
-      ignoreHTTPSErrors: true,
+    }))
+    .pipe(sass({
+      includePaths: [config.css.includePaths],
+      outputStyle: 'compressed'
+    }).on('error', function (err) {
+      console.log(err.message);
+      // sass.logError
+      this.emit('end');
+    }))
+    .pipe(prefix(['last 2 versions', '> 1%', 'ie 9', 'ie 10'], {
+      cascade: true
+    }))
+    //.pipe(minifyCSS())
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(config.css.dest));
+}
 
-      ignoreDefaultArgs: [
-        "--enable-potentially-annoying-security-features"
-      ]
-    }
-  }
-};
+// BrowserSync
+function browserSync() {
+  browsersync({
+    // server: {
+    //   baseDir: paths.build
+    // },
+    notify: false,
+    browser: "google chrome",
+    proxy: "http://iowasuperfund.uiowa.local.site"
+  });
+}
 
-// Tasks
-require('./gulp-tasks/browser-sync')(gulp, plugins, options);
-require('./gulp-tasks/build')(gulp, plugins, options);
-require('./gulp-tasks/clean')(gulp, plugins, options);
-require('./gulp-tasks/clean-css')(gulp, plugins, options);
-require('./gulp-tasks/compile-sass')(gulp, plugins, options);
-require('./gulp-tasks/compile-js')(gulp, plugins, options);
-require('./gulp-tasks/default')(gulp, plugins, options);
-require('./gulp-tasks/lint-js')(gulp, plugins, options);
-require('./gulp-tasks/lint-css')(gulp, plugins, options);
-require('./gulp-tasks/minify-css')(gulp, plugins, options);
-require('./gulp-tasks/serve')(gulp, plugins, options);
-require('./gulp-tasks/test-css')(gulp, plugins, options);
-require('./gulp-tasks/watch')(gulp, plugins, options);
-require('./gulp-tasks/pa11y')(gulp, plugins, options);
+// BrowserSync reload
+function browserReload() {
+  return browsersync.reload;
+}
 
-// Credits:
-//
-// - http://drewbarontini.com/articles/building-a-better-gulpfile/
-// - https://teamgaslight.com/blog/small-sips-of-gulp-dot-js-4-steps-to-reduce-complexity
-// - http://cgit.drupalcode.org/zen/tree/STARTERKIT/gulpfile.js?h=7.x-6.x
-// - https://github.com/google/web-starter-kit/blob/master/gulpfile.js
+// Watch files
+function watchFiles() {
+  // Watch SCSS changes
+  // todo: handle watching patterns in a consistent way
+  // todo: add watcher for template files
+  watch([paths.scss + '**/*.scss', './patterns/**/*.scss'], parallel(css))
+    .on('change', browserReload());
+}
+
+const watching = parallel(watchFiles, browserSync);
+
+//exports.js = js;
+exports.css = css;
+exports.default = parallel(css);
+exports.watch = watching;
