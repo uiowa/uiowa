0dd212ab655f91ea9dc06c5d597a2d07f12f7831
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2019-06-27 13:19:16 -0500

    js clean up, standards.

--- docroot/sites/brand.uiowa.edu/modules/brand_core/js/lockup-preview.behaviors.js
+++ docroot/sites/brand.uiowa.edu/modules/brand_core/js/lockup-preview.behaviors.js
@@ -3,29 +3,36 @@
  * Lockup Preview.
  */
 (function ($, Drupal) {
-  Drupal.behaviors.lockupPreview = {
-    attach: function(context, settings) {
-      $('#lockup-preview').hide();
-      if($("#edit-field-lockup-primary-unit-0-value").val() != ''){
-        $('#lockup-preview').show();
-        $('.lockup-stacked .primary-unit').text($("#edit-field-lockup-primary-unit-0-value").val());
-        $('.lockup-horizontal .primary-unit').text($("#edit-field-lockup-primary-unit-0-value").val());
-      }
-      if($("#edit-field-lockup-sub-unit-0-value").val() != ''){
-        $('#lockup-preview').show();
-        $('.lockup-stacked .sub-unit').text($("#edit-field-lockup-sub-unit-0-value").val());
-        $('.lockup-horizontal .sub-unit').text($("#edit-field-lockup-sub-unit-0-value").val());
-      }
-      $("#edit-field-lockup-primary-unit-0-value").on('input', function(event) {
-        $('#lockup-preview').show();
-        $('.lockup-stacked .primary-unit').text(event.target.value);
-        $('.lockup-horizontal .primary-unit').text(event.target.value);
-      });
-      $("#edit-field-lockup-sub-unit-0-value").on('input', function(event) {
-        $('#lockup-preview').show();
-        $('.lockup-stacked .sub-unit').text(event.target.value);
-        $('.lockup-horizontal .sub-unit').text(event.target.value);
-      });
-    }
-  };
+    Drupal.behaviors.lockupPreview = {
+        attach: function () {
+            var lockupPreview = $("#lockup-preview");
+            var primaryUnit = $("#edit-field-lockup-primary-unit-0-value");
+            var subUnit = $("#edit-field-lockup-sub-unit-0-value");
+
+            // Hide preview markup initially.
+            lockupPreview.hide();
+
+            // Show preview and inject content based on existing input or live input.
+            if (primaryUnit.val() !== "") {
+                $("#lockup-preview").show();
+                $(".lockup-stacked .primary-unit").text(primaryUnit.val());
+                $(".lockup-horizontal .primary-unit").text(primaryUnit.val());
+            }
+            if (subUnit.val() !== "") {
+                $("#lockup-preview").show();
+                $(".lockup-stacked .sub-unit").text(subUnit.val());
+                $(".lockup-horizontal .sub-unit").text(subUnit.val());
+            }
+            primaryUnit.on("input", function (event) {
+                $("#lockup-preview").show();
+                $(".lockup-stacked .primary-unit").text(event.target.value);
+                $(".lockup-horizontal .primary-unit").text(event.target.value);
+            });
+            subUnit.on("input", function (event) {
+                $("#lockup-preview").show();
+                $(".lockup-stacked .sub-unit").text(event.target.value);
+                $(".lockup-horizontal .sub-unit").text(event.target.value);
+            });
+        }
+    };
 })(jQuery, Drupal);
