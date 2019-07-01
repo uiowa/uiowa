fa6ebc089a694f84a09f260fed915758a022fef3
Richard B. Porter <richard-b-porter@uiowa.edu>
2019-07-01 08:53:59 -0500

    Rename uiowa_seo to sitenow_seo

--- docroot/modules/custom/sitenow_seo/src/ConfigOverrides/GoogleAnalyticsOverride.php
+++ docroot/modules/custom/sitenow_seo/src/ConfigOverrides/GoogleAnalyticsOverride.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\uiowa_seo\ConfigOverrides;
+namespace Drupal\sitenow_seo\ConfigOverrides;
 
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryOverrideInterface;
