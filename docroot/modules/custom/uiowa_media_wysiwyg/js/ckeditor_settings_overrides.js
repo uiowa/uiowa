ad9ae488fd495c2886a348c5ee879c7f0658a303
Ethan St. John <estjohn2000@gmail.com>
2019-07-18 13:55:14 -0500

    condensed code #326

--- docroot/modules/custom/uiowa_media_wysiwyg/js/ckeditor_settings_overrides.js
+++ docroot/modules/custom/uiowa_media_wysiwyg/js/ckeditor_settings_overrides.js
@@ -3,7 +3,7 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
     var dialogName = ev.data.name;
     var dialogDefinition = ev.data.definition;
 
-    if (dialogName == 'table') {
+    if (dialogName == 'table' || 'tableProperties') {
         // Get the properties tab reference.
         var infoTab = dialogDefinition.getContents('info');
 
@@ -24,17 +24,5 @@ CKEDITOR.on( 'dialogDefinition', function( ev )
         infoTab.remove('vAlign');
         infoTab.remove('hAlign');
         infoTab.remove('wordWrap');
-    }
-    if (dialogName == 'tableProperties') {
-        // Get the properties tab reference.
-        var infoTab = dialogDefinition.getContents('info');
-
-        // Remove unnecessary bits from this tab.
-        infoTab.remove('txtBorder');
-        infoTab.remove('cmbAlign');
-        infoTab.remove('txtWidth');
-        infoTab.remove('txtHeight');
-        infoTab.remove('txtCellSpace');
-        infoTab.remove('txtCellPad');
     }
 });
