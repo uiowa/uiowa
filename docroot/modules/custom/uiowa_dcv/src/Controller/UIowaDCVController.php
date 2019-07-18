0af42c505af05996bed659e203d4a0ebcaa2b547
cory-skeers <cory-skeers@uiowa.edu>
2019-07-18 16:40:32 -0500

    Functional DCV upload

--- docroot/modules/custom/uiowa_dcv/src/Controller/UIowaDCVController.php
+++ docroot/modules/custom/uiowa_dcv/src/Controller/UIowaDCVController.php
@@ -14,8 +14,8 @@ class UiowaDcvController extends ControllerBase {
    * Returns file content.
    */
   public function dcvFile($filename) {
-    if (file_exists('public://dcv/{$filename}')) {
-      return new BinaryFileResponse('public://dcv/{$filename}', 200, ['Content-Type' => 'text/plain']);
+    if (file_exists("public://dcv/{$filename}")) {
+      return new BinaryFileResponse("public://dcv/{$filename}", 200, ['Content-Type' => 'text/plain']);
     }
     else {
       throw new NotFoundHttpException();
