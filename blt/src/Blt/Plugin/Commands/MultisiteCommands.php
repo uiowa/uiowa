1e393ec7e41f4b8c0e33faf8d423650ae9efa42f
Richard B. Porter <richard-b-porter@uiowa.edu>
2019-07-25 12:30:58 -0500

    Add another provision step

--- blt/src/Blt/Plugin/Commands/MultisiteCommands.php
+++ blt/src/Blt/Plugin/Commands/MultisiteCommands.php
@@ -257,6 +257,7 @@ EOD;
       'Re-deploy the pipelines-build-master branch to the dev environment in the Cloud UI. This will run the cloud hooks successfully.',
       'Sync the multisite database to the test and prod environments using the Cloud UI.',
       'Coordinate a new release to deploy to the test and prod environments.',
+      'Clear the cache on the test and prod sites and re-deploy the tag artifacts.',
       'Sync the multisite files to the test and prod environments using Drush.',
       'Add the multisite domains to environments as needed.',
     ];
