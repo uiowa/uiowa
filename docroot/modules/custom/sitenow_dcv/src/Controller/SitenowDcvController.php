c998e2d55e6a18083c6c69610dbf93ccedca9888
Richard B. Porter <richard-b-porter@uiowa.edu>
2019-07-30 10:41:44 -0500

    Inject EntityTypeManager service into DCV controller

--- docroot/modules/custom/sitenow_dcv/src/Controller/SitenowDcvController.php
+++ docroot/modules/custom/sitenow_dcv/src/Controller/SitenowDcvController.php
@@ -3,6 +3,8 @@
 namespace Drupal\sitenow_dcv\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -12,11 +14,37 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class SitenowDcvController extends ControllerBase {
 
   /**
+   * The EntityTypeManager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * SitenowDcvController constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The EntityTypeManager service.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
    * Returns file content.
    */
   public function dcvFile($filename) {
     /* @var \Drupal\file\Entity\File[] $file */
-    $file = \Drupal::entityTypeManager()
+    $file = $this->entityTypeManager
       ->getStorage('file')
       ->loadByProperties(['filename' => $filename]);
 
