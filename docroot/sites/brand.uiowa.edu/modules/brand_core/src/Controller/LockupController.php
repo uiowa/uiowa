a270c842ff95f6e21370f2278e6abcb8d26d1fcb
Joe Whitsitt <joewhitsitt@gmail.com>
2019-07-30 08:34:51 -0500

    stacked fine-tuning, clean-up, start horizontal.

--- docroot/sites/brand.uiowa.edu/modules/brand_core/src/Controller/LockupController.php
+++ docroot/sites/brand.uiowa.edu/modules/brand_core/src/Controller/LockupController.php
@@ -85,123 +85,177 @@ class LockupController extends ControllerBase {
     $bold = drupal_get_path('module', 'brand_core') . '/fonts/RobotoBold.svg';
     $regular = drupal_get_path('module', 'brand_core') . '/fonts/RobotoRegular.svg';
 
+
+
     // Determine all text features needed.
-    $lines = 1;
     $alt = FALSE;
     $hasSecondary = FALSE;
     $tagline = FALSE;
     $primary_text = '';
-    $svg_width = 0;
     $sub_height = 0;
 
-    $primary = new EasySVG();
+    $lockup = new EasySVG();
+
+    $svg_center = 216.005;
+
+    if (!empty($node->field_lockup_sub_unit->value)) {
+      $sub_text = $node->field_lockup_sub_unit->value;
+      $sub_explode = explode(PHP_EOL, $sub_text);
+      $lockup->setFont($regular, 6, $text_color);
+      $sub_dimensions = $lockup->textDimensions($sub_text);
+      foreach ($sub_explode as $line) {
+        str_replace('\r', '', $line);
+        $sublines[] = $lockup->textDimensions($line);
+      }
+    }
 
     // Unit name generation.
     $primary_text = $node->field_lockup_primary_unit->value;
     $primary_explode = explode(PHP_EOL, $primary_text);
-    $lines = [];
-    $primary_lines = count($primary_explode);
-
-    $primary->setFont($bold, 8, $text_color);
+    $primary_lines = [];
+    $primary_count = count($primary_explode);
+    $lockup->setFont($bold, 8, $text_color);
+    $lockup->setLineHeight(9.6);
+    $lockup->setLetterSpacing(0);
 
     foreach ($primary_explode as $line) {
       str_replace('\r', '', $line);
-      $lines[] = $primary->textDimensions($line);
-    }
-    $max = max($lines);
-
-    $primary_dimensions = $primary->textDimensions($primary_text);
-    if ($primary_dimensions[0] > $svg_width) {
-      $svg_width = $primary_dimensions[0];
+      $primary_lines[] = $lockup->textDimensions($line);
     }
 
-    $svg_half_width = $svg_width / 2;
+    $primary_dimensions = $lockup->textDimensions($primary_text);
+    $primary_width = $primary_dimensions[0];
+    $primary_center = $primary_width / 2;
+    $horizontal_height = $primary_dimensions[1] + $sub_dimensions[1];
+    $horizontal_middle = $horizontal_height / 2;
 
-    if (!empty($node->field_lockup_sub_unit->value)) {
-      $hasSecondary = TRUE;
-      $sub_text = $node->field_lockup_sub_unit->value;
-      $sub_text = $node->field_lockup_sub_unit->value;
-      $sub_explode = explode(PHP_EOL, $sub_text);
-      $sub = new EasySVG();
-    }
     switch ($type) {
       case 'stacked':
-        LockupController::addStackedLogo($primary, $iowa_color);
+        LockupController::addStackedLogo($lockup, $iowa_color);
         // Border. Width based on primary width.
-        $primary->addRect([
-          'x' => 683.05 - $svg_half_width - 3.528,
-          'y' => '376',
-          'width' => $svg_width + 7.506,
+        $lockup->addRect([
+          'x' => $svg_center - $primary_center - 3.528,
+          'y' => '134.98',
+          'width' => $primary_width + 7.506,
           'height' => '0.8',
           'style' => 'fill:' . $text_color,
         ]);
         // Primary Line 1.
-        $primary->addText(html_entity_decode(
+        $lockup->addText(html_entity_decode(
           $primary_explode[0],
           ENT_QUOTES | ENT_XML1,
           'UTF-8'),
-          683.05 - $svg_half_width,
-          '382'
+          $svg_center - $primary_center,
+          '141.213'
         );
         // Primary Line 2.
-        $line2_midpoint = $lines[1][0] / 2;
-        $primary->addText(html_entity_decode(
+        $p2_center = $primary_lines[1][0] / 2;
+        $lockup->addText(html_entity_decode(
           $primary_explode[1],
           ENT_QUOTES | ENT_XML1,
           'UTF-8'),
-          683.05 - $line2_midpoint,
-          '392'
+          $svg_center - $p2_center,
+          '150.662'
         );
         // Primary Line 3.
-        $line3_midpoint = $lines[2][0] / 2;
-        $primary->addText(html_entity_decode(
+        $p3_center = $primary_lines[2][0] / 2;
+        $lockup->addText(html_entity_decode(
           $primary_explode[2],
           ENT_QUOTES | ENT_XML1,
           'UTF-8'),
-          683.05 - $line3_midpoint,
-          '402'
+          $svg_center - $p3_center,
+          '160.111'
         );
-        $primary->setLineHeight(9.6);
-        $primary->setLetterSpacing(0);
+
+        switch ($primary_count) {
+          case 1:
+            $sub_y = 152.248;
+            break;
+          case 2:
+            $sub_y = 161.697;
+            break;
+          case 3:
+            $sub_y = 171.146;
+            break;
+        }
+        $lockup->setFont($regular, 6, $text_color);
+        $lockup->setLineHeight(7.5);
+        $lockup->setLetterSpacing(0);
+
+        $s1_center = $sublines[0][0] / 2;
+        $lockup->addText(html_entity_decode(
+          $sub_explode[0],
+          ENT_QUOTES | ENT_XML1,
+          'UTF-8'),
+          $svg_center - $s1_center,
+          $sub_y
+        );
+
+        $s2_center = $sublines[1][0] / 2;
+        $lockup->addText(html_entity_decode(
+          $sub_explode[1],
+          ENT_QUOTES | ENT_XML1,
+          'UTF-8'),
+          $svg_center - $s2_center,
+          $sub_y + 7.718
+        );
+
+        $s3_center = $sublines[2][0] / 2;
+        $lockup->addText(html_entity_decode(
+          $sub_explode[2],
+          ENT_QUOTES | ENT_XML1,
+          'UTF-8'),
+          $svg_center - $s3_center,
+          $sub_y + 15.436
+        );
         break;
 
       case 'horizontal':
-        LockupController::addHorizontalLogo($primary, $iowa_color);
+        LockupController::addHorizontalLogo($lockup, $iowa_color);
         // Border. Height based on primary and secondary combined height.
-        $primary->addRect([
-          'x' => 675.15,
-          'y' => '384.4',
+        $lockup->addRect([
+          'x' => 206.77,
+          'y' => 144 - $horizontal_middle,
           'width' => 0.8,
-          'height' => '30.6',
+          'height' => $horizontal_height,
           'style' => 'fill:' . $text_color,
         ]);
+
+        // Primary Line 1.
+        $lockup->addText(html_entity_decode(
+          $primary_explode[0],
+          ENT_QUOTES | ENT_XML1,
+          'UTF-8'),
+          '255.126',
+          144 - $horizontal_middle
+        );
         break;
 
     }
 
-    $primary->addAttribute('viewBox', '0 0 1366 768');
+    $lockup->addAttribute('viewBox', '0 0 432 288');
 
-    return $primary->asXML();
+    return $lockup->asXML();
   }
 
   /**
    * Add Stacked Logo.
    */
   public function addStackedLogo($svg, $iowa_color) {
-    $svg->addPath('M656.1,354.1h-1.7v12.4h1.7v4.5h-9.5v-4.5h1.7v-12.4h-1.7v-4.5h9.5V354.1z', ["fill" => $iowa_color]);
-    $svg->addPath('M668.7,371h-6.9c-2.5,0-4.1-1.6-4.1-4.3v-12.8c-0.2-2.2,1.4-4.1,3.6-4.3c0.2,0,0.4,0,0.5,0h6.9c2.2-0.1,4,1.6,4.1,3.8   c0,0.2,0,0.4,0,0.5v12.8C672.8,369.4,671.2,371,668.7,371z M666.8,366.5v-12.4h-3v12.4H666.8z', ["fill" => $iowa_color]);
-    $svg->addPath('M675.2,354.1h-1.5v-4.5h9v4.5H681l1.5,10.3l3.2-14.8h4.6l3.4,14.8l1.2-10.3h-1.6v-4.5h8.9v4.5h-1.5L698,371h-7.2l-2.8-12.6   l-2.8,12.6h-7L675.2,354.1z', ["fill" => $iowa_color]);
-    $svg->addPath('M700.2,366.5h1.6l3.2-16.9h9.7l3.2,16.9h1.5v4.5h-7.2l-0.7-5.2h-3.6l-0.6,5.2h-7.1V366.5z M711.2,361.9l-1.4-8.6l-1.4,8.6   H711.2z', ["fill" => $iowa_color]);
+    $svg->addPath('M189,113.1h-1.72v12.42H189V130h-9.49v-4.5h1.72V113.1h-1.72v-4.47H189Z', ["fill" => $iowa_color]);
+    $svg->addPath('M201.65,130h-6.88c-2.49,0-4.13-1.63-4.13-4.32V112.94a4,4,0,0,1,4.13-4.31h6.88a4,4,0,0,1,4.13,4.31V125.7A4,4,0,0,1,201.65,130Zm-1.94-4.5V113.1h-3v12.42Z', ["fill" => $iowa_color]);
+    $svg->addPath('M208.17,113.1h-1.51v-4.47h9v4.47H214l1.54,10.29,3.2-14.76h4.56l3.42,14.76,1.24-10.29h-1.57v-4.47h8.87v4.47h-1.48L231.07,130h-7.24L221,117.41,218.16,130h-7Z', ["fill" => $iowa_color]);
+    $svg->addPath('M233.19,125.52h1.63l3.24-16.89h9.71l3.2,16.89h1.51V130h-7.24l-.68-5.21H241l-.64,5.21h-7.12Zm11-4.62-1.39-8.63-1.41,8.63Z', ["fill" => $iowa_color]);
   }
 
   /**
    * Add Horizontal Logo.
    */
   public function addHorizontalLogo($svg, $iowa_color) {
-    $svg->addPath('M606,377h-1.7v12.4H606v4.5h-9.5v-4.5h1.7V377h-1.7v-4.5H606Z', ["fill" => $iowa_color]);
-    $svg->addPath('M618.6,393.9h-6.9c-2.5,0-4.1-1.6-4.1-4.3V376.8a3.94,3.94,0,0,1,4.1-4.3h6.9a3.94,3.94,0,0,1,4.1,4.3v12.8A3.89,3.89,0,0,1,618.6,393.9Zm-1.9-4.5V377h-3v12.4Z', ["fill" => $iowa_color]);
-    $svg->addPath('M625.2,377h-1.5v-4.5h9V377H631l1.5,10.3,3.2-14.8h4.6l3.4,14.8,1.2-10.3h-1.6v-4.5h8.9V377h-1.5L648,393.9h-7.2L638,381.3l-2.9,12.6h-7Z', ["fill" => $iowa_color]);
-    $svg->addPath('M650.2,389.4h1.6l3.2-16.9h9.7l3.2,16.9h1.5v4.5h-7.2l-.7-5.2h-3.6l-.6,5.2h-7.1Zm11-4.6-1.4-8.6-1.4,8.6Z', ["fill" => $iowa_color]);
+    $svg->addPath('M138,137.77h-1.72V150.2H138v4.49h-9.49V150.2h1.73V137.77h-1.73V133.3H138Z', ["fill" => $iowa_color]);
+    $svg->addPath('M150.64,154.69h-6.87c-2.5,0-4.13-1.63-4.13-4.31V137.62a4,4,0,0,1,4.13-4.32h6.87a4,4,0,0,1,4.13,4.32v12.76A4,4,0,0,1,150.64,154.69Zm-1.94-4.49V137.77h-3V150.2Z', ["fill" => $iowa_color]);
+    $svg->addPath('M157.16,137.77h-1.51V133.3h9v4.47H163l1.54,10.3,3.2-14.77h4.56l3.43,14.77,1.23-10.3h-1.57V133.3h8.87v4.47h-1.48l-2.68,16.92h-7.24L170,142.09l-2.86,12.6h-7Z', ["fill" => $iowa_color]);
+    $svg->addPath('M182.18,150.2h1.63l3.24-16.9h9.71l3.2,16.9h1.51v4.49h-7.24l-.68-5.2H190l-.65,5.2h-7.12Zm11-4.63-1.39-8.63-1.41,8.63Z', ["fill" => $iowa_color]);
   }
 
 }
