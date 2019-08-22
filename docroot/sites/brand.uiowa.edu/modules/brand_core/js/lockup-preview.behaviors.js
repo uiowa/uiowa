50587719c3777fb4995aa95bef24446762f54ef3
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2019-08-22 14:48:34 -0500

    changed a lot of stuff for line limitations and cursor placement

--- docroot/sites/brand.uiowa.edu/modules/brand_core/js/lockup-preview.behaviors.js
+++ docroot/sites/brand.uiowa.edu/modules/brand_core/js/lockup-preview.behaviors.js
@@ -8,6 +8,8 @@
             var lockupPreview = $("#lockup-preview");
             var primaryUnit = $("#edit-field-lockup-primary-unit-0-value");
             var subUnit = $("#edit-field-lockup-sub-unit-0-value");
+            var primaryUnitPreviousText = primaryUnit.val();
+            var subUnitPreviousText = subUnit.val();
             var primaryValueText;
             var primaryPreviewText;
             var secondaryValueText;
@@ -27,31 +29,43 @@
 
             // Show preview and inject content based on existing input or live input.
             if (primaryUnit.val() !== "") {
-                $("#lockup-preview").addClass('show-preview');
+                lockupPreview.addClass('show-preview');
                 $(".lockup-stacked .primary-unit").text(primaryUnit.val());
                 $(".lockup-horizontal .primary-unit").text(primaryUnit.val());
                 calcDivider();
             }
 
             if (subUnit.val() !== "") {
-                $("#lockup-preview").addClass('show-preview');
+                lockupPreview.addClass('show-preview');
                 $(".lockup-stacked .sub-unit").text(subUnit.val());
                 $(".lockup-horizontal .sub-unit").text(subUnit.val());
             }
 
             primaryUnit.on("input", function (event) {
+
                 $("#lockup-preview").addClass('show-preview');
 
                 var textarea = $(this),
                     text = textarea.val(),
                     numberOfLines = (text.match(/\n/g) || []).length + 1;
 
-                primaryValueText    = textareaSanitize(text, numberOfLines, 'primary');
-                primaryPreviewText  = previewSanitize(text, numberOfLines, 'primary');
+                var cursorSelectionStart = textarea[0].selectionStart;
+                var cursorSelectionEnd = textarea[0].selectionEnd;
+
+                var primaryValueTextObj = textareaSanitize(textarea, text, numberOfLines, 'primary', event.originalEvent.inputType);
+                primaryValueText = primaryValueTextObj.text
+                primaryPreviewText  = previewSanitize(primaryValueText);
 
                 textarea.val(primaryValueText);
                 $(".lockup-stacked .primary-unit").text(primaryPreviewText);
                 $(".lockup-horizontal .primary-unit").text(primaryPreviewText);
+
+                // Resets cursor position.             
+                setSelectionRange(
+                    textarea[0],
+                    cursorSelectionStart + primaryValueTextObj.selectionOffset,
+                    cursorSelectionEnd + primaryValueTextObj.selectionOffset
+                );
 
                 // Set the divider line width.
                 calcDivider();
@@ -61,18 +75,30 @@
             });
 
             subUnit.on("input", function (event) {
-                $("#lockup-preview").show();
+
+                $("#lockup-preview").addClass('show-preview');
 
                 var textarea = $(this),
                     text = textarea.val(),
                     numberOfLines = (text.match(/\n/g) || []).length + 1;
 
-                secondaryValueText    = textareaSanitize(text, numberOfLines, 'sub');
-                secondaryPreviewText  = previewSanitize(text, numberOfLines, 'sub');
+                var cursorSelectionStart = textarea[0].selectionStart;
+                var cursorSelectionEnd = textarea[0].selectionEnd;
+
+                var secondaryValueTextObj = textareaSanitize(textarea, text, numberOfLines, 'sub', event.originalEvent.inputType);
+                secondaryValueText = secondaryValueTextObj.text;
+                secondaryPreviewText  = previewSanitize(secondaryValueText);
 
                 textarea.val(secondaryValueText);
                 $(".lockup-stacked .sub-unit").text(secondaryPreviewText);
                 $(".lockup-horizontal .sub-unit").text(secondaryPreviewText);
+
+                // Resets cursor position.              
+                setSelectionRange(
+                    textarea[0],
+                    cursorSelectionStart + secondaryValueTextObj.selectionOffset,
+                    cursorSelectionEnd + secondaryValueTextObj.selectionOffset
+                );
 
                 // Check if input is valid.
                 isInputValid();
@@ -104,32 +130,29 @@
             });
 
             // Sanitizes text for the textarea.
-            function textareaSanitize(text, numberOfLines, unit) {
+            function textareaSanitize(selector, text, numberOfLines, unit, eventType) {
                 var sanitizedText;
                 // Change all dumb quotes to smart quotes.
                 sanitizedText = dumbQuotes(text);
                 // Redundantly limit any newlines and prevent pasting more than the max number of lines.
-                sanitizedText = limitLines(numberOfLines , sanitizedText, unit);
-                return (sanitizedText);
+                sanitizedTextObj = limitLines(selector, numberOfLines , sanitizedText, unit, eventType);
+                return (sanitizedTextObj);
             }
             // Sanitizes text for the Preview.
-            function previewSanitize(text, numberOfLines, unit) {
+            function previewSanitize(text) {
                 var sanitizedText;
                 // Remove all non quote, space, or dash special characters.
                 sanitizedText = limitCharacters(text);
                 // Remove spaces and dashes from sides of lines.
                 sanitizedText = wingCharacterRemove(sanitizedText);
-                // Change all dumb quotes to smart quotes.
-                sanitizedText = dumbQuotes(sanitizedText);
-                // Redundantly limit any newlines and prevent pasting more than the max number of lines.
-                sanitizedText = limitLines(numberOfLines , sanitizedText, unit);
                 return (sanitizedText);
             }
 
             // Creates a limit on newlines and deletes any after the limit has been reached.
             // Also limits the number of characters per line.
-            function limitLines(nLines, string, unit) {
+            function limitLines(selector, nLines, string, unit, eventType) {
                 var modText = string;
+                var prevLines = [];
 
                 // Limits newlines.
                 if (nLines > maxRows) {
@@ -147,18 +170,53 @@
                 switch (unit) {
                     case 'primary':
                         maxChars = primaryMaxChars;
-                      break;
+                        prevLines = primaryUnitPreviousText.split('\n');
+                        break;
                     case 'sub':
                         maxChars = subMaxChars;
-                      break;
+                        prevLines = subUnitPreviousText.split('\n');
+                        break;
                 }
 
-                var lines = modText.split('\n');
-                for (var i = 0; i < lines.length; i++) {
-                    lines[i] = lines[i].substring(0, maxChars);
+                /*  This is a little complicated, but it is essentially taking in to account whether the 
+                    user pastes or types something in to the box. Upon paste it needs to be trimmed, but
+                    we dont want to simply trim things when it comes to typing, because that causes some 
+                    odd interactions. we store the previous lines of the current textbox so that we can
+                    just take that instead of any new characters as to not cause confusion for the user.
+                    The UX is better if they feel like they just cant add any more characters than if the 
+                    stuff they are writing is pushing other text off the end of the line to be trimmed.
+                */
+               var lines = modText.split('\n');
+               var cursorMod = 0;
+
+                if (eventType === 'insertFromPaste') {
+                    for (var i = 0; i < lines.length; i++) {
+                        lines[i] = lines[i].substring(0, maxChars);
+                    }
                 }
-                                                     
-                return lines.join('\n');
+                else {
+                    for (var i = 0; i < lines.length; i++) {
+                        if(lines[i].length > maxChars) {
+                            lines[i] = prevLines[i];
+                            cursorMod = -1;
+                        }
+                    }
+                }
+                        
+                switch (unit) {
+                    case 'primary':
+                        primaryUnitPreviousText = lines.join('\n');
+                        return {
+                            text: primaryUnitPreviousText,
+                            selectionOffset: cursorMod
+                        };
+                    case 'sub':
+                        subUnitPreviousText = lines.join('\n');
+                        return {
+                            text: subUnitPreviousText,
+                            selectionOffset: cursorMod
+                        };
+                }
             }
 
             // Replace Dumb quotes with Smart Quotes.
@@ -308,6 +366,21 @@
             function limitCharacters(lockupText) {
                 var matchRegex = /[\w :;.,\\\/()|'"‘’“” `\—\-\–\—\‒\–\—\―\n]*/g;
                 return lockupText.match(matchRegex).join('');
+            }
+
+            // This function manages the position of the cursor and/or the highlighted selection.
+            // This is used to reset the cursor and/or selection position after rewriting the input box.
+            function setSelectionRange(input, selectionStart, selectionEnd) {
+                if (input.setSelectionRange) {
+                  input.focus();
+                  input.setSelectionRange(selectionStart, selectionEnd);
+                } else if (input.createTextRange) {
+                  var range = input.createTextRange();
+                  range.collapse(true);
+                  range.moveEnd('character', selectionEnd);
+                  range.moveStart('character', selectionStart);
+                  range.select();
+                }
             }
         }
     };
