26c67d07be8d0c64a3d11e4749708b3eacfa12a5
Ethan St. John <estjohn2000@gmail.com>
2019-08-22 15:33:11 -0500

    Add sites.php entries for yangs.lab.uiowa.edu

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -192,3 +192,8 @@ $sites['labmeyerholz.prod.drupal.uiowa.edu'] = 'meyerholz.lab.uiowa.edu';
 $sites['accreditation.dev.drupal.uiowa.edu'] = 'accreditation.uiowa.edu';
 $sites['accreditation.stage.drupal.uiowa.edu'] = 'accreditation.uiowa.edu';
 $sites['accreditation.prod.drupal.uiowa.edu'] = 'accreditation.uiowa.edu';
+
+// Directory aliases for yangs.lab.uiowa.edu.
+$sites['labyangs.dev.drupal.uiowa.edu'] = 'yangs.lab.uiowa.edu';
+$sites['labyangs.stage.drupal.uiowa.edu'] = 'yangs.lab.uiowa.edu';
+$sites['labyangs.prod.drupal.uiowa.edu'] = 'yangs.lab.uiowa.edu';
