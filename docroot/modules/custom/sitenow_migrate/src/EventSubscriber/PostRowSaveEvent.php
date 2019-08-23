b6a0f074cd86f9f66c5b341bce2598ce6848a254
cory-skeers <cory-skeers@uiowa.edu>
2019-08-23 14:09:40 -0500

    Initial build

---
+++ docroot/modules/custom/sitenow_migrate/src/EventSubscriber/PostRowSaveEvent.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\sitenow_migrate\EventSubscriber;
+
+use Drupal\migrate\Event\MigrateEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class PostRowSaveEvent.
+ *
+ * @package Drupal\sitenow_migrate\EventSubscriber
+ */
+class PostRowSaveEvent implements EventSubscriberInterface {
+
+  /**
+   * Get subscribed events.
+   *
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[MigrateEvents::POST_ROW_SAVE][] = ['onPostRowSave'];
+    return $events;
+  }
+
+  /**
+   * Calls for the creation of a media entity after file creation.
+   *
+   * {@inheritdoc}
+   */
+  public function onPostRowSave($event) {
+    $row = $event->getRow();
+    $fids = $event->getDestinationIdValues();
+    drush_print("..." . $fids[0]);
+    $this->makeEntity($row, $fids);
+  }
+
+  /**
+   * Create a media entity for images.
+   */
+  public function makeEntity($row, $fids) {
+    $entityManager = \Drupal::entityTypeManager();
+    $file = $entityManager->getStorage('file')->load($fids[0]);
+    if ($file) {
+      // Currently only images are handled.
+      // Will need to be updated for other types of files.
+      $bundle = 'image';
+      $alt = '';
+      $title = '';
+      $media = $entityManager->getStorage('media')->create([
+        'bundle' => $bundle,
+        'field_media_image' => [
+          'target_id' => $fids[0],
+          'alt' => $alt,
+          'title' => $title,
+        ],
+        'langcode' => 'en',
+      ]);
+      $media->setName($file->getFileName());
+      $media->setOwnerId(0);
+      $media->save();
+    }
+  }
+
+}
---
+++ docroot/modules/custom/sitenow_migrate/src/Plugin/migrate/source/Files.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\sitenow_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Basic implementation of the source plugin.
+ *
+ * @MigrateSource(
+ *  id = "files",
+ *  source_module = "sitenow_migrate"
+ * )
+ */
+class Files extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('file_managed', 'f')
+      ->fields('f', [
+        'fid',
+        'filename',
+        'uri',
+        'filemime',
+      ]);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'fid' => $this->t('fid'),
+      'filename' => $this->t('filename'),
+      'uri' => $this->t('uri'),
+      'filemime' => $this->t('filemime'),
+      'timestamp' => $this->t('timestamp'),
+      'type' => $this->t('file type'),
+      'created' => $this->t('created timestamp'),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'fid' => [
+        'type' => 'integer',
+        'alias' => 'f',
+      ],
+    ];
+  }
+
+  /**
+   * Prepare Row used for altering source data prior to its insertion into the destination.
+   */
+  public function prepareRow(Row $row) {
+    /* Can do extra preparation work here,
+     * possibly including the media entity creation heavylifting.
+     *
+     * Do we want to do a file cleanup?
+     * Can be done with the following.
+     * file_delete($file->id());
+     */
+    return parent::prepareRow($row);
+  }
+
+}
