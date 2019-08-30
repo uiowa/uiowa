edbae7b5dd9c2b7d6c12f986a14dbd1d4da26727
cory-skeers <cory-skeers@uiowa.edu>
2019-08-30 14:31:02 -0500

    Structure for further migrations put in to place.

---
+++ docroot/modules/custom/sitenow_migrate/src/Plugin/migrate/source/BasicPages.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\sitenow_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Row;
+
+/**
+ * Basic implementation of the source plugin.
+ *
+ * @MigrateSource(
+ *  id = "basic_pages",
+ *  source_module = "sitenow_migrate"
+ * )
+ */
+class BasicPages extends SqlBase {
+
+  /**
+   * The public file directory path.
+   *
+   * @var string
+   */
+  protected $publicPath;
+
+  /**
+   * The private file directory path, if any.
+   *
+   * @var string
+   */
+  protected $privatePath;
+
+  /**
+   * The temporary file directory path.
+   *
+   * @var string
+   */
+  protected $temporaryPath;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('node', 'n')
+      ->fields('n', [
+        'nid',
+        'vid',
+        'language',
+        'title',
+        'uid',
+        'status',
+        'created',
+        'changed',
+        'comment',
+        'promote',
+        'sticky',
+        'tnid',
+        'translate',
+      ])
+      ->condition('n.type', 'page');
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'nid' => $this->t('Node ID'),
+      'vid' => $this->t('Node revision ID'),
+      'language' => $this->t('Language'),
+      'title' => $this->t('Node Title'),
+      'uid' => $this->t('User ID of node author'),
+      'status' => $this->t('Published/unpublished'),
+      'created' => $this->t('Timestamp of creation'),
+      'changed' => $this->t('Timestamp of last change'),
+      'comment' => $this->t('Comments enabled/disabled'),
+      'promote' => $this->t('Promoted'),
+      'sticky' => $this->t('Stickied'),
+      'tnid' => $this->t('Translation ID'),
+      'translate' => $this->t('Page being translated?'),
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'nid' => [
+        'type' => 'integer',
+        'alias' => 'n',
+      ],
+    ];
+  }
+
+  /**
+   * Prepare Row used for altering source data prior to its insertion into the destination.
+   */
+  public function prepareRow(Row $row) {
+    // Can do extra preparation work here.
+    return parent::prepareRow($row);
+  }
+
+}
