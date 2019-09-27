fce54badc08ea0b63192baa140cde98035cda9ef
Richard B. Porter <richard-b-porter@uiowa.edu>
2019-09-27 09:36:30 -0500

    Create multisite getDatabase static method

--- blt/src/Multisite.php
+++ blt/src/Multisite.php
@@ -8,12 +8,34 @@ namespace Sitenow;
 class Multisite {
 
   /**
+   * Given a site directory name, return the standardized database name.
+   *
+   * @param string $dir
+   *   The multisite directory, i.e. the URI without the scheme.
+   *
+   * @return string
+   *   The AC database name.
+   */
+  public static function getDatabase($dir) {
+    // @todo: Access BLT project prefix.
+    if ($dir = 'default') {
+      $db = 'uiowa';
+    }
+    else {
+      $db = str_replace('.', '_', $dir);
+      $db = str_replace('-', '_', $db);
+    }
+
+    return $db;
+  }
+
+  /**
    * Given a URI, create and return a unique identifier.
    *
    * Used for internal subdomain and Drush alias group name, i.e. file name.
    *
    * @param string $uri
-   *   The multisite URI.
+   *   The multisite URI including the scheme.
    *
    * @return string
    *   The ID.
