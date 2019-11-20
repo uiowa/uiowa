6577b140ee303ca272120161dab8ef33bb6cb176
Ethan St. John <estjohn2000@gmail.com>
2019-11-20 10:39:06 -0600

    Add sites.php entries for microvascularphys.lab.uiowa.edu

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -353,3 +353,8 @@ $sites['centerforconferencesvanallenprobes.prod.drupal.uiowa.edu'] = 'vanallenpr
 $sites['studiouibass.dev.drupal.uiowa.edu'] = 'uibass.studio.uiowa.edu';
 $sites['studiouibass.stage.drupal.uiowa.edu'] = 'uibass.studio.uiowa.edu';
 $sites['studiouibass.prod.drupal.uiowa.edu'] = 'uibass.studio.uiowa.edu';
+
+// Directory aliases for microvascularphys.lab.uiowa.edu.
+$sites['labmicrovascularphys.dev.drupal.uiowa.edu'] = 'microvascularphys.lab.uiowa.edu';
+$sites['labmicrovascularphys.stage.drupal.uiowa.edu'] = 'microvascularphys.lab.uiowa.edu';
+$sites['labmicrovascularphys.prod.drupal.uiowa.edu'] = 'microvascularphys.lab.uiowa.edu';
