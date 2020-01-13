3166d7bf661c8624f8e9f09174916ac45682504b
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-01-13 18:53:57 +0000

    Updated uiowa01 sites blt and settings files to pass tests.

--- docroot/sites/iowasuperfund.uiowa.edu/settings.php
+++ docroot/sites/iowasuperfund.uiowa.edu/settings.php
@@ -777,6 +777,11 @@ $settings['entity_update_backup'] = TRUE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+if (file_exists('/var/www/site-php')) {
+  require '/var/www/site-php/uiowa/iowasuperfund_uiowa_edu-settings.inc';
+}
+
 require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";
 /**
  * IMPORTANT.
