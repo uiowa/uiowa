40eb003680f0f78e1138efdc71c5d61e93704931
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-02-05 11:12:55 -0600

    Move sync directory to global settings

--- docroot/sites/settings/global.settings.php
+++ docroot/sites/settings/global.settings.php
@@ -5,7 +5,20 @@
  * Global settings for every multisite.
  */
 
+use Acquia\Blt\Robo\Config\ConfigInitializer;
 use Drupal\Core\Installer\InstallerKernel;
+use Symfony\Component\Console\Input\ArgvInput;
+
+// Prevent access to uninstalled sites through the web interface.
+if (InstallerKernel::installationAttempted() && php_sapi_name() != 'cli') {
+  exit;
+}
+
+// Initialize new config object to access BLT config. Similar to ACSF.
+// @see: blt.settings.php
+$config_initializer = new ConfigInitializer($repo_root, new ArgvInput());
+$config_initializer->setSite($site_dir);
+$blt_config = $config_initializer->initialize();
 
 /**
  * Set BLT to not override the config directories.
@@ -17,12 +30,23 @@ use Drupal\Core\Installer\InstallerKernel;
  */
 $blt_override_config_directories = FALSE;
 
+if ($blt_sync_path = $blt_config->get('cm.core.dirs.sync.path')) {
+  $settings['config_sync_directory'] = DRUPAL_ROOT . '/' . $blt_sync_path;
+}
+
 // Unset the VCS config directory so cim/cex default to sync.
 if (isset($config_directories['vcs'])) {
   unset($config_directories['vcs']);
 }
 
-// Prevent access to uninstalled sites through the web interface.
-if (InstallerKernel::installationAttempted() && php_sapi_name() != 'cli') {
-  exit;
-}
+/**
+ * A custom theme for the offline page.
+ *
+ * This applies when the site is explicitly set to maintenance mode through the
+ * administration page or when the database is inactive due to an error.
+ * The template file should also be copied into the theme. It is located inside
+ * 'core/modules/system/templates/maintenance-page.html.twig'.
+ *
+ * Note: This setting does not apply to installation and update pages.
+ */
+$settings['maintenance_theme'] = 'seven';
