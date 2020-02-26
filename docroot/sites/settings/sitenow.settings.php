c9be93794f13e1e20022e4ec5f270d9eca040922
Richard B. Porter <rbp@richardbporter.com>
2020-02-26 13:52:17 -0600

    Activate sitenow_migrate feature split in local env for sitenow sites

--- docroot/sites/settings/sitenow.settings.php
+++ docroot/sites/settings/sitenow.settings.php
@@ -7,12 +7,20 @@
  * from individual SiteNow sites through their own include chain.
  */
 
+use Acquia\Blt\Robo\Common\EnvironmentDetector;
+
 /**
- * Set the environment indicator colors.
+ * Environment specific SiteNow configuration.
  */
-$env = getenv('AH_SITE_ENVIRONMENT');
+$env = EnvironmentDetector::getAhEnv() ?? 'local';
 
 switch ($env) {
+  case 'local':
+    $settings['simple_environment_indicator'] = '#31873E local';
+    $config['config_split.config_split.sitenow_migrate']['status'] = TRUE;
+
+    break;
+
   case 'dev':
     $settings['simple_environment_indicator'] = '#4363d8 dev';
     break;
@@ -23,9 +31,5 @@ switch ($env) {
 
   case 'prod':
     $settings['simple_environment_indicator'] = '#e6194b prod';
-    break;
-
-  default:
-    $settings['simple_environment_indicator'] = '#31873E local';
     break;
 }
