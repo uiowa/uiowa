138f334f73c61bd3dff6640a9734012d2de3bbc3
vagrant <vagrant@local.drupal.uiowa.edu>
2020-03-17 14:50:34 +0000

    Initialize iowasummermusiccamps.uiowa.edu multisite

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1182,3 +1182,9 @@ $sites['orglgbtq-council.local.drupal.uiowa.edu'] = 'lgbtq-council.org.uiowa.edu
 $sites['orglgbtq-council.dev.drupal.uiowa.edu'] = 'lgbtq-council.org.uiowa.edu';
 $sites['orglgbtq-council.stage.drupal.uiowa.edu'] = 'lgbtq-council.org.uiowa.edu';
 $sites['orglgbtq-council.prod.drupal.uiowa.edu'] = 'lgbtq-council.org.uiowa.edu';
+
+// Directory aliases for iowasummermusiccamps.uiowa.edu.
+$sites['iowasummermusiccamps.local.drupal.uiowa.edu'] = 'iowasummermusiccamps.uiowa.edu';
+$sites['iowasummermusiccamps.dev.drupal.uiowa.edu'] = 'iowasummermusiccamps.uiowa.edu';
+$sites['iowasummermusiccamps.stage.drupal.uiowa.edu'] = 'iowasummermusiccamps.uiowa.edu';
+$sites['iowasummermusiccamps.prod.drupal.uiowa.edu'] = 'iowasummermusiccamps.uiowa.edu';
