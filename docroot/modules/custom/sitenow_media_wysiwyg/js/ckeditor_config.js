1183389260efa379baf87b214b1a86de7f9c6ce1
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-04-02 08:12:41 -0500

    Combine ckeditor config override scripts

---
+++ docroot/modules/custom/sitenow_media_wysiwyg/js/ckeditor_config.js
@@ -0,0 +1,34 @@
+/**
+ * @file
+ *
+ * Custom CKEditor configuration.
+ */
+
+// Allow empty spans for FontAwesome icons.
+CKEDITOR.dtd.$removeEmpty['span'] = false;
+
+// Remove table and cell properties that can make them unusable/inaccessible.
+CKEDITOR.on('dialogDefinition', function (ev) {
+  var dialogName = ev.data.name;
+  var dialogDefinition = ev.data.definition;
+
+  if (dialogName == 'table' || 'tableProperties') {
+    var infoTab = dialogDefinition.getContents('info');
+
+    infoTab.remove('txtBorder');
+    infoTab.remove('cmbAlign');
+    infoTab.remove('txtWidth');
+    infoTab.remove('txtHeight');
+    infoTab.remove('txtCellSpace');
+    infoTab.remove('txtCellPad');
+  }
+  if (dialogName == 'cellProperties') {
+    var infoTab = dialogDefinition.getContents('info');
+
+    infoTab.remove('borderColor');
+    infoTab.remove('bgColor');
+    infoTab.remove('vAlign');
+    infoTab.remove('hAlign');
+    infoTab.remove('wordWrap');
+  }
+});
