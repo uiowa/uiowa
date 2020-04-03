8c79eaa82f99f9a78073d2c2454bdcccfbabd39d
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-04-03 17:10:51 +0000

    Fixed coding standards errors and updated description in the module to indicate that it is not ready for use.

--- docroot/modules/custom/uiowa_core/src/Routing/UiowaCoreRouteSubscriber.php
+++ docroot/modules/custom/uiowa_core/src/Routing/UiowaCoreRouteSubscriber.php
@@ -8,20 +8,19 @@ use Symfony\Component\Routing\RouteCollection;
 /**
  * Listens to the dynamic route events.
  */
-class UiowaCoreRouteSubscriber extends RouteSubscriberBase
-{
+class UiowaCoreRouteSubscriber extends RouteSubscriberBase {
+
   /**
    * {@inheritdoc}
    */
-  protected function alterRoutes(RouteCollection $collection)
-  {
+  protected function alterRoutes(RouteCollection $collection) {
     // Change form for the system.site_information_settings route
     // to Drupal\describe_site\Form\DescribeSiteSiteInformationForm
     // First, we need to act only on the system.site_information_settings route.
-    if($route = $collection->get('system.site_information_settings'))
-    {
+    if ($route = $collection->get('system.site_information_settings')) {
       // Next, we need to set the value for _form to the form we have created.
       $route->setDefault('_form', 'Drupal\uiowa_core\Form\UiowaCoreSiteInformationForm');
     }
   }
+
 }
