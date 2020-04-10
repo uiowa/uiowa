a8091cd4a431794f1a62ac603e72bfeb8a62b5ff
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2020-04-10 15:07:06 -0500

    fix oops.

--- docroot/modules/custom/uiowa_core/src/Access/UiowaCoreAccess.php
+++ docroot/modules/custom/uiowa_core/src/Access/UiowaCoreAccess.php
@@ -20,7 +20,7 @@ class UiowaCoreAccess implements AccessInterface {
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access result.
    */
-  public function access(AccountInterface $account, Route $route) {
+  public function access(AccountInterface $account) {
     // Is this user an admin or user 1?
     return ($account->id() == 1 || in_array('administrator', $account->getRoles())) ? AccessResult::allowed() : AccessResult::forbidden();
   }
