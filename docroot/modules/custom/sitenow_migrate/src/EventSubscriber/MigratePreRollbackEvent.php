b67790b33b45e2f6f9fb40aa10ab3f21ededd5f5
Cory Skeers <cory-skeers@uiowa.edu>
2020-05-22 14:33:33 -0500

    Rollback event subscriber for media entity deletion

--- docroot/modules/custom/sitenow_migrate/src/EventSubscriber/MigratePreRollbackEvent.php
+++ docroot/modules/custom/sitenow_migrate/src/EventSubscriber/MigratePreRollbackEvent.php
@@ -4,10 +4,7 @@ namespace Drupal\sitenow_migrate\EventSubscriber;
 
 use Drupal\Core\Database\Database;
 use Drupal\Core\Entity\EntityTypeManager;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\migrate\Event\MigrateEvents;
-use Drupal\migrate\Event\MigrateImportEvent;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -59,8 +56,36 @@ class MigratePreRollbackEvent implements EventSubscriberInterface {
   }
 
   public function onMigratePreRollback(MigrateRollbackEvent $event) {
-    $dest = $event->getMigration()->getDestinationConfiguration();
+    $migration = $event->getMigration();
+    switch ($migration->id()) {
 
+      // Calls for creating a media entity for imported files.
+      case 'd7_file':
+        $row = $event->getRow();
+        $fids = $event->getDestinationIdValues();
+        $this->removeEntity($fids);
+        break;
+      }
+  }
+
+  /**
+   * Remove associated media entities prior to file removal.
+   */
+  public function removeEntity($fids) {
+    $connection = \Drupal::database();
+    // Grab our image media entities that reference files to be removed.
+    $query1 = $connection->select('media__field_media_image', 'm_image')
+      ->fields('m_image', ['entity_id'])
+      ->condition('m_image.field_media_image_target_id', $fids, 'in');
+    // Grab our file media entities that reference files to be removed.
+    $query2 = $connection->select('media__field_media_file', 'm_file')
+      ->fields('m_file', ['entity_id'])
+      ->condition('m_file.field_media_file_target_id', $fids, 'in');
+    $results = $query1->execute()->fetchCol();
+    $results = array_merge($results, $query2->execute()->fetchCol());
+
+    $mediaEntities = $this->entityTypeManager->getStorage('media')->load($results);
+    $this->entityTypeManager->getStorage('media')->delete($mediaEntities);
   }
 
 }
