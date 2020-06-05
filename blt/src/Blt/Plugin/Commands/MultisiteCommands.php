b3650cc7ee22a8e6a464c703bdf3bb17ca24afe8
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-06-05 12:23:03 -0500

    Refactor umc for better san matching

--- blt/src/Blt/Plugin/Commands/MultisiteCommands.php
+++ blt/src/Blt/Plugin/Commands/MultisiteCommands.php
@@ -463,23 +463,21 @@ EOD
     $table->setHeaders(['Application', 'DBs', 'SANs', 'SAN Match']);
     $rows = [];
 
-    // Search for a SANs match.
-    // If the host is a double subdomain, search for the parent.
-    // Ex. foo.bar.uiowa.edu -> search for bar.uiowa.edu.
+    // Search for a SANs match. The app name will be used as the default choice.
+    $sans_match = NULL;
+
+    // Explode by domain and limit to two parts. Search for wildcard coverage.
+    // Ex. foo.bar.uiowa.edu -> search for *.bar.uiowa.edu.
+    // Ex. foo.bar.baz.uiowa.edu -> search for *.bar.baz.uiowa.edu.
     $host_parts = explode('.', $host, 2);
-    $sans_search = $host_parts[1];
+    $sans_search = '*.' . $host_parts[1];
 
-    // If the host is one subdomain off uiowa.edu, search for it instead.
-    // This mostly just checks to see if the domain already exists in a cert.
+    // If the host is one subdomain off uiowa.edu or a vanity domain,
+    // search for the host instead.
     // Ex. foo.uiowa.edu -> search for foo.uiowa.edu.
-    if ($host_parts[1] == 'uiowa.edu') {
+    // Ex. foo.com -> search for foo.com.
+    if ($host_parts[1] == 'uiowa.edu' || !stristr($host_parts[1], '.')) {
       $sans_search = $host;
-    }
-
-    // If the host is one subdomain off a TLD, do not search.
-    // Ex. foo.com -> FALSE.
-    if (!stristr($host_parts[1], '.')) {
-      $sans_search = FALSE;
     }
 
     foreach ($applications as $name => $meta) {
@@ -499,8 +497,9 @@ EOD
 
               if ($sans_search) {
                 foreach ($cert->domains as $domain) {
-                  if (stristr($domain, $sans_search)) {
+                  if ($domain == $sans_search) {
                     $row[] = $domain;
+                    $sans_match = $name;
                     break;
                   }
                 }
@@ -516,7 +515,14 @@ EOD
     $table->setRows($rows);
     $table->render();
 
-    $app = $this->askChoice('Which cloud application should be used?', array_keys($applications));
+    if ($sans_match) {
+      $app = $sans_match;
+    }
+    if (!$sans_match) {
+      $this->logger->error("No SSL coverage found on any application for {$host}. Be sure to install new SSL certificate before launching site.");
+      $app = $this->askChoice('Which cloud application should be used?', array_keys($applications));
+    }
+
     $this->say("Selected <comment>{$app}</comment> application.");
     $app_id = $applications[$app]['id'];
 
