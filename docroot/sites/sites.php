0ff2f3e0fc1043424eb15c82148748bd9b6d4f6c
vagrant <vagrant@local.drupal.uiowa.edu>
2020-06-08 14:33:06 +0000

    Initialize emfisis.physics.uiowa.edu multisite on uiowa02

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1421,3 +1421,9 @@ $sites['labweber.local.drupal.uiowa.edu'] = 'weber.lab.uiowa.edu';
 $sites['labweber.dev.drupal.uiowa.edu'] = 'weber.lab.uiowa.edu';
 $sites['labweber.stage.drupal.uiowa.edu'] = 'weber.lab.uiowa.edu';
 $sites['labweber.prod.drupal.uiowa.edu'] = 'weber.lab.uiowa.edu';
+
+// Directory aliases for emfisis.physics.uiowa.edu.
+$sites['physicsemfisis.local.drupal.uiowa.edu'] = 'emfisis.physics.uiowa.edu';
+$sites['physicsemfisis.dev.drupal.uiowa.edu'] = 'emfisis.physics.uiowa.edu';
+$sites['physicsemfisis.stage.drupal.uiowa.edu'] = 'emfisis.physics.uiowa.edu';
+$sites['physicsemfisis.prod.drupal.uiowa.edu'] = 'emfisis.physics.uiowa.edu';
