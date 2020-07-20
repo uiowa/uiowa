d8d164c12b492c56e7a78781daa21dc5d349b0b5
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-07-20 14:15:31 -0500

    Refactor layout_builder_custom SI library as generic, adjust tray width and handle

---
+++ docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ */
+(function ($, Drupal) {
+    Drupal.behaviors.layout_builder_custom_overrides = {
+        attach: function (context, settings) {
+            $('.node-page-layout-builder-form', context).once('layout_builder_custom_overrides').each(function () {
+              $(window).on('dialog:aftercreate', function (event, dialog, $element) {
+                if (Drupal.offCanvas.isOffCanvas($element)) {
+                  $($element).parent().attr('style', 'position: fixed; height: 281px; width: 500px; top: 79px; left: 925px;');
+                }
+              });
+            });
+        }
+    };
+})(jQuery, Drupal);
