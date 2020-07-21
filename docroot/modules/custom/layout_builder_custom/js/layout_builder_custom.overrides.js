072084213a45c84cbf8bd23b282e9e453042a43a
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-07-21 14:18:46 -0500

    Refactor LBC JS to act on ajax complete

--- docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
+++ docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
@@ -5,9 +5,14 @@
     Drupal.behaviors.layout_builder_custom_overrides = {
         attach: function (context, settings) {
             $('.node-page-layout-builder-form', context).once('layout_builder_custom_overrides').each(function () {
-              $(window).on('dialog:aftercreate', function (event, dialog, $element) {
-                if (Drupal.offCanvas.isOffCanvas($element) && $element.find('.layout-selection').length == 0) {
-                  $($element).parent().attr('style', 'position: fixed; height: 281px; width: 500px; top: 79px; left: 925px;');
+              $(document).ajaxComplete(function (event, xhr, settings) {
+                if (event.delegateTarget.visibilityState === 'visible' && settings.url.includes('section') === false) {
+                  $(context)
+                    .find('.ui-dialog-off-canvas')
+                    .each(function () {
+                      $(this).css('width', '500px');
+                      $(this).css('left', '925px');
+                    });
                 }
               });
             });
