7dcf9421c5ee7d56d8cd93a370660f55a1dc3c17
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-07-21 11:51:04 -0500

    Do not modify the off canvas dialog width for layout selections

--- docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
+++ docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
@@ -6,7 +6,7 @@
         attach: function (context, settings) {
             $('.node-page-layout-builder-form', context).once('layout_builder_custom_overrides').each(function () {
               $(window).on('dialog:aftercreate', function (event, dialog, $element) {
-                if (Drupal.offCanvas.isOffCanvas($element)) {
+                if (Drupal.offCanvas.isOffCanvas($element) && $element.find('.layout-selection').length == 0) {
                   $($element).parent().attr('style', 'position: fixed; height: 281px; width: 500px; top: 79px; left: 925px;');
                 }
               });
