75248fa631ca2de9e7ed623ac7ace4a2390e0a7f
Richard B. Porter <richardbporter@users.noreply.github.com>
2020-07-22 15:58:14 -0500

    Default LB off-canvas dialog to 500px and style drag handle. (#1965)
    * Refactor layout_builder_custom SI library as generic, adjust tray width and handle
    
    * Set the off canvas width to always be 100%, even on redraw after drag
    
    * Fix filter format help link icon overflow
    
    * Do not modify the off canvas dialog width for layout selections
    
    * Refactor LBC JS to act on ajax complete
    
    * Update styles using previous event
    
    * updated drag handle styles
    
    * Added a cookie to store your off-canvas width preference so that it stays at that width.
    
    * Update comment.
    
    * Added a variable to keep track of whether the the off canvas tray was just created or not, so that we don't create the cookie if there is no interaction with the drag handle.
    
    * Removed debugging.
    
    * Replace template literals with string concatenation for ie11
    
    * Expand cookie domain to all of uiowa.edu
    
    Co-authored-by: Gary Alan Ridgway <gary-ridgway@uiowa.edu>
    Co-authored-by: Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>

---
+++ docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
@@ -0,0 +1,35 @@
+/**
+ * @file
+ */
+(function ($, Drupal) {
+    Drupal.behaviors.layoutBuilderCustomOverrides = {
+        attach: function (context, settings) {
+          $(window).once('off-canvas-overrides').on({
+            'dialog:aftercreate': function (event, dialog, $element) {
+              let justCreated = true;
+              if (Drupal.offCanvas.isOffCanvas($element) && $element.find('.layout-selection').length === 0) {
+                let offCanvasWidth;
+                const offCanvasCookie = $.cookie('ui_off_canvas_width');
+                if (offCanvasCookie === undefined) {
+                  offCanvasWidth = 500;
+                } else {
+                  offCanvasWidth = offCanvasCookie;
+                }
+                $($element).parent().attr('style', 'position: fixed; width: ' + offCanvasWidth + 'px; right: 0; left: auto;');
+
+                let eventData = { settings: settings, $element: $element, offCanvasDialog: Drupal.offCanvas };
+                const $container = Drupal.offCanvas.getContainer($element);
+                $element.on('dialogContentResize.off-canvas', eventData, function() {
+                  if (!justCreated) {
+                    // Cookie that expires in 99 years.
+                    const width = $container.outerWidth();
+                    $.cookie('ui_off_canvas_width', width, { expires: 36135, path: '/', domain: '.uiowa.edu'});
+                  }
+                  justCreated = false;
+                });
+              }
+            }
+          });
+        }
+    };
+})(jQuery, Drupal);
