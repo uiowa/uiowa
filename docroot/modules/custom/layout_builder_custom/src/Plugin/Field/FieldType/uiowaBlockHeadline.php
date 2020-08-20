99aef416c362fa58bfb5a581860beac5b876e460
Cory Skeers <cory-skeers@uiowa.edu>
2020-08-20 08:54:16 -0500

    Building out basic field definitions, defining widget.

--- docroot/modules/custom/layout_builder_custom/src/Plugin/Field/FieldType/uiowaBlockHeadline.php
+++ docroot/modules/custom/layout_builder_custom/src/Plugin/Field/FieldType/uiowaBlockHeadline.php
@@ -2,12 +2,15 @@
 
 namespace Drupal\layout_builder_custom\Plugin\Field\FieldType;
 
+use Drupal\Core\TypedData\DataDefinition;
+
 /**
  * Provides a field type of uiowaBlockHeadline.
  * 
  * @FieldType(
  *   id = "field_uiowa_block_headline",
  *   label = @Translation("Block Headline"),
+ *   description = @Translation("Parent headline for collections of content."),
  *   default_formatter = "uiowa_block_headline_formatter",
  *   default_widget = "uiowa_block_headline_widget",
  * )
@@ -21,12 +24,14 @@ public static function schema(FieldStorageDefinitionInterface $field_definition)
     return [
       'columns' => [
         'headline' => [
-          'type' => 'text_long',
+          'type' => 'varchar',
           'length' => 255,
           'not null' => FALSE,
         ],
         'heading_size' => [
-            'type' => 'list_string',
+          'type' => 'varchar',
+          'length' => 255,
+          'not null' => TRUE,
         ],
         'hide_headline' => [
             'type' => 'boolean',
@@ -48,13 +53,17 @@ public static function schema(FieldStorageDefinitionInterface $field_definition)
    */
   public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
     $properties['headline'] = DataDefinition::create('string')
-      ->setLabel(t('Block headline'));
+      ->setLabel(t('Block Headline'))
+      ->setDescription(t('Parent headline over collections of content.'));
 
     $properties['heading_size'] = DataDefinition::create('string')
-      ->setLabel(t('Block headline size'));
+      ->setLabel(t('Block Headline Size'))
+      ->setDescription(t('Heading size for the parent headline.'));
 
     $properties['hide_headline'] = DataDefinition::create('string')
-      ->setLabel(t('Visually hide block headline'));
+      ->setLabel(t('Hide Headline'))
+      ->setDescription(t('Visually hide block headline'));
+
     return $properties;
   }
 
