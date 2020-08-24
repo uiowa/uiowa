c38d5456e7cbdb5734325dcea948a9529cbe8576
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-08-24 12:35:07 -0500

    Create iAmLoggedInAsUserWithRole custom feature context for Drush user:login

--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -4,6 +4,8 @@ namespace Drupal;
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use Drupal\user\Entity\User;
+use Drupal\user\UserInterface;
 
 /**
  * FeatureContext class defines custom step definitions for Behat.
@@ -19,5 +21,34 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
   public function __construct() {
 
   }
+
+  /**
+   * @Given I am logged in as a user with role :role
+   *
+   * Do a one-time login and assign the given role.
+   *
+   * This is necessary because the profile disables local Drupal accounts
+   * making regular Behat login useless. Must be run against the default site.
+   */
+  public function iAmLoggedInAsUserWithRole($role) {
+    $name = "behat_{$role}";
+    $user = user_load_by_name($name);
+
+    if (!$user) {
+      /** @var UserInterface $user */
+      $user = User::create([
+        'name' => $name,
+        'mail' => 'noreply@default.local.drupal.uiowa.edu',
+        'status' => 1,
+      ]);
+
+      $user->addRole($role);
+      $user->save();
+    }
+
+    $reset_url = user_pass_reset_url($user) . '/login';
+    $this->getSession()->visit($reset_url);
+  }
+
 
 }
