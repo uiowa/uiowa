b890e4fd35d9f5e093dddf66ec29137d9d96e047
vagrant <vagrant@local.drupal.uiowa.edu>
2020-09-11 17:01:25 +0000

    Initialize shpe.org.uiowa.edu multisite on uiowa02

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1577,3 +1577,9 @@ $sites['pharmacy.local.drupal.uiowa.edu'] = 'pharmacy.uiowa.edu';
 $sites['pharmacy.dev.drupal.uiowa.edu'] = 'pharmacy.uiowa.edu';
 $sites['pharmacy.stage.drupal.uiowa.edu'] = 'pharmacy.uiowa.edu';
 $sites['pharmacy.prod.drupal.uiowa.edu'] = 'pharmacy.uiowa.edu';
+
+// Directory aliases for shpe.org.uiowa.edu.
+$sites['orgshpe.local.drupal.uiowa.edu'] = 'shpe.org.uiowa.edu';
+$sites['orgshpe.dev.drupal.uiowa.edu'] = 'shpe.org.uiowa.edu';
+$sites['orgshpe.stage.drupal.uiowa.edu'] = 'shpe.org.uiowa.edu';
+$sites['orgshpe.prod.drupal.uiowa.edu'] = 'shpe.org.uiowa.edu';
