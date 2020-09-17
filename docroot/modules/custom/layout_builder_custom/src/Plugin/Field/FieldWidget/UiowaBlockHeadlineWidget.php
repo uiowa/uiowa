7d7c839d92ec05510861df0b4cc7e40113666d1a
Cory Skeers <cory-skeers@uiowa.edu>
2020-09-17 09:32:59 -0500

    Update field machine name in use on blocks.

--- docroot/modules/custom/layout_builder_custom/src/Plugin/Field/FieldWidget/UiowaBlockHeadlineWidget.php
+++ docroot/modules/custom/layout_builder_custom/src/Plugin/Field/FieldWidget/UiowaBlockHeadlineWidget.php
@@ -87,7 +87,6 @@ class UiowaBlockHeadlineWidget extends WidgetBase {
       $values[$delta]['headline'] = $data['container']['headline'];
       $values[$delta]['hide_headline'] = $data['container']['hide_headline'];
       $values[$delta]['heading_size'] = $data['container']['heading_size'];
-      $values[$delta]['child_heading_size'] = $data['container']['child_heading_size'];
       unset($values[$delta]['container']);
     }
 
