34be71db660ac445637bf363708f03fd72e9c790
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-10-12 14:15:15 -0500

    Alter feed add path to match admin breadcrubms

--- docroot/modules/custom/uiowa_aggregator/src/EventSubscriber/RouteSubscriber.php
+++ docroot/modules/custom/uiowa_aggregator/src/EventSubscriber/RouteSubscriber.php
@@ -14,8 +14,14 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
+    // Remove OPML access until we know what this is.
     if ($route = $collection->get('aggregator.opml_add')) {
       $route->setRequirement('_uiowa_core_access_check', 'TRUE');
+    }
+
+    // Set feed add path to match admin breadcrumbs so its easier to navigate.
+    if ($route = $collection->get('aggregator.feed_add')) {
+      $route->setPath('/admin/config/services/aggregator/add/feed');
     }
   }
 
