59585f2e14c0598b8e7315c0bdcc337339352ac7
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-10-19 09:49:50 -0500

    Alter aggrgator admin controller to add message about clear/import date

---
+++ docroot/modules/custom/uiowa_aggregator/src/EventSubscriber/ControllerAlterSubscriber.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\uiowa_aggregator\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * uiowa_aggregator event subscriber.
+ */
+class ControllerAlterSubscriber implements EventSubscriberInterface {
+  use StringTranslationTrait;
+
+  /**
+   * The config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $config;
+
+  protected $dateFormatter;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface
+   *   The config factory interface.
+   */
+  public function __construct(ConfigFactoryInterface $config, DateFormatter $dateFormatter) {
+    $this->config = $config;
+    $this->dateFormatter = $dateFormatter;
+  }
+
+  public function onView(GetResponseForControllerResultEvent $event) {
+    $request = $event->getRequest();
+    $route = $request->attributes->get('_route');
+
+    if ($route == 'aggregator.admin_overview') {
+      $build = $event->getControllerResult();
+
+      if (is_array($build)) {
+        $expire = $this->config->get('aggregator.settings')->get('items.expire');
+        $interval = $this->dateFormatter->formatInterval($expire);
+
+        $build['disclaimer'] = [
+          '#markup' => $this->t('<em>Note that feed items will be imported if their post date is within @interval or less. Items already imported will be deleted after @interval</em>.', [
+            '@interval' => $interval,
+          ]),
+        ];
+
+        $event->setControllerResult($build);
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::VIEW => ['onView', 50],
+    ];
+  }
+
+}
