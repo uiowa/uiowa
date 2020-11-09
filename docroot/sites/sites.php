84edc8434b0bb244d14be8f0c49e443ada966010
Danielle Kemper <danielle-kemper@uiowa.edu>
2020-11-09 12:53:58 -0600

    Initialize collections.pharmacy.uiowa.edu multisite on uiowa02 (#2481)
    * Initialize collections.pharmacy.uiowa.edu multisite on uiowa02
    
    * Update merge conflict
    
    Co-authored-by: vagrant <vagrant@local.drupal.uiowa.edu>
    Co-authored-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1800,6 +1800,12 @@ $sites['conferencesla2021.dev.drupal.uiowa.edu'] = 'sla2021.conference.uiowa.edu
 $sites['conferencesla2021.stage.drupal.uiowa.edu'] = 'sla2021.conference.uiowa.edu';
 $sites['conferencesla2021.prod.drupal.uiowa.edu'] = 'sla2021.conference.uiowa.edu';
 
+// Directory aliases for collections.pharmacy.uiowa.edu.
+$sites['pharmacycollections.local.drupal.uiowa.edu'] = 'collections.pharmacy.uiowa.edu';
+$sites['pharmacycollections.dev.drupal.uiowa.edu'] = 'collections.pharmacy.uiowa.edu';
+$sites['pharmacycollections.stage.drupal.uiowa.edu'] = 'collections.pharmacy.uiowa.edu';
+$sites['pharmacycollections.prod.drupal.uiowa.edu'] = 'collections.pharmacy.uiowa.edu';
+
 // Directory aliases for lhpdc.law.uiowa.edu.
 $sites['lawlhpdc.local.drupal.uiowa.edu'] = 'lhpdc.law.uiowa.edu';
 $sites['lawlhpdc.dev.drupal.uiowa.edu'] = 'lhpdc.law.uiowa.edu';
