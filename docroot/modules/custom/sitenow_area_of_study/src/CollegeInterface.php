9f387ed2dc2b579cd2bbd93bf51871782e2f9786
Cory Skeers <cory-skeers@uiowa.edu>
2020-11-13 15:04:43 -0600

    Create colleges as a config entity; areas of study module to house everything; initial college listings (without links) created

---
+++ docroot/modules/custom/sitenow_area_of_study/src/CollegeInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\sitenow_area_of_study;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface defining a college entity type.
+ */
+interface CollegeInterface extends ConfigEntityInterface {
+
+  const TYPE_COMPONENT = 'component';
+
+}
