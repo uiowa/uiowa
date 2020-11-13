88123b3d1dcd1a2e125aaf06d9776c53015dc629
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2020-11-13 17:05:37 -0600

    load territory data and use it on the map

--- docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Plugin/Block/AdmissionsCounselorsMapBlock.php
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Plugin/Block/AdmissionsCounselorsMapBlock.php
@@ -20,6 +20,30 @@ class AdmissionsCounselorsMapBlock extends BlockBase {
    */
   public function build() {
     $html = '<div id="admissions-counselors-map">&nbsp;</div>';
+    // Load persons tagged with the Counselor person type
+    // and create array of unique territory values.
+    $territories = [];
+    $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', 'person')
+      ->condition('field_person_types', 'counselor');
+    $nids = $query->execute();
+    if (!empty($nids)) {
+      $nodes = $node_storage->loadMultiple($nids);
+      foreach ($nodes as $node) {
+        // Get the field_person_territory values and assign them to an array.
+        if ($node->hasField('field_person_territory') &&
+          !$node->get('field_person_territory')->isEmpty()) {
+          $values = $node->get('field_person_territory')->getValue();
+          array_walk_recursive($values, function ($v) use (&$territories) {
+            $territories[] = $v;
+          });
+        }
+      }
+      // Filter out territory duplicates.
+      $territories = array_unique($territories);
+    }
+
     return [
       '#type' => 'markup',
       '#markup' => $this->t($html),
@@ -27,6 +51,11 @@ class AdmissionsCounselorsMapBlock extends BlockBase {
         'library' => [
           'uids_base/leaflet',
           'admissions_core/counselors-map',
+        ],
+        'drupalSettings' => [
+          'admissions_core' => [
+            'territories' => $territories,
+          ],
         ],
       ],
     ];
