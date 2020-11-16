495c91df58be349f48a041e38abf1454e4bd76c8
vagrant <vagrant@local.drupal.uiowa.edu>
2020-11-16 03:10:13 +0000

    Initialize reno.sites.uiowa.edu multisite on uiowa02

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1829,3 +1829,9 @@ $sites['siteslicensing.local.drupal.uiowa.edu'] = 'licensing.sites.uiowa.edu';
 $sites['siteslicensing.dev.drupal.uiowa.edu'] = 'licensing.sites.uiowa.edu';
 $sites['siteslicensing.stage.drupal.uiowa.edu'] = 'licensing.sites.uiowa.edu';
 $sites['siteslicensing.prod.drupal.uiowa.edu'] = 'licensing.sites.uiowa.edu';
+
+// Directory aliases for reno.sites.uiowa.edu.
+$sites['sitesreno.local.drupal.uiowa.edu'] = 'reno.sites.uiowa.edu';
+$sites['sitesreno.dev.drupal.uiowa.edu'] = 'reno.sites.uiowa.edu';
+$sites['sitesreno.stage.drupal.uiowa.edu'] = 'reno.sites.uiowa.edu';
+$sites['sitesreno.prod.drupal.uiowa.edu'] = 'reno.sites.uiowa.edu';
