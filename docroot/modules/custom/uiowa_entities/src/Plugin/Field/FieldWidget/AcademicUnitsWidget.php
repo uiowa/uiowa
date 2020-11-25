18058b3aafcc43fecf7f18bc8a41f54a25ad6157
Cory Skeers <cory-skeers@uiowa.edu>
2020-11-25 12:04:08 -0600

    Update to use dependency injection for entity_type.manager service.

--- docroot/modules/custom/uiowa_entities/src/Plugin/Field/FieldWidget/AcademicUnitsWidget.php
+++ docroot/modules/custom/uiowa_entities/src/Plugin/Field/FieldWidget/AcademicUnitsWidget.php
@@ -5,6 +5,10 @@ namespace Drupal\uiowa_entities\Plugin\Field\FieldWidget;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
 
 /**
  * Plugin implementation of the 'AcademicUnits' widget.
@@ -18,7 +22,32 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class AcademicUnitsWidget extends WidgetBase {
+class AcademicUnitsWidget extends WidgetBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The EntityTypeManager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * AcademicUnitsWidget constructor.
+   *
+   * {@inheritdoc}
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, EntityTypeManager $entityTypeManager) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings);
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($plugin_id, $plugin_definition, $configuration['field_definition'], $configuration['settings'], $configuration['third_party_settings'], $container
+      ->get('entity_type.manager'));
+  }
 
   /**
    * {@inheritdoc}
@@ -28,7 +57,7 @@ class AcademicUnitsWidget extends WidgetBase {
     $units = [];
     $options = array_filter($this->getSetting('types'));
     foreach ($options as $option) {
-      $units += \Drupal::entityTypeManager()
+      $units += $this->entityTypeManager
         ->getStorage('uiowa_academic_unit')
         ->loadByProperties(['type' => $option]);
     }
