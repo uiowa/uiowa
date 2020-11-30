c89c8de5d21510f0e19d54adf61a02d530d819e0
Gary Alan Ridgway <gary-ridgway@uiowa.edu>
2020-11-30 11:38:59 -0600

    working on getting new cookie file to work

--- docroot/themes/custom/uids_base/assets/js/banner-video-cookie.js
+++ docroot/themes/custom/uids_base/assets/js/banner-video-cookie.js
@@ -1,38 +1,39 @@
-console.log('OUTSIDE');
 (function ($, Drupal) {
 
   "use strict";
 
   Drupal.behaviors.bannerVideoExtend = {
     attach: function (context, settings) {
-      $('.media-video', context).once('banner-video-attach').each(function () {
-        console.log("HELLO");
-        //   const video = document.getElementById("video-container");
-        //   const btn = document.getElementById("video-btn");
-        //   const heroVideoCookie = $.cookie('herovideo');
-        //
-        //   // Check herovideo cookie to see if user paused video previously.
-        //   if (heroVideoCookie === 'paused') {
-        //     video.removeAttribute('autoplay');
-        //     video.pause();
-        //     btn.innerHTML = "<span class='element-invisible'>" + "Play" + "</span>";
-        //     btn.classList.remove("video-btn__pause");
-        //     btn.classList.add("video-btn__play");
-        //   }
-        //
-        //   // Create/erase herovideo cookie.
-        //   if (video) {
-        //     btn.onclick = function() {
-        //       if (video.paused) {
-        //         // Per request, create cookie that expires in 99 years.
-        //         $.cookie('herovideo', 'paused', { expires: 36135, path: '/' });
-        //       }
-        //       else {
-        //         // Remove a cookie.
-        //         $.removeCookie('herovideo', { path: '/' });
-        //       }
-        //     }
-        //   }
+      $('.media--video', context).once('media-video-attach').each(function () {
+        const video = this.querySelector("video");
+        const btn = this.querySelector(".video-controls .video-btn");
+        const bannerVideoCookie = $.cookie('bannervideo');
+        console.log([bannerVideoCookie]);
+
+        // Check bannervideo cookie to see if user paused video previously.
+        if (bannerVideoCookie === 'paused') {
+          video.removeAttribute('autoplay');
+          video.pause();
+          btn.innerHTML = "<span class='element-invisible'>" + "Play" + "</span>";
+          btn.classList.remove("video-btn__pause");
+          btn.classList.add("video-btn__play");
+        }
+
+        // Create/erase herovideo cookie.
+        if (video) {
+          btn.onclick = function() {
+            if (video.paused) {
+              console.log($.cookie('bannervideo'));
+              // Per request, create cookie that expires in 99 years.
+              $.cookie('bannervideo', 'paused', { expires: 36135, path: '/' });
+            }
+            else {
+              console.log('notpaused');
+              // Remove a cookie.
+              $.removeCookie('bannervideo', { path: '/' });
+            }
+          }
+        }
       });
     }
   };
