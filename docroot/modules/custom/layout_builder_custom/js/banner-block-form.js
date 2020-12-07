365702f882c02dd1d486331ed7c11ff032babad8
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-12-07 22:22:34 +0000

    Fixed a console js error for banner block form. Removed redundant '.media--video' class wrapper.

--- docroot/modules/custom/layout_builder_custom/js/banner-block-form.js
+++ docroot/modules/custom/layout_builder_custom/js/banner-block-form.js
@@ -8,22 +8,25 @@
       // good choices for classes/IDs to target in the inline block form.
       $('.media-library-widget', context).once('media-form-attach').each(function () {
 
-        // Check if the referenced media is a video.
-        const mediaTypeVideo = this.querySelector('.media--video');
+        // Check that we can access the next field.
+        // @todo Add a class to this field widget wrapper so that we can target it more precisely.
+        const checkbox_wrapper = $(this).parent().next('.form-wrapper');
 
-        // @todo Add a class to this field so we can target it more precisely.
-        if (mediaTypeVideo) {
-          let checkbox_wrapper = $(this).parent().next('.js-hide.form-wrapper');
-          // Show the autoplay field.
-          checkbox_wrapper.removeClass('js-hide');
-          checkbox_wrapper.removeAttr("tabindex");
-          checkbox_wrapper.removeAttr('aria-hidden');
-        } else {
-          let checkbox_wrapper = $(this).parent().next('.form-wrapper');
-          // Hide the autoplay field.
-          checkbox_wrapper.addClass('js-hide');
-          checkbox_wrapper.tabIndex = -1;
-          checkbox_wrapper.setAttribute('aria-hidden', "true");
+        if (checkbox_wrapper.length) {
+          // Check if the referenced media is a video.
+          const mediaTypeVideo = this.querySelector('.media--video');
+
+          if (mediaTypeVideo) {
+            // Show the autoplay field.
+            checkbox_wrapper.removeClass('js-hide');
+            checkbox_wrapper.removeAttr('tabindex');
+            checkbox_wrapper.removeAttr('aria-hidden');
+          } else {
+            // Hide the autoplay field.
+            checkbox_wrapper.addClass('js-hide');
+            checkbox_wrapper.tabIndex = -1;
+            checkbox_wrapper.attr('aria-hidden', 'true');
+          }
         }
       });
     }
