09ad2dce1f7caaeb69f42b4935b7d95a873a4e26
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-12-08 17:00:11 +0000

    Re-named video-cookie.js to remove reference to banners. Similarly, refactored that file to rename variables to not reference banner. Updated libraries entry andd reference to it. Added a comment about a future improvement to avoid the quick flash of autoplay video when the user has already requested a pause.

---
+++ docroot/themes/custom/uids_base/assets/js/video-cookie.js
@@ -0,0 +1,74 @@
+(function ($, Drupal) {
+
+  'use strict';
+
+  const UIOWA_VIDEO_COOKIE = 'uiowa-video';
+
+  Drupal.behaviors.uidsVideoCookie = {
+    attach: function (context, settings) {
+      $('.media--video', context).once('media-video-attach').each(function () {
+        const video = this.querySelector('video');
+        // We only need to execute the rest of this if the video element exists.
+        if (video) {
+          const btn = this.querySelector('.video-controls .video-btn');
+          // Set videoCookieCollection to be an empty dictionary.
+          let videoCookieCollection = {};
+          // If the video cookie already exists, load it in to the videoCookieCollection variable.
+          if ($.cookie(UIOWA_VIDEO_COOKIE)) {
+            videoCookieCollection = JSON.parse($.cookie(UIOWA_VIDEO_COOKIE));
+          }
+          // Also get the cookie id for later usage.
+          const videoCookieId = video.getAttribute('data-video-cookie-id');
+
+          // Check cookie id entry to see if the video was paused previously.
+          if (videoCookieCollection !== {} && videoCookieCollection[videoCookieId] === 'paused') {
+            // If they did, pause the video.
+            video.removeAttribute('autoplay');
+            video.pause();
+            btn.innerHTML = '<span class="element-invisible">' + 'Play' + '</span>';
+            btn.classList.remove('video-btn__pause');
+            btn.classList.add('video-btn__play');
+          }
+
+          // Create/erase herovideo cookie.
+            // On the video controls button click...
+            btn.onclick = function() {
+              // Update the video cookie to make sure we have the latest one.
+              if ($.cookie(UIOWA_VIDEO_COOKIE)) {
+                videoCookieCollection = JSON.parse($.cookie(UIOWA_VIDEO_COOKIE));
+              }
+              // And then if the video is paused...
+              if (video.paused) {
+                // Set an index equal to the cookie id to 'paused'.
+                videoCookieCollection[videoCookieId] = 'paused';
+                saveVideoCookie(videoCookieCollection);
+              }
+              else {
+                // Remove a cookie based upon the cookie id index.
+                if ($.cookie(UIOWA_VIDEO_COOKIE)) {
+                  delete videoCookieCollection[videoCookieId];
+
+                  // If cookie array is empty, remove cookie dictionary.
+                  if (Object.keys(videoCookieCollection).length === 0) {
+                    $.removeCookie(UIOWA_VIDEO_COOKIE, { path: '/' });
+                  }
+                  // Else, re-save the dictionary.
+                  else {
+                    saveVideoCookie(videoCookieCollection);
+                  }
+                }
+              }
+            }
+        }
+      });
+    }
+  };
+
+  function saveVideoCookie(cookie) {
+    // String-ify the cookie JSON so it can be saved.
+    const cookieString = JSON.stringify(cookie);
+    // Cookie is set to expire in 99 years.
+    $.cookie(UIOWA_VIDEO_COOKIE, cookieString, { expires: 36135, path: '/' });
+  }
+
+})(jQuery, Drupal);
