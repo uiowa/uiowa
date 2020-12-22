95e3aac43c3b26412a988f112559d9e0e195ba7c
Richard B. Porter <richard-b-porter@uiowa.edu>
2020-12-22 14:36:03 -0600

    Inject config factory into search form and set placeholder dynamically

--- docroot/modules/custom/uiowa_search/src/Form/SearchForm.php
+++ docroot/modules/custom/uiowa_search/src/Form/SearchForm.php
@@ -2,14 +2,42 @@
 
 namespace Drupal\uiowa_search\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Implements an example form.
  */
 class SearchForm extends FormBase {
+
+  /**
+   * The uiowa_search config.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * The form constructor.
+   *
+   * @param ConfigFactoryInterface $config
+   *   The config.factory service.
+   */
+  public function __construct(ConfigFactoryInterface $config) {
+    $this->config = $config->get('uiowa_search.settings');
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -32,11 +60,21 @@ class SearchForm extends FormBase {
           'sr-only',
         ],
       ],
-      '#attributes' => [
-        'placeholder' => 'Search this site',
-      ],
       '#maxlength' => '256',
       '#size' => '15',
+    ];
+
+    $placeholder = $this->t('Search');
+
+    if ($this->config->get('uiowa_search.cse_scope') === 1) {
+      $placeholder = $this->t('Search this site');
+    }
+    elseif ($this->config->get('uiowa_search.cse_scope') === 0) {
+      $placeholder = $this->t('Search all University of Iowa');
+    }
+
+    $form['search_terms']['#attributes'] = [
+      'placeholder' => $placeholder,
     ];
 
     $form['submit_search'] = [
