ccb631b5a3d2f02811de7ca7fa44d5de9da07639
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-12-22 22:27:21 +0000

    Added settings form.

---
+++ docroot/modules/custom/uiowa_pages/src/Form/SettingsForm.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Drupal\uiowa_pages\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\path_alias\AliasRepositoryInterface;
+use Drupal\pathauto\AliasCleanerInterface;
+use Drupal\pathauto\PathautoGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configure UIowa Articles settings for this site.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * The alias cleaner.
+   *
+   * @var \Drupal\pathauto\AliasCleanerInterface
+   */
+  protected $aliasCleaner;
+
+  /**
+   * The alias checker.
+   *
+   * @var \Drupal\path_alias\AliasRepositoryInterface
+   */
+  protected $aliasRepository;
+
+  /**
+   * The EntityTypeManager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The PathautoGenerator service.
+   *
+   * @var \Drupal\pathauto\PathautoGenerator
+   */
+  protected $pathAutoGenerator;
+
+  /**
+   * The Constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\pathauto\AliasCleanerInterface $pathauto_alias_cleaner
+   *   The alias cleaner.
+   * @param \Drupal\path_alias\AliasRepositoryInterface $aliasRepository
+   *   The alias checker.
+   * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
+   *   The EntityTypeManager service.
+   * @param \Drupal\pathauto\PathautoGenerator $pathAutoGenerator
+   *   The PathautoGenerator service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, AliasCleanerInterface $pathauto_alias_cleaner, AliasRepositoryInterface $aliasRepository, EntityTypeManager $entityTypeManager, PathautoGenerator $pathAutoGenerator) {
+    parent::__construct($config_factory);
+    $this->aliasCleaner = $pathauto_alias_cleaner;
+    $this->aliasRepository = $aliasRepository;
+    $this->entityTypeManager = $entityTypeManager;
+    $this->pathAutoGenerator = $pathAutoGenerator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('pathauto.alias_cleaner'),
+      $container->get('path_alias.repository'),
+      $container->get('entity_type.manager'),
+      $container->get('pathauto.generator')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'sitenow_articles_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'sitenow_articles.settings',
+      'pathauto.pattern.article',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    $form['global']['default_featured_image_display'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Display featured image'),
+      '#description' => $this->t('Set the default behavior for how to display a featured image.'),
+      '#options' => [
+        'do_not_display' => $this
+          ->t('Do not display'),
+        'small' => $this
+          ->t('Small'),
+        'medium' => $this
+          ->t('Medium'),
+        'large' => $this
+          ->t('Large'),
+      ],
+      '#default_value' => 'large',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // @todo Add code to save 'default_featured_image_display'.
+    parent::submitForm($form, $form_state);
+  }
+
+}
