4d7bb85cec16197dfd1e95cc2ffec2c9da3f460f
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2020-12-22 02:16:35 +0000

    Moved student profile location logic to admissions_core module.

--- docroot/modules/custom/layout_builder_custom/src/EventSubscriber/SectionComponentSubscriber.php
+++ docroot/modules/custom/layout_builder_custom/src/EventSubscriber/SectionComponentSubscriber.php
@@ -49,57 +49,6 @@ class SectionComponentSubscriber implements EventSubscriberInterface {
       }
     }
 
-    // @todo Move this to an Admissions-specific class.
-    if ($block instanceof FieldBlock && $block->getPluginId() === 'field_block:node:student_profile:field_person_hometown') {
-      $contexts = $event->getContexts();
-      if (isset($contexts['layout_builder.entity'])) {
-        if ($node = $contexts['layout_builder.entity']->getContextValue()) {
-          $home_location = [];
-
-          // Add hometown, if it exists.
-          $hometown = $node->hasField('field_person_hometown') ? $node->field_person_hometown->value : NULL;
-          $state = $node->hasField('field_person_territory') ? $node->field_person_territory->value : NULL;
-          if ($hometown) {
-            $home_location[] = $hometown;
-          }
-
-          // Check the country. Add the state if it exists and the country is the US.
-          // Otherwise, add the country.
-          $country = $node->hasField('field_student_profile_country') ? $node->field_student_profile_country->value : NULL;
-          if ($country) {
-            if ($country === 'US') {
-              if ($state) {
-                $home_location[] = $state;
-              }
-            } else {
-              $country_value = \Drupal::service('country_manager')->getList()[$country]->__toString();
-              $home_location[] = $country_value;
-            }
-          }
-          else {
-            if ($state) {
-              $home_location[] = $state;
-            }
-          }
-          if (!empty($home_location)) {
-            $node->field_person_hometown->value =  implode(', ', $home_location);
-            $content = $block->build();
-
-            $build = [
-              // @todo Move this to BlockBase in https://www.drupal.org/node/2931040.
-              '#theme' => 'block',
-              '#configuration' => $block->getConfiguration(),
-              '#plugin_id' => $block->getPluginId(),
-              '#base_plugin_id' => $block->getBaseId(),
-              '#derivative_plugin_id' => $block->getDerivativeId(),
-              '#weight' => $event->getComponent()->getWeight(),
-              'content' => $content,
-            ];
-          }
-        }
-      }
-    }
-
     // For cards, we are going to programmatically set the view mode for
     // the image field. This is necessary to allow selection of different
     // image formats.
---
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/EventSubscriber/SectionComponentSubscriber.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\admissions_core\EventSubscriber;
+
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Render\PreviewFallbackInterface;
+use Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent;
+use Drupal\layout_builder\LayoutBuilderEvents;
+use Drupal\layout_builder\Plugin\Block\FieldBlock;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Add an HTML class to blocks that are displaying placeholder text.
+ */
+class SectionComponentSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY] = [
+      'onBuildRender',
+      50,
+    ];
+    return $events;
+  }
+
+  /**
+   * Builds render arrays for block plugins and sets it on the event.
+   *
+   * @param \Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent $event
+   *   The section component render event.
+   */
+  public function onBuildRender(SectionComponentBuildRenderArrayEvent $event) {
+    $block = $event->getPlugin();
+    if (!$block instanceof BlockPluginInterface) {
+      return;
+    }
+
+    $build = $event->getBuild();
+
+    // @todo Move this to an Admissions-specific class.
+    if ($block instanceof FieldBlock && $block->getPluginId() === 'field_block:node:student_profile:field_person_hometown') {
+      $contexts = $event->getContexts();
+      if (isset($contexts['layout_builder.entity'])) {
+        if ($node = $contexts['layout_builder.entity']->getContextValue()) {
+          $home_location = [];
+
+          // Add hometown, if it exists.
+          $hometown = $node->hasField('field_person_hometown') ? $node->field_person_hometown->value : NULL;
+          $state = $node->hasField('field_person_territory') ? $node->field_person_territory->value : NULL;
+          if ($hometown) {
+            $home_location[] = $hometown;
+          }
+
+          // Check the country. Add the state if it exists and the country is the US.
+          // Otherwise, add the country.
+          $country = $node->hasField('field_student_profile_country') ? $node->field_student_profile_country->value : NULL;
+          if ($country) {
+            if ($country === 'US') {
+              if ($state) {
+                $home_location[] = $state;
+              }
+            } else {
+              $country_value = \Drupal::service('country_manager')->getList()[$country]->__toString();
+              $home_location[] = $country_value;
+            }
+          }
+          else {
+            if ($state) {
+              $home_location[] = $state;
+            }
+          }
+          if (!empty($home_location)) {
+            $node->field_person_hometown->value =  implode(', ', $home_location);
+            $content = $block->build();
+
+            $build = [
+              // @todo Move this to BlockBase in https://www.drupal.org/node/2931040.
+              '#theme' => 'block',
+              '#configuration' => $block->getConfiguration(),
+              '#plugin_id' => $block->getPluginId(),
+              '#base_plugin_id' => $block->getBaseId(),
+              '#derivative_plugin_id' => $block->getDerivativeId(),
+              '#weight' => $event->getComponent()->getWeight(),
+              'content' => $content,
+            ];
+          }
+        }
+      }
+    }
+
+    $event->setBuild($build);
+  }
+
+}
