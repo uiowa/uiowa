876e6fd1ba7078778dcc30b314c693a7da13ceb5
Cory Skeers <cory-skeers@uiowa.edu>
2021-01-13 15:11:00 -0600

    Remove newly created media entities prior to article rollback.

--- docroot/modules/custom/sitenow_migrate/src/EventSubscriber/MigratePreRollbackEvent.php
+++ docroot/modules/custom/sitenow_migrate/src/EventSubscriber/MigratePreRollbackEvent.php
@@ -70,6 +70,11 @@ class MigratePreRollbackEvent implements EventSubscriberInterface {
         $migrate_map = 'migrate_map_' . $migration_id;
         $this->removeMediaEntities($migrate_map);
         break;
+
+      // @todo move this to grad_migrate.
+      case 'd7_grad_article':
+        $this->removeArticleMedia();
+        break;
     }
   }
 
@@ -96,6 +101,31 @@ class MigratePreRollbackEvent implements EventSubscriberInterface {
 
     $entityManager = $this->entityTypeManager->getStorage('media');
     $mediaEntities = $entityManager->loadMultiple($results);
+    $entityManager->delete($mediaEntities);
+  }
+
+  /**
+   * Removes media entities that were created as part of the article migration.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function removeArticleMedia() {
+    // @todo move this to grad_migrate.
+    $connection = Database::getConnection();
+    $query = $connection->select('migrate_map_d7_grad_article', 'mm');
+    $query->innerJoin('entity_usage', 'usage', 'mm.destid1 = usage.target_id');
+    $query = $query->fields('usage', ['target_id']);
+    // @todo check that the media is ONLY used in migrated articles.
+    $mids = $query->execute()
+      ->fetchCol();
+
+    // Delete the media entities.
+    // This should include removal of the file usages,
+    // which will mark them for deletion on next cleanup.
+    $entityManager = $this->entityTypeManager->getStorage('media');
+    $mediaEntities = $entityManager->loadMultiple($mids);
     $entityManager->delete($mediaEntities);
   }
 
