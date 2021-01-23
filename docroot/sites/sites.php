94707e419e2843010cab47182c6805bad2c1eb68
cory-skeers <49074566+cory-skeers@users.noreply.github.com>
2021-01-22 19:13:36 -0600

    Provisions 01/22/21 (#2917)
    * Initialize physicsresearch.sites.uiowa.edu multisite on uiowa02
    
    * Initialize newmoonmagazine.org.uiowa.edu multisite on uiowa02

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -2015,3 +2015,15 @@ $sites['labkang.local.drupal.uiowa.edu'] = 'kang.lab.uiowa.edu';
 $sites['labkang.dev.drupal.uiowa.edu'] = 'kang.lab.uiowa.edu';
 $sites['labkang.stage.drupal.uiowa.edu'] = 'kang.lab.uiowa.edu';
 $sites['labkang.prod.drupal.uiowa.edu'] = 'kang.lab.uiowa.edu';
+
+// Directory aliases for physicsresearch.sites.uiowa.edu.
+$sites['sitesphysicsresearch.local.drupal.uiowa.edu'] = 'physicsresearch.sites.uiowa.edu';
+$sites['sitesphysicsresearch.dev.drupal.uiowa.edu'] = 'physicsresearch.sites.uiowa.edu';
+$sites['sitesphysicsresearch.stage.drupal.uiowa.edu'] = 'physicsresearch.sites.uiowa.edu';
+$sites['sitesphysicsresearch.prod.drupal.uiowa.edu'] = 'physicsresearch.sites.uiowa.edu';
+
+// Directory aliases for newmoonmagazine.org.uiowa.edu.
+$sites['orgnewmoonmagazine.local.drupal.uiowa.edu'] = 'newmoonmagazine.org.uiowa.edu';
+$sites['orgnewmoonmagazine.dev.drupal.uiowa.edu'] = 'newmoonmagazine.org.uiowa.edu';
+$sites['orgnewmoonmagazine.stage.drupal.uiowa.edu'] = 'newmoonmagazine.org.uiowa.edu';
+$sites['orgnewmoonmagazine.prod.drupal.uiowa.edu'] = 'newmoonmagazine.org.uiowa.edu';
