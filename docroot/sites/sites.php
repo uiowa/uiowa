b21e8d796d59045c7f16e9ae521b443d8a1dcce6
vagrant <vagrant@local.drupal.uiowa.edu>
2021-02-01 14:54:07 +0000

    Initialize peate.lab.uiowa.edu multisite on uiowa02

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -2117,3 +2117,9 @@ $sites['osc.local.drupal.uiowa.edu'] = 'osc.uiowa.edu';
 $sites['osc.dev.drupal.uiowa.edu'] = 'osc.uiowa.edu';
 $sites['osc.stage.drupal.uiowa.edu'] = 'osc.uiowa.edu';
 $sites['osc.prod.drupal.uiowa.edu'] = 'osc.uiowa.edu';
+
+// Directory aliases for peate.lab.uiowa.edu.
+$sites['labpeate.local.drupal.uiowa.edu'] = 'peate.lab.uiowa.edu';
+$sites['labpeate.dev.drupal.uiowa.edu'] = 'peate.lab.uiowa.edu';
+$sites['labpeate.stage.drupal.uiowa.edu'] = 'peate.lab.uiowa.edu';
+$sites['labpeate.prod.drupal.uiowa.edu'] = 'peate.lab.uiowa.edu';
