fbee6e67500b07ecde5f2850ace651a80453b4ba
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2021-02-05 10:39:48 -0600

    custom aos buttons block

---
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Plugin/Block/AOSButtons.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\admissions_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * An Area of Study Buttons block.
+ *
+ * @Block(
+ *   id = "aosbuttons_block",
+ *   admin_label = @Translation("Area of Study Buttons Block"),
+ *   category = @Translation("Area of Study")
+ * )
+ */
+class AOSButtons extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['label_display' => FALSE];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $markup = '<div class="layout-builder-block block-margin__bottom">
+      <p>
+          <a class="bttn bttn--full bttn--primary bttn--caps" href="https://www.example.com">
+              Apply Now <span class="fa-arrow-right fas"></span>
+          </a>
+      </p>
+    </div>
+    <div class="layout-builder-block block-margin__bottom block-margin__top">
+      <p>
+        <a class="bttn bttn--full bttn--secondary bttn--caps" href="https://www.maui.uiowa.edu/maui/pub/admissions/webinquiry/undergraduate.page">
+            Request Info <span class="fa-arrow-right fas"></span>
+        </a>
+      </p>
+    </div>';
+
+    return ['#markup' => $markup];
+  }
+}
