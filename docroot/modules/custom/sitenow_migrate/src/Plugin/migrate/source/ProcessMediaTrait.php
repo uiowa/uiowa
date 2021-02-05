b6bce58b455d7f53f80241bfee6b6e4cffe03ac0
Cory Skeers <cory-skeers@uiowa.edu>
2021-02-05 10:26:50 -0600

    Move some methods into the general ProcessMediaTrait. May need some edits for use outside grad_migrate.

--- docroot/modules/custom/sitenow_migrate/src/Plugin/migrate/source/ProcessMediaTrait.php
+++ docroot/modules/custom/sitenow_migrate/src/Plugin/migrate/source/ProcessMediaTrait.php
@@ -18,6 +18,24 @@ trait ProcessMediaTrait {
   protected $fileSystem;
 
   /**
+   * Get the source path from the migration config.
+   */
+  protected function getSourceBasePath() {
+    if (isset($this->configuration['constants']) && isset($this->configuration['constants']['SOURCE_BASE_PATH'])) {
+      return $this->configuration['constants']['SOURCE_BASE_PATH'];
+    }
+
+    return '';
+  }
+
+  /**
+   * Return the path we'll be writing to.
+   */
+  protected function getDrupalFileDirectory() {
+    return 'public://' . date('Y-m') . '/';
+  }
+
+  /**
    * Regex to find Drupal 7 JSON for inline embedded files.
    */
   public function entityReplace($match) {
@@ -213,6 +231,51 @@ trait ProcessMediaTrait {
         default:
           return FALSE;
       }
+    }
+  }
+
+  /**
+   * Process an image field.
+   */
+  protected function processImageField(&$row, $field_name) {
+    // Check if an image was attached, and if so, update with new fid.
+    $original_fid = $row->getSourceProperty("{$field_name}_fid");
+
+    if (isset($original_fid)) {
+      $uri = $this->fidQuery($original_fid)['uri'];
+      $filename_w_subdir = str_replace('public://', '', $uri);
+      // Split apart the filename from the subdirectory path.
+      $filename_w_subdir = explode('/', $filename_w_subdir);
+      $filename = array_pop($filename_w_subdir);
+      $subdir = implode('/', $filename_w_subdir) . '/';
+      // Get a connection for the destination database.
+      $dest_connection = \Drupal::database();
+      $dest_query = $dest_connection->select('file_managed', 'f');
+      $new_fid = $dest_query->fields('f', ['fid'])
+        ->condition('f.filename', $filename)
+        ->execute()
+        ->fetchField();
+
+      $meta = [
+        'alt' => $row->getSourceProperty("{$field_name}_alt"),
+        'title' => $row->getSourceProperty("{$field_name}_title"),
+      ];
+
+      if (!$new_fid) {
+        // Use the filename, update the source base path with the subdirectory.
+        $new_fid = $this->downloadFile($filename, $this->getSourceBasePath() . $subdir, $this->getDrupalFileDirectory());
+        if ($new_fid) {
+          $mid = $this->createMediaEntity($new_fid, $meta, 1);
+        }
+      }
+      else {
+        $mid = $this->getMid($filename);
+        // And in case we had the file, but not the media entity.
+        if (!$mid) {
+          $mid = $this->createMediaEntity($new_fid, $meta, 1);
+        }
+      }
+      $row->setSourceProperty("{$field_name}_fid", $mid);
     }
   }
 
