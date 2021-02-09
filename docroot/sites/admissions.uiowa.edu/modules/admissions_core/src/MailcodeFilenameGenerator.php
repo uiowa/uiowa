98f2547d4a586cfa1f9814d5a8329ba25d231185
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-02-09 11:21:59 -0600

    Override generateFilename method for AOS print code field

--- docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/MailcodeFilenameGenerator.php
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/MailcodeFilenameGenerator.php
@@ -19,4 +19,27 @@ class MailcodeFilenameGenerator extends FilenameGenerator {
     $this->innerService = $filenameGenerator;
     parent::__construct($transliteration);
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function generateFilename(array $entities, callable $entity_label_callback = NULL) {
+    $filenames = [];
+
+    /** @var \Drupal\Core\Entity\EntityInterface $entity */
+    foreach ($entities as $entity) {
+      if ($entity->getType() == 'area_of_study') {
+        $label = $entity->get('field_area_of_study_mail_code')->getString();
+      }
+      else {
+        $label = $entity->label();
+      }
+
+      if ($label = trim($this->sanitizeFilename($label, $entity->language()->getId()))) {
+        $filenames[] = $label;
+      }
+    }
+
+    return $filenames ? implode('-', $filenames) : static::DEFAULT_FILENAME;
+  }
 }
