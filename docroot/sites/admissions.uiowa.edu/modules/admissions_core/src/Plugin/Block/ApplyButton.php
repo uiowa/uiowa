954c2a0bbb2920febfd8fb208ef8bd8c21f2cc57
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2021-03-12 16:59:10 -0600

    split buttons out so they can be used on student profile

--- docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Plugin/Block/ApplyButton.php
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Plugin/Block/ApplyButton.php
@@ -5,15 +5,15 @@ namespace Drupal\admissions_core\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 
 /**
- * An Area of Study Buttons block.
+ * An Apply button block.
  *
  * @Block(
- *   id = "aosbuttons_block",
- *   admin_label = @Translation("Area of Study Buttons Block"),
- *   category = @Translation("Area of Study")
+ *   id = "applybutton_block",
+ *   admin_label = @Translation("Apply Block"),
+ *   category = @Translation("Admissions Core")
  * )
  */
-class AOSButtons extends BlockBase {
+class ApplyButton extends BlockBase {
 
   /**
    * {@inheritdoc}
@@ -30,11 +30,6 @@ class AOSButtons extends BlockBase {
         <a class="bttn bttn--full bttn--primary bttn--caps" href="https://apply.admissions.uiowa.edu/admissions/login.page">
             Apply Now <span class="fa-arrow-right fas"></span>
         </a>
-    </div>
-    <div class="layout-builder-block block-margin__bottom block-margin__top--extra">
-      <a class="bttn bttn--full bttn--secondary bttn--caps" href="https://www.maui.uiowa.edu/maui/pub/admissions/webinquiry/undergraduate.page">
-          Request Info <span class="fa-arrow-right fas"></span>
-      </a>
     </div>';
 
     return ['#markup' => $markup];
