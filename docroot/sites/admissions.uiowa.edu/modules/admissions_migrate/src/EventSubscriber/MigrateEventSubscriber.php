8f46f5bb25f9628f106e6b8b7fcc15f47dab4ad9
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-03-15 14:45:09 -0500

    Create and migrate transfer tips after saving row

---
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_migrate/src/EventSubscriber/MigrateEventSubscriber.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\admissions_migrate\EventSubscriber;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\migrate\Event\MigrateEvents;
+use Drupal\migrate\Event\MigratePostRowSaveEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Admissions Migrate event subscriber.
+ */
+class MigrateEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The entity_type.manager service.
+   *
+   * @var EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param EntityTypeManagerInterface $entityTypeManager
+   *   The entity_type.manager service.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      MigrateEvents::POST_ROW_SAVE => ['onPostRowSave']
+    ];
+  }
+
+  /**
+   * Create transfer tips nodes after saving a row so we know the destination.
+   *
+   * @param MigratePostRowSaveEvent $event
+   *   The migrate post row save event.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function onPostRowSave(MigratePostRowSaveEvent $event) {
+    $row = $event->getRow();
+
+    if ($tips = $row->getSourceProperty('field_transfer_tips')) {
+      $node = $this->entityTypeManager->getStorage('node')->create([
+        'type' => 'transfer_tips',
+        'field_transfer_tips_aos' => $event->getDestinationIdValues()[0],
+        'body' => $row->getSourceProperty('field_transfer_tips'),
+      ]);
+
+      $node->save();
+    }
+  }
+
+}
