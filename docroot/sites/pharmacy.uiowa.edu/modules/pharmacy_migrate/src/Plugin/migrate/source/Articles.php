b73d21b72c43d9f8a50c16da83bff6784f9cf124
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-03-24 12:55:35 -0500

    Map teaser, process inline media

--- docroot/sites/pharmacy.uiowa.edu/modules/pharmacy_migrate/src/Plugin/migrate/source/Articles.php
+++ docroot/sites/pharmacy.uiowa.edu/modules/pharmacy_migrate/src/Plugin/migrate/source/Articles.php
@@ -3,6 +3,7 @@ namespace Drupal\pharmacy_migrate\Plugin\migrate\source;
 
 use Drupal\migrate\Row;
 use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
+use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
 
 /**
  * Migrate Source plugin.
@@ -13,6 +14,7 @@ use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
  * )
  */
 class Articles extends BaseNodeSource {
+  use ProcessMediaTrait;
 
   /**
    * {@inheritdoc}
@@ -38,6 +40,27 @@ class Articles extends BaseNodeSource {
    */
   public function prepareRow(Row $row) {
     parent::prepareRow($row);
+
+    // Search for D7 inline embeds and replace with D8 inline entities.
+    $content = $row->getSourceProperty('body');
+
+    if (!empty($content)) {
+      $content[0]['value'] = preg_replace_callback("|\[\[\{.*?\"fid\":\"(.*?)\".*?\]\]|", [
+        $this,
+        'entityReplace',
+      ], $content[0]['value']);
+
+      $row->setSourceProperty('body', $content);
+
+      // Check summary, and create one if none exists.
+      if (empty($content[0]['summary'])) {
+        $new_summary = $this->extractSummaryFromText($content[0]['value']);
+        $row->setSourceProperty('body_summary', $new_summary);
+      }
+      else {
+        $row->setSourceProperty('body_summary', $content[0]['summary']);
+      }
+    }
 
     //@todo: Unlink anchors in body from articles before 2016.
     return TRUE;
