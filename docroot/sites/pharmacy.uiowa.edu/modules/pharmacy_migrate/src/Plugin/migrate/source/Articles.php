44e13bc10972a121bab7abb9415637fdf0ec43ee
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-04-07 13:18:59 -0500

    Fix some links using manual lookup map

--- docroot/sites/pharmacy.uiowa.edu/modules/pharmacy_migrate/src/Plugin/migrate/source/Articles.php
+++ docroot/sites/pharmacy.uiowa.edu/modules/pharmacy_migrate/src/Plugin/migrate/source/Articles.php
@@ -69,10 +69,23 @@ class Articles extends BaseNodeSource {
         }
         else {
           if (strpos($href, '/node/') === 0 || stristr($href, 'pharmacy.uiowa.edu/node/')) {
-            $this->logger->info('Internal link @link found in article @article.', [
-              '@link' => $href,
-              '@article' => $row->getSourceProperty('title'),
-            ]);
+            $nid = explode('node/', $href)[1];
+
+            if ($lookup = $this->manualLookup($nid)) {
+              $link->setAttribute('href', $lookup);
+              $link->parentNode->replaceChild($link, $link);
+              $this->logger->info('Replaced internal link @link in article @article.', [
+                '@link' => $href,
+                '@article' => $row->getSourceProperty('title'),
+              ]);
+
+            }
+            else {
+              $this->logger->notice('Unable to replace internal link @link in article @article.', [
+                '@link' => $href,
+                '@article' => $row->getSourceProperty('title'),
+              ]);
+            }
           }
         }
 
@@ -113,6 +126,96 @@ class Articles extends BaseNodeSource {
     $row->setSourceProperty('tags', $tags);
 
     return TRUE;
+  }
+
+  /**
+   * Return the destination given a NID on the old site.
+   *
+   * @param int $nid
+   *   The node ID.
+   *
+   * @return false|string
+   *   The new path or FALSE if not in the map.
+   */
+  protected function manualLookup($nid) {
+    $map = [
+      1122 => '/people/jeanine-p-abrons',
+      1234 => '/people/guohua',
+      5022 => '/people/ethan-j-anderson',
+      3993 => '/people/karen-ann-baker',
+      497 => '/people/elizabeth-beltz',
+      606 => '/people/nicole-k-brogden',
+      896 => '/people/mike-brownlee',
+      15436 => '/people/andrean-burnett',
+      17056 => '/people/renan-cabrera-lafuente',
+      585 => '/people/matthew-cantrell',
+      3484 => '/people/cole-g-chapman',
+      498 => '/people/elizabeth-chrischilles',
+      650 => '/people/jay-dean-currie',
+      16506 => '/people/david-dick',
+      520 => '/people/maureen-d-donovan',
+      833 => '/people/jonathan-doorn',
+      506 => '/people/william-r-doucette',
+      748 => '/people/vern-kent-duba',
+      515 => '/people/michael-w-duffel',
+      4491 => '/people/satheesh-elangovan',
+      757 => '/people/erika-j-ernst',
+      619 => '/people/michael-e-ernst',
+      792 => '/people/brett-faine',
+      526 => '/people/t-michael-farley',
+      980 => '/people/jennifer-fiegel',
+      4128 => '/people/lorin-fisher',
+      586 => '/people/michelle-fravel',
+      15441 => '/people/marie-e-gaine',
+      525 => '/people/amber-m-goedken',
+      3946 => '/people/ramprakash-govindarajan',
+      642 => '/people/ronald-herman',
+      851 => '/people/morgan-sayler-herring',
+      626 => '/people/jim-hoehns',
+      782 => '/people/ryan-b-jacobsen',
+      473 => '/people/zhendong-jin',
+      1049 => '/people/jill-kauer',
+      4835 => '/people/korey-kennelty',
+      597 => '/people/robert-j-kerns',
+      601 => '/people/laura-e-knockel',
+      8486 => '/people/lee-kral',
+      1011 => '/people/donald-e-letendre',
+      4654 => '/people/kashelle-lockman',
+      740 => '/people/leonard-richard-macgillivray',
+      3992 => '/people/cindy-lou-marek',
+      4527 => '/people/james-martin',
+      783 => '/people/nic-mastascusa',
+      587 => '/people/deanna-l-mcdanel',
+      517 => '/people/gary-milavetz',
+      15276 => '/people/benjamin-miskle',
+      15186 => '/people/reza-nejadnik',
+      12751 => '/people/theodore-pham-nguyen',
+      6051 => '/people/eric-nuxoll',
+      945 => '/people/stuart-k-pitman',
+      486 => '/people/linnea-polgreen',
+      4480 => '/people/laura-l-ponto',
+      3756 => '/people/james-b-ray',
+      3798 => '/people/mary-elizabeth-ray',
+      539 => '/people/jeffrey-c-reist',
+      738 => '/people/kevin-g-rice',
+      1044 => '/people/dave-l-roman',
+      828 => '/people/aliasger-k-salem',
+      1128 => '/people/mary-chen-schroeder',
+      957 => '/people/jordan-l-schultz',
+      845 => '/people/jenny-l-seyfer',
+      1136 => '/people/ashley-spies',
+      1163 => '/people/lewis-l-stevens',
+      683 => '/people/john-swegle',
+      919 => '/people/traviss-tubbs',
+      510 => '/people/julie-m-urmie',
+      553 => '/people/stevie-veach',
+      998 => '/people/susan-staggs-vos',
+      4479 => '/people/george-j-weiner',
+      1009 => '/people/sara-ann-wiedenfeld',
+      634 => '/people/matthew-j-witry',
+    ];
+
+    return isset($map[$nid]) ? $map[$nid] : FALSE;
   }
 
 }
