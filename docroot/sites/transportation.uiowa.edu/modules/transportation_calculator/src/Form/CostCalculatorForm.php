fc559b4e84add098e9de949ed895a249ab5b934a
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-05-07 15:02:01 -0500

    phpcs

--- docroot/sites/transportation.uiowa.edu/modules/transportation_calculator/src/Form/CostCalculatorForm.php
+++ docroot/sites/transportation.uiowa.edu/modules/transportation_calculator/src/Form/CostCalculatorForm.php
@@ -34,32 +34,32 @@ class CostCalculatorForm extends FormBase {
       ],
       'distance' => [
         '#type' => 'number',
-        '#title' => t('Distance'),
-        '#description' => t('What is your daily round trip commute distance?'),
-        '#field_suffix' => t('Miles'),
+        '#title' => $this->t('Distance'),
+        '#description' => $this->t('What is your daily round trip commute distance?'),
+        '#field_suffix' => $this->t('Miles'),
         '#default_value' => 45,
       ],
       'days_travel' => [
-        '#title' => t('Days of travel'),
+        '#title' => $this->t('Days of travel'),
         '#type' => 'number',
-        '#description' => t('How many days a month do you normally travel to work?'),
-        '#field_suffix' => t('Days'),
+        '#description' => $this->t('How many days a month do you normally travel to work?'),
+        '#field_suffix' => $this->t('Days'),
         '#default_value' => 21,
       ],
       'aaa_cost_per_mile' => [
-        '#title' => t('AAA cost per mile'),
+        '#title' => $this->t('AAA cost per mile'),
         '#type' => 'number',
-        '#description' => t('Based on <a href="@aaa">AAA’s average cost per mile</a> for operating a vehicle 15,000 miles per year.', ['@aaa' => 'http://exchange.aaa.com/automobiles-travel/automobiles/driving-costs/#.WH6WfLYrJsZ']),
-        '#field_prefix' => t('$'),
+        '#description' => $this->t('Based on <a href="@aaa">AAA’s average cost per mile</a> for operating a vehicle 15,000 miles per year.', ['@aaa' => 'http://exchange.aaa.com/automobiles-travel/automobiles/driving-costs/#.WH6WfLYrJsZ']),
+        '#field_prefix' => $this->t('$'),
         '#default_value' => 0.5899,
         '#disabled' => TRUE,
         '#step' => 0.0001,
       ],
       'cost_to_park' => [
-        '#title' => t('Parking Cost'),
+        '#title' => $this->t('Parking Cost'),
         '#type' => 'number',
-        '#description' => t('How much do you currently pay for monthly parking?'),
-        '#field_prefix' => t('$'),
+        '#description' => $this->t('How much do you currently pay for monthly parking?'),
+        '#field_prefix' => $this->t('$'),
         '#default_value' => 62,
         '#step' => 0.1,
       ],
@@ -92,9 +92,15 @@ class CostCalculatorForm extends FormBase {
   }
 
   /**
+   * AJAX form callback.
+   *
    * @param array $form
+   *   The form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state.
+   *
    * @return array
+   *   The form results.
    */
   public function calculateCost(array &$form, FormStateInterface $form_state): array {
     $monthly = $form_state->getValue('distance') * $form_state->getValue('days_travel') * $form_state->getValue('aaa_cost_per_mile') + $form_state->getValue('cost_to_park');
@@ -125,8 +131,8 @@ class CostCalculatorForm extends FormBase {
           ],
           'item' => [
             '#type' => 'item',
-            '#title' => t('Monthly commute costs'),
-            '#markup' => t('<span>@monthly</span>', [
+            '#title' => $this->t('Monthly commute costs'),
+            '#markup' => $this->t('<span>@monthly</span>', [
               '@monthly' => number_format($monthly, 2),
             ]),
             '#field_prefix' => '$',
@@ -141,8 +147,8 @@ class CostCalculatorForm extends FormBase {
           ],
           'item' => [
             '#type' => 'item',
-            '#title' => t('Yearly commute costs'),
-            '#markup' => t('<span>@yearly</span>', [
+            '#title' => $this->t('Yearly commute costs'),
+            '#markup' => $this->t('<span>@yearly</span>', [
               '@yearly' => number_format($yearly, 2),
             ]),
             '#field_prefix' => '$',
@@ -160,41 +166,41 @@ class CostCalculatorForm extends FormBase {
           '#type' => 'table',
           '#caption' => $this->t('Depending on your distance from campus, some modes may not apply.'),
           '#header' => [
-            t('Mode of Transportation'), t('Cost Per Year'), t('Yearly Savings'),
+            $this->t('Mode of Transportation'), $this->t('Cost Per Year'), $this->t('Yearly Savings'),
           ],
           '#rows' => [
             [
-              'mode' => t('CAMBUS'),
+              'mode' => $this->t('CAMBUS'),
               'cost' => '$' . number_format(0, 2),
               'savings' => '$' . number_format($yearly, 2),
             ],
             [
-              'mode' => t('Parking'),
+              'mode' => $this->t('Parking'),
               'cost' => '$' . number_format($yearly, 2),
               'savings' => '$' . number_format(0, 2),
             ],
             [
-              'mode' => t('Vanpool'),
+              'mode' => $this->t('Vanpool'),
               'cost' => '$' . number_format($vanpool, 2),
               'savings' => '$' . number_format($yearly - $vanpool, 2),
             ],
             [
-              'mode' => t('Carpool'),
+              'mode' => $this->t('Carpool'),
               'cost' => '$' . number_format($yearly / 2, 2),
               'savings' => '$' . number_format($yearly / 2, 2),
             ],
             [
-              'mode' => t('380 Express'),
+              'mode' => $this->t('380 Express'),
               'cost' => '$690.00',
               'savings' => '$' . number_format($yearly - 690),
             ],
             [
-              'mode' => t('Bus Pass (U-PASS)'),
+              'mode' => $this->t('Bus Pass (U-PASS)'),
               'cost' => '$' . number_format($upass, 2),
               'savings' => '$' . number_format($yearly - $upass, 2),
             ],
             [
-              'mode' => t('Bike/Walk'),
+              'mode' => $this->t('Bike/Walk'),
               'cost' => '$0.00',
               'savings' => '$' . number_format($yearly, 2),
             ],
