2dbd333fe240a8258cf6167da17259fd2feb8f70
Cory Skeers <cory-skeers@uiowa.edu>
2021-05-07 08:45:07 -0500

    Missed a camelCase update.

--- docroot/modules/custom/sitenow_migrate/src/Plugin/migrate/source/ProcessMediaTrait.php
+++ docroot/modules/custom/sitenow_migrate/src/Plugin/migrate/source/ProcessMediaTrait.php
@@ -161,7 +161,7 @@ trait ProcessMediaTrait {
         'data-align' => $align,
         'data-entity-type' => 'media',
         'data-entity-uuid' => $uuid,
-        'data-view-mode' => isset($view_mode) ? $view_mode : $this->view_mode,
+        'data-view-mode' => isset($view_mode) ? $view_mode : $this->viewMode,
       ],
     ];
 
