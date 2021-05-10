88250ea4ad9c33616db7875fa6f1a6ccc042588c
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-05-10 16:25:46 -0500

    Chain config sets

--- docroot/sites/transportation.uiowa.edu/modules/transportation_calculator/src/Form/CostCalculatorSettingsForm.php
+++ docroot/sites/transportation.uiowa.edu/modules/transportation_calculator/src/Form/CostCalculatorSettingsForm.php
@@ -153,32 +153,14 @@ class CostCalculatorSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('transportation_calculator.settings')
       ->set('distance', $form_state->getValue('distance'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('days-of-travel', $form_state->getValue('days-of-travel'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('parking-cost', $form_state->getValue('parking-cost'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('van-base-rate', $form_state->getValue('van-base-rate'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('van-mileage-rate', $form_state->getValue('van-mileage-rate'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('maximum-van-riders', $form_state->getValue('maximum-van-riders'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('average-working-days', $form_state->getValue('average-working-days'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('upass-cost', $form_state->getValue('upass-cost'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('aaa-cost', $form_state->getValue('aaa-cost'))
-      ->save();
-    $this->config('transportation_calculator.settings')
       ->set('380-express', $form_state->getValue('380-express'))
       ->save();
 
