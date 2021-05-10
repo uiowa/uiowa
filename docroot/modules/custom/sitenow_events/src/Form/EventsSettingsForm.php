a9530379a9edb692bf1a9f5caf9974c72b8ba639
Benjamin Speare <benjamin-speare@uiowa.edu>
2021-05-10 20:06:14 +0000

    Adding Danielle's work in hopefully the right place

--- docroot/modules/custom/sitenow_events/src/Form/EventsSettingsForm.php
+++ docroot/modules/custom/sitenow_events/src/Form/EventsSettingsForm.php
@@ -13,20 +13,28 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Configure events settings for this site.
  */
 class EventsSettingsForm extends ConfigFormBase {
-
   /**
    * The alias cleaner.
    *
    * @var \Drupal\pathauto\AliasCleanerInterface
    */
   protected $aliasCleaner;
-
   /**
    * The alias checker.
    *
    * @var \Drupal\path_alias\AliasRepositoryInterface
    */
   protected $aliasRepository;
+  /**
+   * The EntityTypeManager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  /**
+   * The PathautoGenerator service.
+   *
+   * @var \Drupal\pathauto\PathautoGenerator
+   */
 
   /**
    * The Constructor.
@@ -51,7 +59,8 @@ class EventsSettingsForm extends ConfigFormBase {
     return new static(
       $container->get('config.factory'),
       $container->get('pathauto.alias_cleaner'),
-      $container->get('path_alias.repository')
+      $container->get('path_alias.repository'),
+      $container->get('pathauto.generator')
     );
   }
 
@@ -75,18 +84,29 @@ class EventsSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
     $config = $this->config('sitenow_events.settings');
-
     $form['markup'] = [
       '#type' => 'markup',
       '#markup' => $this->t('<p>These settings let you configure the SiteNow Events module.</p>'),
     ];
-
     $form['global'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Site-wide settings'),
       '#description' => $this->t('These settings affect all event lists and single instances.'),
     ];
-
+    $form['global']['sitenow_events_status'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable Event page'),
+      '#default_value' => $config->get('event_status'),
+      '#description' => $this->t('If checked, an event page will display at the configurable path below.'),
+      '#size' => 60,
+    ];
+    $form['global']['sitenow_events_title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Event title'),
+      '#description' => $this->t('The title for the event listing. Defaults to <em>Event</em>.'),
+      '#default_value' => $config->get('event_title'),
+      '#required' => TRUE,
+    ];
     $form['global']['sitenow_events_event_link'] = [
       '#type' => 'select',
       '#title' => $this->t('Link Option'),
@@ -97,7 +117,6 @@ class EventsSettingsForm extends ConfigFormBase {
         'event-link-internal' => $this->t('Link to page on this site'),
       ],
     ];
-
     $form['global']['sitenow_events_single_event_path'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Single event path'),
@@ -105,7 +124,13 @@ class EventsSettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('single_event_path'),
       '#required' => TRUE,
     ];
-
+    $form['global']['sitenow_events_header_content'] = [
+      '#type' => 'text_format',
+      '#format' => 'filtered_html',
+      '#title' => $this->t('Header Content'),
+      '#description' => $this->t('Enter any content that is displayed above the Event listing.'),
+      '#default_value' => $config->get('event_header'),
+    ];
     return $form;
   }
 
@@ -121,7 +146,6 @@ class EventsSettingsForm extends ConfigFormBase {
     if ($path_exists) {
       $form_state->setErrorByName('path', $this->t('This path is already in-use.'));
     }
-
     parent::validateForm($form, $form_state);
   }
 
@@ -132,10 +156,12 @@ class EventsSettingsForm extends ConfigFormBase {
     $path = $form_state->getValue('sitenow_events_single_event_path');
     // Clean path.
     $path = $this->aliasCleaner->cleanString($path);
-
     $this->config('sitenow_events.settings')
+      ->set('event_status', $form_state->getValue('sitenow_events_status'))
+      ->set('event_title', $form_state->getValue('sitenow_events_title'))
       ->set('event_link', $form_state->getValue('sitenow_events_event_link'))
       ->set('single_event_path', $path)
+      ->set('event_header', $form_state->getValue('sitenow_events_header_content'))
       ->save();
     parent::submitForm($form, $form_state);
   }
