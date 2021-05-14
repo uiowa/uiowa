2d28f53d4551c05010588fe52d08d1158e6dd0fb
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-05-14 15:12:24 -0500

    Refactor SF clone attribute removal JS

--- docroot/themes/custom/uids_base/assets/js/uids_base.js
+++ docroot/themes/custom/uids_base/assets/js/uids_base.js
@@ -5,18 +5,14 @@
 
 document.documentElement.className = document.documentElement.className.replace("no-js", "js");
 
-// @todo: Scope click event listener to only a.menuparents.
+// Remove unnecessary attributes from SF clone menu items.
 document.addEventListener('click', function (event) {
 
   // If the clicked element doesn't have the right selector, bail.
   if (!event.target.matches('#superfish-main-accordion a.menuparent')) return;
 
-  // @todo: Only remove attributes for descendant sf-clone-parent.
-  let clones = document.querySelectorAll('.sf-clone-parent a');
-
-  clones.forEach(function (clone) {
-    clone.removeAttribute('role');
-    clone.removeAttribute('aria-haspopup');
-  });
+  let clone = event.target.nextElementSibling.querySelector('.sf-clone-parent a');
+  clone.removeAttribute('role');
+  clone.removeAttribute('aria-haspopup');
 
 }, false);
