f5eacd339461ab9ebe303eca44030a40621e8a91
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-05-14 14:22:50 -0500

    Remove unnecessary attributes from sf-clone-parents

--- docroot/themes/custom/uids_base/assets/js/uids_base.js
+++ docroot/themes/custom/uids_base/assets/js/uids_base.js
@@ -4,3 +4,19 @@
  */
 
 document.documentElement.className = document.documentElement.className.replace("no-js", "js");
+
+// @todo: Scope click event listener to only a.menuparents.
+document.addEventListener('click', function (event) {
+
+  // If the clicked element doesn't have the right selector, bail.
+  if (!event.target.matches('#superfish-main-accordion a.menuparent')) return;
+
+  // @todo: Only remove attributes for descendant sf-clone-parent.
+  let clones = document.querySelectorAll('.sf-clone-parent a');
+
+  clones.forEach(function (clone) {
+    clone.removeAttribute('role');
+    clone.removeAttribute('aria-haspopup');
+  });
+
+}, false);
