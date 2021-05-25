d59b0ca254b44dcee9a7a380115529cc0464ea55
dnkemper <danielle-kemper@uiowa.edu>
2021-05-25 13:42:57 -0500

    Updated dashboard functionality for pdf export

---
+++ docroot/sites/grad.uiowa.edu/modules/grad_core/src/EventSubscriber/SectionComponentSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\grad_core\EventSubscriber;
+
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent;
+use Drupal\layout_builder\LayoutBuilderEvents;
+use Drupal\layout_builder\Plugin\Block\FieldBlock;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Add an HTML class to blocks that are displaying placeholder text.
+ */
+class SectionComponentSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY] = [
+      'onBuildRender',
+      50,
+    ];
+    return $events;
+  }
+
+  /**
+   * Builds render arrays for block plugins and sets it on the event.
+   *
+   * @param \Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent $event
+   *   The section component render event.
+   */
+  public function onBuildRender(SectionComponentBuildRenderArrayEvent $event) {
+    $block = $event->getPlugin();
+    if (!$block instanceof BlockPluginInterface) {
+      return;
+    }
+
+    $build = $event->getBuild();
+
+    $event->setBuild($build);
+  }
+
+}
