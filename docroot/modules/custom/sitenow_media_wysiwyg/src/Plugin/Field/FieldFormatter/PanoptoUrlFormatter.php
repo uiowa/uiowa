42eb5201873916c59ca8520027b0c17241121f9c
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-06-15 10:08:20 -0500

    Use BASE_URL constant

--- docroot/modules/custom/sitenow_media_wysiwyg/src/Plugin/Field/FieldFormatter/PanoptoUrlFormatter.php
+++ docroot/modules/custom/sitenow_media_wysiwyg/src/Plugin/Field/FieldFormatter/PanoptoUrlFormatter.php
@@ -6,6 +6,7 @@ use Drupal\Component\Utility\Html;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
+use Drupal\sitenow_media_wysiwyg\Plugin\media\Source\Panopto;
 
 /**
  * Panopto URL field formatter.
@@ -47,7 +48,7 @@ class PanoptoUrlFormatter extends LinkFormatter {
           '#type' => 'html_tag',
           '#tag' => 'iframe',
           '#attributes' => [
-            'src' => "https://uicapture.hosted.panopto.com/Panopto/Pages/Embed.aspx?id={$id}&&autoplay=false&offerviewer=true&showtitle=false&showbrand=false&start=0&interactivity=none",
+            'src' => Panopto::BASE_URL . "/Panopto/Pages/Embed.aspx?id={$id}&&autoplay=false&offerviewer=true&showtitle=false&showbrand=false&start=0&interactivity=none",
             'width' => '1920',
             'height' => '1080',
             'allow' => 'autoplay; fullscreen',
