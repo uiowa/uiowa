9319a2db19ea0d5e2c7ba9dbc091d79f5c095ac2
Cory Skeers <cory-skeers@uiowa.edu>
2021-07-07 16:58:22 -0500

    Remove excess cli output, only necessary for debugging.

--- docroot/sites/international.uiowa.edu/modules/international_migrate/src/Plugin/migrate/source/Articles.php
+++ docroot/sites/international.uiowa.edu/modules/international_migrate/src/Plugin/migrate/source/Articles.php
@@ -54,13 +54,11 @@ class Articles extends BaseNodeSource {
    * {@inheritdoc}
    */
   public function prepareRow(Row $row) {
+    // Skip this node if it comes after our last migrated.
     if ($row->getSourceProperty('nid') < $this->getLastMigrated()) {
-      $this->logger->notice('Migrated node: Skipping.');
       return FALSE;
     }
     parent::prepareRow($row);
-    $this->logger->notice("Memory usage: @mem",
-      ['@mem' => $this->clearMemory(10)]);
 
     // Only import news newer than January 2015.
     $created_year = date('Y', $row->getSourceProperty('created'));
@@ -97,28 +95,6 @@ class Articles extends BaseNodeSource {
       // Search for D7 inline embeds and replace with D8 inline entities.
       $this->viewMode = 'large__no_crop';
       $body[0]['value'] = $this->replaceInlineFiles($body[0]['value']);
-
-      // Parse links.
-      $doc = Html::load($body[0]['value']);
-      $links = $doc->getElementsByTagName('a');
-      $i = $links->length - 1;
-
-      while ($i >= 0) {
-        $link = $links->item($i);
-        $href = $link->getAttribute('href');
-
-        if (strpos($href, '/node/') === 0 || stristr($href, 'international.uiowa.edu/node/')) {
-          $this->logger->notice('Unable to replace internal link @link in article @article.', [
-            '@link' => $href,
-            '@article' => $row->getSourceProperty('title'),
-          ]);
-        }
-
-        $i--;
-      }
-
-      $html = Html::serialize($doc);
-      $body[0]['value'] = $html;
 
       // Set the format to filtered_html while we have it.
       $body[0]['format'] = 'filtered_html';
