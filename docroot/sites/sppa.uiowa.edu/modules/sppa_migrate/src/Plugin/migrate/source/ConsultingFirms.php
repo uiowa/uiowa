0d0e9c2637fa46bb7e63715bd98e3e67f4ce9ca0
Benjamin Speare <benjamin-speare@uiowa.edu>
2021-07-09 21:05:46 +0000

    Initial work on sppa migrations

---
+++ docroot/sites/sppa.uiowa.edu/modules/sppa_migrate/src/Plugin/migrate/source/ConsultingFirms.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\sppa_migrate\Plugin\migrate\source;
+
+use Drupal\Component\Utility\Html;
+use Drupal\migrate\Row;
+use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
+use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
+
+/**
+ * Migrate Source plugin.
+ *
+ * @MigrateSource(
+ *   id = "sppa_consulting_firms",
+ *   source_module = "node"
+ * )
+ */
