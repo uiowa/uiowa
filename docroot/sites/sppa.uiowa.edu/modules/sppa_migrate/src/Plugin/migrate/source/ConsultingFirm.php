ab43a5048452d3135ec338cc9c141b73e0c5e124
Benjamin Speare <benjamin-speare@uiowa.edu>
2021-07-12 16:54:03 +0000

    Trying to map to states listing

---
+++ docroot/sites/sppa.uiowa.edu/modules/sppa_migrate/src/Plugin/migrate/source/ConsultingFirm.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\sppa_migrate\Plugin\migrate\source;
+
+use Drupal\Component\Utility\Html;
+use Drupal\migrate\Row;
+use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
+use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
+
+/**
+ * Migrate Source plugin.
+ *
+ * @MigrateSource(
+ *   id = "sppa_potential_employer",
+ *   source_module = "node"
+ * )
+ */
+class PotentialEmployer extends BaseNodeSource {
+
+}
