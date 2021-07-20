c50a322a98cf6991eba41e6bb940378990dab712
Cory Skeers <cory-skeers@uiowa.edu>
2021-07-20 16:52:36 -0500

    Remove pager from the exposed filters form.

--- docroot/modules/custom/layout_builder_custom/src/Plugin/Display/ListBlock.php
+++ docroot/modules/custom/layout_builder_custom/src/Plugin/Display/ListBlock.php
@@ -582,6 +582,7 @@ class ListBlock extends CoreBlock {
     if (!empty($config['pager'])) {
       $pager = $this->view->display_handler->getOption('pager');
       $pager['type'] = $config['pager'];
+      $pager["options"]["expose"]["items_per_page"] = FALSE;
       $this->view->display_handler->setOption('pager', $pager);
     }
 
