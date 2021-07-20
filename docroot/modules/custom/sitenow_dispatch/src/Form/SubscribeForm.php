32ca117fc3d8f7aa62f8c598fd18d96a6fb78c1d
Gary Ridgway <gary-ridgway@uiowa.edu>
2021-07-20 16:13:43 -0500

    form validation

--- docroot/modules/custom/sitenow_dispatch/src/Form/SubscribeForm.php
+++ docroot/modules/custom/sitenow_dispatch/src/Form/SubscribeForm.php
@@ -2,15 +2,24 @@
 
 namespace Drupal\sitenow_dispatch\Form;
 
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use http\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configure sitenow_dispatch settings for this site.
  */
 class SubscribeForm extends ConfigFormBase {
+
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $client;
 
   /**
    * The config factory service.
@@ -84,25 +93,49 @@ class SubscribeForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $email      = $form_state->getValue('email');
+    $first      = $form_state->getValue('first');
+    $last       = $form_state->getValue('last');
+    $APIkey     = $this->configFactory->get('sitenow_dispatch.settings')->get('API_key');
+    $population = $form_state->getValue('population');
+
     // This try block will add someone to the subscriber list.
     try {
-      $response = $this->client->request('POST', 'https://apps.its.uiowa.edu/dispatch/api/v1/populations/' . $form_state->getValue('population') . '/subscribers', [
+      $response = $this->client->request('POST', 'https://apps.its.uiowa.edu/dispatch/api/v1/populations/' . $population . '/subscribers', [
         'headers' => [
           'Accept' => 'application/json',
-          'x-dispatch-api-key' => $this->configFactory->get('sitenow_dispatch.settings')->get('API_key'),
+          'x-dispatch-api-key' => $APIkey,
         ],
         'body' => json_encode([
-          "toAddress" => $form_state->getValue('email'),
-          "firstName" => $form_state->getValue('first'),
-          "lastName" => $form_state->getValue('last'),
+          "toAddress" => $email,
+          "firstName" => $first,
+          "lastName" => $last,
         ]),
       ]);
     }
     catch (RequestException | GuzzleException $e) {
       $this->logger->error($e->getMessage());
     }
-    $this->messenger()->addStatus($this->t("You've been Aardvarked!"));
+    $this->messenger()->addStatus($this->t('"' . $first . ' ' . $last . '" has been added to the subscription list with the email "' . $email . '"'));
     parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $email      = $form_state->getValue('email');
+    $population = $form_state->getValue('population');
+    $encoded_email = UrlHelper::buildQuery(['search' => $email]);
+
+    $response = $this->dispatch->getFromDispatch('https://apps.its.uiowa.edu/dispatch/api/v1/populations/' . $population . '/subscribers?' . $encoded_email);
+
+    if ($response->recordsReturned > 0) {
+      $form_state->setErrorByName('email', 'This email is already subscribed to the related subscription list.');
+    }
+
+    parent::validateForm($form, $form_state);
   }
 
 }
