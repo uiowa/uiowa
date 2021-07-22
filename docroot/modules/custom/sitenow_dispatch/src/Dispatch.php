93d47f1a4e0dcc2a3ee9d546bcd38633c44b677c
GaryRidgway <gary-ridgway@uiowa.edu>
2021-07-22 11:01:44 -0500

    SiteNow Dispatch Module (#3823)
    * adding generated module code
    
    * adding requests to dispatch and adding config to block
    
    * config export
    
    * crafting form manually in drupal instead of with an embed
    
    * docs
    
    * consistent formatting
    
    * phpcs fixes
    
    * PHPCBF
    
    * Inject form_builder instead of using \Drupal
    
    * Move constructor first in SignUpFormBlock
    
    * Move constructor first in SubscribeForm
    
    * fixing up unneccessary files
    
    * adding new service file to hold helpers
    
    * service config
    
    * re-add routing because i goofed
    
    * form validation
    
    * refactor to use service
    
    * fixing settings form api key validation
    
    * alphabetizing
    
    * perms
    
    * adding dispatch docs to settings page
    
    * PHPCS
    
    * PHP errors
    
    * PHP errors 2 again final.wav
    
    * PHP errors final.final.txt
    
    * Delete SitenowDispatchController.php
    
    * Delete sitenow_dispatch.module
    
    * rickpicking
    
    * Improve Dispatch class comment
    
    * Remove unused variable
    
    * Import LoggerInterface into Dispatch service, fixes fatal error
    
    * Use new lowercase config key
    
    * perms
    
    * Remove parent form submit which just outputs another message
    
    * Capitalize route title
    
    * Get link from route name instead of hard-coding and use translate
    
    * add antibot to the dispatch form
    
    * Update Dispatch settings form docs
    
    * Sanitize dispatch settings
    
    Co-authored-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>
    Co-authored-by: Richard B. Porter <richard-b-porter@uiowa.edu>
    Co-authored-by: Richard B. Porter <richardbporter@users.noreply.github.com>

---
+++ docroot/modules/custom/sitenow_dispatch/src/Dispatch.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Drupal\sitenow_dispatch;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\ClientException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * A Dispatch API service.
+ *
+ * @see: https://apps.its.uiowa.edu/dispatch/api-ref#/
+ */
+class Dispatch {
+
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $client;
+
+  /**
+   * The config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The config factory service.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructs a Dispatch object.
+   *
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   The HTTP client.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The Config Factory object.
+   * @param Psr\Log\LoggerInterface $logger
+   *   The logger.
+   */
+  public function __construct(ClientInterface $http_client, ConfigFactoryInterface $configFactory, LoggerInterface $logger) {
+    $this->client = $http_client;
+    $this->configFactory = $configFactory;
+    $this->logger = $logger;
+  }
+
+  /**
+   * Helper function for doing get commands from dispatch.
+   */
+  public function getFromDispatch(string $request, string $api_key = NULL) {
+
+    if (!isset($api_key)) {
+      $api_key = $this->configFactory->get('sitenow_dispatch.settings')->get('api_key');
+    }
+
+    $response = NULL;
+
+    try {
+      $response = $this->client->request('GET', $request, [
+        'headers' => [
+          'Accept' => 'application/json',
+          'x-dispatch-api-key' => $api_key,
+        ],
+      ]);
+    }
+    catch (RequestException | GuzzleException | ClientException $e) {
+      $this->logger->error($e->getMessage());
+
+      // We've encountered an error, return a blank array.
+      return [];
+    }
+
+    return json_decode($response->getBody()->getContents());
+  }
+
+}
---
+++ docroot/modules/custom/sitenow_dispatch/src/Form/SubscribeForm.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Drupal\sitenow_dispatch\Form;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configure sitenow_dispatch settings for this site.
+ */
+class SubscribeForm extends ConfigFormBase {
+
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $client;
+
+  /**
+   * The config factory service.
+   *
+   * @var Dispatch
+   */
+  protected $dispatch;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, $client, $dispatch) {
+    parent::__construct($config_factory);
+    $this->client = $client;
+    $this->dispatch = $dispatch;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('config.factory'), $container->get('http_client'), $container->get('sitenow_dispatch.dispatch'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'sitenow_dispatch_subscribe_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['sitenow_dispatch.subscribe_form'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $population = NULL) {
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email'),
+      '#default_value' => $this->config('sitenow_dispatch.subscribe_form')->get('email'),
+    ];
+    $form['first'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('First name'),
+      '#default_value' => $this->config('sitenow_dispatch.subscribe_form')->get('first'),
+    ];
+    $form['last'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Last name'),
+      '#default_value' => $this->config('sitenow_dispatch.subscribe_form')->get('last'),
+    ];
+    $form['population'] = [
+      '#type' => 'hidden',
+      '#value' => $population,
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $email      = $form_state->getValue('email');
+    $first      = $form_state->getValue('first');
+    $last       = $form_state->getValue('last');
+    $api_key    = $this->configFactory->get('sitenow_dispatch.settings')->get('api_key');
+    $population = $form_state->getValue('population');
+
+    // This try block will add someone to the subscriber list.
+    try {
+      $this->client->request('POST', 'https://apps.its.uiowa.edu/dispatch/api/v1/populations/' . $population . '/subscribers', [
+        'headers' => [
+          'Accept' => 'application/json',
+          'x-dispatch-api-key' => $api_key,
+        ],
+        'body' => json_encode([
+          "toAddress" => $email,
+          "firstName" => $first,
+          "lastName" => $last,
+        ]),
+      ]);
+    }
+    catch (RequestException | GuzzleException $e) {
+      $this->logger->error($e->getMessage());
+    }
+
+    $this->messenger()->addStatus(
+      $this->t(
+        '"@first @last" has been added to the subscription list with the email "@email"',
+        ['@first' => $first, '@last' => $last, '@email' => $email]
+      )
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $email         = $form_state->getValue('email');
+    $population    = $form_state->getValue('population');
+    $encoded_email = UrlHelper::buildQuery(['search' => $email]);
+
+    $response = $this->dispatch->getFromDispatch('https://apps.its.uiowa.edu/dispatch/api/v1/populations/' . $population . '/subscribers?' . $encoded_email);
+
+    if ($response->recordsReturned > 0) {
+      $form_state->setErrorByName('email', 'This email is already subscribed to the related subscription list.');
+    }
+
+    parent::validateForm($form, $form_state);
+  }
+
+}
