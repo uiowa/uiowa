8d4f90fbfa9e935e07a4d07e775c8f27b2fe497f
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-07-28 16:22:17 -0500

    Comment out validate

--- docroot/modules/custom/uiowa_profiles/src/Form/SettingsForm.php
+++ docroot/modules/custom/uiowa_profiles/src/Form/SettingsForm.php
@@ -308,12 +308,16 @@ class SettingsForm extends ConfigFormBase {
     $this->routeBuilder->rebuild();
   }
 
-  /**
-   * {@inheritDoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-//    foreach ($form_state->getValue(['profiles_fieldset', 'tabs_container', 'directories']) as $directory) {
-//      $path = $this->aliasCleaner->cleanAlias($directory['path']);
+//  /**
+//   * {@inheritDoc}
+//   */
+//  public function validateForm(array &$form, FormStateInterface $form_state) {
+//    $fields = [
+//      'path',
+//    ];
+//
+//    foreach ($fields as $field) {
+//      $path = $this->aliasCleaner->cleanAlias($form_state->getValue($field));
 //
 //      /** @var \Drupal\Core\Url $url */
 //      $url = $this->pathValidator->getUrlIfValid($path);
@@ -321,14 +325,14 @@ class SettingsForm extends ConfigFormBase {
 //      // If $url is anything besides FALSE then the path is already in use. We
 //      // also check if the route belongs to another module.
 //      if ($url && !str_starts_with($url->getRouteName(), 'uiowa_profiles')) {
-//        $form_state->setErrorByName('path', 'This path is already in use.');
+//        $form_state->setErrorByName($field, 'This path is already in use.');
 //      }
 //      else {
-//        $form_state->setValue('path', $path);
+//        $form_state->setValue($field, $path);
 //      }
 //    }
 //
 //    parent::validateForm($form, $form_state);
-  }
+//  }
 
 }
