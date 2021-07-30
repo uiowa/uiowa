ecac724a2f7ffb5671eaa7139a56c61751b0fd27
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-07-30 14:41:33 -0500

    Remove default path value from settings form

--- docroot/modules/custom/uiowa_profiles/src/Form/SettingsForm.php
+++ docroot/modules/custom/uiowa_profiles/src/Form/SettingsForm.php
@@ -191,7 +191,7 @@ class SettingsForm extends ConfigFormBase {
       $form['profiles_fieldset']['tabs_container']['directories'][$key]['path'] = [
         '#type' => 'textfield',
         '#title' => $this->t('Path'),
-        '#default_value' => $directory['path'] ?? '/profiles/people',
+        '#default_value' => $directory['path'] ?? '',
         '#description' => $this->t('The path for the Profiles directory. Serves as the base for all profiles.'),
         '#required' => TRUE,
       ];
