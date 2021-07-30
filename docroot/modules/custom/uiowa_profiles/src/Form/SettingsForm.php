1eb3c1df1d037fad68c84ac6ac91ef7663a04187
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-07-30 09:30:22 -0500

    Get keyed directory sitemap route

--- docroot/modules/custom/uiowa_profiles/src/Form/SettingsForm.php
+++ docroot/modules/custom/uiowa_profiles/src/Form/SettingsForm.php
@@ -177,7 +177,7 @@ class SettingsForm extends ConfigFormBase {
           '#default_value' => $directory['path'] ?? '/profiles/people',
           '#description' => $this->t('The path for the Profiles directory. Serves as the base for all profiles and an additional <a href=":url">sitemap</a> to submit to search engines.', [
             // @TODO: is this the right thing we are getting here for multiple directory directories?
-            ':url' => Url::fromRoute('uiowa_profiles.sitemap')->toString(),
+            ':url' => Url::fromRoute("uiowa_profiles.sitemap.{$key}")->toString(),
           ]),
           '#required' => TRUE,
         ];
