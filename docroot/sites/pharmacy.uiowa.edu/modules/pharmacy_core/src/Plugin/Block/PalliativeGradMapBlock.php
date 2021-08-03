65a539025f57bcf6fd0cd9050697187abac44495
Benjamin Speare <benjamin-speare@uiowa.edu>
2021-08-03 20:06:33 +0000

    Added custom block

---
+++ docroot/sites/pharmacy.uiowa.edu/modules/pharmacy_core/src/Plugin/Block/PalliativeGradMapBlock.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\pharmacy_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides an palliative care graduates map block.
+ *
+ * @Block(
+ *   id = "pharmacy_core_palliative_care_map",
+ *   admin_label = @Translation("Palliative Care Graduate Map"),
+ *   category = @Translation("Site custom")
+ * )
+ */
+class PalliativeGradMapBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $html = '<div id="pharmacy-palliative-grad-map">&nbsp;</div>';
+    // Load persons tagged with the Counselor person type
+    // and create array of unique territory values.
+    $territories = [];
+    $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', 'person')
+      ->condition('field_person_types', 'counselor');
+    $nids = $query->execute();
+    if (!empty($nids)) {
+      $nodes = $node_storage->loadMultiple($nids);
+      foreach ($nodes as $node) {
+        // Get the field_person_territory values and assign them to an array.
+        if ($node->hasField('field_person_territory') &&
+          !$node->get('field_person_territory')->isEmpty()) {
+          $values = $node->get('field_person_territory')->getValue();
+          array_walk_recursive($values, function ($v) use (&$territories) {
+            $territories[] = $v;
+          });
+        }
+      }
+      // Filter out territory duplicates.
+      $territories = array_values(array_unique($territories));
+    }
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t($html),
+      '#cache' => [
+        'tags' => ['node_type:person'],
+      ],
+      '#attached' => [
+        'library' => [
+          'uids_base/leaflet',
+          'pharmacy_core/palliative-grad-map',
+        ],
+        'drupalSettings' => [
+          'pharmacy_core' => [
+            'territories' => $territories,
+          ],
+        ],
+      ],
+    ];
+  }
+
+}
