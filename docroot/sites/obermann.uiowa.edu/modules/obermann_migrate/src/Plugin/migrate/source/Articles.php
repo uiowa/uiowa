40af36db203c9d116660887dec6a21dd6f2251fb
Benjamin Speare <benjamin-speare@uiowa.edu>
2021-08-04 18:39:45 +0000

    Initial work on Obermann articles migration

---
+++ docroot/sites/obermann.uiowa.edu/modules/obermann_migrate/src/Plugin/migrate/source/Articles.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\obermann_migrate\Plugin\migrate\source;
+
+use Drupal\Component\Utility\Html;
+use Drupal\migrate\Row;
+use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
+use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
+
+/**
+ * Migrate Source plugin.
+ *
+ * @MigrateSource(
+ *   id = "obermann_articles",
+ *   source_module = "node"
+ * )
+ */
+class Articles extends BaseNodeSource {
+  use ProcessMediaTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    parent::prepareRow($row);
+
+    // Only import articles if made before January 2020.
+    $created_year = date('Y', $row->getSourceProperty('created'));
+    if ($created_year > 2020) {
+      $this->logger->notice('Made in 2020. Skipping.');
+      return FALSE;
+    }
+
+    $body = $row->getSourceProperty('body');
+
+    if (!empty($body)) {
+      // Search for D7 inline embeds and replace with D8 inline entities.
+      $body[0]['value'] = $this->replaceInlineFiles($body[0]['value']);
+
+      // Extract the summary.
+      $row->setSourceProperty('body_summary', $this->getSummaryFromTextField($body));
+
+      // Parse links.
+      $doc = Html::load($body[0]['value']);
+      $links = $doc->getElementsByTagName('a');
+      $i = $links->length - 1;
+
+      while ($i >= 0) {
+        $link = $links->item($i);
+        $href = $link->getAttribute('href');
+
+        if (strpos($href, '/node/') === 0 || stristr($href, 'obermann.uiowa.edu/node/')) {
+          $nid = explode('node/', $href)[1];
+
+          if ($lookup = $this->manualLookup($nid)) {
+            $link->setAttribute('href', $lookup);
+            $link->parentNode->replaceChild($link, $link);
+            $this->logger->info('Replaced internal link @link in article @article.', [
+              '@link' => $href,
+              '@article' => $row->getSourceProperty('title'),
+            ]);
+
+          }
+          else {
+            $this->logger->notice('Unable to replace internal link @link in article @article.', [
+              '@link' => $href,
+              '@article' => $row->getSourceProperty('title'),
+            ]);
+          }
+        }
+
+        $i--;
+      }
+
+      $html = Html::serialize($doc);
+      $body[0]['value'] = $html;
+      $body[0]['format'] = 'filtered_html';
+
+      $row->setSourceProperty('body', $body);
+    }
+
+    // Process the image field.
+    $image = $row->getSourceProperty('field_image_attach');
+
+    if (!empty($image)) {
+      $fid = $this->processImageField($image[0]['fid'], $image[0]['alt'], $image[0]['title']);
+      $row->setSourceProperty('field_image_attach_fid', $fid);
+    }
+
+    return TRUE;
+  }
+
+}
