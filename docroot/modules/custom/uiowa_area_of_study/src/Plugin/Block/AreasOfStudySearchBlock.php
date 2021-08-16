85a22d1033d60881ed34e3598eabbe68e269ce84
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-08-16 13:44:07 -0500

    Refactor AOS block to use DI, CR updb

--- docroot/modules/custom/uiowa_area_of_study/src/Plugin/Block/AreasOfStudySearchBlock.php
+++ docroot/modules/custom/uiowa_area_of_study/src/Plugin/Block/AreasOfStudySearchBlock.php
@@ -3,7 +3,10 @@
 namespace Drupal\uiowa_area_of_study\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Form\FormState;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides the Areas of Study Search block.
@@ -14,14 +17,28 @@ use Drupal\Core\Form\FormState;
  *   category = @Translation("Site custom")
  * )
  */
-class AreasOfStudySearchBlock extends BlockBase {
+class AreasOfStudySearchBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  protected $formBuilder;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
     $form_state = new FormState();
-    return \Drupal::formBuilder()->buildForm('Drupal\uiowa_area_of_study\Form\AreasOfStudySearchForm', $form_state);
+    return $this->formBuilder->buildForm('Drupal\uiowa_area_of_study\Form\AreasOfStudySearchForm', $form_state);
   }
-
 }
