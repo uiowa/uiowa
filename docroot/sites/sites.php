e9d4b864532f9a963b43bbc70a5ca08938d02d59
Benjamin Speare <benjamin-speare@uiowa.edu>
2021-08-19 18:37:11 +0000

    Initialize lynch.lab.uiowa.edu multisite on uiowa01

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -3077,3 +3077,9 @@ $sites['dancescreendancefest.local.drupal.uiowa.edu'] = 'screendancefest.dance.u
 $sites['dancescreendancefest.dev.drupal.uiowa.edu'] = 'screendancefest.dance.uiowa.edu';
 $sites['dancescreendancefest.stage.drupal.uiowa.edu'] = 'screendancefest.dance.uiowa.edu';
 $sites['dancescreendancefest.prod.drupal.uiowa.edu'] = 'screendancefest.dance.uiowa.edu';
+
+// Directory aliases for lynch.lab.uiowa.edu.
+$sites['lablynch.local.drupal.uiowa.edu'] = 'lynch.lab.uiowa.edu';
+$sites['lablynch.dev.drupal.uiowa.edu'] = 'lynch.lab.uiowa.edu';
+$sites['lablynch.stage.drupal.uiowa.edu'] = 'lynch.lab.uiowa.edu';
+$sites['lablynch.prod.drupal.uiowa.edu'] = 'lynch.lab.uiowa.edu';
