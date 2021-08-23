c62f25d507bb7b149564a3808c1547b13e939b51
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-08-23 16:07:12 -0500

    Get API data based on date logic

--- docroot/sites/coronavirus.uiowa.edu/modules/custom/uiowa_covid/src/Controller/DataController.php
+++ docroot/sites/coronavirus.uiowa.edu/modules/custom/uiowa_covid/src/Controller/DataController.php
@@ -4,6 +4,7 @@ namespace Drupal\uiowa_covid\Controller;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DateHelper;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\RequestException;
@@ -56,28 +57,66 @@ class DataController extends ControllerBase {
    * Builds the response.
    */
   public function build() {
-    // @todo Limit release of new data until 10am central.
+    $date = $this->getDataDate();
     $data = [];
+
     $endpoint = $this->configFactory->get('uiowa.covid')->get('endpoint');
     $user = $this->configFactory->get('uiowa.covid')->get('user');
     $key = $this->configFactory->get('uiowa.covid')->get('key');
 
     try {
-      $response = $this->client->request('GET', $endpoint, [
+      $response = $this->client->request('GET', "$endpoint/$date", [
         'auth' => [
           $user,
           $key,
         ],
       ]);
 
-      // @todo Verify status/messages/JSON.
-      $data = json_decode($response->getBody()->getContents());
+      $data = ['date' => $date] + json_decode($response->getBody()->getContents(), TRUE);;
     }
     catch (RequestException | GuzzleException | ClientException $e) {
       watchdog_exception('uiowa_covid', $e);
     }
 
     return new JsonResponse($data);
+  }
+
+  /**
+   * Calculate the day to get data for based on the day of the week.
+   */
+  private function getDataDate() {
+    $weekdays = DateHelper::weekDaysUntranslated();
+    $dow = $weekdays[date('w')];
+    $time = date('G');
+
+    // If Monday, Wednesday or Friday past 10am.
+    if ($dow == 'Monday' || $dow == 'Wednesday' || $dow == 'Friday' && $time >= 10) {
+      $date = date('m-d-Y');
+    }
+    else {
+      // Anything else should go back to the previous reporting date.
+      switch ($dow) {
+        case 'Sunday':
+        case 'Monday':
+          $previous = 'last Friday';
+          break;
+
+        case 'Tuesday':
+        case 'Thursday':
+        case 'Saturday':
+          $previous = 'yesterday';
+          break;
+
+        case 'Wednesday':
+        case 'Friday':
+          $previous = 'two days ago';
+          break;
+      }
+
+      $date = date('m-d-Y', strtotime($previous));
+    }
+
+    return $date;
   }
 
 }
