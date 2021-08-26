dc4f9c434568a2d387a0e4a8c3504a0c04e85fbd
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-08-26 10:02:35 -0500

    Update class placeholders

--- docroot/sites/coronavirus.uiowa.edu/modules/uiowa_covid/js/uiowa-covid.js
+++ docroot/sites/coronavirus.uiowa.edu/modules/uiowa_covid/js/uiowa-covid.js
@@ -17,12 +17,21 @@
           .then(response => response.json())
           .then(data => {
             for (const datum in data) {
-              // Each placeholder ID should match the JSON key.
+              // Check for a matching ID first and then classes second.
               // @see: Drupal\uiowa_covid\Plugin\Block\CovidDataBlock::build().
               let element = document.getElementById(`uiowa-covid-${datum}`);
 
               if (element) {
                 element.innerText = data[datum];
+              }
+              else {
+                let elements = document.getElementsByClassName(`uiowa-covid-${datum}`);
+
+                if (elements) {
+                  for (const element of elements) {
+                    element.innerText = data[datum];
+                  }
+                }
               }
             }
           })
