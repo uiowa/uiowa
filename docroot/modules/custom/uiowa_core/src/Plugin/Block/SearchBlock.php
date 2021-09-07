39c1de094a9e9354087ce8f5f82092f95e4d812c
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2021-09-07 09:41:06 -0500

    AoS Feature Revisited (#3926)
    * initial work
    
    * moving things around
    
    * add degree types and locations
    
    * check if field is present
    
    * capture sitemap in feature
    
    * update perms, split
    
    * SEO in feature with admissions override
    
    * add seo field
    
    * prettify node a bit
    
    * small tweak
    
    * Moving some config.
    
    * Disable the AoS split on admissions.
    
    * Moved functions related to admssions_core out of uiowa_area_of_study and back into admissions_core
    
    * Phpcs
    
    * Phpcs
    
    * Added custom search block form to admissions_core so that it doesn't use the form from uiowa_area_of_study module
    
    * Added css for uiowa_area_of_study block
    
    * update module info file
    
    * keep module on for admissions
    
    * deprecate but keep block for admissions' use
    
    * remove duplicate code found in admissions_core
    
    * generic search block
    
    * switch to linkit to handle internal paths, relative paths
    
    * Added uiowa-search-form class to use across out multiple inline search blocks
    
    * Added new search block class to form wrapper
    
    * Added background color adjustments for submit button based on layout builder background color
    
    * handle relative paths, internal paths, exclude external paths
    
    * remove previously added bloat that admissions doesn't want
    
    * account for moderation_control, remove unused vocabularies
    
    * block description, off-canvas style tweaks, fix url
    
    * remove sticky option for now
    
    * title tweak
    
    * card teaser
    
    Co-authored-by: Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
    Co-authored-by: Cory Skeers <cory-skeers@uiowa.edu>
    Co-authored-by: cory-skeers <49074566+cory-skeers@users.noreply.github.com>
    Co-authored-by: bspeare <bspeare@users.noreply.github.com>
    Co-authored-by: Benjamin Speare <benjamin-speare@uiowa.edu>
    Co-authored-by: Richard B. Porter <richardbporter@users.noreply.github.com>
    Co-authored-by: Danielle Kemper <danielle-kemper@uiowa.edu>

---
+++ docroot/modules/custom/uiowa_core/src/Plugin/Block/SearchBlock.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Drupal\uiowa_core\Plugin\Block;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a basic search block.
+ *
+ * @Block(
+ *   id = "uiowa_core_search_block",
+ *   admin_label = @Translation("Search Block"),
+ *   category = @Translation("Site custom")
+ * )
+ */
+class SearchBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form_builder service.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = $this->getConfiguration();
+    $search_config = [
+      'endpoint' => $config['endpoint'],
+      'query_parameter' => $config['query_parameter'],
+      'button_text' => $config['button_text'],
+      'search_label' => $config['search_label'],
+    ];
+    $form_state = new FormState();
+    $form_state->addBuildInfo('search_config', $search_config);
+    return $this->formBuilder->buildForm('Drupal\uiowa_core\Form\SearchBlock', $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+    $form['markup'] = [
+      '#type' => 'markup',
+      '#markup' => $this->t('<p>This is a generic search block you can use to send queries to site search or a view filter.</p>'),
+    ];
+    $form['#attached']['library'][] = 'linkit/linkit.autocomplete';
+    $form['endpoint'] = [
+      '#type' => 'linkit',
+      '#title' => $this->t('Endpoint Path'),
+      '#description' => $this->t('Start typing to see a list of results. Click to select. Relative paths are allowed. External links are not allowed.'),
+      '#autocomplete_route_name' => 'linkit.autocomplete',
+      '#autocomplete_route_parameters' => [
+        'linkit_profile_id' => 'default',
+      ],
+      '#default_value' => isset($config['endpoint']) ? $config['endpoint'] : '/search',
+      '#required' => TRUE,
+    ];
+    $form['query_parameter'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Query Parameter'),
+      '#description' => $this->t('<em>title</em> is common for content filtering, <em>terms</em> is used for search on this site'),
+      '#default_value' => isset($config['query_parameter']) ? $config['query_parameter'] : 'terms',
+    ];
+    $form['search_label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search Label'),
+      '#default_value' => isset($config['search_label']) ? $config['search_label'] : 'Search',
+      '#required' => TRUE,
+    ];
+    $form['button_text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Button Text'),
+      '#default_value' => isset($config['button_text']) ? $config['button_text'] : 'Search',
+      '#required' => TRUE,
+    ];
+
+    return $form;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockValidate($form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $external = UrlHelper::isExternal($values['endpoint']);
+    if ($external) {
+      $form_state->setErrorByName('endpoint', $this->t('External links are not allowed.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $values = $form_state->getValues();
+    $this->configuration['endpoint'] = $values['endpoint'];
+    $this->configuration['query_parameter'] = $values['query_parameter'];
+    $this->configuration['button_text'] = $values['button_text'];
+    $this->configuration['search_label'] = $values['search_label'];
+  }
+
+}
