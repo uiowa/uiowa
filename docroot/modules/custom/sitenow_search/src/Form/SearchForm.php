fe51789bc64ced9827939808841459890675638e
Cory Skeers <cory-skeers@uiowa.edu>
2021-09-14 13:39:49 -0500

    General cleanup, edits. Output results on form page.

---
+++ docroot/modules/custom/sitenow_search/src/Form/SearchForm.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Drupal\sitenow_search\Form;
+
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure Sitenow Search settings for this site.
+ */
+class SearchForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'sitenow_search';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $wrapper_id = $this->getFormId() . '-wrapper';
+
+    $form = [
+      '#prefix' => '<div id="' . $wrapper_id . '" aria-live="polite">',
+      '#suffix' => '</div>',
+    ];
+    $form['needle'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Needle'),
+      '#default_value' => '',
+    ];
+    $form['regexed'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('REGEXP?'),
+      '#default_value' => 0,
+    ];
+    $form['search'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Search'),
+      '#button_type' => 'primary',
+      '#name' => 'search',
+      '#submit' => [
+        [$this, 'searchButton'],
+      ],
+      '#ajax' => [
+        'callback' => [$this, 'searchButton'],
+        'wrapper' => $wrapper_id,
+        'method' => 'html',
+        'disable-refocus' => TRUE,
+        'effect' => 'fade',
+      ]
+    ];
+
+    // Unset the original, currently unused submit button.
+    // It might be used at another time if settings are needed.
+    unset($form['actions']['submit']);
+    return $form;
+  }
+
+  /**
+   * Perform a search with the given needle..
+   */
+  public function searchButton(array &$form, FormStateInterface $form_state) {
+    // Grab all the fields.
+    $fields = get_all_text_fields();
+    $needle = $form_state->getValue('needle');
+    $regexed = $form_state->getValue('regexed');
+    $results = search_fields($fields, $needle, $regexed);
+    $table = $this->buildResultsTable($results);
+    $markup = \Drupal::service('renderer')->render($table);
+    $form['results'] = [
+      '#type' => 'markup',
+      '#markup' => $markup,
+    ];
+    return $form;
+  }
+
+  /**
+   * Helper function to form our table array.
+   *
+   * @param array $results
+   *   The search form results to prepare.
+   *
+   * @return array
+   *   The renderable table array.
+   */
+  public function buildResultsTable(array $results) {
+    $rows = [];
+    foreach ($results as $nid => $matches) {
+      // Form our edit link with the node id.
+      $node_value = new FormattableMarkup('<a href="/node/@nid/edit">@nid</a>', [
+        '@nid' => $nid,
+      ]);
+      $rows[] = [
+        'nid' => [
+          'data' => $node_value,
+          // Stretch the node row to cover all its matches.
+          'rowspan' => count($matches)
+        ],
+        // Shift the first element out of the ray to match
+        // to the node row.
+        array_shift($matches),
+      ];
+      // If we have more matches after the shift, we need to
+      // add each to its own row.
+      foreach ($matches as $match) {
+        $rows[] = [$match];
+      }
+    }
+    return [
+      '#type' => 'table',
+      '#header' => [
+        'nid' => 'Node',
+        'field' => 'Field',
+      ],
+      '#rows' => $rows,
+      '#attributes' => NULL,
+    ];
+  }
+
+}
