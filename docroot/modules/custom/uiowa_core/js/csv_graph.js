ce639be2218b12a9ffbad88bc2887a1b37aa0bee
Gary Ridgway <gary-ridgway@uiowa.edu>
2021-09-15 15:11:06 -0500

    add graphing solution

---
+++ docroot/modules/custom/uiowa_core/js/csv_graph.js
@@ -0,0 +1,148 @@
+(($, Drupal, drupalSettings) => {
+  // Attach csv_graph behavior.
+  Drupal.behaviors.csv_graph = {
+    attach: (context, settings) => {
+      $('.graph-container', context).once('csv_graph').each((index) => {
+        // .----start behavior container----.
+
+        let graph_container = document.querySelectorAll('.graph-container')[index];
+        setupGraph(graph_container);
+
+        async function getTableData(element) {
+          let graphTable = element;
+
+          let tableDataByRow = [];
+          let tableDataByColumn = {};
+
+          // Get each row.
+          let rows = graphTable.querySelectorAll('tr');
+          // For each row...
+          rows.forEach(row => {
+            let rowData = [];
+            // Get the elements in that row.
+            let rowEls = row.querySelectorAll('th,td');
+            // For each element in that row...
+            rowEls.forEach(element =>  {
+              // Add that elements text to the row data.
+              let text = element.textContent;
+              if (text == '') {
+                text = null;
+              }
+              rowData.push(text);
+            });
+
+            // Put the row's data in the `tableDataByRow` variable.
+            tableDataByRow.push(rowData);
+          });
+
+          // If all of the arrays in `tableDataByRow` are the same...
+          // I know this looks weird but it works.
+          if (function() {
+            let equalLength = true;
+
+            tableDataByRow.forEach( row => {
+              equalLength = equalLength && (row.length = tableDataByRow[0].length);
+            });
+
+            return equalLength;
+          }) {
+            tableDataByRow[0].forEach( (header, headerIndex) => {
+              tableDataByColumn[header] = [];
+              for (row = 1; row <tableDataByRow.length; row++) {
+                tableDataByColumn[header].push(tableDataByRow[row][headerIndex]);
+              }
+            });
+          }
+          else {
+            console.error("The data in the given table is formatted incorrectly. Please check your data and make sure that there are the same number of values in each row.")
+            return;
+          }
+
+          return {
+            'headers' : tableDataByRow[0],
+            'data'    : tableDataByColumn
+          };
+        }
+
+        async function setupGraph(element) {
+          const canvas = element.querySelectorAll('.graph-canvas')[0];
+          const canvasContext = canvas.getContext('2d');
+          const graphData = await getTableData(element.querySelectorAll('.graph-table')[0]);
+          let datasets = [];
+
+          for (let i = 1; i < graphData.headers.length; i++) {
+            // Default to UI Gold.
+            let color = random_rgb(255, 205, 0);
+            // If we are on the second dataset, set the color to Dark Grey.
+            if (i == 2) {
+              color = random_rgb(65, 65, 65);
+            }
+            // Else if it is not the first two datasets, pick a random color.
+            else if (i > 2) {
+              color = random_rgb();
+            }
+
+            datasets.push(
+              {
+                type: 'line',
+                label: graphData.headers[i],
+                data: graphData.data[graphData.headers[i]],
+                fill: false,
+                borderColor: color.fullAlpha,
+                backgroundColor: color.halfAlpha,
+                borderWidth: 1
+              }
+            );
+          }
+
+          const myChart = new Chart(canvasContext, {
+            data: {
+              labels: graphData.data[graphData.headers[0]],
+              datasets: datasets,
+            },
+            options: {
+              responsive: true,
+              maintainAspectRatio: false
+            }
+          });
+
+          console.log(myChart);
+
+          let canvas__container = canvas.parentElement;
+          $( window ).resize(function() {
+            let layout__region = canvas__container.parentElement.parentElement.parentElement;
+
+            if (layout__region.classList.contains('layout__region')) {
+              canvas__container.style.width = layout__region.clientWidth + "px";
+
+              setHeightCanvas(canvas, canvas__container);
+            }
+          });
+
+          setHeightCanvas(canvas, canvas__container);
+        }
+
+        // Generate a random RGBA() color.
+        function random_rgb(givenR = null, givenG = null, givenB = null) {
+          var o = Math.round, r = Math.random, s = 255;
+          let R = (givenR != null) ? givenR : o(r()*s);
+          let G = (givenG != null) ? givenG : o(r()*s);
+          let B = (givenB != null) ? givenB : o(r()*s);
+          return {
+            'fullAlpha' : 'rgba(' + R + ',' + G + ',' + B + ',' + 1 + ')',
+            'halfAlpha' : 'rgba(' + R + ',' + G + ',' + B + ',' + 0.5 + ')'
+          }
+        }
+
+        function setHeightCanvas(canvas, canvas__container) {
+          canvas__container.style.height = parseInt(canvas.style.width, 10)/2 + "px";
+        }
+
+        // .----end behavior container----.
+      });
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
+
+
+
---
+++ docroot/modules/custom/uiowa_core/src/Plugin/Block/GraphBlock.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Drupal\uiowa_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Xss;
+
+/**
+ * Provides the Graph block.
+ *
+ * @Block(
+ *   id = "uiowa_core_graph",
+ *   admin_label = @Translation("Graph"),
+ *   category = @Translation("Site custom")
+ * )
+ */
+class GraphBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = $this->getConfiguration();
+
+    $csv_text = isset($config['graph_CSV_data']) ? $config['graph_CSV_data'] : '';
+
+    $rows = preg_split("/\r\n|\n|\r/", $csv_text);
+
+    $build['graph_container'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => ['graph-container'], /* Class on the wrapping DIV element */
+      ],
+
+    ];
+    $build['graph_container']['canvas'] = [
+      '#type' => 'markup',
+      '#markup' => '<div class="graph-canvas__container"><canvas class="graph-canvas"></canvas></div>',
+      '#allowed_tags' => array_merge(Xss::getHtmlTagList(), ['canvas', 'div']),
+    ];
+    $build['graph_container']['#attached']['library'][] = 'uiowa_core/graph';
+    $build['graph_container']['#attached']['library'][] = 'uiowa_core/chartjs';
+
+    $build['graph_container']['graph_table'] = [
+      '#theme' => 'table',
+//      '#caption' => 'My Table Caption',
+      '#attributes' => array('class' => ['graph-table', 'sr-only']),
+
+    ];
+
+    $build['graph_container']['graph_table']['#header'] = [];
+    $build['graph_container']['graph_table']['#rows'] = [];
+
+    foreach ($rows as $row_key => $row) {
+      if ($row_key == 0) {
+        foreach (explode( ',', $row) as $column_key => $column) {
+          array_push($build['graph_container']['graph_table']['#header'], ['data' => $column]);
+        }
+      }
+      else {
+        $build['graph_container']['graph_table']['#rows']['row-' . $row_key] = [];
+        foreach (explode( ',', $row) as $column_key => $column) {
+          array_push($build['graph_container']['graph_table']['#rows']['row-' . $row_key], ['data' => $column]);
+        }
+      }
+    }
+
+
+//    $build['#header'] = [
+//      ['data' => 'Cell Value'], /* Cell */
+//      ['data' => 'Cell Value', '#attributes' => ['class' => 'text-align-right']], /* Cell aligned right */
+//      ['data' => 'Cell Value'], /* Cell */
+//    ];
+//    $build['#rows'] = [
+//      'data' => [ /* Row */
+//        [ 'data' => 'Cell Value'], /* Cell */
+//        [ 'data' => 'Cell Value', '#attributes' => ['class' => 'text-align-right']], /* Cell aligned right */
+//        [ 'data' => 'Cell Value'], /* Cell */
+//      ],
+//    ];
+//    return [
+//      '#markup' =>
+//        '<div>' . $csv_text . ' </div>',
+//    ];
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['graph_CSV_data'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('CSV data'),
+      '#description' => $this->t('Copy and paste your properly formatted CSV file here.'),
+      '#default_value' => isset($config['graph_CSV_data']) ? $config['graph_CSV_data'] : '',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $values = $form_state->getValues();
+    $this->configuration['graph_CSV_data'] = $values['graph_CSV_data'];
+  }
+}
