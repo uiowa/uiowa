154e632974b4d7529d3a50765e5565d04318700a
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2021-09-16 08:38:33 -0500

    local task routing and pre_view alter

---
+++ docroot/modules/custom/sitenow_people/src/Routing/RouteSubscriber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\sitenow_people\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    // Make this view use the admin theme by mocking an admin route.
+    if ($route = $collection->get('view.people_sort.page_sort_people_tag')) {
+      $route->setOption('_admin_route', TRUE);
+    }
+  }
+}
