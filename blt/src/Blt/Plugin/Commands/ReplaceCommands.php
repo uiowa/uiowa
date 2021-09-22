8a77f25f3ba73dbd70812e42703c6d1c24afec5b
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-09-22 08:39:01 -0500

    Clear each sites plugin cache to hopefully prevent #3585

--- blt/src/Blt/Plugin/Commands/ReplaceCommands.php
+++ blt/src/Blt/Plugin/Commands/ReplaceCommands.php
@@ -59,11 +59,20 @@ class ReplaceCommands extends BltTasks {
           }
 
           try {
-            // Define a site-specific cache directory. For some reason, putenv
-            // did not work here. This would not be necessary if Drush
-            // supported per-site config file loading.
+            // Define a site-specific cache directory.
             // @see: https://github.com/drush-ops/drush/pull/4345
-            $_ENV['DRUSH_PATHS_CACHE_DIRECTORY'] = "/tmp/.drush-cache-{$app}/{$env}/{$multisite}";
+            $tmp = "/tmp/.drush-cache-{$app}/{$env}/{$multisite}";
+
+            $this->taskDrush()
+              ->drush('cc plugin')
+              ->option('define', "drush.paths.cache-directory={$tmp}")
+              ->printMetadata(FALSE)
+              ->run();
+
+            // Ensure BLT uses the site-specific cache directory. For some
+            // reason, putenv did not work here. This would not be necessary if
+            // Drush supported per-site config file loading.
+            $_ENV['DRUSH_PATHS_CACHE_DIRECTORY'] = $tmp;
             $this->invokeCommand('drupal:update');
             $this->logger->info("Finished deploying updates to <comment>{$multisite}</comment>.");
           }
