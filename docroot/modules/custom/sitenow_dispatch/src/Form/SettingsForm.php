8158059aa0369bf9ba79920519545a727f1f2c2b
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-10-29 14:45:46 -0500

    Ooops

--- docroot/modules/custom/sitenow_dispatch/src/Form/SettingsForm.php
+++ docroot/modules/custom/sitenow_dispatch/src/Form/SettingsForm.php
@@ -108,7 +108,7 @@ class SettingsForm extends ConfigFormBase {
     // The thank you form requires a separate key set for csi.drupal to not
     // expose our communications with clients. It also requires an HR token.
     // Both of these should be set in configuration overrides.
-    if ($api_key && $config->get('thanks.hr_token')) {
+    if ($config->get('thanks.api_key') && $config->get('thanks.hr_token')) {
       $form['thanks']['campaign'] = [
         '#type' => 'hidden',
         '#value' => 985361362,
