f991a9b5bc2812066bd275c623b3685b1d9399d6
makurj <makur-jain@uiowa.edu>
2021-11-02 15:48:20 -0500

    Removed image size for left, right alignment

--- docroot/modules/custom/layout_builder_custom/js/card-block-form.js
+++ docroot/modules/custom/layout_builder_custom/js/card-block-form.js
@@ -12,21 +12,18 @@
         // @todo Add a class to this field widget wrapper so that we can target it more precisely.
         const $form_wrapper = $(this).parent().parent().parent();
         const $image_format_field = $form_wrapper.find('.form-item-layout-builder-style-media-format').find('select');
-        const $image_size_field = $form_wrapper.find('.form-item-layout-builder-style-card-image-size').find('select');
+        const $image_size_field = $form_wrapper.find('.form-item-layout-builder-style-card-image-size').find('select, label');
 
         if ($image_format_field.length || $image_size_field.length) {
-          $image_size_field.css({ 'background-color': 'red'});
-          // $image_size_field.parent().css({'display': none});
           // Check if the referenced media is a video.
           const mediaTypeVideo = this.querySelector('.media--video');
+          $image_size_field.hide();
 
           if (mediaTypeVideo) {
             // Set image format to widescreen.
             $image_format_field.val('media_format_widescreen').change();
             // Hide image format.
             $image_format_field.parent().hide();
-            $image_size_field.parent().hide();
-
 
             // @ todo investigate alternative ways of hiding image format that does not affect image size state.
           } else {
