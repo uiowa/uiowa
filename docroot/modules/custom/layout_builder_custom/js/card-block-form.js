6d950c1c285608978a7dcf73e79d135d9072dcd8
makurj <makur-jain@uiowa.edu>
2021-11-02 15:31:55 -0500

    hiding image size for left and right alignment

--- docroot/modules/custom/layout_builder_custom/js/card-block-form.js
+++ docroot/modules/custom/layout_builder_custom/js/card-block-form.js
@@ -10,9 +10,13 @@
 
         // Check that we can access the next field.
         // @todo Add a class to this field widget wrapper so that we can target it more precisely.
-        const $image_format_field = $(this).parent().parent().parent().find('.form-item-layout-builder-style-media-format').find('select');
+        const $form_wrapper = $(this).parent().parent().parent();
+        const $image_format_field = $form_wrapper.find('.form-item-layout-builder-style-media-format').find('select');
+        const $image_size_field = $form_wrapper.find('.form-item-layout-builder-style-card-image-size').find('select');
 
-        if ($image_format_field.length) {
+        if ($image_format_field.length || $image_size_field.length) {
+          $image_size_field.css({ 'background-color': 'red'});
+          // $image_size_field.parent().css({'display': none});
           // Check if the referenced media is a video.
           const mediaTypeVideo = this.querySelector('.media--video');
 
@@ -21,10 +25,14 @@
             $image_format_field.val('media_format_widescreen').change();
             // Hide image format.
             $image_format_field.parent().hide();
+            $image_size_field.parent().hide();
+
+
             // @ todo investigate alternative ways of hiding image format that does not affect image size state.
           } else {
               // Show image format.
             $image_format_field.parent().show();
+            $image_size_field.parent().show();
           }
         }
       });
