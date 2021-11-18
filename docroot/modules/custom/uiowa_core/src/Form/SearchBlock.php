1627cf501149ed537c3b6f2589429c51c2179ae9
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-11-18 13:18:38 -0600

    Pass search_config to submit with hidden value

--- docroot/modules/custom/uiowa_core/src/Form/SearchBlock.php
+++ docroot/modules/custom/uiowa_core/src/Form/SearchBlock.php
@@ -24,6 +24,11 @@ class SearchBlock extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state, array $search_config = []) {
     $form['#attributes']['class'][] = 'form-inline clearfix uiowa-search-form';
 
+    $form['search_config'] = [
+      '#type' => 'hidden',
+      '#value' => $search_config,
+    ];
+
     $form['search'] = [
       '#type' => 'search',
       '#title' => $search_config['search_label'] ?? $this->t('Search'),
@@ -43,15 +48,16 @@ class SearchBlock extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $build_info = $form_state->getBuildInfo();
     $values = $form_state->getValues();
-    $uri = $build_info['search_config']['endpoint'];
+    $uri = $values['search_config']['endpoint'];
     $query = $values['search'];
+
     // Support root-relative URLs.
     if (str_starts_with($uri, '/')) {
       $uri = 'base:' . substr($uri, 1);
     }
-    $url = Url::fromUri($uri, ['query' => [$build_info['search_config']['query_parameter'] => $query]]);
+
+    $url = Url::fromUri($uri, ['query' => [$values['search_config']['query_parameter'] => $query]]);
     $form_state->setRedirectUrl($url);
   }
 
