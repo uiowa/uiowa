4c50d7d32e53951e13f6cd5a5f3968a6cf17761f
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-11-22 12:18:36 -0600

    Clean up vm code to get some tests working locally

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -3636,25 +3636,25 @@ $sites['educationwild.stage.drupal.uiowa.edu'] = 'wild.education.uiowa.edu';
 $sites['educationwild.prod.drupal.uiowa.edu'] = 'wild.education.uiowa.edu';
 
 // Directory aliases for tedabel.lab.uiowa.edu.
-$sites['labtedabel.local.drupal.uiowa.edu'] = 'tedabel.lab.uiowa.edu';
+$sites['labtedabel.uiowa.ddev.site'] = 'tedabel.lab.uiowa.edu';
 $sites['labtedabel.dev.drupal.uiowa.edu'] = 'tedabel.lab.uiowa.edu';
 $sites['labtedabel.stage.drupal.uiowa.edu'] = 'tedabel.lab.uiowa.edu';
 $sites['labtedabel.prod.drupal.uiowa.edu'] = 'tedabel.lab.uiowa.edu';
 
 // Directory aliases for jabbari.lab.uiowa.edu.
-$sites['labjabbari.local.drupal.uiowa.edu'] = 'jabbari.lab.uiowa.edu';
+$sites['labjabbari.uiowa.ddev.site'] = 'jabbari.lab.uiowa.edu';
 $sites['labjabbari.dev.drupal.uiowa.edu'] = 'jabbari.lab.uiowa.edu';
 $sites['labjabbari.stage.drupal.uiowa.edu'] = 'jabbari.lab.uiowa.edu';
 $sites['labjabbari.prod.drupal.uiowa.edu'] = 'jabbari.lab.uiowa.edu';
 
 // Directory aliases for epx.org.uiowa.edu.
-$sites['orgepx.local.drupal.uiowa.edu'] = 'epx.org.uiowa.edu';
+$sites['orgepx.uiowa.ddev.site'] = 'epx.org.uiowa.edu';
 $sites['orgepx.dev.drupal.uiowa.edu'] = 'epx.org.uiowa.edu';
 $sites['orgepx.stage.drupal.uiowa.edu'] = 'epx.org.uiowa.edu';
 $sites['orgepx.prod.drupal.uiowa.edu'] = 'epx.org.uiowa.edu';
 
 // Directory aliases for haar.lab.uiowa.edu.
-$sites['labhaar.local.drupal.uiowa.edu'] = 'haar.lab.uiowa.edu';
+$sites['labhaar.uiowa.ddev.site'] = 'haar.lab.uiowa.edu';
 $sites['labhaar.dev.drupal.uiowa.edu'] = 'haar.lab.uiowa.edu';
 $sites['labhaar.stage.drupal.uiowa.edu'] = 'haar.lab.uiowa.edu';
 $sites['labhaar.prod.drupal.uiowa.edu'] = 'haar.lab.uiowa.edu';
