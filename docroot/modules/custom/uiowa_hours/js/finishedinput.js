e2231e621b79066ed8754fb0a4b25a224e4a1923
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2021-12-13 15:19:56 -0600

    handle multiple instances

--- docroot/modules/custom/uiowa_hours/js/finishedinput.js
+++ docroot/modules/custom/uiowa_hours/js/finishedinput.js
@@ -4,14 +4,17 @@
     attach: function (context, settings) {
       var typingTimer;
       var delay = 1100;
-      $('#edit-date').once('finished_input').on('input', function (e) {
-        clearTimeout(typingTimer);
-        if ($(this).val()) {
-          var trigid = $(this);
-          typingTimer = setTimeout(function () {
-            trigid.triggerHandler('finishedinput');
-          }, delay);
-        }
+
+      $('.form-date').once('finished_input').each(function () {
+        $(this).on('input', function () {
+          clearTimeout(typingTimer);
+          if ($(this).val()) {
+            var trigid = $(this);
+            typingTimer = setTimeout(function () {
+              trigid.triggerHandler('finishedinput');
+            }, delay);
+          }
+        });
       });
     }
   };
