6512d3233576911c98ec6b7dad87867084c74c17
Richard B. Porter <richard-b-porter@uiowa.edu>
2021-12-21 15:04:18 -0600

    Revert "check to see if stored entity still exists"
    This reverts commit 30d757e19ac571e86ffe00380a1772d549215847.

--- docroot/modules/custom/uiowa_core/src/Plugin/Block/RegionContentBlock.php
+++ docroot/modules/custom/uiowa_core/src/Plugin/Block/RegionContentBlock.php
@@ -77,17 +77,15 @@ class RegionContentBlock extends BlockBase implements ContainerFactoryPluginInte
 
     if ($fid != NULL) {
       $fragment = $this->entityTypeManager->getStorage('fragment')->load($fid);
-      // Check to see if entity stored in config still exists.
-      if ($fragment) {
-        $fragment = $this->entityTypeManager->getViewBuilder('fragment')->view($fragment, 'default');
+      $fragment = $this->entityTypeManager->getViewBuilder('fragment')->view($fragment, 'default');
 
-        $build['fragment'] = $fragment;
-        $build['#contextual_links'] = [
-          'region_settings' => [
-            'route_parameters' => ['regions_settings' => 'region_settings'],
-          ],
-        ];
-      }
+      $build['fragment'] = $fragment;
+      $build['#contextual_links'] = [
+        'region_settings' => [
+          'route_parameters' => ['regions_settings' => 'region_settings'],
+        ],
+      ];
+
     }
 
     return $build;
