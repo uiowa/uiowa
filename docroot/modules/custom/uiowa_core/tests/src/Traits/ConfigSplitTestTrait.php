cae0329027725a7b2e2b9d45a8352d393eee55f1
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2021-12-22 11:43:44 -0600

    Event views block (#4483)
    * Initial version of event list block view, layout builder style overrides.
    
    * Added new generic template.
    
    * Updated LB styles to new machine name.
    
    * Updated event list view.
    
    * Generalized some logic for the various list block view implementations.
    
    * Changed event view featured image format. Re-factored the logic around setting defaults for card type blocks and lists to hopefully be less confusing.
    
    * Make sure that card classes don't get processed on event list block.
    
    * Fixed a few double quotes. Started logic for preprocessing the view details specific to the event list. Added more fields to the view output. Updated the load order for sitenow_events module.
    
    * Updated templates to use new card__meta class to replace card__date and card__label
    
    * Undoing indentation change
    
    * Updated test docs link. Fixed PHPCS errors. Added test case for event feature. Added logic for displaying additional event fields.
    
    * Updated more occurrences of card_date
    
    * Fixed syntax issue.
    
    * Added fix for person page icons
    
    * Worked on trying to get split config imported correctly in test.
    
    * Working on logic to get location and virtual details to show up.
    
    * Added function docblock.
    
    * Updated view settings
    
    * Updated so that date and locations will display
    
    * Moved enableConfigSplit method to a trait to be usable in other tests. Refactored to try to get that work, but still doesn't seem to be working.
    
    * Hid Location label from display
    
    * Updated styling for location field
    
    * Added view-events--list scss file
    
    * Added flex layout for consistent icon alignment with locations
    
    * Automatically pass -y flag.
    
    * Updated UIDS version
    
    * Some test refactoring to try a different approach to importing config.
    
    * Fixed some but not all PHPCS errors.
    
    * Added people block view test. Not working yet.
    
    * Fixed mispelled namespace.
    
    * Removed '(default)' label since we add it programatically. Fixed defaults for cards.
    
    * Added other media format options.
    
    * Only display virtual and locationif they exist.
    
    * Render the virtual event details.
    
    * Fixed PHPCS errors.
    
    * Moved the config split trait to uiowa_core because its not really related to layout builder.
    
    * Updated reference.
    
    * Tried to adjust output formatting
    
    * Fixed closing tag
    
    * Added event list block as option in page LB settings.
    
    * Temporary fix for date filter states issue.
    
    * Hard-coded solution for display date fields.
    
    * Updated event list block view. Removed previous customizations.
    
    * Refactoring the sorting logic to not show table drag handles when there is only one sort option.
    
    * Fixed PHPCS warning.
    
    * Updated UIDS version
    
    * Added some margin to the top of these profile cards
    
    * Updated page title underline style
    
    * Updated list block views to use standard labels for fields.
    
    * Fixed other labels.
    
    Co-authored-by: Benjamin Speare <benjamin-speare@uiowa.edu>
    Co-authored-by: bspeare <bspeare@users.noreply.github.com>
    Co-authored-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>

---
+++ docroot/modules/custom/uiowa_core/tests/src/Traits/ConfigSplitTestTrait.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\Tests\uiowa_core\Traits;
+
+use Drupal\Core\Config\FileStorage;
+
+/**
+ * A trait for enabling a config split in a test.
+ */
+trait ConfigSplitTestTrait {
+
+  /**
+   * The CLI service.
+   *
+   * @var null|\Drupal\config_split\ConfigSplitCliService
+   */
+  protected $cliService = NULL;
+
+  /**
+   * Enable a config split.
+   *
+   * @param string $split_name
+   *   The split being enabled.
+   * @param null|\Drupal\user\UserInterface $user
+   *   The user to give permissions to.
+   */
+  public function enableConfigSplit($split_name, $user = NULL) {
+    // @todo Add check that split exists.
+    // Import the split configuration.
+    $config_path = DRUPAL_ROOT . '/../config/default';
+    $source = new FileStorage($config_path);
+    $config_storage = \Drupal::service('config.storage');
+    $split = $source->read("config_split.config_split.$split_name");
+    $config_storage->write("config_split.config_split.$split_name", $split);
+
+    // Enable the split.
+    $config_factory = \Drupal::configFactory();
+    $config = $config_factory->getEditable("config_split.config_split.$split_name");
+    $config->set('status', TRUE);
+    $config->save(TRUE);
+
+    if (is_null($user)) {
+      $user = $this->drupalCreateUser(['synchronize configuration']);
+    }
+    $this->drupalLogin($user);
+
+    // Import the configuration thereby re-installing all the modules.
+    $this->drupalGet('admin/config/development/configuration');
+    // @todo Figure out how to fix message about staged configuration.
+    $this->submitForm([], 'Import all');
+    // Modules have been installed that have services.
+    $this->rebuildContainer();
+  }
+
+  /**
+   * Load the config_split.cli service.
+   */
+  protected function cliService() {
+    if (is_null($this->cliService)) {
+      $this->cliService = $this->container->get('config_split.cli');
+    }
+    return $this->cliService;
+  }
+
+  /**
+   * A mock io object for testing.
+   */
+  protected function cliIo() {
+    // Anonymous function to mock $io calls.
+    $io = new class() {
+
+      /**
+       * {@inheritdoc}
+       */
+      public function __call($string, $arguments) {
+        return '';
+      }
+
+    };
+
+    return $io;
+  }
+
+}
