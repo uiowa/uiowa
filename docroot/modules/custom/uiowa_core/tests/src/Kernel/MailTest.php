9122ad562f92515f1bb32e32a9de7d6f28bc4014
Richard B. Porter <richard-b-porter@uiowa.edu>
2022-01-06 10:39:12 -0600

    Add a quick kernel test for mail alter

---
+++ docroot/modules/custom/uiowa_core/tests/src/Kernel/MailTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\Tests\uiowa_core\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Test description.
+ *
+ * @group uiowa_core
+ */
+class MailTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['uiowa_core'];
+
+  /**
+   * The email message.
+   *
+   * @var array
+   */
+  protected $message;
+
+  /**
+   * The service account email address.
+   *
+   * @var string
+   */
+  protected $serviceAccount;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->message = [
+      'subject' => 'Foo',
+      'body' => 'Bar',
+    ];
+
+    $this->serviceAccount = base64_decode('aXRzLXdlYkB1aW93YS5lZHU=');
+  }
+
+  /**
+   * Test O365 header is always set.
+   */
+  public function testO365HeaderSet() {
+    $this->message['from_mail'] = 'foo@uiowa.edu';
+    $this->message['from_name'] = 'Foo';
+    $result = $this->container->get('plugin.manager.mail')->doMail('uiowa_core', 'key', 'admin@example.com', 'en', $this->message);
+    $this->assertEquals('ITS-Acquia', $result['headers']['X-UI-Hosted']);
+  }
+
+  /**
+   * Test the from email is overridden if not originating from uiowa.edu.
+   */
+  public function testFromAddressOverriddenIfNotUiowa() {
+    $this->message['from_mail'] = 'foo@bar.com';
+    $this->message['from_name'] = 'Foo';
+    $result = $this->container->get('plugin.manager.mail')->doMail('uiowa_core', 'key', 'admin@example.com', 'en', $this->message);
+    $this->assertEquals("\"Foo\" <$this->serviceAccount>", $result['headers']['From']);
+  }
+
+  /**
+   * Test the from email is not overridden if originating from uiowa.edu.
+   */
+  public function testFromAddressNotOverriddenIfUiowa() {
+    $this->message['from_mail'] = 'foo@uiowa.edu';
+    $this->message['from_name'] = 'Foo';
+    $result = $this->container->get('plugin.manager.mail')->doMail('uiowa_core', 'key', 'admin@example.com', 'en', $this->message);
+    $this->assertNotEquals("\"Foo\" <$this->serviceAccount>", $result['headers']['From']);
+  }
+
+}
