53733650724f236f9443e1b0a9854f259581cc14
Richard B. Porter <richard-b-porter@uiowa.edu>
2022-01-06 16:53:29 -0600

    Improve uiowa_core mail kernel tests

--- docroot/modules/custom/uiowa_core/tests/src/Kernel/MailTest.php
+++ docroot/modules/custom/uiowa_core/tests/src/Kernel/MailTest.php
@@ -14,14 +14,14 @@ class MailTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = ['uiowa_core', 'filter', 'system'];
+  public static $modules = ['uiowa_core', 'uiowa_core_test', 'filter', 'system'];
 
   /**
    * The email message.
    *
    * @var array
    */
-  protected $message;
+  protected $params;
 
   /**
    * The service account email address.
@@ -36,11 +36,9 @@ class MailTest extends KernelTestBase {
   protected function setUp() {
     parent::setUp();
 
-    $this->message = [
+    $this->params = [
       'subject' => 'Test Email',
       'body' => 'This is a test.',
-      'from_name' => 'Foo',
-      'from_mail' => 'foo@uiowa.edu',
     ];
 
     $this->serviceAccount = base64_decode('aXRzLXdlYkB1aW93YS5lZHU=');
@@ -48,9 +46,13 @@ class MailTest extends KernelTestBase {
 
   /**
    * Test O365 header is always set.
+   *
+   * @dataProvider providerO365
    */
-  public function testO365HeaderSet() {
-    $result = $this->container->get('plugin.manager.mail')->doMail('uiowa_core', 'key', 'admin@example.com', 'en', $this->message);
+  public function testO365HeaderSet($from, $name) {
+    $this->config('system.site')->set('mail', $from)->save();
+    $this->config('system.site')->set('name', $name)->save();
+    $result = $this->container->get('plugin.manager.mail')->mail('uiowa_core', 'key', 'admin@example.com', 'en', $this->params);
     $this->assertEquals('ITS-Acquia', $result['headers']['X-UI-Hosted']);
   }
 
@@ -58,9 +60,9 @@ class MailTest extends KernelTestBase {
    * Test the from email is overridden if not originating from uiowa.edu.
    */
   public function testFromAddressOverriddenIfNotUiowa() {
-    $this->message['from_mail'] = 'foo@bar.com';
-    $this->message['from_name'] = 'Foo';
-    $result = $this->container->get('plugin.manager.mail')->doMail('uiowa_core', 'key', 'admin@example.com', 'en', $this->message);
+    $this->config('system.site')->set('mail', 'foo@bar.com')->save();
+    $this->config('system.site')->set('name', 'Foo')->save();
+    $result = $this->container->get('plugin.manager.mail')->mail('uiowa_core', 'key', 'admin@example.com', 'en', $this->params);
     $this->assertEquals("\"Foo\" <$this->serviceAccount>", $result['headers']['From']);
   }
 
@@ -68,7 +70,9 @@ class MailTest extends KernelTestBase {
    * Test the from email is not overridden if originating from uiowa.edu.
    */
   public function testFromAddressNotOverriddenIfUiowa() {
-    $result = $this->container->get('plugin.manager.mail')->doMail('uiowa_core', 'key', 'admin@example.com', 'en', $this->message);
+    $this->config('system.site')->set('mail', 'foo@uiowa.edu')->save();
+    $this->config('system.site')->set('name', 'Foo')->save();
+    $result = $this->container->get('plugin.manager.mail')->mail('uiowa_core', 'key', 'admin@example.com', 'en', $this->params);
     $this->assertNotEquals("\"Foo\" <$this->serviceAccount>", $result['headers']['From']);
   }
 
@@ -76,11 +80,29 @@ class MailTest extends KernelTestBase {
    * Test the from name is set to site name if empty for non-uiowa.edu emails.
    */
   public function testFromNameSetToSiteNameIfEmptyAndNotUiowa() {
+    $this->config('system.site')->set('mail', 'foo@uiowa.edu')->save();
     $this->config('system.site')->set('name', 'Test Site')->save();
-    $this->message['from_mail'] = 'foo@bar.com';
-    $this->message['from_name'] = '';
-    $result = $this->container->get('plugin.manager.mail')->doMail('uiowa_core', 'key', 'admin@example.com', 'en', $this->message);
+    $result = $this->container->get('plugin.manager.mail')->mail('uiowa_core_test', 'key', 'admin@example.com', 'en', $this->params);
     $this->assertEquals("\"Test Site\" <$this->serviceAccount>", $result['headers']['From']);
+  }
+
+  /**
+   * Data provider for O365 header test.
+   *
+   * @return array
+   *   Array of arguments.
+   */
+  public function providerO365() {
+    return [
+      [
+        'someone@external.com',
+        'Someone External',
+      ],
+      [
+        'someone@uiowa.edu',
+        'Someone Internal',
+      ],
+    ];
   }
 
 }
