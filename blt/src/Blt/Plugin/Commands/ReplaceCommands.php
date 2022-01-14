1d946fb10ff9ab149c5c746fd27a9ba1e85cb60e
Richard B. Porter <richard-b-porter@uiowa.edu>
2022-01-14 16:02:09 -0600

    Exclude docroot/modules/custom/uiowa_core/src/Form/UiowaCoreSiteInformationForm.php for now

--- blt/src/Blt/Plugin/Commands/ReplaceCommands.php
+++ blt/src/Blt/Plugin/Commands/ReplaceCommands.php
@@ -119,7 +119,7 @@ class ReplaceCommands extends BltTasks {
     $paths = [
       "{$root}/tests/" => '',
       "{$docroot}/profiles/custom/" => '',
-      "{$docroot}/modules/custom/" => '',
+      "{$docroot}/modules/custom/" => "$docroot/modules/custom/uiowa_core/src/Form/UiowaCoreSiteInformationForm.php",
       "{$docroot}/themes/custom/" => '',
       "{$docroot}/sites/" => "{$docroot}/sites/simpletest",
     ];
