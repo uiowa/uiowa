55f9a09ca2b57044bb2652f40e3888eb81339eb9
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2022-01-26 10:37:20 -0600

    add map of nids

--- docroot/sites/grad.admissions.uiowa.edu/modules/grad_admissions_migrate/src/Plugin/migrate/source/AreaOfStudy.php
+++ docroot/sites/grad.admissions.uiowa.edu/modules/grad_admissions_migrate/src/Plugin/migrate/source/AreaOfStudy.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\grad_admissions_migrate\Plugin\migrate\source;
 
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\State\StateInterface;
+use Drupal\migrate\Event\MigrateImportEvent;
 use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\pathauto\AliasCleanerInterface;
 use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
@@ -87,6 +89,87 @@ class AreaOfStudy extends BaseNodeSource implements ContainerFactoryPluginInterf
     $row->setSourceProperty('custom_related_links', $related_links);
 
     return TRUE;
+  }
+
+  /**
+   * Return the destination given a NID on the old site.
+   *
+   * @param int $nid
+   *   The node ID.
+   *
+   * @return false|string
+   *   The new path or FALSE if not in the map.
+   */
+  protected function manualLookup($nid) {
+    $map = [
+      // source_nid => target_nid,
+      // MAc. mac-program-estimated-costs.
+      121 => 211,
+      // MD. md-program-estimated-costs.
+      112 => 246,
+      // JD. jd-program-estimated-costs.
+      415 => 256,
+      // SJD. sjd-program-estimated-costs.
+      111 => 266,
+      // LLM. llm-program-estimated-costs.
+      421 => 276,
+      // MSL. master-studies-law-msl-program-estimated-costs.
+      583 => 286,
+      // PharmD. pharmd-program-estimated-costs.
+      116 => 296,
+      // DNP. doctor-nursing-practice-dnp-program-estimated-costs.
+      113 => 306,
+      // DPT. dpt-program-estimated-costs.
+      131 => 316,
+      // MPH. master-public-health-program-estimated-costs.
+      118 => 326,
+      // Public health (not MPH). graduate-programs-public-health-estimated-costs.
+      103 => 336,
+      // Sustainable Water Development. sustainable-water-development-certificate-estimated-costs.
+      931 => 346,
+      // Oral Science Program. oral-science-program-estimated-costs.
+      115 => 356,
+      // Applied Math & Computational Sciences. applied-math-computational-sciences-estimated-costs.
+      105 => 366,
+      // LIS. library-information-science-estimated-costs.
+      109 => 376,
+      // Second Language Acquisition. second-language-acquisition-estimated-costs.
+      119 => 386,
+      // Grad Programs in Pharmacy. graduate-programs-pharmacy-estimated-costs.
+      117 => 396,
+      // Informatics. informatics-estimated-costs.
+      412 => 406,
+      // Dental Public Health. dental-public-health-estimated-costs.
+      106 => 416,
+      // Urban & Regional Planning. urban-regional-planning-estimated-costs.
+      120 => 426,
+      // Endodontics. certificate-program-endodontics-estimated-costs.
+      132 => 436,
+      // DDS. doctor-dental-surgery-dds-program-estimated-costs.
+      107 => 446,
+      // Speech Pathology & Audiology. speech-pathology-and-audiology-ma-or-aud-estimated-costs.
+      417 => 456,
+      // Health Administration. health-administration-program-estimated-costs.
+      108 => 466,
+      // Business (PhD). phd-programs-business-estimated-costs.
+      96 => 476,
+      // Education. education-estimated-costs.
+      101 => 486,
+      // Liberal Arts & Sciences. graduate-programs-liberal-arts-sciences-costs.
+      99 => 496,
+      // Engineering. engineering-estimated-costs.
+      102 => 506,
+      // Nursing (PhD). phd-program-nursing-estimated-costs.
+      133 => 516,
+      // Nursing (MSN). msn-cnl-program-estimated-costs.
+      114 => 526,
+      // Medicine (non-MD). ms-phd-mme-mca-and-mpa-programs-medicine-estimated-costs.
+      100 => 536,
+      // Business (MBA). full-time-mba-program-estimated-costs.
+      110 => 546,
+    ];
+
+    return isset($map[$nid]) ? $map[$nid] : FALSE;
   }
 
 }
