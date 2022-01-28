9a7fe0d9a1570228d0b9a30fffe76e63bae5ca83
Richard B. Porter <richard-b-porter@uiowa.edu>
2022-01-28 15:44:29 -0600

    Roll back to previous versions

--- docroot/modules/custom/uiowa_hours/src/Plugin/Block/HoursBlock.php
+++ docroot/modules/custom/uiowa_hours/src/Plugin/Block/HoursBlock.php
@@ -7,7 +7,6 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Security\TrustedCallbackInterface;
 use Drupal\Core\Url;
 use Drupal\uiowa_core\HeadlineHelper;
 use Drupal\uiowa_hours\HoursApi;
@@ -22,7 +21,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   category = @Translation("Site custom")
  * )
  */
-class HoursBlock extends BlockBase implements ContainerFactoryPluginInterface, TrustedCallbackInterface {
+class HoursBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
    * The Hours API service.
@@ -97,16 +96,16 @@ class HoursBlock extends BlockBase implements ContainerFactoryPluginInterface, T
    * {@inheritdoc}
    */
   public function build() {
+    $config = $this->getConfiguration();
 
-    $build = [
-      '#lazy_builder' => [
-        static::class . '::lazyBuilder',
-        ['foo'],
-      ],
-      '#cache' => [
-        'max-age' => 0,
-      ],
+    $build['heading'] = [
+      '#theme' => 'uiowa_core_headline',
+      '#headline' => $config['headline'],
+      '#hide_headline' => $config['hide_headline'],
+      '#heading_size' => $config['heading_size'],
+      '#headline_style' => $config['headline_style'],
     ];
+    $build['form'] = $this->formBuilder->getForm('Drupal\uiowa_hours\Form\HoursFilterForm', $config);
 
     return $build;
   }
@@ -182,26 +181,6 @@ class HoursBlock extends BlockBase implements ContainerFactoryPluginInterface, T
     $this->configuration['display_summary'] = $form_state->getValue('display_summary');
     $this->configuration['display_datepicker'] = $form_state->getValue('display_datepicker');
     parent::blockSubmit($form, $form_state);
-  }
-
-  /**
-   * Lazy builder callback for hours block build.
-   */
-  public static function lazyBuilder($config) {
-    $build = [
-      'foo' => [
-        '#markup' => '<p>This is a test.</p>',
-      ],
-    ];
-
-    return $build;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function trustedCallbacks() {
-    return ['lazyBuilder'];
   }
 
 }
