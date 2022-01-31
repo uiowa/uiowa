0cc2be9e34298fa855facbf3b626fbf8edd6495f
Richard B. Porter <richard-b-porter@uiowa.edu>
2022-01-31 11:53:56 -0600

    Trigger mouseown on form input on doc ready

--- docroot/modules/custom/uiowa_hours/js/filter_form.js
+++ docroot/modules/custom/uiowa_hours/js/filter_form.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.uiowa_hours_filter_form = {
     attach: function (context, settings) {
       $('.uiowa-hours-filter-form').once('filter_form').each(function () {
-
+        $('input[type="submit"]', this).mousedown();
       });
     }
   };
