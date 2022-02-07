b68a0cdc165dc7d512bb034ea8d7c8f43919aff2
Benjamin Speare <benjamin-speare@uiowa.edu>
2022-02-07 14:51:26 -0600

    Added new Uiowa Search block to use in new search region

---
+++ docroot/modules/custom/uiowa_search/src/Plugin/Block/SearchBlock.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\uiowa_search\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides global search block for Google.
+ *
+ * @Block(
+ *   id = "uiowa_search",
+ *   admin_label = @Translation("UIowa Search"),
+ *   category = @Translation("Site custom")
+ * )
+ */
+class SearchBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * The form_builder service.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Search block constructor.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $form_state = new FormState();
+    return $this->formBuilder->buildForm('Drupal\uiowa_search\Form\SearchForm', $form_state);
+  }
+
+}
