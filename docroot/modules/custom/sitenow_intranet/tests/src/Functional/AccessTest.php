35712f81d2dcda303b382b67a8fb6a28d6e3d4c0
Richard B. Porter <richard-b-porter@uiowa.edu>
2022-02-24 11:06:05 -0600

    Write failing test for response code of hook_restrict_ip_access_denied_page_alter

---
+++ docroot/modules/custom/sitenow_intranet/tests/src/Functional/AccessTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\Tests\sitenow_intranet\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test description.
+ *
+ * @group sitenow_intranet
+ */
+class AccessTest extends BrowserTestBase {
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'uids_base';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'block',
+    'config_split',
+    'node',
+    'restrict_ip',
+    'samlauth',
+    'sitenow_intranet',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    // Set uids_base header type to avoid Twig error. There is some additional
+    // setup happening in sitenow_intranet.install.
+    $this->config('uids_base.settings')->set('header.type', 'inline')->save();
+  }
+
+  /**
+   * Test hook_restrict_ip_access_denied_page_alter in sitenow_intranet.
+   */
+  public function testNodeReturnsAccessDenied() {
+    $node = $this->drupalCreateNode();
+    $this->drupalGet('node/' . $node->id());
+    $this->assertSession()->statusCodeEquals(401);
+  }
+
+}
