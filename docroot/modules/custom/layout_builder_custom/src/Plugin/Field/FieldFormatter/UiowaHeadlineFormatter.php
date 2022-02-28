f89e4d01eb6e74a0963065316c461a8561bd15e1
e-marie-w <elsie-wakeman@uiowa.edu>
2022-02-28 16:56:44 -0600

    Added missing default and hide settings.

--- docroot/modules/custom/layout_builder_custom/src/Plugin/Field/FieldFormatter/UiowaHeadlineFormatter.php
+++ docroot/modules/custom/layout_builder_custom/src/Plugin/Field/FieldFormatter/UiowaHeadlineFormatter.php
@@ -33,7 +33,8 @@ class UiowaHeadlineFormatter extends FormatterBase {
       $item_style = isset($styles[$item->get('headline_style')->getValue()]) ?
         $styles[$item->get('headline_style')->getValue()] . $hidden : $hidden;
 
-      $item_alignment = $alignment[$item->get('headline_alignment')->getValue()] ?? 'default';
+      $item_alignment = isset($alignment[$item->get('headline_alignment')->getValue()]) ?
+        $alignment[$item->get('headline_alignment')->getValue()] . $hidden : $hidden;
 
       $element[$delta] = [
         '#theme' => 'uiowa_headline_field_type',
