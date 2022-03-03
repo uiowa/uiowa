16e605af67e33680182051cd5f35d71e16a954bb
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2022-03-03 11:28:32 -0600

    Re-add some things from MenuBlockTest.php

--- docroot/modules/custom/layout_builder_custom/tests/src/Functional/MenuBlockTest.php
+++ docroot/modules/custom/layout_builder_custom/tests/src/Functional/MenuBlockTest.php
@@ -19,6 +19,27 @@ class MenuBlockCustomTest extends BrowserTestBase {
   protected $defaultTheme = 'stark';
 
   /**
+   * The menu link plugin manager.
+   *
+   * @var \Drupal\Core\Menu\MenuLinkManagerInterface
+   */
+  protected $menuLinkManager;
+
+  /**
+   * The menu link content storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $menuLinkContentStorage;
+
+  /**
+   * An array containing the menu link plugin ids.
+   *
+   * @var array
+   */
+  protected $links;
+
+  /**
    * {@inheritdoc}
    */
   public static $modules = [
@@ -34,6 +55,11 @@ class MenuBlockCustomTest extends BrowserTestBase {
    */
   protected function setUp(): void {
     parent::setUp();
+
+    $this->menuLinkManager = \Drupal::service('plugin.manager.menu.link');
+    $this->menuLinkContentStorage = \Drupal::service('entity_type.manager')
+      ->getStorage('menu_link_content');
+    $this->links = $this->createLinkHierarchy();
 
     $this->drupalPlaceBlock('local_tasks_block');
 
@@ -51,6 +77,73 @@ class MenuBlockCustomTest extends BrowserTestBase {
     $this->drupalLogin($this->drupalCreateUser([
       'configure any layout',
     ]));
+  }
+
+
+  /**
+   * Creates a simple hierarchy of links.
+   */
+  protected function createLinkHierarchy() {
+    // First remove all the menu links in the menu.
+    $this->menuLinkManager->deleteLinksInMenu('main');
+
+    // Then create a simple link hierarchy:
+    // - parent menu item
+    //   - child-1 menu item
+    //     - child-1-1 menu item
+    //     - child-1-2 menu item
+    //   - child-2 menu item.
+    $base_options = [
+      'provider' => 'menu_block',
+      'menu_name' => 'main',
+    ];
+
+    $parent = $base_options + [
+        'title' => 'parent menu item',
+        'link' => ['uri' => 'internal:/menu-block-test/hierarchy/parent'],
+      ];
+    /** @var \Drupal\menu_link_content\MenuLinkContentInterface $link */
+    $link = $this->menuLinkContentStorage->create($parent);
+    $link->save();
+    $links['parent'] = $link->getPluginId();
+
+    $child_1 = $base_options + [
+        'title' => 'child-1 menu item',
+        'link' => ['uri' => 'internal:/menu-block-test/hierarchy/parent/child-1'],
+        'parent' => $links['parent'],
+      ];
+    $link = $this->menuLinkContentStorage->create($child_1);
+    $link->save();
+    $links['child-1'] = $link->getPluginId();
+
+    $child_1_1 = $base_options + [
+        'title' => 'child-1-1 menu item',
+        'link' => ['uri' => 'internal:/menu-block-test/hierarchy/parent/child-1/child-1-1'],
+        'parent' => $links['child-1'],
+      ];
+    $link = $this->menuLinkContentStorage->create($child_1_1);
+    $link->save();
+    $links['child-1-1'] = $link->getPluginId();
+
+    $child_1_2 = $base_options + [
+        'title' => 'child-1-2 menu item',
+        'link' => ['uri' => 'internal:/menu-block-test/hierarchy/parent/child-1/child-1-2'],
+        'parent' => $links['child-1'],
+      ];
+    $link = $this->menuLinkContentStorage->create($child_1_2);
+    $link->save();
+    $links['child-1-2'] = $link->getPluginId();
+
+    $child_2 = $base_options + [
+        'title' => 'child-2 menu item',
+        'link' => ['uri' => 'internal:/menu-block-test/hierarchy/parent/child-2'],
+        'parent' => $links['parent'],
+      ];
+    $link = $this->menuLinkContentStorage->create($child_2);
+    $link->save();
+    $links['child-2'] = $link->getPluginId();
+
+    return $links;
   }
 
   /**
