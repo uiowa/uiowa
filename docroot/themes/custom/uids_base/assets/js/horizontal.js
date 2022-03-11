1bd50bc1f31d68e45f6ea990c78d69afb2486440
Benjamin Speare <benjamin-speare@uiowa.edu>
2022-03-11 16:31:28 -0600

    Reverting change

--- docroot/themes/custom/uids_base/assets/js/horizontal.js
+++ docroot/themes/custom/uids_base/assets/js/horizontal.js
@@ -1,12 +1,14 @@
-const menuItems = document.querySelectorAll('li.menu-item--expanded');
+
+
+var menuItems = document.querySelectorAll('li.menu-item--expanded');
 Array.prototype.forEach.call(menuItems, function(el, i){
   el.querySelector('a').addEventListener("click",  function(event){
     if (this.parentNode.className == "menu-item--expanded") {
-      this.parentNode.className = "menu-item--expanded";
-      this.setAttribute('aria-expanded', "false");
-    } else {
       this.parentNode.className = "menu-item--expanded open";
       this.setAttribute('aria-expanded', "true");
+    } else {
+      this.parentNode.className = "menu-item--expanded";
+      this.setAttribute('aria-expanded', "false");
     }
     event.preventDefault();
     return false;
