6f6529ade5bbf071d233105d3ea6a64289bbf308
Benjamin Speare <benjamin-speare@uiowa.edu>
2022-03-11 20:37:35 -0600

    Small fix to get toggle working

--- docroot/themes/custom/uids_base/assets/js/horizontal.js
+++ docroot/themes/custom/uids_base/assets/js/horizontal.js
@@ -3,11 +3,11 @@
 var menuItems = document.querySelectorAll('li.menu-item--expanded');
 Array.prototype.forEach.call(menuItems, function(el, i){
   el.querySelector('a').addEventListener("click",  function(event){
-    if (this.parentNode.className == "menu-item--expanded") {
-      this.parentNode.className = "menu-item--expanded open";
+    if (this.parentNode.className == "menu-item menu-item--expanded") {
+      this.parentNode.className = "menu-item menu-item--expanded open";
       this.setAttribute('aria-expanded', "true");
     } else {
-      this.parentNode.className = "menu-item--expanded";
+      this.parentNode.className = "menu-item menu-item--expanded";
       this.setAttribute('aria-expanded', "false");
     }
     event.preventDefault();
