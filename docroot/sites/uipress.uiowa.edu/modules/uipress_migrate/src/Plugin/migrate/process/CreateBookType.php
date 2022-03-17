b539cc4b4588aa43cfbbfdff905d7debb376d51e
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2022-03-17 13:54:23 -0500

    stub out book type creator

---
+++ docroot/sites/uipress.uiowa.edu/modules/uipress_migrate/src/Plugin/migrate/process/CreateBookType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\uipress_migrate\Plugin\migrate\process;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Logger\LoggerChannelTrait;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Custom process plugin to create book type items on book nodes.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "create_book_type"
+ * )
+ */
+class CreateBookType extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+  use LoggerChannelTrait;
+
+  /**
+   * The entity_type.manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entityTypeManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    /** @var \Drupal\paragraphs\Entity\Paragraph $paragraph */
+    $paragraph = $this->entityTypeManager->getStorage('paragraph')->create([
+      'type' => 'book_type',
+      'field_book_type' => [
+        'value' => 'eBook',
+      ],
+      'field_book_isbn' => [
+        'value' => $value['value'],
+      ],
+    ]);
+
+    $paragraph->save();
+
+    return [
+      'target_id' => $paragraph->id(),
+      'target_revision_id' => $paragraph->getRevisionId(),
+    ];
+  }
+
+}
