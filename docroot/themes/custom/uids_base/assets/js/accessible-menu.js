20d21eefa1788b564f393c08a0bd0098600a2542
Benjamin Speare <benjamin-speare@uiowa.edu>
2022-03-29 15:38:28 -0500

    Removed and renamed horizontal.js

---
+++ docroot/themes/custom/uids_base/assets/js/accessible-menu.js
@@ -0,0 +1,5 @@
+
+const menu = new AccessibleMenu.DisclosureMenu({
+  menuElement: document.querySelector(".menu-wrapper--horizontal > .menu"),
+  submenuItemSelector: "li.menu-item--expanded",
+});
