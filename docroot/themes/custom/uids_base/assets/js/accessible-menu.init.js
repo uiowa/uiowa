310b799e1fdb28b5e077920f40b76e0f4320b629
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2022-03-30 16:11:24 -0500

    Added comment to clarify configuration choice.

--- docroot/themes/custom/uids_base/assets/js/accessible-menu.init.js
+++ docroot/themes/custom/uids_base/assets/js/accessible-menu.init.js
@@ -3,6 +3,7 @@
     attach: function (context, settings) {
       const menu = new AccessibleMenu.TopLinkDisclosureMenu({
         menuElement: document.querySelector(".menu-wrapper--horizontal > .menu"),
+        // We have to add 'span' to handle <nolink>.
         menuLinkSelector: "a, span",
         submenuItemSelector: "li.menu-item--expanded",
       });
