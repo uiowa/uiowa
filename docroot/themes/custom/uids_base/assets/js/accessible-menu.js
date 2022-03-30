f4ed726f8ce54ece5d63e1a446bd6f29a70f946e
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2022-03-30 14:33:38 -0500

    Updates to get accessible-menu library attaching under the right conditions. Updated implementation JS to use common Drupal pattern for attaching behaviors.

--- docroot/themes/custom/uids_base/assets/js/accessible-menu.js
+++ docroot/themes/custom/uids_base/assets/js/accessible-menu.js
@@ -1,5 +1,10 @@
-
-const menu = new AccessibleMenu.DisclosureMenu({
-  menuElement: document.querySelector(".menu-wrapper--horizontal > .menu"),
-  submenuItemSelector: "li.menu-item--expanded",
-});
+(function ($, AccessibleMenu) {'use strict';
+  Drupal.behaviors.accessible_menu = {
+    attach: function (context, settings) {
+      const menu = new AccessibleMenu.TopLinkDisclosureMenu({
+        menuElement: document.querySelector(".menu-wrapper--horizontal > .menu"),
+        submenuItemSelector: "li.menu-item--expanded",
+      });
+    }
+  }
+})(jQuery, AccessibleMenu);
