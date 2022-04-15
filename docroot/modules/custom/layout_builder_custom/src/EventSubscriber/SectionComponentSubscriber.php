c2623f41e97439a5587e4434bade10782ac3f434
makurj <makur-jain@uiowa.edu>
2022-04-15 13:49:47 -0500

    Added LB styles to image block, created ultrawide LB style, updated event subscriber code for image block view mode.

--- docroot/modules/custom/layout_builder_custom/src/EventSubscriber/SectionComponentSubscriber.php
+++ docroot/modules/custom/layout_builder_custom/src/EventSubscriber/SectionComponentSubscriber.php
@@ -12,12 +12,14 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 /**
  * Add an HTML class to blocks that are displaying placeholder text.
  */
-class SectionComponentSubscriber implements EventSubscriberInterface {
+class SectionComponentSubscriber implements EventSubscriberInterface
+{
 
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents()
+  {
     $events[LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY] = [
       'onBuildRender',
       50,
@@ -31,7 +33,8 @@ class SectionComponentSubscriber implements EventSubscriberInterface {
    * @param \Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent $event
    *   The section component render event.
    */
-  public function onBuildRender(SectionComponentBuildRenderArrayEvent $event) {
+  public function onBuildRender(SectionComponentBuildRenderArrayEvent $event)
+  {
     $block = $event->getPlugin();
     if (!$block instanceof BlockPluginInterface) {
       return;
@@ -81,38 +84,49 @@ class SectionComponentSubscriber implements EventSubscriberInterface {
     // view mode for the image field. This is necessary to allow selection
     // of different image formats.
     if (isset($build['#derivative_plugin_id'])) {
-      switch ($build['#derivative_plugin_id']) {
-        case 'uiowa_card':
-          if (isset($build["#attributes"]["class"])) {
+      if (isset($build['#attributes']['class'])) {
+        switch ($build['#derivative_plugin_id']) {
+          case 'uiowa_card':
             // Map the layout builder styles to the view mode to be used.
             $media_formats = [
-              'media--widescreen' => 'large__widescreen',
-              'media--square' => 'large__square',
               'media--circle' => 'large__square',
+              'media--square' => 'large__square',
+              'media--ultrawide' => 'large__ultrawide',
+              'media--widescreen' => 'large__widescreen',
             ];
+            break;
 
-            // Loop through the map to check if any of them are being used and
-            // adjust the view mode accordingly.
-            foreach ($media_formats as $style => $view_mode) {
-              if (in_array($style, $build["#attributes"]["class"])) {
-                // Change the view mode to match the format.
-                $build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#view_mode'] = $view_mode;
-                // Important: Delete the cache keys to prevent this from being
-                // applied to all the instances of the same image.
-                if (isset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']) && isset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']['keys'])) {
-                  unset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']['keys']);
-                }
-
-                // We only want this to execute once.
-                break;
+          case 'uiowa_image':
+            // Map the layout builder styles to the view mode to be used.
+            $media_formats = [
+              'media--circle' => 'full__square',
+              'media--square' => 'full__square',
+              'media--ultrawide' => 'full__ultrawide',
+              'media--widescreen' => 'full__widescreen',
+            ];
+          break;
+        }
+        if (isset($media_formats)) {
+          // Loop through the map to check if any of them are being used and
+          // adjust the view mode accordingly.
+          foreach ($media_formats as $style => $view_mode) {
+            if (in_array($style, $build['#attributes']['class'])) {
+              // Change the view mode to match the format.
+              $build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#view_mode'] = $view_mode;
+              // Important: Delete the cache keys to prevent this from being
+              // applied to all the instances of the same image.
+              if (isset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']) && isset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']['keys'])) {
+                unset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']['keys']);
               }
+
+              // We only want this to execute once.
+              break;
             }
           }
-          break;
+        }
       }
     }
 
     $event->setBuild($build);
   }
-
 }
