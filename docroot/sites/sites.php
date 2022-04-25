9ee266b938ff554efc567fc088f74cb4b81bf97f
bspeare <bspeare@users.noreply.github.com>
2022-04-25 09:31:38 -0500

    Initialize vpma.jobs.uiowa.edu multisite on uiowa03 (#5166)
    Co-authored-by: Richard B. Porter <richardbporter@users.noreply.github.com>

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4066,3 +4066,9 @@ $sites['orgsipe.uiowa.ddev.site'] = 'sipe.org.uiowa.edu';
 $sites['orgsipe.dev.drupal.uiowa.edu'] = 'sipe.org.uiowa.edu';
 $sites['orgsipe.stage.drupal.uiowa.edu'] = 'sipe.org.uiowa.edu';
 $sites['orgsipe.prod.drupal.uiowa.edu'] = 'sipe.org.uiowa.edu';
+
+// Directory aliases for vpma.jobs.uiowa.edu.
+$sites['jobsvpma.uiowa.ddev.site'] = 'vpma.jobs.uiowa.edu';
+$sites['jobsvpma.dev.drupal.uiowa.edu'] = 'vpma.jobs.uiowa.edu';
+$sites['jobsvpma.stage.drupal.uiowa.edu'] = 'vpma.jobs.uiowa.edu';
+$sites['jobsvpma.prod.drupal.uiowa.edu'] = 'vpma.jobs.uiowa.edu';
