2224a6d903b8c6b64c92c2797a097cdbe351bc8a
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2022-04-29 14:30:40 -0500

    Initialize mu.lab.uiowa.edu multisite on uiowa03

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4084,3 +4084,9 @@ $sites['labsheffield.uiowa.ddev.site'] = 'sheffield.lab.uiowa.edu';
 $sites['labsheffield.dev.drupal.uiowa.edu'] = 'sheffield.lab.uiowa.edu';
 $sites['labsheffield.stage.drupal.uiowa.edu'] = 'sheffield.lab.uiowa.edu';
 $sites['labsheffield.prod.drupal.uiowa.edu'] = 'sheffield.lab.uiowa.edu';
+
+// Directory aliases for mu.lab.uiowa.edu.
+$sites['labmu.uiowa.ddev.site'] = 'mu.lab.uiowa.edu';
+$sites['labmu.dev.drupal.uiowa.edu'] = 'mu.lab.uiowa.edu';
+$sites['labmu.stage.drupal.uiowa.edu'] = 'mu.lab.uiowa.edu';
+$sites['labmu.prod.drupal.uiowa.edu'] = 'mu.lab.uiowa.edu';
