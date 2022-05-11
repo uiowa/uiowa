e49a1b64b7cb3c709ed90f90ef4befdeaf7faf33
cory-skeers <49074566+cory-skeers@users.noreply.github.com>
2022-05-11 12:27:43 -0500

    2 plus 2 part 3 (#5257)

---
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/AdmissionsCoreInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\admissions_core;
+
+/**
+ * Provides an interface for admissions_core constants.
+ */
+interface AdmissionsCoreInterface {
+  /**
+   * Slug for href construction of 2 plus 2 links.
+   */
+  const TWO_PLUS_TWO_PATH = '/2plus2/majors/';
+
+  /**
+   * Overrides for query alters for areas of study.
+   */
+  const AOS_QUERY_OVERRIDES = [
+    'nursing rn bsn' => 'nursing rn-bsn',
+    'resilience and trauma informed perspectives certificate' => 'resilience and trauma-informed perspectives certificate',
+    'interscholastic athleticactivities administration' => 'interscholastic athletic/activities administration',
+  ];
+
+}
