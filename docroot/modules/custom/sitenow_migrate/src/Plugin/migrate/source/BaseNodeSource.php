39c21ebda87556211ceba2d0e6e1d4cebac60ea9
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2022-06-02 15:10:33 -0500

    IISC - Remaining content updates (#5281)
    * Added text list field for academic years to the project content type. Added options list callback and first attempt at default value callback.
    
    * Refactored allowed values function and fixed logic with default value function.
    
    * Checking in progress.
    
    * Adding extra fields to colleges and departments vocab.
    
    * Adding extra fields to colleges and departments vocab.
    
    * Trying to figure out how to do media objects.
    
    * Creating a way to specify multi-value fields through the migration YAML.
    
    * PHPCS fixes and started a README for creating migrations.
    
    * Refactored the ProcessMediaTrait to reduce code duplication and increase flexibility. Updated Project migration to provide media-specific fields to process the a user friendly filename if it exists.
    
    * Defining a new process plugin for processing a file field into a media entity or getting an existing media entity ID. Not working yet. Refactoring related to this.
    
    * More progress on process plugin. Modifications to BaseSourceNode to get it working.
    
    * Move some processing to _construct so it doesn't run on every row. Row processing only happens if relevant.
    
    * A little bit of cleanup. Added a note about inspriation for plugin.
    
    * A few PHPCS fixes.
    
    * Some code cleanup and PHPCS fixes.
    
    * Trying out building a basic process plugin for summary extraction. Updated associated IISC migrations to use the new plugin.
    
    * Missed an array adjustment. Check if 'summary' is set first, and if it is not, proceed extracting from the value like normal.
    
    * Minor adjustment to allow passing a basic string as well as compound field array.
    
    * Minor adjustments to migrate README.
    
    * Adjust people migration to use the new process plugin.
    
    * Create a new file if not already exists; create a media entity for it, because we know it doesn't already exist, and return the media id.
    
    * Some refactoring and reordering. @todo How do we want to handle the possibility of matching filenames from different subdirectories?
    
    * Adjustments to get the check for existing media to work.
    
    * Some cleanup. phpcs. Update other migrations to use the source plugin.
    
    * Remove field_image processing from source classes--they are using the process plugins.
    
    * Move lower level methods to the createMediaTrait for possible reuse elsewhere.
    
    * phpcs
    
    * Small readme update.
    
    * Change node_type to default_bundle
    
    * Move alias join into BaseNodeSource with a check if we're using the entity:redirect plugin. Remove the additional query/field repetition in custom source files.
    
    * Added option to limit dates fetched at the BaseNodeSource level, if a date_limiter config value is provided in the YAML definition. Updated Readme to reflect this and state the desired format. @todo Make the formatting more robust?
    
    * Adding a patch to hopefully help with migration performance. Also removing a property that is unnecessary.
    
    * Refactor to correctly handle article source.
    
    * Added a todo not about linking directly to article source.
    
    * composer update
    
    * Convert remaining html chars if they're there (such as &rsquo;)
    
    * Update yml with migration_lookup process plugin. Flatten reference ids array in prepareRow
    
    * Pass in queried value data for media creation. Use filename in media naming if title not available for files.
    
    * phpcs
    
    Co-authored-by: Cory Skeers <cory-skeers@uiowa.edu>
    Co-authored-by: cory-skeers <49074566+cory-skeers@users.noreply.github.com>

--- docroot/modules/custom/sitenow_migrate/src/Plugin/migrate/source/BaseNodeSource.php
+++ docroot/modules/custom/sitenow_migrate/src/Plugin/migrate/source/BaseNodeSource.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\sitenow_migrate\Plugin\migrate\source;
 
+use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\File\FileSystemInterface;
-use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Logger\LoggerChannelTrait;
 use Drupal\Core\State\StateInterface;
 use Drupal\migrate\Event\ImportAwareInterface;
@@ -12,8 +12,8 @@ use Drupal\migrate\Event\MigrateImportEvent;
 use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Row;
 use Drupal\node\Plugin\migrate\source\d7\Node;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\smart_trim\Truncate\TruncateHTML;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides base node source abstract class with additional functionality.
@@ -75,12 +75,75 @@ abstract class BaseNodeSource extends Node implements ImportAwareInterface {
   protected $multiValueFields = [];
 
   /**
+   * Files with media fields on the source.
+   *
+   * @var array
+   */
+  protected $sourceMediaFields = [];
+
+  /**
    * {@inheritdoc}
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, StateInterface $state, ModuleHandlerInterface $module_handler, FileSystemInterface $file_system, EntityTypeManager $entityTypeManager) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $migration, $state, $entityTypeManager, $module_handler);
     $this->fileSystem = $file_system;
     $this->logger = $this->getLogger('sitenow_migrate');
+    // Add a 'source_file_path' entry to configuration so that we can use it in
+    // process plugins later. This is necessary because querying against the
+    // source database in a process plugin is not at all straightforward.
+    if (!isset($configuration['constants']['source_file_path'])) {
+      $this->configuration['constants']['source_file_path'] = $this->variableGet('file_public_path', NULL);
+    }
+    // Loop through each entry in the process section of the migration config.
+    foreach ($this->migration->getProcess() as $processes) {
+      // If processes isn't an array, then the plugin is 'get' and we can bail.
+      if (!is_array($processes)) {
+        continue;
+      }
+      foreach ($processes as $process) {
+        // Check if we are using a plugin that we need this additional
+        // processing for.
+        // @todo Handle sub-processes as well.
+        if (empty($process['plugin'])
+          || empty($process['source'])
+          || !in_array($process['plugin'], [
+            'create_media_from_file_field',
+          ])) {
+          continue;
+        }
+        $this->sourceMediaFields[] = $process['source'];
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = parent::query();
+    if ($date_limiter = $this->migration->getSourceConfiguration()['date_limiter']) {
+      // Only import news newer than the date limiter provided.
+      $query->condition('created', strtotime($date_limiter), '>=');
+    }
+    // Only add the aliases to the query if we're
+    // in the redirect migration, otherwise row counts
+    // will be off due to one-to-many mapping of nodes to aliases.
+    if ($this->migration->getDestinationConfiguration()['plugin'] === 'entity:redirect') {
+      $query->leftJoin('url_alias', 'alias', "alias.source = CONCAT('node/', n.nid)");
+      $query->fields('alias', ['alias']);
+    }
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = parent::fields();
+    if ($this->migration->getDestinationConfiguration()['plugin'] === 'entity:redirect') {
+      $fields['alias'] = $this->t('The URL alias for this node.');
+    }
+    return $fields;
   }
 
   /**
@@ -106,9 +169,71 @@ abstract class BaseNodeSource extends Node implements ImportAwareInterface {
     parent::prepareRow($row);
     $moderation_state = $row->getSourceProperty('status') == 1 ? 'published' : 'draft';
     $row->setSourceProperty('moderation_state', $moderation_state);
+    $this->processMultiValueFields($row);
+    $this->processMediaFields($row);
+  }
+
+  /**
+   * Process multi-value fields from source class or YAML.
+   *
+   * @param \Drupal\migrate\Row $row
+   *   The migration row result.
+   */
+  protected function processMultiValueFields(Row $row) {
+    if (!empty($this->configuration['multi_value_fields'])) {
+      foreach ($this->configuration['multi_value_fields'] as $field_name => $fields) {
+        if (!isset($this->multiValueFields[$field_name])) {
+          $this->multiValueFields[$field_name] = $fields;
+        }
+      }
+    }
     if (!empty($this->multiValueFields)) {
       $this->fetchAdditionalFields($row, $this->multiValueFields);
     }
+  }
+
+  /**
+   * Add additional properties for media fields.
+   *
+   * @param \Drupal\migrate\Row $row
+   *   The migration row result.
+   *
+   * @throws \Exception
+   */
+  public function processMediaFields(Row $row) {
+    // Loop through each entry in the process section of the migration config.
+    foreach ($this->sourceMediaFields as $field_name) {
+      $field = $row->getSourceProperty($field_name);
+      $vid = $row->getSourceProperty('vid');
+      $query = $this->select("field_revision_$field_name", 'fr')
+        ->condition('fr.revision_id', $vid)
+        ->orderBy('fm.fid');
+
+      $query->leftJoin('file_managed', 'fm', "fm.fid = fr.{$field_name}_fid");
+
+      $results = $query
+        ->fields('fm', ['fid', 'filename', 'uri'])
+        ->execute()
+        ->fetchAllAssoc('fid');
+      // Check if field is multi-value.
+      if (is_array($field[0])) {
+        // Loop through each value on the field.
+        foreach ($field as &$values) {
+          $fid = $values['fid'];
+          if (isset($results[$fid])) {
+            $values += $results[$fid];
+          }
+        }
+      }
+      $row->setSourceProperty($field_name, $field);
+    }
+
+    // Memory clean-up.
+    $field = NULL;
+    $vid = NULL;
+    $query = NULL;
+    $results = NULL;
+    $field_name = NULL;
   }
 
   /**
--- docroot/sites/iisc.uiowa.edu/modules/iisc_migrate/src/Plugin/migrate/source/Partners.php
+++ docroot/sites/iisc.uiowa.edu/modules/iisc_migrate/src/Plugin/migrate/source/Partners.php
@@ -27,38 +27,8 @@ class Partners extends BaseNodeSource {
   /**
    * {@inheritdoc}
    */
-  public function query() {
-    $query = parent::query();
-    // Only add the aliases to the query if we're
-    // in the redirect migration, otherwise row counts
-    // will be off due to one-to-many mapping of nodes to aliases.
-    if ($this->migration->id() === 'iisc_partner_redirects') {
-      $query->leftJoin('url_alias', 'alias', "alias.source = CONCAT('node/', n.nid)");
-      $query->fields('alias', ['alias']);
-    }
-    return $query;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function fields() {
-    $fields = parent::fields();
-    $fields['alias'] = $this->t('The URL alias for this node.');
-    return $fields;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function prepareRow(Row $row) {
     parent::prepareRow($row);
-    // Download image and attach it for the person photo.
-    if ($image = $row->getSourceProperty('field_image')) {
-      $this->entityId = $row->getSourceProperty('nid');
-      $row->setSourceProperty('field_image', $this->processImageField($image[0]['fid'], $image[0]['alt'], $image[0]['title']));
-      $image = NULL;
-    }
 
     // ID's for counties are off by -1, so just make that adjustment.
     if ($counties = $row->getSourceProperty('field_ref_ia_counties_target_id')) {
