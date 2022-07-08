62eb2603e9213a99f5639cdfbeeb1c8d803180db
Gary Ridgway <gary-ridgway@uiowa.edu>
2022-07-08 16:17:29 -0500

    setting merged src and dest, still not grabbing the right uids4 files

--- docroot/themes/custom/uids_base/gulpfile.js
+++ docroot/themes/custom/uids_base/gulpfile.js
@@ -16,6 +16,10 @@ const cssnano = require('cssnano')
 const glob = require('gulp-sass-glob');
 const sourcemaps = require('gulp-sourcemaps');
 const mode = require('gulp-mode')();
+const fs = require('fs');
+const path = require('path');
+const gulp = require('gulp');
+const merge = require('merge-stream');
 
 /*
  * Directories here
@@ -32,7 +36,14 @@ const uids = {
 
 const uids4 = {
   src: '../../../../node_modules/@uiowa/uids4/src',
+  readylist: [
+    'button',
+  ],
 }
+
+// Globals
+let uids4list = [];
+let uids3list = [];
 
 // Clean
 function clean() {
@@ -42,15 +53,83 @@ function clean() {
   ]);
 }
 
-function copyUids() {
-  return src([
-    `${uids.src}/**/*.scss`,
-    `${uids4.src}/**/*.scss`,
-    `${uids.src}/**/*.js`,
-    `${uids.src}/**/*.{jpg,png,svg}`,
-    `${uids.src}/**/*.{woff,woff2}`,
-  ])
-    .pipe(dest(`${uids.dest}`));
+function copyUids(done) {
+  const createUidsList = new Promise(((resolve, reject) => {
+    resolve(copyScss());
+  }));
+
+  createUidsList.then(value => {
+    done();
+
+    var tasks = uids4list.map(function(folder){
+      const folderName = folder.substring(folder.lastIndexOf('/') + 1)
+      return gulp.src([
+        `${folder}/*.scss`,
+        `${folder}/*.js`,
+        `${folder}/*.{jpg,png,svg}`,
+        `${folder}/*.{woff,woff2}`,
+      ]).pipe(gulp.dest(`${uids.dest}${folderName}`));
+    });
+
+    return merge(tasks);
+
+    // let files = [];
+    // uids4list.forEach(file => {
+    //   files.push(`${file}/*.scss`);
+    //   files.push(`${file}/*.js`);
+    //   files.push(`${file}/*.{jpg,png,svg}`);
+    //   files.push(`${file}/*.{woff,woff2}`);
+    // })
+    // console.log(files);
+    // return src(files)
+    //   .pipe(dest(`${uids.dest}`));
+
+    // return src([
+    //   `${uids.src}/**/*.scss`,
+    //   `${uids.src}/**/*.js`,
+    //   `${uids.src}/**/*.{jpg,png,svg}`,
+    //   `${uids.src}/**/*.{woff,woff2}`,
+    // ])
+    //   .pipe(dest(`${uids.dest}`));
+  });
+}
+
+function copyScss() {
+  return Promise.all([addToList(`${uids4.src}/components`), addToList(`${uids.src}/components`, true)]);
+
+  // loop through the node modules components directory for uids4.
+
+  // for any folder name that matches a name in the readyList...
+    // We want to add that directory to an array. We want a list of paths that we can do the src.pipe thing with
+  // loop through the components directory for uids3
+  // for any folder name that does not match a name in the readyList...
+    // We want to add that directory to an array. We want a list of paths that we can do the src.pipe thing with
+  // Loop through all the files in the temp directory
+
+}
+
+function addToList(filePath, invert = false) {
+  return fs.promises.readdir(filePath)
+    .then(files => {
+      files.forEach(function (file, index) {
+        if (
+          (!invert && !uids4.readylist.includes(file))
+          ||
+          (invert && uids4.readylist.includes(file))
+        ) {
+          return;
+        }
+        const fullFilePath = path.join(filePath, file);
+
+        if (fs.existsSync(fullFilePath) && fs.lstatSync(fullFilePath).isDirectory()) {
+          // Make one pass and make the file complete
+          uids4list.push(fullFilePath);
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
 }
 
 function fontCopy() {
@@ -83,6 +162,8 @@ const copy = parallel(copyUids, fontCopy);
 const compile = series(clean, copy, css);
 
 exports.copy = copy;
+exports.copyScss = copyScss;
+exports.copyUids = copyUids;
 exports.css = css;
 exports.default = compile;
 exports.watch = watchFiles;
