0469dd37370e495eec3e13182cff90ee1c6fae3e
Gary Ridgway <gary-ridgway@uiowa.edu>
2022-08-24 19:56:34 -0500

    preliminary dismissal button

--- docroot/themes/custom/uids_base/assets/js/after-content.js
+++ docroot/themes/custom/uids_base/assets/js/after-content.js
@@ -1,7 +1,6 @@
 Drupal.behaviors.after_content = {
   attach: function (context, settings) {
     jQuery('.content__container.after_content', context).once('after_content').each(function () {
-      // Code here.
       const el = document.querySelector('.content__container.after_content')
       let first = true;
       const pinned_class = 'after_content--is_pinned';
@@ -17,6 +16,25 @@ Drupal.behaviors.after_content = {
       );
 
       observer.observe(el);
+
+      // This is all the dismissal button stuff.
+      setTimeout(() => {
+        const layout_container = el.querySelector('.layout__container');
+        const dismiss_button = document.createElement('div');
+        dismiss_button.classList.add('after_content__dismiss');
+        dismiss_button.innerHTML = '<i role="presentation" class="fas fa-times"></i>';
+        layout_container.prepend(dismiss_button);
+
+        document.addEventListener('click', function (event) {
+
+          // If the clicked element doesn't have the right selector, bail
+          console.log(event);
+          if (!event.target.matches('.after_content__dismiss, .after_content__dismiss svg, .after_content__dismiss path')) return;
+
+          el.classList.add('after_content__dismissed');
+
+        }, false);
+      }, 300);
     });
   }
 }
