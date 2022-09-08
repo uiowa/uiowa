91d75a3753e9f06ebc07675f343c43404ad75022
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2022-09-08 10:11:39 -0500

    Initialize comparative-cognition.lab.uiowa.edu multisite on uiowa03

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4444,3 +4444,9 @@ $sites['researchdsp.uiowa.ddev.site'] = 'dsp.research.uiowa.edu';
 $sites['researchdsp.dev.drupal.uiowa.edu'] = 'dsp.research.uiowa.edu';
 $sites['researchdsp.stage.drupal.uiowa.edu'] = 'dsp.research.uiowa.edu';
 $sites['researchdsp.prod.drupal.uiowa.edu'] = 'dsp.research.uiowa.edu';
+
+// Directory aliases for comparative-cognition.lab.uiowa.edu.
+$sites['labcomparative-cognition.uiowa.ddev.site'] = 'comparative-cognition.lab.uiowa.edu';
+$sites['labcomparative-cognition.dev.drupal.uiowa.edu'] = 'comparative-cognition.lab.uiowa.edu';
+$sites['labcomparative-cognition.stage.drupal.uiowa.edu'] = 'comparative-cognition.lab.uiowa.edu';
+$sites['labcomparative-cognition.prod.drupal.uiowa.edu'] = 'comparative-cognition.lab.uiowa.edu';
