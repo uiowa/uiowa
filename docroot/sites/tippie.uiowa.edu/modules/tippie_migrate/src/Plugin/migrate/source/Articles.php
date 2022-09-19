7aa56797e36165f4b255ed4edf2eeb6f527c7088
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2022-09-19 11:54:45 -0500

    site migrations comparison update

--- docroot/sites/tippie.uiowa.edu/modules/tippie_migrate/src/Plugin/migrate/source/Articles.php
+++ docroot/sites/tippie.uiowa.edu/modules/tippie_migrate/src/Plugin/migrate/source/Articles.php
@@ -214,7 +214,7 @@ class Articles extends BaseNodeSource {
 
     // Map field_news_featured_research to "faculty research" tag.
     $research = $row->getSourceProperty('field_news_featured_research')[0]['value'];
-    if ($research == '1') {
+    if ((int) $research === 1) {
       $tag_names[] = 'faculty research';
       // Make sure 'research' is in the mix. Duplicates will be removed later.
       $tag_names[] = 'research';
