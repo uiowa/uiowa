873f5fc60a597ff2088809e8e2c4883d2978f494
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2022-09-27 14:40:17 -0500

    Initialize dei-speaker-series.asian-slavic.uiowa.edu multisite on uiowa01

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4486,3 +4486,9 @@ $sites['orgvapap.uiowa.ddev.site'] = 'vapap.org.uiowa.edu';
 $sites['orgvapap.dev.drupal.uiowa.edu'] = 'vapap.org.uiowa.edu';
 $sites['orgvapap.stage.drupal.uiowa.edu'] = 'vapap.org.uiowa.edu';
 $sites['orgvapap.prod.drupal.uiowa.edu'] = 'vapap.org.uiowa.edu';
+
+// Directory aliases for dei-speaker-series.asian-slavic.uiowa.edu.
+$sites['asian-slavicdei-speaker-series.uiowa.ddev.site'] = 'dei-speaker-series.asian-slavic.uiowa.edu';
+$sites['asian-slavicdei-speaker-series.dev.drupal.uiowa.edu'] = 'dei-speaker-series.asian-slavic.uiowa.edu';
+$sites['asian-slavicdei-speaker-series.stage.drupal.uiowa.edu'] = 'dei-speaker-series.asian-slavic.uiowa.edu';
+$sites['asian-slavicdei-speaker-series.prod.drupal.uiowa.edu'] = 'dei-speaker-series.asian-slavic.uiowa.edu';
