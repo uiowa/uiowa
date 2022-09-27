3e0fdfc61eaaf53baf69b47082cf907f8343938e
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2022-09-27 14:43:57 -0500

    Initialize campuscouncil.stanleymuseum.uiowa.edu multisite on uiowa02

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4492,3 +4492,9 @@ $sites['asian-slavicdei-speaker-series.uiowa.ddev.site'] = 'dei-speaker-series.a
 $sites['asian-slavicdei-speaker-series.dev.drupal.uiowa.edu'] = 'dei-speaker-series.asian-slavic.uiowa.edu';
 $sites['asian-slavicdei-speaker-series.stage.drupal.uiowa.edu'] = 'dei-speaker-series.asian-slavic.uiowa.edu';
 $sites['asian-slavicdei-speaker-series.prod.drupal.uiowa.edu'] = 'dei-speaker-series.asian-slavic.uiowa.edu';
+
+// Directory aliases for campuscouncil.stanleymuseum.uiowa.edu.
+$sites['stanleymuseumcampuscouncil.uiowa.ddev.site'] = 'campuscouncil.stanleymuseum.uiowa.edu';
+$sites['stanleymuseumcampuscouncil.dev.drupal.uiowa.edu'] = 'campuscouncil.stanleymuseum.uiowa.edu';
+$sites['stanleymuseumcampuscouncil.stage.drupal.uiowa.edu'] = 'campuscouncil.stanleymuseum.uiowa.edu';
+$sites['stanleymuseumcampuscouncil.prod.drupal.uiowa.edu'] = 'campuscouncil.stanleymuseum.uiowa.edu';
