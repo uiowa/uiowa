5767d140f555391a450a8735f979af1ada7c861a
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2022-10-21 15:01:22 -0500

    stubbing out initial migration work

---
+++ docroot/sites/now.uiowa.edu/modules/now_migrate/src/Plugin/migrate/source/People.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\now_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
+use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
+
+/**
+ * Migrate Source plugin.
+ *
+ * @MigrateSource(
+ *   id = "now_people",
+ *   source_module = "node"
+ * )
+ */
+class People extends BaseNodeSource {
+  use ProcessMediaTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    parent::prepareRow($row);
+
+    // Set our tagMapping if it's not already.
+    if (empty($this->tagMapping)) {
+      $this->tagMapping = \Drupal::database()
+        ->select('taxonomy_term_field_data', 't')
+        ->fields('t', ['name', 'tid'])
+        ->condition('t.vid', 'research_areas', '=')
+        ->execute()
+        ->fetchAllKeyed();
+    }
+
+    return TRUE;
+  }
+
+}
