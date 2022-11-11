3640e4d3534dfc83f39f7579a2dde5e8fb9c29bf
Cory Skeers <cory-skeers@uiowa.edu>
2022-11-11 08:35:29 -0600

    Stub out mapping/processing for some other fields.

--- docroot/sites/now.uiowa.edu/modules/now_migrate/src/Plugin/migrate/source/Achievement.php
+++ docroot/sites/now.uiowa.edu/modules/now_migrate/src/Plugin/migrate/source/Achievement.php
@@ -32,7 +32,63 @@ class Achievement extends BaseNodeSource {
       $row->setSourceProperty('field_image', $fid);
     }
 
+    // Set our tagMapping if it's not already.
+    if (empty($this->tagMapping)) {
+      $this->tagMapping = \Drupal::database()
+        ->select('taxonomy_term_field_data', 't')
+        ->fields('t', ['name', 'tid'])
+        ->condition('t.vid', 'tags', '=')
+        ->execute()
+        ->fetchAllKeyed();
+    }
+
+    // Map various old fields into Tags.
+    $category = $row->getSourceProperty('field_achievement_category')[0]['value'];
+    if (!empty($category)) {
+      $tid = $this->createTag($category);
+      $row->setSourceProperty('tags', $tid);
+    }
+    $news_from = $row->getSourceProperty('field_news_from')[0]['value'];
+    if (!empty($news_from)) {
+      $tid = $this->createTag($news_from);
+      $row->setSourceProperty('tags', $tid);
+    }
+    $news_about = $row->getSourceProperty('field_news_about')[0]['value'];
+    if (!empty($news_about)) {
+      $tid = $this->createTag($news_about);
+      $row->setSourceProperty('tags', $tid);
+    }
+    $news_for = $row->getSourceProperty('field_news_for')[0]['value'];
+    if (!empty($news_for)) {
+      $tid = $this->createTag($news_for);
+      $row->setSourceProperty('tags', $tid);
+    }
+    $news_keywords = $row->getSourceProperty('field_news_keywords')[0]['value'];
+    if (!empty($news_keywords)) {
+      $tid = $this->createTag($news_keywords);
+      $row->setSourceProperty('tags', $tid);
+    }
+
     return TRUE;
   }
 
+  /**
+   * Helper function to check for existing tags and create if they don't exist.
+   */
+  private function createTag($tag_name) {
+    // Check if we have a mapping. If we don't yet,
+    // then create a new tag and add it to our map.
+    if (!isset($this->tagMapping[$tag_name])) {
+      $term = Term::create([
+        'name' => $tag_name,
+        'vid' => 'tags',
+      ]);
+      if ($term->save()) {
+        $this->tagMapping[$tag_name] = $term->id();
+      }
+    }
+
+    // Return tid for mapping to field.
+    return $this->tagMapping[$tag_name];
+  }
 }
