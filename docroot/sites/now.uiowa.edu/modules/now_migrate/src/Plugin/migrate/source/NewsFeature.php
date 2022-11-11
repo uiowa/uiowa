26ea7213216308562bedd83c153c6675b1b40bcf
Cory Skeers <cory-skeers@uiowa.edu>
2022-11-11 15:00:17 -0600

    News Feature: map various tags. Prepend subhead to body.

--- docroot/sites/now.uiowa.edu/modules/now_migrate/src/Plugin/migrate/source/NewsFeature.php
+++ docroot/sites/now.uiowa.edu/modules/now_migrate/src/Plugin/migrate/source/NewsFeature.php
@@ -32,7 +32,84 @@ class NewsFeature extends BaseNodeSource {
       $row->setSourceProperty('field_image', $fid);
     }
 
+    $subhead = $row->getSourceProperty('field_subhead');
+    if (!empty($subhead)) {
+      $subhead = '<p class="uids-component--light-intro">' . $subhead[0]['value'] . '</p>';
+      $body = $row->getSourceProperty('body');
+      $body[0]['value'] = $subhead . $body[0]['value'];
+      $row->setSourceProperty('body', $body);
+    }
+
+    // Set our tagMapping if it's not already.
+    if (empty($this->tagMapping)) {
+      $this->tagMapping = \Drupal::database()
+        ->select('taxonomy_term_field_data', 't')
+        ->fields('t', ['name', 'tid'])
+        ->condition('t.vid', 'tags', '=')
+        ->execute()
+        ->fetchAllKeyed();
+    }
+
+    // Map various old fields into Tags.
+    $tag_tids = [];
+    foreach (['field_news_from', 'field_news_about', 'field_news_for', 'field_news_keywords'] as $field_name) {
+      $values = $row->getSourceProperty($field_name);
+      if (!isset($values)) {
+        continue;
+      }
+      foreach ($values as $tid_array) {
+        $tag_tids[] = $tid_array['tid'];
+      }
+    }
+
+    if (!empty($tag_tids)) {
+      // Fetch tag names based on TIDs from our old site.
+      $tag_results = $this->select('taxonomy_term_data', 't')
+        ->fields('t', ['name'])
+        ->condition('t.tid', $tag_tids, 'IN')
+        ->execute();
+      $tags = [];
+      foreach ($tag_results as $result) {
+        $tag_name = $result['name'];
+        // Check if we have a mapping. If we don't yet,
+        // then create a new tag and add it to our map.
+        if (!isset($this->tagMapping[$tag_name])) {
+          $term = Term::create([
+            'name' => $tag_name,
+            'vid' => 'tags',
+          ]);
+          if ($term->save()) {
+            $this->tagMapping[$tag_name] = $term->id();
+          }
+        }
+
+        // Add the mapped TID to match our tag name.
+        $tags[] = $this->tagMapping[$tag_name];
+
+      }
+      $row->setSourceProperty('tags', $tags);
+    }
+
     return TRUE;
   }
 
+  /**
+   * Helper function to check for existing tags and create if they don't exist.
+   */
+  private function createTag($tag_name) {
+    // Check if we have a mapping. If we don't yet,
+    // then create a new tag and add it to our map.
+    if (!isset($this->tagMapping[$tag_name])) {
+      $term = Term::create([
+        'name' => $tag_name,
+        'vid' => 'tags',
+      ]);
+      if ($term->save()) {
+        $this->tagMapping[$tag_name] = $term->id();
+      }
+    }
+
+    // Return tid for mapping to field.
+    return $this->tagMapping[$tag_name];
+  }
 }
