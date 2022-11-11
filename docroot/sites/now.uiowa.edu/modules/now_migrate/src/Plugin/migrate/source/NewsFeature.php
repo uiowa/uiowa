5ebb7f8d4cda76cc153885aa85423c9f7bee6f96
Cory Skeers <cory-skeers@uiowa.edu>
2022-11-11 08:21:00 -0600

    Stubbing out the trio

---
+++ docroot/sites/now.uiowa.edu/modules/now_migrate/src/Plugin/migrate/source/NewsFeature.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\now_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
+use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
+use Drupal\taxonomy\Entity\Term;
+
+/**
+ * Migrate Source plugin.
+ *
+ * @MigrateSource(
+ *   id = "now_news_feature",
+ *   source_module = "node"
+ * )
+ */
+class NewsFeature extends BaseNodeSource {
+  use ProcessMediaTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    parent::prepareRow($row);
+
+    // Process the image field.
+    $image = $row->getSourceProperty('field_image');
+
+    if (!empty($image)) {
+      $fid = $this->processImageField($image[0]['fid'], $image[0]['alt'], $image[0]['title']);
+      $row->setSourceProperty('field_image', $fid);
+    }
+
+    return TRUE;
+  }
+
+}
