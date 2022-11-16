40452142a6fa8300d41f249fa701726f8363f55b
Bennett, Mark D <mark-bennett@uiowa.edu>
2022-11-16 11:58:12 -0600

    Initialize hhp.uiowa.edu multisite on uiowa04

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4624,3 +4624,9 @@ $sites['gwss.uiowa.ddev.site'] = 'gwss.uiowa.edu';
 $sites['gwss.dev.drupal.uiowa.edu'] = 'gwss.uiowa.edu';
 $sites['gwss.stage.drupal.uiowa.edu'] = 'gwss.uiowa.edu';
 $sites['gwss.prod.drupal.uiowa.edu'] = 'gwss.uiowa.edu';
+
+// Directory aliases for hhp.uiowa.edu.
+$sites['hhp.uiowa.ddev.site'] = 'hhp.uiowa.edu';
+$sites['hhp.dev.drupal.uiowa.edu'] = 'hhp.uiowa.edu';
+$sites['hhp.stage.drupal.uiowa.edu'] = 'hhp.uiowa.edu';
+$sites['hhp.prod.drupal.uiowa.edu'] = 'hhp.uiowa.edu';
