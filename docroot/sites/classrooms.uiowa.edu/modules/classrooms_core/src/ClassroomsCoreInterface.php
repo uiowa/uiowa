77c4d13aac3d202d8298a52df5eb794aa8cfca62
Benjamin Speare <benjamin-speare@uiowa.edu>
2022-11-21 12:18:53 -0600

    Added drush generated code for config entity

---
+++ docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/ClassroomsCoreInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\classrooms_core;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface defining a classrooms core entity type.
+ */
+interface ClassroomsCoreInterface extends ConfigEntityInterface {
+
+}
---
+++ docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/Form/ClassroomsCoreForm.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\classrooms_core\Form;
+
+use Drupal\Core\Entity\EntityForm;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Classrooms core form.
+ *
+ * @property \Drupal\classrooms_core\ClassroomsCoreInterface $entity
+ */
+class ClassroomsCoreForm extends EntityForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+
+    $form = parent::form($form, $form_state);
+
+    $form['label'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Label'),
+      '#maxlength' => 255,
+      '#default_value' => $this->entity->label(),
+      '#description' => $this->t('Label for the classrooms core.'),
+      '#required' => TRUE,
+    ];
+
+    $form['id'] = [
+      '#type' => 'machine_name',
+      '#default_value' => $this->entity->id(),
+      '#machine_name' => [
+        'exists' => '\Drupal\classrooms_core\Entity\ClassroomsCore::load',
+      ],
+      '#disabled' => !$this->entity->isNew(),
+    ];
+
+    $form['status'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enabled'),
+      '#default_value' => $this->entity->status(),
+    ];
+
+    $form['description'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Description'),
+      '#default_value' => $this->entity->get('description'),
+      '#description' => $this->t('Description of the classrooms core.'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $result = parent::save($form, $form_state);
+    $message_args = ['%label' => $this->entity->label()];
+    $message = $result == SAVED_NEW
+      ? $this->t('Created new classrooms core %label.', $message_args)
+      : $this->t('Updated classrooms core %label.', $message_args);
+    $this->messenger()->addStatus($message);
+    $form_state->setRedirectUrl($this->entity->toUrl('collection'));
+    return $result;
+  }
+
+}
