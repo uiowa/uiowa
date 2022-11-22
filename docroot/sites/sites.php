5326328f984ed89b70c3e2b6b5494b43d3d09c41
Marie Wakeman <elsie-wakeman@uiowa.edu>
2022-11-22 10:23:31 -0600

    Initialize hamann.lab.uiowa.edu multisite on uiowa04

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -4804,3 +4804,9 @@ $sites['writingcenter.uiowa.ddev.site'] = 'writingcenter.uiowa.edu';
 $sites['writingcenter.dev.drupal.uiowa.edu'] = 'writingcenter.uiowa.edu';
 $sites['writingcenter.stage.drupal.uiowa.edu'] = 'writingcenter.uiowa.edu';
 $sites['writingcenter.prod.drupal.uiowa.edu'] = 'writingcenter.uiowa.edu';
+
+// Directory aliases for hamann.lab.uiowa.edu.
+$sites['labhamann.uiowa.ddev.site'] = 'hamann.lab.uiowa.edu';
+$sites['labhamann.dev.drupal.uiowa.edu'] = 'hamann.lab.uiowa.edu';
+$sites['labhamann.stage.drupal.uiowa.edu'] = 'hamann.lab.uiowa.edu';
+$sites['labhamann.prod.drupal.uiowa.edu'] = 'hamann.lab.uiowa.edu';
