305ead8468ab429c434cc971e657ff5bf3fd256e
Benjamin Speare <benjamin-speare@uiowa.edu>
2022-11-22 16:21:41 -0600

    Cleaned up code a bit

--- docroot/modules/custom/uiowa_maui/src/MauiApi.php
+++ docroot/modules/custom/uiowa_maui/src/MauiApi.php
@@ -300,12 +300,7 @@ class MauiApi {
   }
 
   /**
-   * Get room data based on building ID and room ID.
-   *
-   * @param string $building_id
-   *   The building id of the room.
-   * @param string $room_id
-   *   The room id of the room.
+   * Get complete building list.
    *
    * @return mixed
    *   The API response data.
