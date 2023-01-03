791477584a55bf7cff1800b0ae8333b811d923fc
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2023-01-03 08:42:40 -0600

    pushing tinkering from Friday

--- docroot/modules/custom/sitenow_microsite/src/Controller/SitenowMicrositeController.php
+++ docroot/modules/custom/sitenow_microsite/src/Controller/SitenowMicrositeController.php
@@ -3,7 +3,6 @@
 namespace Drupal\sitenow_microsite\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\node\Entity\Node;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -23,29 +22,24 @@ class SitenowMicrositeController extends ControllerBase {
    *   A renderable array for the page.
    */
   public function build($nid, $page): array {
-    $node = Node::load($nid);
-    $paths = [];
-    $title = $this->t('This works!');
-    $body = '';
+    $node_storage = $this->entityTypeManager()->getStorage('node');
+    $node = $node_storage->load($nid);
     foreach ($node->get('field_microsite_subpages') as $paragraph) {
       if ($paragraph->entity->getType() == 'subpage') {
         $subpage = $paragraph->entity;
         if (!empty($subpage)) {
           $path = $subpage->field_subpage_slug->value;
           if ($page == $path) {
-            $title = $subpage->field_subpage_title->value;
-            $body = $subpage->field_subpage_body->view('full');
+            $build['content']['title'] = [
+              '#type' => 'item',
+              '#markup' => '<h2>' . $subpage->field_subpage_title->value . '</h2>',
+            ];
+            $build['content']['paragraph'] = $paragraph->view();
           }
-          $paths[] = $path;
         }
       }
     }
-    if (in_array($page, $paths)) {
-      $build['content']['title'] = [
-        '#type' => 'item',
-        '#markup' => '<h2>' . $title . '</h2>',
-      ];
-      $build['content']['body'] = $body;
+    if (!empty($build)) {
       return $build;
     }
     else {
