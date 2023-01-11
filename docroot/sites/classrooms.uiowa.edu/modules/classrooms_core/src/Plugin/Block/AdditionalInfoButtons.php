d0da38f101bdc21e68ec468416f9a436035ad714
Makur Jain <makur-jain@uiowa.edu>
2023-01-11 14:06:00 -0600

    Added a block for the additional info buttons.

---
+++ docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/Plugin/Block/AdditionalInfoButtons.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\classrooms_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * A Request Info button block.
+ *
+ * @Block(
+ *   id = "requestinfobutton_block",
+ *   admin_label = @Translation("Request Info Block"),
+ *   category = @Translation("Site custom")
+ * )
+ */
+class AdditionalInfoButtons extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['label_display' => FALSE];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $markup = '<div class="layout-builder-block">
+      <a class="bttn bttn--full bttn--secondary" href="http://classroomscheduling.registrar.uiowa.edu/AdAstra7Prod/Portal/GuestPortal.aspx">
+          Check Availability <span class="fa-arrow-right fas"></span>
+      </a>
+      <a class="bttn bttn--full bttn--secondary" href="https://workflow.uiowa.edu/entry/new/667/">
+        Request this Room <span class="fa-arrow-right fas"></span>
+      </a>
+      <a class="bttn bttn--full bttn--secondary" href="https://classrooms.prod.drupal.uiowa.edu/classroom-assistance">
+        Report an Issue <span class="fa-arrow-right fas"></span>
+      </a>
+    </div>';
+
+    return ['#markup' => $markup];
+  }
+
+}
