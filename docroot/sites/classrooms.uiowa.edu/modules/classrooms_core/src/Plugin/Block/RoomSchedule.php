8204fe77c37e063298aa775550d39ecd48ef1f7d
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-01-23 13:25:10 -0600

    Moved node check up a line.

--- docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/Plugin/Block/RoomSchedule.php
+++ docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/Plugin/Block/RoomSchedule.php
@@ -31,47 +31,48 @@ class RoomSchedule extends BlockBase {
     $room_id = 0;
 
     $node = \Drupal::routeMatch()->getParameter('node');
-
-    if (!empty($node) && $node->hasField('field_room_building_id') && $node->hasField('field_room_room_id')) {
-      $building_id = $node->get('field_room_building_id')?->first()?->getValue()["target_id"];
-      $room_id = $node->get('field_room_room_id')?->first()?->getValue()["value"];
-    }
-
-    $cid = 'roomschedule_block:' . $building_id . ':' . $room_id;
-    if ($cache = \Drupal::cache()->get($cid)) {
-      $data = $cache->data;
-    }
-    else {
-      // Grab MAUI room data.
-      $maui_api = \Drupal::service('uiowa_maui.api');
-      $data = $maui_api->getRoomSchedule(date('Y-m-d'), date('Y-m-d'), $building_id, $room_id);
-
-      // Save data to cache.
-      \Drupal::cache()->set($cid, $data, time() + 3600, ['roomschedule_block']);
-    }
-
-    if (!empty($data)) {
-      $items = [];
-      // Iterate through the data and push the values to the $items array.
-      foreach ($data as $item) {
-        $items[] = $item;
+    if (!empty($node)) {
+      if ($node->hasField('field_room_building_id') && $node->hasField('field_room_room_id')) {
+        $building_id = $node->get('field_room_building_id')?->first()?->getValue()["target_id"];
+        $room_id = $node->get('field_room_room_id')?->first()?->getValue()["value"];
       }
-      return [
-        '#theme' => 'room_schedule_list',
-        '#items' => $items,
-        '#attached' => [
-          'library' => [
-            'classrooms_core/room_schedule_list',
+
+      $cid = 'roomschedule_block:' . $building_id . ':' . $room_id;
+      if ($cache = \Drupal::cache()->get($cid)) {
+        $data = $cache->data;
+      }
+      else {
+        // Grab MAUI room data.
+        $maui_api = \Drupal::service('uiowa_maui.api');
+        $data = $maui_api->getRoomSchedule(date('Y-m-d'), date('Y-m-d'), $building_id, $room_id);
+
+        // Save data to cache.
+        \Drupal::cache()->set($cid, $data, time() + 3600, ['roomschedule_block']);
+      }
+
+      if (!empty($data)) {
+        $items = [];
+        // Iterate through the data and push the values to the $items array.
+        foreach ($data as $item) {
+          $items[] = $item;
+        }
+        return [
+          '#theme' => 'room_schedule_list',
+          '#items' => $items,
+          '#attached' => [
+            'library' => [
+              'classrooms_core/room_schedule_list',
+            ],
           ],
-        ],
-      ];
-    }
-    else {
-      return [
-        '#prefix' => '<div class="room-schedule__no-results">',
-        '#suffix' => '</div>',
-        '#markup' => '<h2 class="h4 block__headline headline headline--serif headline--underline">Today\'s Schedule</h2><p class="room-schedule__no-results__message">There are no scheduled events for this room today.</p>',
-      ];
+        ];
+      }
+      else {
+        return [
+          '#prefix' => '<div class="room-schedule__no-results">',
+          '#suffix' => '</div>',
+          '#markup' => '<h2 class="h4 block__headline headline headline--serif headline--underline">Today\'s Schedule</h2><p class="room-schedule__no-results__message">There are no scheduled events for this room today.</p>',
+        ];
+      }
     }
   }
 
