8106a08ed3332f1d0cc3ecf9c5d0203f2d0fcd8c
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-02-16 12:42:00 -0600

    Started on student profile view in admissions.

---
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Entity/StudentProfile.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\admissions_core\Entity;
+
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\TeaserCardInterface;
+
+/**
+ * Provides an interface for student profile page entries.
+ */
+class StudentProfile extends NodeBundleBase implements TeaserCardInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    parent::buildCard($build);
+
+    // Process additional card mappings.
+    $this->mapFieldsToCardBuild($build, [
+      '#media' => 'field_student_profile_image',
+      '#subtitle' => 'field_student_profile_major',
+      '#meta' => [
+        'field_student_profile_state',
+        'field_student_profile_country',
+      ],
+    ]);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultCardStyles(): array {
+    $default_classes = [
+      ...parent::getDefaultCardStyles(),
+      'card_media_position' => '',
+      'media_border' => 'media--border',
+      'media_format' => 'media--circle',
+      'media_size' => 'media--medium',
+    ];
+
+    return $default_classes;
+  }
+
+}
