c91558a4ea404b0577ccab3ca23ed10bfe4f7650
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-02-16 16:05:51 -0600

    Added area of study bundle.

---
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Entity/AreaStudy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\admissions_core\Entity;
+
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\TeaserCardInterface;
+
+/**
+ * Provides an interface for student profile page entries.
+ */
+class AreaStudy extends NodeBundleBase implements TeaserCardInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    parent::buildCard($build);
+
+    // Process additional card mappings.
+  }
+
+}
