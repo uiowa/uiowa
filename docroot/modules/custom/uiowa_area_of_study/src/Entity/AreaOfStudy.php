25aa8d329bdd06b6a2e6a2167dafd4e10d1a7330
Cory Skeers <cory-skeers@uiowa.edu>
2023-02-17 08:13:17 -0600

    Add scaffolding for Area of Study content type (feature split).

---
+++ docroot/modules/custom/uiowa_area_of_study/src/Entity/AreaOfStudy.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\uiowa_area_of_study\Entity;
+
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\TeaserCardInterface;
+
+/**
+ * Provides an interface for article entries.
+ */
+class AreaOfStudy extends NodeBundleBase implements TeaserCardInterface {
+
+  /**
+   * If entity has link directly to source field.
+   *
+   * @var string|null
+   *   field name or null.
+   */
+  protected $source_link_direct = 'field_person_website_link_direct';
+
+  /**
+   * If entity has source link field.
+   *
+   * @var string|null
+   *   field name or null.
+   */
+  protected $source_link = 'field_person_website';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    parent::buildCard($build);
+
+    // Process additional card mappings.
+    $this->mapFieldsToCardBuild($build, [
+      '#subtitle' => 'field_person_position',
+      '#meta' => ['field_person_email', 'field_person_phone'],
+    ]);
+
+    // Handle link directly to source functionality.
+    $build['#url'] = $this->getNodeUrl();
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultCardStyles(): array {
+    $default_classes = [
+      ...parent::getDefaultCardStyles(),
+      'card_media_position' => 'card--layout-left',
+      'media_border' => 'media--border',
+      'media_format' => 'media--circle',
+      'media_size' => 'media--small',
+    ];
+
+    return $default_classes;
+  }
+
+}
