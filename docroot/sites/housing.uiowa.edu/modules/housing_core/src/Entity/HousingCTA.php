87c1770c39040fbfa139e1c42fc441170771bbeb
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-02-20 15:52:39 -0600

    Removed preprocess_paragraph function and moved to HousingCTA, deleted field templates and updated functionality through preprocess_field.

--- docroot/sites/housing.uiowa.edu/modules/housing_core/src/Entity/HousingCTA.php
+++ docroot/sites/housing.uiowa.edu/modules/housing_core/src/Entity/HousingCTA.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\housing_core\Entity;
 
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\uiowa_core\Entity\RendersAsCardInterface;
 use Drupal\uiowa_core\Entity\RendersAsCardTrait;
@@ -23,7 +24,7 @@ class HousingCTA extends Paragraph implements RendersAsCardInterface {
     $this->mapFieldsToCardBuild($build, [
       '#content' => 'field_housing_cta_description',
       '#media' => 'field_housing_cta_image',
-      '#title' => '	field_housing_cta_title',
+      '#title' => 'field_housing_cta_title',
     ]);
 
     $build['#url'] = $this->get('field_housing_cta_link')?->get(0)?->getUrl()?->toString();
@@ -35,9 +36,40 @@ class HousingCTA extends Paragraph implements RendersAsCardInterface {
    * {@inheritdoc}
    */
   public function getDefaultCardStyles(): array {
+    $admin_context = \Drupal::service('router.admin_context');
+    $ctaCardClasses = '';
+
+    if (!$admin_context->isAdminRoute()) {
+      $parent = $this->getParentEntity();
+      if ($parent instanceof ContentEntityInterface) {
+        $field_map = [
+          'field_residence_hall_cta' => 'card--centered',
+          'field_residence_hall_contact' => 'card--layout-left borderless',
+          'field_residence_hall_bldg_links' => 'card--layout-left borderless',
+        ];
+
+        $parent_fields = array_keys($field_map);
+
+        foreach ($parent_fields as $parent_field) {
+          if ($parent->hasField($parent_field)) {
+            foreach ($parent->get($parent_field)->getValue() as $item) {
+              if ($item['target_id'] === $this->id()) {
+                $ctaCardClasses = $field_map[$parent_field];
+                break 2;
+              }
+            }
+          }
+        }
+      }
+    }
+
     return [
       'headline_class' => 'headline--serif',
-      'styles' => 'bg--white',
+      'styles' => $ctaCardClasses,
+      'card_media_position' => '',
+      'media_border' => 'media--border',
+      'media_format' => 'media--circle',
+      'media_size' => 'media--small',
     ];
   }
 
