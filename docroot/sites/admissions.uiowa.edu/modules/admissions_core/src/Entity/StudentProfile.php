3975827f687bc4bdbb86c13c30a6e469c85ae3df
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-02-22 10:51:08 -0600

    Removed custom view check logic from StudentProfile.

--- docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Entity/StudentProfile.php
+++ docroot/sites/admissions.uiowa.edu/modules/admissions_core/src/Entity/StudentProfile.php
@@ -16,28 +16,14 @@ class StudentProfile extends NodeBundleBase implements RendersAsCardInterface {
   public function buildCard(array &$build) {
     parent::buildCard($build);
 
-    $current_route_name = \Drupal::routeMatch()->getRouteName();
-    $current_display_id = \Drupal::routeMatch()->getParameter('display_id');
-
-    if ($current_route_name == "view.student_profiles.page_1" && $current_display_id == 'page_1') {
-      $this->mapFieldsToCardBuild($build, [
-        '#media' => 'field_student_profile_image',
-        '#subtitle' => 'field_student_profile_major',
-        '#meta' => [
-          'field_person_hometown',
-        ],
-        '#content' => 'field_student_profile_blurb',
-      ]);
-    }
-    else {
-      $this->mapFieldsToCardBuild($build, [
-        '#media' => 'field_student_profile_image',
-        '#subtitle' => 'field_student_profile_major',
-        '#meta' => [
-          'field_person_hometown',
-        ],
-      ]);
-    }
+    $this->mapFieldsToCardBuild($build, [
+      '#media' => 'field_student_profile_image',
+      '#subtitle' => 'field_student_profile_major',
+      '#meta' => [
+        'field_person_hometown',
+      ],
+      '#content' => 'field_student_profile_blurb',
+    ]);
 
   }
 
