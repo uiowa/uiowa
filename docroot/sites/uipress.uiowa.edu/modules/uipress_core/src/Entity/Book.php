2536e9400b7171497fc5f9535bfe8eca2d5782c9
Cory Skeers <cory-skeers@uiowa.edu>
2023-02-22 09:00:02 -0600

    Scaffolding for uipress book teaser.

---
+++ docroot/sites/uipress.uiowa.edu/modules/uipress_core/src/Entity/Book.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\uipress_core\Entity;
+
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\RendersAsCardInterface;
+
+/**
+ * Provides an interface for book entries.
+ */
+class Book extends NodeBundleBase implements RendersAsCardInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    parent::buildCard($build);
+
+    // Process additional card mappings.
+    $this->mapFieldsToCardBuild($build, [
+//      '#subtitle' => 'field_person_position',
+      '#meta' => ['field_book_type'],
+    ]);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultCardStyles(): array {
+    $default_classes = [
+      ...parent::getDefaultCardStyles(),
+      'card_media_position' => 'card--layout-right',
+      'media_size' => 'media--medium',
+    ];
+
+    return $default_classes;
+  }
+
+}
