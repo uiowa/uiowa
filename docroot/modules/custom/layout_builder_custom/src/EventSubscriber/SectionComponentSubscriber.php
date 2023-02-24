015ef6d10792c82bf6e1765874e385773cd705ba
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2023-02-24 12:57:02 -0600

    Pre-card updates (#6203)
    * Copying over some non-card changes from the card PR to ease the review/testing process a little bit.
    
    * Updated composer.lock file.
    
    * Fixed issues with setMediaViewModeFromStyle implementation.
    
    * Pulled over some more config changes and admissions field template removals that seemed uncomplicated.
    
    * Adding service entry that was missing.
    
    * Removing stuff that will require a scheduled deployment.
    
    * Moving over more admissions field template changes, as well as uids_base base field template.
    
    * Pulling in the remaining field updates.
    
    * A fix for icon placement being off.
    
    ---------
    
    Co-authored-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>

--- docroot/modules/custom/layout_builder_custom/src/EventSubscriber/SectionComponentSubscriber.php
+++ docroot/modules/custom/layout_builder_custom/src/EventSubscriber/SectionComponentSubscriber.php
@@ -3,6 +3,8 @@
 namespace Drupal\layout_builder_custom\EventSubscriber;
 
 use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Render\PreviewFallbackInterface;
 use Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent;
 use Drupal\layout_builder\LayoutBuilderEvents;
@@ -40,8 +42,8 @@ class SectionComponentSubscriber implements EventSubscriberInterface {
     $build = $event->getBuild();
 
     if ($event->inPreview()) {
-      if ($block instanceof PreviewFallbackInterface && isset($build['content']) && isset($build['content']['#markup'])) {
-        $is_placeholder = (strpos($build['content']['#markup'], 'Placeholder for the ') === 0);
+      if ($block instanceof PreviewFallbackInterface && isset($build['content']['#markup'])) {
+        $is_placeholder = (str_starts_with($build['content']['#markup'], 'Placeholder for the '));
 
         if ($is_placeholder) {
           $build['#attributes']['class'][] = 'layout-builder-block--placeholder';
@@ -88,46 +90,16 @@ class SectionComponentSubscriber implements EventSubscriberInterface {
     if (isset($build['#plugin_id'])) {
       switch ($build['#plugin_id']) {
         case 'inline_block:uiowa_card':
-        case 'inline_block:uiowa_image':
-          if (isset($build['#attributes']['class'])) {
-            if ($build['#plugin_id'] === 'inline_block:uiowa_card') {
-              // Map the layout builder styles to the view mode to be used.
-              $media_formats = [
-                'media--circle' => 'large__square',
-                'media--square' => 'large__square',
-                'media--ultrawide' => 'large__ultrawide',
-                'media--widescreen' => 'large__widescreen',
-              ];
-            }
-            if ($build['#plugin_id'] === 'inline_block:uiowa_image') {
-              // Map the layout builder styles to the view mode to be used.
-              $media_formats = [
-                'media--circle' => 'full__square',
-                'media--square' => 'full__square',
-                'media--ultrawide' => 'full__ultrawide',
-                'media--widescreen' => 'full__widescreen',
-                'media--no-crop' => 'full__no_crop',
-              ];
-            }
+          // Map the layout builder styles to the view mode to be used.
+          if (isset($build['content']['field_uiowa_card_image']) && isset($build['#attributes']['class'])) {
+            $this->setMediaViewModeFromStyle($build['content']['field_uiowa_card_image'], 'large', $build['#attributes']['class']);
           }
+          break;
 
-          if (isset($media_formats)) {
-            // Loop through the map to check if any of them are being used and
-            // adjust the view mode accordingly.
-            foreach ($media_formats as $style => $view_mode) {
-              if (in_array($style, $build['#attributes']['class'])) {
-                // Change the view mode to match the format.
-                $build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#view_mode'] = $view_mode;
-                // Important: Delete the cache keys to prevent this from being
-                // applied to all the instances of the same image.
-                if (isset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']) && isset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']['keys'])) {
-                  unset($build['content']['field_' . $build['#derivative_plugin_id'] . '_image'][0]['#cache']['keys']);
-                }
-
-                // We only want this to execute once.
-                break;
-              }
-            }
+        case 'inline_block:uiowa_image':
+          // Map the layout builder styles to the view mode to be used.
+          if (isset($build['#attributes']['class'])) {
+            $this->setMediaViewModeFromStyle($build['content']['field_uiowa_image_image'], 'full', $build['#attributes']['class']);
           }
           break;
 
@@ -144,6 +116,36 @@ class SectionComponentSubscriber implements EventSubscriberInterface {
     }
 
     $event->setBuild($build);
+  }
+
+  /**
+   * Change the media view mode based on the selected format.
+   */
+  private function setMediaViewModeFromStyle(array &$build, $size, array $classes = []) {
+    $media_formats = [
+      'media--circle' => 'square',
+      'media--square' => 'square',
+      'media--ultrawide' => 'ultrawide',
+      'media--widescreen' => 'widescreen',
+    ];
+
+    // Loop through the map to check if any of them are being used and
+    // adjust the view mode accordingly.
+    foreach ($media_formats as $style => $shape) {
+      $view_mode = "{$size}__$shape";
+      if (in_array($style, $classes)) {
+        // Change the view mode to match the format.
+        $build[0]['#view_mode'] = $view_mode;
+        // Important: Delete the cache keys to prevent this from being
+        // applied to all the instances of the same image.
+        if (isset($build[0]['#cache']['keys'])) {
+          unset($build[0]['#cache']['keys']);
+        }
+
+        // We only want this to execute once.
+        break;
+      }
+    }
   }
 
 }
