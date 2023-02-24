25eece8cde84993d771d99556af989c5bda9fa30
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-02-24 14:06:46 -0600

    Added GradMentor bundle to grad_core.

---
+++ docroot/sites/grad.uiowa.edu/modules/grad_core/src/Entity/GradMentor.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\grad_core\Entity;
+
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\RendersAsCardInterface;
+
+/**
+ * Provides an interface for admissions.uiowa.edu mentor entries.
+ */
+class GradMentor extends NodeBundleBase implements RendersAsCardInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    parent::buildCard($build);
+
+    // Add the person library.
+    $build['#attached']['library'][] = 'uids_base/person';
+    // Add the media library.
+    $build['#attached']['library'][] = 'uids_base/media';
+
+    // Handle the case when there is no image.
+    if (empty($build['#media'])) {
+      $build['#media']['empty'] = [
+        '#markup' => '<div class="img--empty">&nbsp;</div>',
+      ];
+    }
+
+    // Process additional card mappings.
+    $this->mapFieldsToCardBuild($build, [
+      '#subtitle' => 'field_person_position',
+      '#meta' => [
+        'field_person_email',
+        'field_person_phone',
+      ],
+      '#content' => 'field_scholar_bio_headline',
+    ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultCardStyles(): array {
+    $default_classes = [
+      ...parent::getDefaultCardStyles(),
+      'card_media_position' => 'card--layout-left',
+      'styles' => 'borderless',
+      'headline_class' => 'headline--serif',
+      'media_border' => 'media--border',
+      'media_format' => 'media--circle',
+      'media_size' => 'media--small',
+    ];
+
+    return $default_classes;
+  }
+
+}
