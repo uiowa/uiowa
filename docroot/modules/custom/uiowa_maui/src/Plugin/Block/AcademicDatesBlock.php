e05900c5d4f9a3da9e628b02fb66af42f458bd89
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-04-12 15:48:38 -0500

    Removed margin css and added more-link class for button spacing to resolve https://github.com/uiowa/uiowa/issues/5565.

--- docroot/modules/custom/uiowa_maui/src/Plugin/Block/AcademicDatesBlock.php
+++ docroot/modules/custom/uiowa_maui/src/Plugin/Block/AcademicDatesBlock.php
@@ -335,7 +335,7 @@ class AcademicDatesBlock extends BlockBase implements ContainerFactoryPluginInte
         '#type' => 'link',
         '#url' => Url::fromUri($more_link),
         '#attributes' => [
-          'class' => ['bttn', 'bttn--primary'],
+          'class' => ['bttn', 'bttn--primary', 'more-link'],
         ],
       ];
     }
