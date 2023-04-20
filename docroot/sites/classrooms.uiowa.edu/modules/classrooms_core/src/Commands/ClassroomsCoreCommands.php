37cf1f0bff4f5d5275d711a636151d1bf205ba36
Cory Skeers <cory-skeers@uiowa.edu>
2023-04-20 16:57:51 -0500

    Start of room import drush command.

--- docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/Commands/ClassroomsCoreCommands.php
+++ docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/Commands/ClassroomsCoreCommands.php
@@ -2,11 +2,14 @@
 
 namespace Drupal\classrooms_core\Commands;
 
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\classrooms_core\Entity\Building;
 use Drupal\Core\Logger\LoggerChannelTrait;
 use Drupal\Core\Session\AccountSwitcherInterface;
 use Drupal\Core\Session\UserSession;
 use Drush\Commands\DrushCommands;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
 
 /**
  * A Drush commandfile.
@@ -107,6 +110,122 @@ class ClassroomsCoreCommands extends DrushCommands {
     else {
       $this->getLogger('classrooms_core')->notice('Bummer. No new buildings were created. Maybe next time.');
     }
+
+    // Switch user back.
+    $this->accountSwitcher->switchBack();
+  }
+
+  /**
+   * Triggers the classrooms rooms import.
+   *
+   * @command classrooms_core:rooms_import
+   * @aliases classrooms-rooms
+   * @usage classrooms_core:rooms_import
+   *  Ideally this is done as a crontab that is only run once a day.
+   */
+  public function importRooms() {
+    // Switch to the admin user to pass access check.
+    $this->accountSwitcher->switchTo(new UserSession(['uid' => 1]));
+
+    // Establish a count for message at the end.
+    $entities_updated = 0;
+
+    // Get existing room nodes.
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', 'room')
+      ->accessCheck(TRUE);
+    $entities = $query->execute();
+
+    // Retrieve building and room number values from existing nodes.
+    if ($entities) {
+      $storage = \Drupal::getContainer()->get('entity_type.manager')->getStorage('node');
+      $nodes = $storage->loadMultiple($entities);
+      $existing_nodes = [];
+      foreach ($nodes as $nid => $node) {
+        if ($node instanceof FieldableEntityInterface) {
+          if ($node->hasField('field_room_building_id') &&
+            !$node->get('field_building_number')->isEmpty() &&
+            $node->hasField('field_room_room_id') &&
+            !$node->get('field_room_room_id')->isEmpty()
+          ) {
+            $existing_nodes[$nid] = [
+              $node->get('field_room_building_id')->value,
+              $node->get('field_room_room_id')->value,
+            ];
+          }
+        }
+      }
+    }
+
+    // @todo Update this with classrooms-specific logic.
+    if ($data) {
+      $buildings = [];
+      foreach ($data as $building) {
+        $buildings[] = $building->buildingNumber;
+        // Get building number and check to see if existing node exists.
+        if (isset($existing_nodes) && $existing_nid = array_search($building->buildingNumber, $existing_nodes)) {
+          // If existing, update values if different.
+          $node = $storage->load($existing_nid);
+          if ($node instanceof NodeInterface) {
+            $changed = FALSE;
+            if ($node->get('title')->value !== $building->buildingCommonName) {
+              $node->set('title', $building->buildingCommonName);
+              $changed = TRUE;
+            }
+            // There is at least one building with a blank space instead of
+            // NULL for this value.
+            // @todo Remove if FM can clean up their source.
+            // https://github.com/uiowa/uiowa/issues/6084
+            if ($building->buildingAbbreviation === '') {
+              $building->buildingAbbreviation = NULL;
+            }
+            if ($node->get('field_building_abbreviation')->value !== $building->buildingAbbreviation) {
+              $node->set('field_building_abbreviation', $building->buildingAbbreviation);
+              $changed = TRUE;
+            }
+
+            if ($changed) {
+              $node->setNewRevision(TRUE);
+              $node->revision_log = 'Updated building from source';
+              $node->setRevisionCreationTime(REQUEST_TIME);
+              $node->setRevisionUserId(1);
+              $node->save();
+              $entities_updated++;
+            }
+          }
+        }
+        else {
+          // If not, create new.
+          $node = Node::create([
+            'type' => 'building',
+            'title' => $building->buildingCommonName,
+            'field_building_number' => $building->buildingNumber,
+            'field_building_abbreviation' => $building->buildingAbbreviation,
+          ]);
+          $node->enforceIsNew();
+          $node->save();
+          $entities_created++;
+        }
+      }
+
+      // Loop through to remove nodes that no longer exist in API data.
+      if ($entities) {
+        foreach ($existing_nodes as $nid => $existing_node) {
+          if (!in_array($existing_node, $buildings)) {
+            $node = $storage->load($nid);
+            $node->delete();
+            $entities_deleted++;
+          }
+        }
+      }
+    }
+
+    $arguments = [
+      '@created' => $entities_created,
+      '@updated' => $entities_updated,
+      '@deleted' => $entities_deleted,
+    ];
+    $this->getLogger('facilities_core')->notice('@created buildings were created, @updated updated, @deleted deleted. That is neat.', $arguments);
 
     // Switch user back.
     $this->accountSwitcher->switchBack();
