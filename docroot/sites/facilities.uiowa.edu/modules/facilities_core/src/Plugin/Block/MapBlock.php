82b2358e0b03ec9342a7a6ec6901552a161dd504
Marie Wakeman <elsie-wakeman@uiowa.edu>
2023-05-26 15:39:40 -0500

    Still hardcoded but now using variables.

--- docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/MapBlock.php
+++ docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/MapBlock.php
@@ -19,9 +19,11 @@ class MapBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $latitude = 41.66130018;
+    $longitude = -91.53614157;
     return [
       '#type' => 'inline_template',
-      '#template' => '<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="UI Buildings New" src="//uiadmin.maps.arcgis.com/apps/Embed/index.html?webmap=b8916ce59fb74e17893822f62b0db58c&center= -91.5361,41.6613&level=9&previewImage=false&scale=true&searchextent=false&disable_scroll=true&theme=light"></iframe>',
+      '#template' => '<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="UI Buildings New" src="//uiadmin.maps.arcgis.com/apps/Embed/index.html?webmap=b8916ce59fb74e17893822f62b0db58c&center= ' . $longitude . ',' . $latitude . '&level=9&previewImage=false&scale=true&searchextent=false&disable_scroll=true&theme=light"></iframe>',
     ];
   }
 
