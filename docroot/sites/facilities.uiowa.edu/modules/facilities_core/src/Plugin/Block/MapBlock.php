5da16dc5207ea1e10288146785c7d55b14a229e5
Marie Wakeman <elsie-wakeman@uiowa.edu>
2023-05-30 15:47:41 -0500

    Iframe only renders if building has lat and long values.

--- docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/MapBlock.php
+++ docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/MapBlock.php
@@ -27,11 +27,13 @@ class MapBlock extends BlockBase {
     $longitude = $node->get('field_building_longitude')->getString();
     $latitude = $node->get('field_building_latitude')->getString();
 
-    return [
-      '#type' => 'inline_template',
-      '#template' => '<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="UI Buildings New" src="//uiadmin.maps.arcgis.com/apps/Embed/index.html?webmap=b8916ce59fb74e17893822f62b0db58c&center= ' . $longitude . ',' . $latitude . '&level=9&previewImage=false&scale=true&searchextent=false&disable_scroll=true&theme=light"></iframe>',
-    ];
-
+    if ($longitude && $latitude) {
+      return [
+        '#type' => 'inline_template',
+        '#template' => '<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="UI Buildings New" src="//uiadmin.maps.arcgis.com/apps/Embed/index.html?webmap=b8916ce59fb74e17893822f62b0db58c&center= ' . $longitude . ',' . $latitude . '&level=9&previewImage=false&scale=true&searchextent=false&disable_scroll=true&theme=light"></iframe>',
+      ];
+    }
+    return [];
   }
 
 }
