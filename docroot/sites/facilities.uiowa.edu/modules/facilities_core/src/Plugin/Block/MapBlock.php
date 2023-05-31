97ecbb1502048deb437de746e67ba55562bbb797
Marie Wakeman <elsie-wakeman@uiowa.edu>
2023-05-31 13:11:08 -0500

    Added library load, iframe classes.

--- docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/MapBlock.php
+++ docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/MapBlock.php
@@ -30,7 +30,7 @@ class MapBlock extends BlockBase {
     if ($longitude && $latitude) {
       return [
         '#type' => 'inline_template',
-        '#template' => '<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="UI Buildings New" src="//uiadmin.maps.arcgis.com/apps/Embed/index.html?webmap=b8916ce59fb74e17893822f62b0db58c&center= ' . $longitude . ',' . $latitude . '&level=9&previewImage=false&scale=true&searchextent=false&disable_scroll=true&theme=light"></iframe>',
+        '#template' => '<iframe class="media--type-remote-video media--view-mode-large" title="UI Buildings New" src="//uiadmin.maps.arcgis.com/apps/Embed/index.html?webmap=b8916ce59fb74e17893822f62b0db58c&center= ' . $longitude . ',' . $latitude . '&level=9&previewImage=false&scale=true&searchextent=false&disable_scroll=true&theme=light"></iframe>',
       ];
     }
     return [];
