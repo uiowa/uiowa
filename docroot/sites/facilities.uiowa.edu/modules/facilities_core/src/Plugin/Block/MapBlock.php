d99666fd46ac57749dc3df9b3c85769af6a4f1d5
Marie Wakeman <elsie-wakeman@uiowa.edu>
2023-06-01 09:38:48 -0500

    Changed iframe title to match content title plus map.

--- docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/MapBlock.php
+++ docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/MapBlock.php
@@ -26,11 +26,12 @@ class MapBlock extends BlockBase {
 
     $longitude = $node->get('field_building_longitude')->getString();
     $latitude = $node->get('field_building_latitude')->getString();
+    $title = $node->get('title')->getString();
 
     if ($longitude && $latitude) {
       return [
         '#type' => 'inline_template',
-        '#template' => '<div class="block media--type-remote-video block-facilities-core block-map-block"><iframe title="UI Buildings New" src="//uiadmin.maps.arcgis.com/apps/Embed/index.html?webmap=b8916ce59fb74e17893822f62b0db58c&center= ' . $longitude . ',' . $latitude . '&level=9&previewImage=false&scale=true&searchextent=false&disable_scroll=true&theme=light"></iframe></div>',
+        '#template' => '<div class="block media--type-remote-video block-facilities-core block-map-block"><iframe title="' . $title . ' Map" src="//uiadmin.maps.arcgis.com/apps/Embed/index.html?webmap=b8916ce59fb74e17893822f62b0db58c&center= ' . $longitude . ',' . $latitude . '&level=9&previewImage=false&scale=true&searchextent=false&disable_scroll=true&theme=light"></iframe></div>',
         '#attached' => [
           'library' => [
             'uids_base/remote-video',
