c3e69521228ea5cee79f057c1d363337d53dbf09
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2023-06-20 13:24:33 -0500

    Removed the off-canvas drag handle functionality and styling.

--- docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
+++ docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
@@ -2,38 +2,6 @@
  * @file
  */
 (function ($, Drupal, drupalSettings, cookies) {
-    Drupal.behaviors.layoutBuilderCustomOverrides = {
-        attach: function (context, settings) {
-          $(window).once('off-canvas-overrides').on({
-            'dialog:aftercreate': function (event, dialog, $element) {
-              let justCreated = true;
-              if (Drupal.offCanvas.isOffCanvas($element) && $element.find('.layout-selection').length === 0) {
-                let offCanvasWidth;
-                const offCanvasCookie = cookies.get('ui_off_canvas_width');
-                if (offCanvasCookie === undefined) {
-                  offCanvasWidth = 500;
-                } else {
-                  offCanvasWidth = offCanvasCookie;
-                }
-                const container = $($element).parent()[0];
-                container.style.width = offCanvasWidth + 'px';
-
-                let eventData = { settings: settings, $element: $element, offCanvasDialog: Drupal.offCanvas };
-                const $container = Drupal.offCanvas.getContainer($element);
-                $element.on('dialogContentResize.off-canvas', eventData, function() {
-                  if (!justCreated) {
-                    // Cookie that expires in 99 years.
-                    const width = $container.outerWidth();
-                    cookies.set('ui_off_canvas_width', width, { expires: 36135, path: '/', domain: drupalSettings.layoutBuilderCustom.cookieDomain });
-                  }
-                  justCreated = false;
-                });
-              }
-            }
-          });
-        }
-    };
-
   // Allows saving filtered and minimal html content while Source is open.
   // Solution from https://www.drupal.org/project/drupal/issues/3095304.
   var origBeforeSubmit = Drupal.Ajax.prototype.beforeSubmit;
