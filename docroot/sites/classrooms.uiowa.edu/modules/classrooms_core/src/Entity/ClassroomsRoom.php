730b965b14b7730dcfb13225c6aeb5de5b78e390
Cory Skeers <cory-skeers@uiowa.edu>
2023-07-07 14:42:13 -0500

    Add 'number' property to building, update form to include.Update link constructions. Update buildings drush command to use new endpoint(s) and save number to the building entity.

--- docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/Entity/ClassroomsRoom.php
+++ docroot/sites/classrooms.uiowa.edu/modules/classrooms_core/src/Entity/ClassroomsRoom.php
@@ -69,7 +69,8 @@ class ClassroomsRoom extends NodeBundleBase implements RendersAsCardInterface {
 
     // Add the building link field value to the #meta array.
     $building = \Drupal::entityTypeManager()->getStorage('building')->load($building_id);
-    $url = Url::fromUri('base:/buildings/' . $building_id)->toString();
+    $number = $building->get('number');
+    $url = "https://www.facilities.uiowa.edu/building/{$number}";
     $build['#meta']['building_link'] = [
       '#theme' => 'building_link_teaser',
       '#weight' => -1,
