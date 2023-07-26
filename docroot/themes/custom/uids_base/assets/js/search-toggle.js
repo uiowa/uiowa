1768afc3c88b3f9cb4fe0bdaa5fa4fbdbc523cca
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-07-26 10:00:50 -0500

    Updated search-toggle.js to use Drupal behavior.

--- docroot/themes/custom/uids_base/assets/js/search-toggle.js
+++ docroot/themes/custom/uids_base/assets/js/search-toggle.js
@@ -1,50 +1,56 @@
-const wrapper = document.querySelector('.search-overlay');
-const body = document.body;
-const button = document.querySelector('button.search-button');
+class SearchOverlay {
+  constructor(context) {
+    this.context = context || document;
+    this.wrapper = this.context.querySelector('.search-overlay');
+    this.body = this.context.body;
+    this.button = this.context.querySelector('button.search-button');
+    this.searchButton = this.context.querySelector('.search-button');
 
-if (document.querySelector('.search-button')) {
-  document
-    .querySelector('.search-button')
-    .addEventListener('click', searchToggle);
-  document
-    .querySelector('.search-button')
-    .addEventListener('keydown', function (event) {
-      if (event.key == 'Escape') {
-        this.setAttribute('aria-expanded', 'false');
-        document
-          .querySelector('.search-overlay')
-          .setAttribute('aria-hidden', 'true');
+    if (this.searchButton) {
+      this.searchButton.addEventListener('click', this.searchToggle.bind(this));
+      this.searchButton.addEventListener('keydown', (event) => {
+        if (event.key == 'Escape') {
+          this.searchButton.setAttribute('aria-expanded', 'false');
+          this.wrapper.setAttribute('aria-hidden', 'true');
+        }
+      });
+    }
+
+    this.context.addEventListener('click', (event) => {
+      if (!event.target.closest('.search-wrapper')) {
+        if (this.context.getElementById('search-button-label')) {
+          this.body.classList.remove('search-is-open');
+          this.context.getElementById('search-button-label').innerHTML = 'Search';
+          this.wrapper.setAttribute('aria-hidden', 'true');
+          this.button.setAttribute('aria-expanded', 'false');
+        }
       }
     });
-}
+  }
 
-function searchToggle() {
-  if (document.getElementById('search-button-label')) {
-    if (body.classList.contains('search-is-open')) {
-      this.setAttribute('aria-expanded', 'false');
-      wrapper.setAttribute('aria-hidden', 'true');
-      body.classList.remove('search-is-open');
-    } else {
-      wrapper.setAttribute('aria-hidden', 'false');
-      this.setAttribute('aria-expanded', 'true');
-      body.classList.add('search-is-open');
+  searchToggle() {
+    if (this.context.getElementById('search-button-label')) {
+      if (this.body.classList.contains('search-is-open')) {
+        this.searchButton.setAttribute('aria-expanded', 'false');
+        this.wrapper.setAttribute('aria-hidden', 'true');
+        this.body.classList.remove('search-is-open');
+      } else {
+        this.wrapper.setAttribute('aria-hidden', 'false');
+        this.searchButton.setAttribute('aria-expanded', 'true');
+        this.body.classList.add('search-is-open');
+      }
     }
   }
 }
 
-// click outside of menu drawer
+(function ($, Drupal) {
+  'use strict';
 
-document.addEventListener(
-  'click',
-  function (event) {
-    if (!event.target.closest('.search-wrapper')) {
-      if (document.getElementById('search-button-label')) {
-        document.body.classList.remove('search-is-open');
-        document.getElementById('search-button-label').innerHTML = 'Search';
-        wrapper.setAttribute('aria-hidden', 'true');
-        button.setAttribute('aria-expanded', 'false');
-      }
+  Drupal.behaviors.searchOverlay = {
+    attach: function (context, settings) {
+      // Instantiate the SearchOverlay class to attach the behavior
+      new SearchOverlay(context);
     }
-  },
-  false
-);
+  };
+})(jQuery, Drupal);
+
