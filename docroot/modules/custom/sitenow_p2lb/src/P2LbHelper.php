f13575742dab521db4462dcd46df1d18824ad781
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2023-07-31 17:06:54 -0500

    Added comments to class file.

--- docroot/modules/custom/sitenow_p2lb/src/P2LbHelper.php
+++ docroot/modules/custom/sitenow_p2lb/src/P2LbHelper.php
@@ -5,10 +5,26 @@ namespace Drupal\sitenow_p2lb;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\sitenow_pages\Entity\Page;
 
+/**
+ * A helper class for P2LB.
+ */
 class P2LbHelper {
 
   use StringTranslationTrait;
-  public static function formattedTextIsEquivalent($text, $format_one, $format_two) {
+
+  /**
+   * Compare a string of text using two different formats.
+   *
+   * @param string $text
+   *   The text being tested.
+   * @param string $format_one
+   *   The first format.
+   * @param string $format_two
+   *   The second format.
+   *
+   * @return bool
+   */
+  public static function formattedTextIsEquivalent(string $text, string $format_one, string $format_two): bool {
     return check_markup($text, $format_one) === check_markup($text, $format_two);
   }
 }
