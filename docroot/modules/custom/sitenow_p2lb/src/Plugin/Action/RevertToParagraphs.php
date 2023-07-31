2c22f9143799ed1ece9d536e35dd12e78fca081e
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-07-31 16:04:05 -0500

    Start on action.

---
+++ docroot/modules/custom/sitenow_p2lb/src/Plugin/Action/RevertToParagraphs.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\sitenow_p2lb\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Action to revert page nodes back to Paragraphs.
+ *
+ * @Action(
+ *   id = "sitenow_p2lb_revert",
+ *   label = @Translation("Revert from V3 to V2"),
+ *   type = "node",
+ *   confirm = TRUE,
+ * )
+ */
+class RevertToParagraphs extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    /** @var \Drupal\node\NodeInterface $object */
+    $result = $object->access('update', $account, TRUE)
+      ->andIf($object->field_page_content_block->access('edit', $account, TRUE));
+
+    return $return_as_object ? $result : $result->isAllowed();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(ContentEntityInterface $entity = NULL) {
+  }
+
+}
