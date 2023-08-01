a87b1d1a7b7a0ebc013e4f5001552ebf85d3fd8d
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-08-01 11:38:15 -0500

    Adding code to get protected revision.

--- docroot/modules/custom/sitenow_p2lb/src/Plugin/Action/RevertToParagraphs.php
+++ docroot/modules/custom/sitenow_p2lb/src/Plugin/Action/RevertToParagraphs.php
@@ -33,7 +33,49 @@ class RevertToParagraphs extends ActionBase {
    * {@inheritdoc}
    */
   public function execute(ContentEntityInterface $entity = NULL) {
+    $nid = $entity->get('nid')?->getValue()[0]['value'] ?? NULL;
+    $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+
+    if (!$nid) {
+      return;
+    }
+
+    $most_recent_revision_id = $node_storage?->getLatestRevisionId($nid);
+
+    // Get the most recent revision.
+    $most_recent_revision = $node_storage?->loadRevision($most_recent_revision_id);
+
+    // Guard against not finding the most recent revision.
+    if (!$most_recent_revision) {
+      return;
+    }
+
+    // Get protected revision id from that revision's values.
+    $protected_revision_id = $most_recent_revision->field_v3_conversion_revision_id?->value;
+
+    // Guard against not finding the protected revision id.
+    if (!$protected_revision_id) {
+      return;
+    }
+
     $foo = $entity;
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
