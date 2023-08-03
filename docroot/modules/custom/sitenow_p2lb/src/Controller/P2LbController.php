a68815da4f70f41131cfbb6679a2b0a7efbe34b4
cory-skeers <49074566+cory-skeers@users.noreply.github.com>
2023-08-03 12:02:11 -0500

    P2LB reporting tool (#6708)
    * Fix magic button
    
    * Map the v2 carousel to the v3 image gallery. No solution for captions yet.
    
    * Quick check for if we successfully processed the section.
    
    * Setup handling for image-only cards. Added a helper function for block definitions.
    
    * Added no_border for list block articles.
    
    * Added style defaults for events block.
    
    * More blocks style updates.
    
    * Updated people block defaults.
    
    * Added missing value for blockquote.
    
    * Moving these changes to a separate pr for testing.
    
    * Moved the analyzer tool POC over from #6713.
    
    * Removed carousel updates.
    
    * Added scaffolding for having a page to display a report about a node's conversion status. Refactored existing reporting tool into the new tab. Added a warning to the node form.
    
    * Added conditions for carousel items. Added caching.
    
    * Prevent warning from being shown in individual paragraphs.
    
    * Fixed PHPCS errors.
    
    * Updates based on feedback.
    
    * Only check the excerpt if there is one. Refactored to consolidate form logic.
    
    * It's magic button time, y'all.
    
    * wrap messages in p tags to help with spacing
    
    ---------
    
    Co-authored-by: Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
    Co-authored-by: Benjamin Speare <benjamin-speare@uiowa.edu>
    Co-authored-by: bspeare <bspeare@users.noreply.github.com>
    Co-authored-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>

---
+++ docroot/modules/custom/sitenow_p2lb/src/Controller/P2LbController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\sitenow_p2lb\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\node\NodeInterface;
+use Drupal\sitenow_p2lb\P2LbHelper;
+use Drupal\sitenow_pages\Entity\Page;
+
+/**
+ * Returns responses for P2LB routes.
+ */
+class P2LbController extends ControllerBase {
+
+  /**
+   * Generates a status report for converting a node to V3.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   A node object.
+   *
+   * @return array
+   *   An array as expected by \Drupal\Core\Render\RendererInterface::render().
+   */
+  public function status(NodeInterface $node) {
+    $build['#title'] = $this->t('V3 Conversion status for %title', ['%title' => $node->label()]);
+
+    if ($node instanceof Page) {
+      if (is_numeric($node->field_v3_conversion_revision_id->value)) {
+        $build['done'] = [
+          '#markup' => $this->t('<p>This page has been converted.</p>'),
+        ];
+      }
+      else {
+        $build['not_done'] = [
+          '#markup' => $this->t('<p>This page has not been converted yet. You can convert it using the @link.</p>', [
+            '@link' => Link::fromTextAndUrl('SiteNow Converter tool', Url::fromRoute('sitenow_p2lb.content_converter'))->toString(),
+          ]),
+          '#weight' => 0,
+        ];
+        $issues = P2LbHelper::analyzeNode($node);
+
+        if (!empty($issues)) {
+          $build['issues'] = [
+            '#type' => 'container',
+            '#weight' => 1,
+          ];
+
+          $build['issues']['title'] = [
+            '#markup' => $this->t('<h2>Conversion Issues</h2><p>The following issues will not prevent this page from being converted to V3, but may result in a degraded version of the content. For more information, please refer to the <a href="https://sitenow.uiowa.edu/documentation/sitenow-v2-v3-conversion">related documentation</a>.</p>'),
+          ];
+
+          $report_issues = [];
+
+          foreach ($issues as $issue => $count) {
+            $report_issues[] = "{$this->t($issue)} ($count)";
+          }
+
+          $build['issues']['list'] = [
+            '#theme' => 'item_list',
+            '#items' => $report_issues,
+          ];
+        }
+        else {
+          $build['no_worries'] = [
+            '#markup' => $this->t('<p>This content is ready to be converted and we do not expect any issues.</p>'),
+          ];
+        }
+      }
+    }
+    else {
+      $build['not_applicable'] = [
+        '#markup' => $this->t('<p>The @type content type does not need to be converted to V3.</p>', [
+          '@type' => $node->getType(),
+        ]),
+      ];
+    }
+
+    return $build;
+  }
+
+}
--- docroot/modules/custom/sitenow_p2lb/src/P2LbHelper.php
+++ docroot/modules/custom/sitenow_p2lb/src/P2LbHelper.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\sitenow_p2lb;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\sitenow_pages\Entity\Page;
 
@@ -27,6 +28,101 @@ class P2LbHelper {
    */
   public static function formattedTextIsSame(string $text, string $format_one, string $format_two): bool {
     return check_markup($text, $format_one) == check_markup($text, $format_two);
+  }
+
+  /**
+   * Analyze a node to identify conversion issues.
+   *
+   * @param \Drupal\sitenow_pages\Entity\Page $page
+   *   The page node being analyzed.
+   *
+   * @return array
+   *   The list of issues.
+   */
+  public static function analyzeNode(Page $page) {
+    // Check the cache first.
+    $cid = "sitenow_p2lb_node_status:{$page->id()}";
+    if ($item = \Drupal::cache()->get($cid)) {
+      return $item->data;
+    }
+    $issues = [];
+    // Add the node cache tags for invalidation.
+    $cache_tags = $page->getCacheTags();
+    /** @var \Drupal\entity_reference_revisions\EntityReferenceRevisionsFieldItemList $section_field */
+    $section_field = $page->field_page_content_block;
+    $sections = $section_field?->referencedEntities();
+    if (!is_null($sections)) {
+      /**
+       * @var int $section_delta
+       * @var \Drupal\paragraphs\ParagraphInterface $section
+       */
+      foreach ($sections as $section_delta => $section) {
+        /** @var \Drupal\entity_reference_revisions\EntityReferenceRevisionsFieldItemList $components_field */
+        $components_field = $section->field_section_content_block;
+        $components = $components_field->referencedEntities();
+        /**
+         * @var int $component_delta
+         * @var \Drupal\paragraphs\ParagraphInterface $component
+         */
+        foreach ($components as $component_delta => $component) {
+          switch ($component->getType()) {
+            case 'card':
+              // Check if card has a title.
+              $label = $component->field_card_title?->value;
+              if (!$label) {
+                static::addIssue($issues, 'Contains cards with no label, which is required for V3. Affected cards will be converted to text areas.');
+              }
+              // Card body isn't required. Check or set to array with empty value.
+              $excerpt = $component->field_card_body?->value;
+              if ($excerpt && !static::formattedTextIsSame($excerpt, 'filtered_html', 'minimal')) {
+                static::addIssue($issues, 'Contains cards with content that uses markup not allowed in V3. Affected cards will be converted to text areas.');
+              }
+              // Add the paragraph cache tags for invalidation.
+              $cache_tags = Cache::mergeTags($cache_tags, $component->getCacheTags());
+              break;
+
+            case 'carousel':
+              /** @var \Drupal\entity_reference_revisions\EntityReferenceRevisionsFieldItemList $carousel_items_field */
+              $carousel_items_field = $component->field_carousel_item;
+              $carousel_items = $carousel_items_field->referencedEntities();
+              foreach ($carousel_items as $carousel_item) {
+                // Cases for carousel image ID and caption being set.
+                $caption = $carousel_item->field_carousel_image_caption?->value;
+                if ($caption) {
+                  static::addIssue($issues, 'Contains carousel items with a caption. The caption will not be converted.');
+                }
+                $html_id = $carousel_item->field_uip_id?->value;
+                if ($html_id) {
+                  static::addIssue($issues, 'Contains a carousel items with an ID. The ID will not be converted.');
+                }
+                // Add the paragraph cache tags for invalidation.
+                $cache_tags = Cache::mergeTags($cache_tags, $component->getCacheTags());
+              }
+              break;
+
+          }
+        }
+      }
+    }
+
+    \Drupal::cache()->set($cid, $issues, Cache::PERMANENT, $page->getCacheTags());
+
+    return $issues;
+  }
+
+  /**
+   * Add an issue to the issues array.
+   *
+   * @param array $issues
+   *   The issues array.
+   * @param string $issue
+   *   The issue being added.
+   */
+  protected static function addIssue(array &$issues, string $issue) {
+    if (!isset($issues[$issue])) {
+      $issues[$issue] = 0;
+    }
+    $issues[$issue]++;
   }
 
 }
