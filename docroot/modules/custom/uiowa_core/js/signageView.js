bdd0f9bf8e87300285c2ced629460a828d089c14
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-09-15 15:56:43 -0500

    Cool transitions, titles, etc.

--- docroot/modules/custom/uiowa_core/js/signageView.js
+++ docroot/modules/custom/uiowa_core/js/signageView.js
@@ -1,24 +1,51 @@
 document.addEventListener('DOMContentLoaded', doSomething, false);
 
+
+let slideIndex = 0;
+let title;
+let slides;
+let transitionTime = 10; // In seconds.
+
 function doSomething () {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const isSignage = urlParams.get('signage');
+  initSlides();
   if (isSignage !== null) {
     document.querySelector('html').classList.add('signage-view');
-    showSlides(slideIndex);
+    setTimeout(showSlides, (transitionTime * 1000));
+  }
+  else {
+    // console.log(title, slides);
   }
 }
 
-let slideIndex = 0;
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("layout__container");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
   slideIndex++;
+  for (i = 0; i < slides.length; i++) {
+    if (slideIndex-1 !== i) {
+      slides[i].classList.remove('show-slide');
+    }
+  }
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  slides[slideIndex-1].classList.add('show-slide');
+  setTimeout(showSlides, (transitionTime * 1000)); // Change image every 2 seconds
+}
+
+function initSlides() {
+  title = document.querySelector('.layout--title--with-background, .layout__container.layout--title');
+  title ? title.classList.add('signage-title') : '';
+
+  slides = document.querySelectorAll('.layout__container:not(.layout--title)');
+  slides.forEach(function(slide, index) {
+    slide.classList.add('signage-slide');
+    if (index === 0) {
+      slide.classList.add('show-slide');
+    }
+    if (title && title !== null) {
+      slide.classList.add('signage--has-title');
+    }
+  });
+
 }
