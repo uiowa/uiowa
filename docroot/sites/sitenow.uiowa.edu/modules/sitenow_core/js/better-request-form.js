3023eeefeb35dfe71567465a24fa97a042e5cb43
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2023-09-22 14:27:08 -0500

    update after move to v3

--- docroot/sites/sitenow.uiowa.edu/modules/sitenow_core/js/better-request-form.js
+++ docroot/sites/sitenow.uiowa.edu/modules/sitenow_core/js/better-request-form.js
@@ -11,20 +11,20 @@
    */
   Drupal.behaviors.betterRequestForm = {
     attach: function (context, settings) {
-      $(once('betterRequestForm', '#edit-request-type input', context)).click(function (e) {
+      $(once('betterRequestForm', '#edit-request-type--2 input', context)).click(function (e) {
         let val = $(this).val();
 
         if (val === 'New') {
-          $('#edit-uri--description').html(Drupal.t('The URL you would like for the new site.'));
-          $('#edit-additional-comments-or-questions--description').html(Drupal.t('Enter any questions or comments you have regarding this site request.'));
+          $('#edit-uri--2--description').html(Drupal.t('The URL you would like for the new site.'));
+          $('#edit-additional-comments-or-questions--2--description').html(Drupal.t('Enter any questions or comments you have regarding this site request.'));
         }
         else if (val === 'Existing') {
-          $('#edit-uri--description').html(Drupal.t('The URL of your existing site.'));
-          $('#edit-additional-comments-or-questions--description').html(Drupal.t('Enter any questions or comments you have regarding this site request.'));
+          $('#edit-uri--2--description').html(Drupal.t('The URL of your existing site.'));
+          $('#edit-additional-comments-or-questions--2--description').html(Drupal.t('Enter any questions or comments you have regarding this site request.'));
         }
         else if (val === 'Approval') {
-          $('#edit-uri--description').html(Drupal.t('The URL you would like to seek UI Hostmaster approval for.'));
-          $('#edit-additional-comments-or-questions--description').html(Drupal.t('Please provide details about the requested domain to help expedite the approval process.'));
+          $('#edit-uri--2--description').html(Drupal.t('The URL you would like to seek UI Hostmaster approval for.'));
+          $('#edit-additional-comments-or-questions--2--description').html(Drupal.t('Please provide details about the requested domain to help expedite the approval process.'));
         }
       })
     }
