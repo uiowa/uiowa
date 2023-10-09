42a4c964d1a15905897ee0e67e38363a29fdcef6
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-10-09 10:34:04 -0500

    Trying to refactor.

--- docroot/modules/custom/uiowa_core/js/signageView.js
+++ docroot/modules/custom/uiowa_core/js/signageView.js
@@ -3,42 +3,57 @@ document.addEventListener('DOMContentLoaded', doSomething, false);
 
 let slideIndex = 0;
 let title;
-let slides;
+let sections;
 let transitionTime = 6; // In seconds.
+let signData = {};
 
 function doSomething () {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const isSignage = urlParams.get('signage');
-  initSlides();
   if (isSignage !== null) {
+    initSections();
     document.querySelector('html').classList.add('signage-view');
-    showSlides();
+    showSections();
   }
   else {
-    // console.log(title, slides);
+    // console.log(title, sections);
   }
 }
 
-function showSlides() {
+function showSections() {
   let i;
   slideIndex++;
-  for (i = 0; i < slides.length; i++) {
+  for (i = 0; i < sections.length; i++) {
     if (slideIndex-1 !== i) {
-      slides[i].classList.remove('show-slide');
+      sections[i].classList.remove('show-slide');
     }
   }
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].classList.add('show-slide');
-  setTimeout(showSlides, (transitionTime * 1000)); // Change image every 2 seconds
+  if (slideIndex > sections.length) {slideIndex = 1}
+  sections[slideIndex-1].classList.add('show-slide');
+  setTimeout(showSections, (transitionTime * 1000)); // Change image every 2 seconds
 }
 
-function initSlides() {
+function initSections() {
   title = document.querySelector('.layout--title--with-background, .layout__container.layout--title');
   title ? title.classList.add('signage-title') : '';
 
-  slides = document.querySelectorAll('.layout__container:not(.layout--title)');
-  slides.forEach(function(slide, index) {
+  sections = document.querySelectorAll('.layout__container:not(.layout--title)');
+  sections.forEach(function(slide, index) {
+    const spacingContainer = slide.querySelector('.layout__spacing_container');
+    const childrenCount = spacingContainer.childElementCount;
+    console.log(spacingContainer);
+    console.log(childrenCount);
+
+    for (let i = 0; i < childrenCount; i++) {
+      if (!(i in signData)) {
+        signData[i] = [];
+      }
+      signData[i].push(spacingContainer.children[i]);
+    }
+    console.log(signData);
+    console.log('______________');
+
     slide.classList.add('signage-slide');
     if (index === 0) {
       slide.classList.add('show-slide');
