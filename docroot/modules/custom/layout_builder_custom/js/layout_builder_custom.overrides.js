b08ef3832363170ac0693a9aab99531b006f2f0e
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-10-25 16:51:52 -0500

    This still isn`t right, but we can make an issue for getting it more right.

--- docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
+++ docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
@@ -5,6 +5,7 @@
   let handle;
   let mainContent;
   let offCanvas;
+  let body;
   let interacting = false;
 
   Drupal.behaviors.layoutBuilderCustomOverrides = {
@@ -17,34 +18,42 @@
       }
       $(window).on({
         'dialog:aftercreate': function (event, dialog, $element) {
+          // This gets the proper elements for the drag handle fix.
+          body = document.querySelector('body');
+          handle = document.querySelector('.ui-resizable-handle.ui-resizable-w');
+          mainContent = document.querySelector('.dialog-off-canvas-main-canvas');
+          offCanvas = handle.parentElement;
+
           let justCreated = true;
-          if (Drupal.offCanvas.isOffCanvas($element) && $element.find('.layout-selection').length === 0) {
+
+          if (Drupal.offCanvas.isOffCanvas($element) && $element.find('.layout-selection').length === 1) {
             let offCanvasWidth;
             const offCanvasCookie = cookies.get('ui_off_canvas_width');
+            console.log(offCanvasCookie);
+            console.log(offCanvasCookie === undefined);
             if (offCanvasCookie === undefined) {
               offCanvasWidth = 500;
             } else {
               offCanvasWidth = offCanvasCookie;
             }
-            const container = $($element).parent()[0];
-            container.style.width = offCanvasWidth + 'px';
+            console.log(offCanvasWidth);
+
+            body.style.setProperty('--off-canvas-width', offCanvasWidth + 'px');
 
             let eventData = { settings: settings, $element: $element, offCanvasDialog: Drupal.offCanvas };
-            const $container = Drupal.offCanvas.getContainer($element);
-            $element.on('dialogContentResize.off-canvas', eventData, function() {
+            $element.parent().on('dialogContentResize.off-canvas', eventData, function() {
+              console.log('recalculating')
               if (!justCreated) {
+                console.log('I`m in');
                 // Cookie that expires in 99 years.
-                const width = $container.outerWidth();
-                cookies.set('ui_off_canvas_width', width, { expires: 36135, path: '/', domain: drupalSettings.layoutBuilderCustom.cookieDomain });
+                const width = offCanvas.getBoundingClientRect().width
+                cookies.set('ui_off_canvas_width', width, { expires: 36135, path: '/', domain: 'drupalSettings.layoutBuilderCustom.cookieDomain' });
               }
               justCreated = false;
             });
           }
+          dragHandleBehaviorStopgap(true);
 
-          // This gets the proper elements for the drag handle fix.
-          handle = document.querySelector('.ui-resizable-handle.ui-resizable-w');
-          mainContent = document.querySelector('.dialog-off-canvas-main-canvas');
-          offCanvas = handle.parentElement;
           handle.addEventListener('mousedown', function(event) {
             dragHandleBehaviorStopgapAwait(event);
           });
@@ -83,20 +92,26 @@
     }
     interacting = true;
     document.addEventListener('mousemove', function(event) {
-      dragHandleBehaviorStopgap(event);
+      dragHandleBehaviorStopgap();
     });
     handle.addEventListener('mouseup', function(event) {
       dragHandleResetEvents(event);
     });
   }
-  function dragHandleBehaviorStopgap(event) {
-    mainContent.style.paddingRight = (parseFloat(offCanvas.style.width) + 13) + 'px';
+  function dragHandleBehaviorStopgap(init = false) {
+    if (init) {
+      body.style.setProperty('--off-canvas-width', parseFloat(offCanvas.getBoundingClientRect().width) + 'px');
+      offCanvas.style.width = parseFloat(offCanvas.getBoundingClientRect().width) + 'px';
+    }
+    else {
+      body.style.setProperty('--off-canvas-width', (parseFloat(offCanvas.style.width)) + 'px');
+    }
   }
 
   function dragHandleResetEvents(event) {
 
     document.removeEventListener('mousemove', function(event) {
-      dragHandleBehaviorStopgap(event);
+      dragHandleBehaviorStopgap();
     });
     handle.removeEventListener('mouseup', function(event) {
       dragHandleResetEvents(event);
