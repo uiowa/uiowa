33c568a7d435a5f7e8ed33d688a143b0a82344a6
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2023-11-09 21:12:44 -0600

    initial work to purge items for specific feeds

---
+++ docroot/modules/custom/uiowa_aggregator/src/ItemsImporterOverride.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\uiowa_aggregator;
+
+use Drupal\aggregator\FeedInterface;
+use Drupal\aggregator\ItemsImporter;
+use Drupal\aggregator\Plugin\AggregatorPluginManager;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ *
+ */
+class ItemsImporterOverride extends ItemsImporter {
+
+  /**
+   * Original service object.
+   *
+   * @var \Drupal\aggregator\ItemsImporter
+   */
+  protected $itemsImporter;
+
+  /**
+   *
+   */
+  public function __construct(
+    ItemsImporter $itemsImporter,
+    ConfigFactoryInterface $configFactory,
+    AggregatorPluginManager $fetcherManager,
+    AggregatorPluginManager $parserManager,
+    AggregatorPluginManager $processorManager,
+    LoggerInterface $logger,
+    KeyValueFactoryInterface $keyValue,
+  ) {
+    $this->itemsImporter = $itemsImporter;
+    parent::__construct($configFactory, $fetcherManager, $parserManager, $processorManager, $logger, $keyValue);
+  }
+
+  /**
+   *
+   */
+  public function refresh(FeedInterface $feed) {
+    //if ($feed->get('purge_items')?->value) {
+      $existingItems = \Drupal::entityTypeManager()->getStorage('aggregator_item')->loadByProperties(['fid' => $feed->id()]);
+
+      foreach ($existingItems as $item) {
+        $item->delete();
+      }
+    //}
+    parent::refresh($feed);
+  }
+
+}
