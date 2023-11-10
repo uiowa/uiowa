37d9022cfa5753d89824fa576e8946ddc07a5684
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-11-10 09:26:33 -0600

    Updated Artwork teaser to display artist and year in subtitle of card.

--- docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Entity/Artwork.php
+++ docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Entity/Artwork.php
@@ -19,12 +19,32 @@ class Artwork extends NodeBundleBase implements RendersAsCardInterface {
     // Process additional card mappings.
     $this->mapFieldsToCardBuild($build, [
       '#content' => 'field_artwork_status',
-      '#subtitle' => 'field_artwork_year',
-      '#meta' => ['field_artwork_artist'],
     ]);
 
-    $build['#link_indicator'] = TRUE;
+    // Combine 'field_artwork_artist' and 'field_artwork_year' into '#subtitle'.
+    $artist_field = $this->get('field_artwork_artist');
+    $year_field = $this->get('field_artwork_year');
 
+    if (!empty($artist_field) && !empty($year_field)) {
+      // Retrieving the entity reference target ID.
+      $artist_target_id = $artist_field->getString();
+
+      // Load the referenced "person" entity.
+      $artist_entity = \Drupal::entityTypeManager()->getStorage('node')->load($artist_target_id);
+
+      if (!empty($artist_entity) && $artist_entity->getType() === 'person') {
+        // Access the title or name of the referenced "person" entity directly.
+        // Use 'label()' method to get the title/name.
+        $artist_name = $artist_entity->label();
+
+        $year_value = $year_field->getString();
+
+        if (!empty($artist_name) && !empty($year_value)) {
+          $subtitle = $artist_name . ', ' . $year_value;
+          $build['#subtitle'] = $subtitle;
+        }
+      }
+    }
   }
 
   /**
