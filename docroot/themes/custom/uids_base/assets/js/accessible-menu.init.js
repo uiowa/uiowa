69a04804b4f8091246adce65ed681da5eb7ea3f2
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-12-06 10:29:59 -0600

    Added a fix to ensure accessible-menu only runs once.

--- docroot/themes/custom/uids_base/assets/js/accessible-menu.init.js
+++ docroot/themes/custom/uids_base/assets/js/accessible-menu.init.js
@@ -1,46 +1,49 @@
-(function ($, AccessibleMenu) {'use strict';
+(function ($, AccessibleMenu) {
+  'use strict';
+
   Drupal.behaviors.accessible_menu = {
     attach: function (context, settings) {
-      const menus = context.querySelectorAll('.menu-wrapper--horizontal > .menu');
-
-      // Bail early if this isn't relevant.
-      if (menus === null) {
-        return false;
+      // Ensure the script runs only once.
+      if (Drupal.behaviors.accessible_menu.executed) {
+        return;
       }
 
-      // Loop through the menus.
-      menus.forEach(function(menuElement) {
+      // Find all elements with class 'menu-wrapper--horizontal'.
+      const menuWrappers = context.querySelectorAll('.menu-wrapper--horizontal');
+
+      // Loop through each menu wrapper.
+      menuWrappers.forEach(function (menuWrapper) {
+        // Find the menu element inside each menu wrapper.
+        const menuElement = menuWrapper.querySelector('.menu');
+
+        // Bail early if the menu element is not found.
+        if (!menuElement) {
+          return false;
+        }
 
         // Add mobile toggle button
-        const menuBlock = context.querySelector('.menu-wrapper--horizontal');
         const toggleBtn = context.createElement('button');
         toggleBtn.setAttribute('id', 'main-menu-toggle');
         toggleBtn.setAttribute('aria-label', 'Toggle secondary menu');
-        let block_name = 'menu_block:main';
-        let block_title = drupalSettings.block_title[block_name];
-        // Create a new span element and append the text to it
         const span = context.createElement('span');
-        span.textContent = block_title + ' Menu';
-
-        // Append the span element to the toggleBtn button element
+        span.textContent = 'Section Menu';
         toggleBtn.appendChild(span);
-        menuBlock.insertAdjacentElement('afterbegin', toggleBtn);
+        menuWrapper.insertAdjacentElement('afterbegin', toggleBtn);
 
         // Find all menu items that can be displayed.
         const expandableMenuItems = menuElement.querySelectorAll('li.menu-item--expanded > a, li.menu-item--expanded > span');
 
-        // Add buttons to toggle menus. Buttons are generated here to support
-        // no JS and sub-menus displayed by default.
-        expandableMenuItems.forEach(function(menuItem) {
+        // Add buttons to toggle menus.
+        expandableMenuItems.forEach(function (menuItem) {
           let btn = context.createElement('button');
           btn.setAttribute('aria-expanded', 'false');
           btn.type = 'button';
           btn.setAttribute('aria-controls', 'id_' + menuItem.innerText.toLowerCase() + '_menu');
           btn.ariaLabel = 'More ' + menuItem.innerText + ' pages';
           menuItem.parentNode.insertBefore(btn, menuItem.nextSibling);
+
           // Add ID to ul for aria-label.
           const menuId = 'id_' + menuItem.innerText.toLowerCase() + '_menu';
-          // Check for 'ul.menu'.
           let potentialUl = menuItem.nextElementSibling.nextElementSibling;
           if (potentialUl.nodeName === 'UL' && potentialUl.classList.contains('menu')) {
             potentialUl.setAttribute('id', menuId);
@@ -50,18 +53,18 @@
         // Initialize menu.
         new AccessibleMenu.TopLinkDisclosureMenu({
           menuElement,
-          // We have to add 'span' to handle <nolink>.
           menuLinkSelector: 'a, span',
           submenuItemSelector: 'li.menu-item--expanded',
           controllerElement: context.querySelector('#main-menu-toggle'),
           submenuToggleSelector: 'button',
-          containerElement: context.querySelector('.menu-wrapper--horizontal'),
+          containerElement: menuWrapper, // Use the current menu wrapper as the container.
           optionalKeySupport: true,
           hoverType: 'off',
         });
       });
+
+      // Set the flag to true to prevent future executions.
+      Drupal.behaviors.accessible_menu.executed = true;
     }
-  }
+  };
 })(jQuery, AccessibleMenu);
-
-
