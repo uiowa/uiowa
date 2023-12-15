ff14dd1029e583ef8f9a43a4aa01233b05fd79c1
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2023-12-15 10:37:02 -0600

    confirmed that this works without this custom js

--- docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
+++ docroot/modules/custom/layout_builder_custom/js/layout_builder_custom.overrides.js
@@ -57,29 +57,6 @@
     }
   };
 
-  // Allows saving filtered and minimal html content while Source is open.
-  // Solution from https://www.drupal.org/project/drupal/issues/3095304.
-  var origBeforeSubmit = Drupal.Ajax.prototype.beforeSubmit;
-  Drupal.Ajax.prototype.beforeSubmit = function (formValues, element, options) {
-    if (typeof(CKEDITOR) !== 'undefined' && CKEDITOR.instances) {
-      const instances = Object.values(CKEDITOR.instances);
-      instances.forEach(editor => {
-        formValues.forEach(formField => {
-          // Get field name from the id in the editor so that it covers all
-          // fields using ckeditor.
-          let element = document.querySelector(`#${editor.name}`)
-          if (element) {
-            let fieldName = element.getAttribute('name');
-            if (formField.name === fieldName && editor.mode === 'source') {
-              formField.value = editor.getData();
-            }
-          }
-        });
-      });
-    }
-    origBeforeSubmit.call(formValues, element, options);
-  };
-
   // Wait for the mouse-up and reset the width of the main content.
   function dragHandleBehaviorStopgapAwait(event) {
     if (interacting) {
