9c14d24c3ab526ab262444c8d912e151ebd18654
Benjamin Speare <benjamin-speare@uiowa.edu>
2023-12-21 12:11:19 -0600

    Updated with changed name values.

--- docroot/modules/custom/layout_builder_custom/js/card-block-form.js
+++ docroot/modules/custom/layout_builder_custom/js/card-block-form.js
@@ -11,8 +11,8 @@
         // Check that we can access the next field.
         const $form_wrapper = $(this).parent().parent().parent();
         const $image_format_field = $form_wrapper.find('.form-item-layout-builder-style-media-format').find('select');
-        const $image_size_field_default = $form_wrapper.find('.form-item-layout-builder-style-card-image-size').find('select');
-        const $image_small_field = $form_wrapper.find('.form-item-layout-builder-style-card-image-size').find('select option[value="card_image_small"]');
+        const $image_size_field_default = $form_wrapper.find('.form-item-layout-builder-style-media-size').find('select');
+        const $image_small_field = $form_wrapper.find('.form-item-layout-builder-style-media-size').find('select option[value="media_size_small"]');
 
         if ($image_format_field.length || $image_small_field.length) {
           // Check if the referenced media is a video.
@@ -23,8 +23,8 @@
             $image_format_field.val('media_format_widescreen').change();
             // Set image size to default to large if it was set at small,
             // as we are not allowing small for card videos.
-            if ($image_size_field_default.val() === 'card_image_small') {
-              $image_size_field_default.val('card_image_large').change();
+            if ($image_size_field_default.val() === 'media_size_small') {
+              $image_size_field_default.val('media_size_large').change();
             }
             // Hide image format.
             $image_format_field.parent().hide();
@@ -32,7 +32,7 @@
 
             // @ todo investigate alternative ways of hiding image format that does not affect image size state.
           } else {
-              // Show image format.
+            // Show image format.
             $image_format_field.parent().show();
             $image_small_field.show();
           }
