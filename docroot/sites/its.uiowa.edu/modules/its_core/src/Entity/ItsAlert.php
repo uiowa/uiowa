fc5f2d2521743d4baa5e98d553d2ff04a332d375
Benjamin Speare <benjamin-speare@uiowa.edu>
2024-01-18 12:58:51 -0600

    Adjusted render of alert to use card_meta for field display.

--- docroot/sites/its.uiowa.edu/modules/its_core/src/Entity/ItsAlert.php
+++ docroot/sites/its.uiowa.edu/modules/its_core/src/Entity/ItsAlert.php
@@ -15,22 +15,13 @@ class ItsAlert extends Alert {
   public function buildCard(array &$build) {
     parent::buildCard($build);
 
-    // Fill the content with the affected services and buildings,
-    // if there are multiple. If there is only one,
-    // it will already be displayed in the title.
-    $labels = [];
-    foreach ([
-      'field_alert_service_affected',
-      'field_alert_building',
-    ] as $field) {
-      $field_list = $this->$field;
-      if ($field_list->count() > 1) {
-        foreach ($field_list->referencedEntities() as $referenced_entity) {
-          $labels[] = $referenced_entity->label();
-        }
-      }
-    }
-    $build['#content'] = implode(', ', $labels);
+    // Process additional card mappings.
+    $this->mapFieldsToCardBuild($build, [
+      '#meta' => [
+        'field_alert_building',
+        'field_alert_service_affected',
+      ],
+    ]);
 
   }
 
@@ -41,7 +32,7 @@ class ItsAlert extends Alert {
     $category_id = $this->field_alert_category?->target_id;
     if (in_array($category_id, ['406', '416'])) {
       return [
-        'card--layout-left' => 'card--layout-left',
+        'card_headline_style' => 'headline--serif',
         'media_size' => 'media--small',
         'media_shape' => 'media--circle',
         'borderless' => 'borderless',
