3b5842bc509051752c5040fde21686f634826061
Benjamin Speare <benjamin-speare@uiowa.edu>
2024-02-05 10:45:12 -0600

    Mapped paragraph to card.

---
+++ docroot/sites/its.uiowa.edu/modules/its_core/src/Entity/AlertUpdate.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\its_core\Entity;
+
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\uiowa_core\Entity\RendersAsCardInterface;
+use Drupal\uiowa_core\Entity\RendersAsCardTrait;
+
+/**
+ * Provides an interface for paragraph cards on the area of study page entries.
+ */
+class AlertUpdate extends Paragraph implements RendersAsCardInterface {
+
+  use RendersAsCardTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    $this->buildCardStyles($build);
+
+    // Process additional card mappings.
+    $this->mapFieldsToCardBuild($build, [
+      '#subtitle' => 'field_alert_update_date',
+      '#meta' => 'field_alert_update_description',
+    ]);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultCardStyles(): array {
+    return [
+      'card_headline_style' => 'headline--serif',
+      'styles' => 'bg--white',
+      'border' => 'borderless',
+    ];
+  }
+
+}
