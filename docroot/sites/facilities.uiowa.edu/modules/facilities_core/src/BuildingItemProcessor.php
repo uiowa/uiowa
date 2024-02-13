a34a9a5b0422e1399a1f0a6c1b7323686fba03ee
Marie Wakeman <elsie-wakeman@uiowa.edu>
2024-02-13 16:43:09 -0600

    Trying to simplify things again.

--- docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/BuildingItemProcessor.php
+++ docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/BuildingItemProcessor.php
@@ -42,7 +42,7 @@ class BuildingItemProcessor extends EntityItemProcessorBase {
    */
   public static function process($entity, $record): bool {
 
-    if ($entity->hasField('field_building_date_updated') && isset($record->buildingCoordinators[0]->updatedDate)) {
+    if (isset($record->buildingCoordinators[0]->updatedDate)) {
       $coordinator_array = [];
 
       // Comparing string to int as date int too large to store in db.
@@ -50,61 +50,38 @@ class BuildingItemProcessor extends EntityItemProcessorBase {
         $entity->set('field_building_date_updated', $record->buildingCoordinators[0]->updatedDate);
 
         // Mapping manager fields from coordinators array.
-        if ($entity->hasField('field_building_ca_manager') && isset($record->buildingCoordinators[0]->custodialAssistantManagerFullName)) {
+        if (isset($record->buildingCoordinators[0]->custodialAssistantManagerFullName)) {
           if ($entity->get('field_building_ca_manager')->value !== $record->buildingCoordinators[0]->custodialAssistantManagerFullName) {
             $entity->set('field_building_ca_manager', $record->buildingCoordinators[0]->custodialAssistantManagerFullName);
           }
         }
-        if ($entity->hasField('field_building_m_manager') && isset($record->buildingCoordinators[0]->maintenanceManagerFullName)) {
+        if (isset($record->buildingCoordinators[0]->maintenanceManagerFullName)) {
           if ($entity->get('field_building_m_manager')->value !== $record->buildingCoordinators[0]->maintenanceManagerFullName) {
             $entity->set('field_building_m_manager', $record->buildingCoordinators[0]->maintenanceManagerFullName);
           }
         }
 
-        // Check if coordinator(s) exist.
-        if ($entity->hasField('field_building_coordinators') && isset($record->buildingCoordinators[0]->mainFullName)) {
-          // Check if paragraph already exists, if not create new Paragraph.
-          if (!isset($entity->get('field_building_coordinators')[0]->target_id)) {
-            $main_coordinator = Paragraph::create([
-              'type' => 'uiowa_building_coordinators',
-              'field_b_coordinator_department' => $record->buildingCoordinators[0]->mainDepartment,
-              'field_b_coordinator_email' => $record->buildingCoordinators[0]->mainCampusEmail,
-              'field_b_coordinator_is_primary' => TRUE,
-              'field_b_coordinator_job_title' => $record->buildingCoordinators[0]->mainJobTitle,
-              'field_b_coordinator_name' => $record->buildingCoordinators[0]->mainFullName,
-              'field_b_coordinator_phone_number' => $record->buildingCoordinators[0]->mainCampusPhone,
-            ]);
-            $main_coordinator->save();
-            $main_array = [
-              'target_id' => $main_coordinator->id(),
-              'target_revision_id' => $main_coordinator->getRevisionId(),
-            ];
-            $coordinator_array[] = $main_array;
-          }
-          // If paragraph already exists, check it matches current API info.
-          else {
-            $existing_ID = $entity->get('field_building_coordinators')[0]->target_id;
-            $existing_paragraph = Paragraph::load($existing_ID);
-            // If info does not match, update paragraph with API info.
-            if ($existing_paragraph->get('field_b_coordinator_name')->getValue()[0]['value'] !== $record->buildingCoordinators[0]->mainFullName) {
-              $existing_paragraph->set('field_b_coordinator_department', $record->buildingCoordinators[0]->mainDepartment);
-              $existing_paragraph->set('field_b_coordinator_email', $record->buildingCoordinators[0]->mainCampusEmail);
-              $existing_paragraph->set('field_b_coordinator_is_primary', TRUE);
-              $existing_paragraph->set('field_b_coordinator_job_title', $record->buildingCoordinators[0]->mainJobTitle);
-              $existing_paragraph->set('field_b_coordinator_name', $record->buildingCoordinators[0]->mainFullName);
-              $existing_paragraph->set('field_b_coordinator_phone_number', $record->buildingCoordinators[0]->mainCampusPhone);
-              $existing_paragraph->save();
-              $updated_array = [
-                'target_id' => $existing_paragraph->id(),
-                'target_revision_id' => $existing_paragraph->getRevisionId(),
-              ];
-              $coordinator_array[] = $updated_array;
-            }
-          }
+        // Check if primary coordinator exists.
+        if (isset($record->buildingCoordinators[0]->mainFullName)) {
+          $main_coordinator = Paragraph::create([
+            'type' => 'uiowa_building_coordinators',
+            'field_b_coordinator_department' => $record->buildingCoordinators[0]->mainDepartment,
+            'field_b_coordinator_email' => $record->buildingCoordinators[0]->mainCampusEmail,
+            'field_b_coordinator_is_primary' => TRUE,
+            'field_b_coordinator_job_title' => $record->buildingCoordinators[0]->mainJobTitle,
+            'field_b_coordinator_name' => $record->buildingCoordinators[0]->mainFullName,
+            'field_b_coordinator_phone_number' => $record->buildingCoordinators[0]->mainCampusPhone,
+          ]);
+          $main_coordinator->save();
+          $main_array = [
+            'target_id' => $main_coordinator->id(),
+            'target_revision_id' => $main_coordinator->getRevisionId(),
+          ];
+          $coordinator_array[] = $main_array;
         }
 
+        // Check if alternate coordinators exist.
         $alternates = ['1', '2', '3', '4'];
-
         foreach ($alternates as $alt_number) {
           if (isset($record->buildingCoordinators[0]->{'alternateFullName' . $alt_number})) {
             $alternate_coordinator = Paragraph::create([
@@ -126,14 +103,7 @@ class BuildingItemProcessor extends EntityItemProcessorBase {
         }
 
         // Update building entity with new coordinator paragraph(s).
-        if (!isset($entity->get('field_building_coordinators')[0]->target_id)) {
-          $entity->set('field_building_coordinators', $coordinator_array);
-        }
-        else {
-          foreach ($coordinator_array as $coordinator) {
-            $entity->get('field_building_coordinators')->appendItem($coordinator);
-          }
-        }
+        $entity->set('field_building_coordinators', $coordinator_array);
         $entity->save();
 
         return TRUE;
