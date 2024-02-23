97059b75338c79c25d9079fb5aed70ca79973b75
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2024-02-23 08:08:27 -0600

    stub out initial email functionality

---
+++ docroot/sites/its.uiowa.edu/modules/its_core/src/Plugin/EmailBuilder/ITSAlertEmail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\its_core\Plugin\EmailBuilder;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\symfony_mailer\Address as EmailAddress;
+use Drupal\symfony_mailer\EmailInterface;
+use Drupal\symfony_mailer\MailerHelperTrait;
+use Drupal\symfony_mailer\Processor\EmailBuilderBase;
+
+/**
+ * Defines an Email Builder for the Lockup Digest email.
+ *
+ * @EmailBuilder(
+ *   id = "its_core",
+ *   sub_types = {"its_alert_email" = @Translation("ITS Alert Email")},
+ * )
+ */
+class ITSAlertEmail extends EmailBuilderBase {
+
+  use MailerHelperTrait;
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createParams(EmailInterface $email, string $alert = NULL) {
+    $email->setParam('alert', $alert);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(EmailInterface $email) {
+    $from = new EmailAddress('IT-Service-Alerts@uiowa.edu', 'IT Service Alerts');
+    $email->setFrom($from);
+    $email->setReplyTo($from);
+    $env = getenv('AH_PRODUCTION');
+    if ((int) $env === 1) {
+      // @todo After testing, should be
+      // "e7199078.iowa.onmicrosoft.com@amer.teams.ms".
+      $email->setBcc('its-web@uiowa.edu');
+    }
+    else {
+      // @todo After testing, use site email to avoid unintentional emails?
+      $email->setBcc('joe-whitsitt@uiowa.edu');
+    }
+
+    if ($email->getSubType() == 'its_alert_email') {
+      $email->setSubject($this->t('Alert'));
+
+      $body = [];
+
+      $body[] = [
+        '#markup' => $email->getParam('alert'),
+      ];
+
+      $email->setBody(['body' => $body]);
+    }
+  }
+
+}
