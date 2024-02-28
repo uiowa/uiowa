fc84cb80e3fdd531c6388bebcad6ca4d13dd265b
Benjamin Speare <benjamin-speare@uiowa.edu>
2024-02-28 11:48:43 -0600

    Added more layout restructuring.

--- docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/AccessibilityLinks.php
+++ docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/AccessibilityLinks.php
@@ -39,17 +39,17 @@ class AccessibilityLinks extends BlockBase {
         'id' => 'b4389a15836343e2bd07899a67560a9f',
       ],
       'lactation_room' => [
-        'label' => 'Lactation Rooms',
+        'label' => 'Lactation Rooms Map',
         'icon' => 'map-marker-alt',
         'id' => '72c9109323954a58abbde5dae8e6ee03',
       ],
       'hearing_loop' => [
-        'label' => 'Hearing Loop Systems',
+        'label' => 'Hearing Loop Systems Map',
         'icon' => 'ear-deaf',
         'id' => '7c639bd4000b4d34979a3a3a628d7180',
       ],
       'gender_inclusive_restroom' => [
-        'label' => 'Gender Inclusive Restrooms',
+        'label' => 'Gender Inclusive Restrooms Map',
         'icon' => 'person-half-dress',
         'id' => 'a787689a1da843018156b2f2e97da119',
       ],
@@ -66,20 +66,9 @@ class AccessibilityLinks extends BlockBase {
 
     $build = [];
 
-    $attributes = [];
-    $attributes['class'] = [
-      'bg--white',
-      'media--medium',
-      'media--widescreen',
-      'borderless',
-    ];
-
     $build['container']['services'][] = [
-      '#type' => 'card',
-      '#attributes' => $attributes,
-      '#content' => [
-        '#markup' => $list_markup,
-      ],
+      '#type' => 'markup',
+      '#markup' => $list_markup,
     ];
 
     return $build;
