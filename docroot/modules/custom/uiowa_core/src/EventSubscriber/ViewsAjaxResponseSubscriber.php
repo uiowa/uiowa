73334436c340193f468c9e7062fd3f611d086647
Alan Ridgway <gary-ridgway@uiowa.edu>
2024-03-05 13:45:46 -0600

    PLANTS.

--- docroot/modules/custom/uiowa_core/src/EventSubscriber/ViewsAjaxResponseSubscriber.php
+++ docroot/modules/custom/uiowa_core/src/EventSubscriber/ViewsAjaxResponseSubscriber.php
@@ -20,6 +20,7 @@ class ViewsAjaxResponseSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     $events[KernelEvents::RESPONSE][] = ['onResponse'];
+    $events[KernelEvents::REQUEST][] = ['onRequest'];
     return $events;
   }
 
@@ -31,6 +32,30 @@ class ViewsAjaxResponseSubscriber implements EventSubscriberInterface {
    */
   public function onResponse(ResponseEvent $event) {
     $response = $event->getResponse();
+
+    // Only act on a Views Ajax Response.
+    if ($response instanceof ViewAjaxResponse) {
+      $view = $response->getView();
+      $view_id = $view->storage->id();
+      $view_display_id = $view->getDisplay()->display['id'];
+
+      // Only act on the view to tweak.
+      if (
+        $view_id === 'alerts_list_block' &&
+        $view_display_id === 'alert_status'
+      ) {
+        $response->addCommand(new AfterViewsAjaxCommand());
+      }
+    }
+  }
+  /**
+   * Allows us to alter the Ajax response from a view.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The event process.
+   */
+  public function onRequest(RequestEvent $event) {
+    $request = $event->getResponse();
 
     // Only act on a Views Ajax Response.
     if ($response instanceof ViewAjaxResponse) {
