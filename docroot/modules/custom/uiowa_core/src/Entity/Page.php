c43825639d139d44293ffc5157488e6c6aeb08e0
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2024-03-06 08:59:18 -0600

    Page listblock (#7431)
    * Added patch to project and composer.json.
    
    * Added page list block view and fixed an issue preventing the filter from being filled in.
    
    * Got LB styles working.
    
    * Moved the filters further down the form and changed the title of the details element.
    
    * Updated composer.lock.
    
    * Updated view display name.
    
    * Added contextual filter to remove current node.
    
    * reducing duplicates caused by multiple tags filtering
    
    * Add 10 item default to page list block too.
    
    ---------
    
    Co-authored-by: Joe Whitsitt <joe-whitsitt@uiowa.edu>

---
+++ docroot/modules/custom/uiowa_core/src/Entity/Page.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\uiowa_core\Entity;
+
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\RendersAsCardInterface;
+
+/**
+ * Provides an interface for article entries.
+ */
+class Page extends NodeBundleBase implements RendersAsCardInterface {
+
+}
