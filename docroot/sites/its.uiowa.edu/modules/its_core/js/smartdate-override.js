f5c6c91acc45436751f5ce373b14fcf96296188f
Benjamin Speare <benjamin-speare@uiowa.edu>
2024-03-20 16:17:31 -0500

    Added default endTime.

--- docroot/sites/its.uiowa.edu/modules/its_core/js/smartdate-override.js
+++ docroot/sites/its.uiowa.edu/modules/its_core/js/smartdate-override.js
@@ -8,8 +8,10 @@
           if (this.value === 'custom') {
             const wrapper = this.closest('.smartdate--widget');
             const endDate = wrapper.querySelector('.time-end.form-date');
-            // Set end date to today's date
+            const endTime = wrapper.querySelector('.time-end.form-time');
+            // Set end date and time to today's date.
             endDate.value = new Date().toISOString().split('T')[0];
+            endTime.value = new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false});
           }
         });
       });
