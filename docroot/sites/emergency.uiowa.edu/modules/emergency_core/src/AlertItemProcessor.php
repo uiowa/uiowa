d424d9cd0e406c6e7e0f497a3a838050c2fba3a2
Marie Wakeman <elsie-wakeman@uiowa.edu>
2024-04-08 10:32:32 -0500

    Created new process function to handle differences in this API call vs. existing.

--- docroot/sites/emergency.uiowa.edu/modules/emergency_core/src/AlertItemProcessor.php
+++ docroot/sites/emergency.uiowa.edu/modules/emergency_core/src/AlertItemProcessor.php
@@ -18,14 +18,14 @@ class AlertItemProcessor extends EntityItemProcessorBase {
   ];
 
   /**
-   * Process an individual entity.
+   * Process an individual hawk alert.
    */
   public static function process($entity, $record): bool {
     $updated = FALSE;
     foreach (static::$fieldMap as $to => $from) {
       if (!$entity->hasField($to)) {
         // Add a log message that the field being mapped to doesn't exist.
-        static::getLogger('uiowa_core')
+        static::getLogger('emergency_core')
           ->notice('While processing the @type, a field was mapped that does not exist: @field_name', [
             '@type' => !is_null($entity->bundle()) ? "{$entity->bundle()} {$entity->getEntityType()}" : $entity->getEntityType(),
             '@field_name' => $to,
--- docroot/sites/emergency.uiowa.edu/modules/emergency_core/src/AlertsProcessor.php
+++ docroot/sites/emergency.uiowa.edu/modules/emergency_core/src/AlertsProcessor.php
@@ -3,6 +3,7 @@
 namespace Drupal\emergency_core;
 
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\uiowa_core\EntityProcessorBase;
 use GuzzleHttp\Client;
 
@@ -63,6 +64,78 @@ class AlertsProcessor extends EntityProcessorBase {
    */
   protected function processEntity(ContentEntityInterface &$entity, $record): bool {
     return AlertItemProcessor::process($entity, $record);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process() {
+    if (!$this->getData()) {
+      // Log a message that data was not returned.
+      static::getLogger('emergency_core')
+        ->notice('No data returned for AlertsProcessor::getData().');
+      return;
+    }
+
+    $storage = $this->entityTypeManager
+      ->getStorage($this->entityType);
+
+    // Retrieve values from existing hawk alerts.
+    if ($this->getEntityIds()) {
+      $entities = $storage->loadMultiple($this->getEntityIds());
+      foreach ($entities as $entity_id => $entity) {
+        if ($entity instanceof FieldableEntityInterface) {
+          if ($entity->hasField($this->fieldSyncKey) && !$entity->get($this->fieldSyncKey)
+              ->isEmpty()) {
+            $this->existingEntities[$entity_id] = $entity;
+            $this->keyMap[$entity->get($this->fieldSyncKey)->value] = $entity_id;
+          }
+        }
+      }
+    }
+
+    $record = $this->getData();
+    $recordSyncKey = $record[$this->apiRecordSyncKey];
+    $this->processedRecords[] = $recordSyncKey;
+
+    $this->processRecord($record);
+
+    $existing_nid = $this->keyMap[$recordSyncKey] ?? NULL;
+
+    // Get building number and check to see if existing node exists.
+    if (!is_null($existing_nid)) {
+      // If existing, update values if different.
+      $entity = $this->existingEntities[$existing_nid] ?? $storage->load($existing_nid);
+    }
+    else {
+      // If not, create new.
+      $entity = $storage->create([
+        'type' => $this->bundle,
+      ]);
+    }
+
+    if ($entity instanceof ContentEntityInterface) {
+      $changed = $this->processEntity($entity, $record);
+
+      if (!is_null($existing_nid)) {
+        if ($changed) {
+          $entity->setNewRevision();
+          $entity->revision_log = 'Updated from source';
+          $entity->setRevisionCreationTime(\Drupal::time()->getRequestTime());
+          $entity->setRevisionUserId(1);
+          $entity->save();
+          $this->updated++;
+        }
+        else {
+          $this->skipped++;
+        }
+      }
+      else {
+        $entity->enforceIsNew();
+        $entity->save();
+        $this->created++;
+      }
+    }
   }
 
 }
