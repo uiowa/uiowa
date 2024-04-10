b1e9d694630877f2b0406c49eb5b9dbcf07e04ab
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2024-04-10 17:23:00 -0500

    Facilities API updates (#7449)
    * Started working on separated API classes using existing API base class.
    
    * Added a service definition.
    
    * Added a basic CPU time profiler. Cleaned up some code I think wasn't necessary. Added a missing use statement.
    
    * Only add an API key if one exists. Make it possible to get the underlying guzzle client.
    
    * Adjusted how parameters are passed in. Removed methods from FacilitiesAPI class which have been moved to a different class. Refactored BuildingsProcessor to remove the need for an init() method. Removed init() call from FacilitiesCoreCommands and added CPU time profiler.
    
    * Added basic scaffolding for BuildUiApiClient. Updated cache ID for BizHubApiClient.
    
    * PHPCS cleanup.
    
    * Added BuildUI API service definition.
    
    * Added API client interfaces to please the PhpStorm gods complaints about polymorphic calls. Finished splitting out BuildUI API.
    
    * Cleaning up the interface.
    
    * Cleaning up the other interface.
    
    * Moved non-API logic from API class to processor class.
    
    * PHPCS cleanup.
    
    * A bunch of refactoring. Moved API key logic to a trait.
    
    * A little cleanup.
    
    * Some refactoring. Added a basic auth trait to handle setting up connection details.
    
    * Oops
    
    * Need process() to return TRUE now.
    
    * PHPCS fixes.
    
    * Updated drush version, reverted changes to Facilities commands based on logging fix.
    
    * Fixed an error with the processing of the username and password.
    
    * Roll back syslog changes now that logs are working.
    
    * Removed the old API class.
    
    * Added config for APIs.
    
    * Named this wrong.
    
    * Fixing an unintentional change to the building image form widget from #7367.
    
    * Fixed issue with function signatures not matching.
    
    * A bit of fine-tuning to make sure properties exist before we access them and ensureing alignment between interaces and the classes that implement them.
    
    * A little more cleanup.
    
    * Updated to production URLs for config.
    
    ---------
    
    Co-authored-by: Cory Skeers <cory-skeers@uiowa.edu>

---
+++ docroot/modules/custom/uiowa_core/src/ApiAuthKeyTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\uiowa_core;
+
+/**
+ * A trait for adding key-based authentication to an API client.
+ */
+trait ApiAuthKeyTrait {
+
+  /**
+   * The API key for accessing the API.
+   *
+   * @var string|null
+   */
+  protected ?string $apiKey = NULL;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getKey(): string|null {
+    return $this->apiKey;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setKey($key): static {
+    $this->apiKey = $key;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addAuthToOptions(array &$options = []): void {
+    if (!is_null($this->apiKey)) {
+      // Merge additional options with default but allow overriding.
+      $options = array_merge([
+        'headers' => [
+          'x-auth-token' => $this->apiKey,
+        ],
+      ], $options);
+    }
+  }
+
+}
