b0dbc9d00ea5500abb970b2809eb2f3244f5ab39
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2024-04-12 16:34:32 -0500

    Scaffolding.

---
+++ docroot/sites/commencement.uiowa.edu/modules/commencement_core/src/EventItemProcessor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\commencement_core;
+
+use Drupal\uiowa_core\EntityItemProcessorBase;
+
+/**
+ * A processor for syncing event nodes.
+ */
+class EventItemProcessor extends EntityItemProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $fieldMap = [
+    // @todo Add the field map.
+  ];
+
+}
---
+++ docroot/sites/commencement.uiowa.edu/modules/commencement_core/src/EventsProcessor.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\commencement_core;
+
+use Drupal\uiowa_core\EntityProcessorBase;
+use Drupal\uiowa_facilities\BizHubApiClient;
+
+/**
+ * Sync event information.
+ */
+class EventsProcessor extends EntityProcessorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public string $bundle = 'event';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $fieldSyncKey = 'id';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $apiRecordSyncKey = 'id';
+
+  /**
+   * The BizHub API client.
+   *
+   * @var \Drupal\uiowa_facilities\BizHubApiClientInterface
+   */
+  protected BizHubApiClient $apiClient;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct() {
+    parent::__construct();
+    $this->apiClient = \Drupal::service('uiowa_events.content_hub_client');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getData() {
+    if (!isset($this->data)) {
+      // Request from BizHub API to get buildings.
+      $this->data = $this->apiClient->getEvents();
+    }
+    return $this->data;
+  }
+
+}
