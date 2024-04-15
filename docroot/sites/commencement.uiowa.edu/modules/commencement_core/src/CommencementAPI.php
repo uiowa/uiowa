593ebbfb5b6f57608f98a9b6c90d96aa9c82a26d
Marie Wakeman <elsie-wakeman@uiowa.edu>
2024-04-15 10:12:56 -0500

    Confirmed can pull API data successfully.

---
+++ docroot/sites/commencement.uiowa.edu/modules/commencement_core/src/CommencementAPI.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Drupal\commencement_core;
+
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Cache\CacheBackendInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\RequestException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Commencement API service.
+ */
+class CommencementAPI {
+
+  const BASE_URL_1 = 'https://content.uiowa.edu/api/v1/views/events_api.json?display_id=events&filters[enddate][value][date]=01-01-2100&filters[types]=355&filters[department]=7266&items_per_page=100';
+
+  /**
+   * The commencement_core logger channel.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * The commencement_core cache.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cache;
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $client;
+
+  /**
+   * Constructs an event object.
+   *
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The commencement_core logger channel.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
+   *   The commencement_core cache.
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   The HTTP client.
+   */
+  public function __construct(LoggerInterface $logger, CacheBackendInterface $cache, ClientInterface $http_client) {
+    $this->logger = $logger;
+    $this->cache = $cache;
+    $this->client = $http_client;
+  }
+
+  /**
+   * Make a Commencement API request and return data.
+   *
+   * @param string $method
+   *   The HTTP method to use.
+   * @param string $path
+   *   The API path to use. Do not include the base URL.
+   * @param array $params
+   *   Optional request parameters.
+   * @param array $options
+   *   Optional request options. All requests expect JSON response data.
+   * @param string $base
+   *   The base URL to use for the request. Defaults to self::BASE_URL_1.
+   *
+   * @return mixed
+   *   The API response data.
+   */
+  public function request($method, $path, array $params = [], array $options = [], $base = self::BASE_URL_1) {
+    // Encode any special characters and trim duplicate slash.
+    $path = UrlHelper::encodePath($path);
+    $uri = $base . ltrim($path, '/');
+    // Append any query string parameters.
+    if (!empty($params)) {
+      $query = UrlHelper::buildQuery($params);
+      $uri .= "?{$query}";
+    }
+    // Merge additional options with default but allow overriding.
+    $options = array_merge([
+      'headers' => [
+        'Accept' => 'application/json',
+      ],
+    ], $options);
+
+    // Create a hash for the CID. Can always be decoded for debugging purposes.
+    $hash = base64_encode($uri . serialize($options));
+    $cid = "commencement_core:request:{$hash}";
+    // Default $data to FALSE in case of API fetch failure.
+    $data = FALSE;
+
+    if ($cache = $this->cache->get($cid)) {
+      $data = $cache->data;
+    }
+    else {
+      try {
+        $response = $this->client->request($method, $uri, $options);
+      }
+      catch (RequestException | GuzzleException $e) {
+        $this->logger->error('Error encountered getting data from @endpoint: @code @error', [
+          '@endpoint' => $uri,
+          '@code' => $e->getCode(),
+          '@error' => $e->getMessage(),
+        ]);
+      }
+      if (isset($response)) {
+        $contents = $response->getBody()->getContents();
+        /** @var object $data */
+        $data = json_decode($contents);
+        // Cache for 15 minutes.
+        $this->cache->set($cid, $data, time() + 900);
+      }
+    }
+    return $data;
+  }
+
+  /**
+   * Get all commencement events.
+   *
+   * @return array
+   *   The alerts object
+   */
+  public function getEvents() {
+    return $this->request('GET', '');
+  }
+}
