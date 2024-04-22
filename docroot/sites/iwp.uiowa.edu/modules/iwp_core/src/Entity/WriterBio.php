d858461a313abfe0bcc4ce4d34335293e9122871
Benjamin Speare <benjamin-speare@uiowa.edu>
2024-04-22 11:48:10 -0500

    Added teaser settings.

---
+++ docroot/sites/iwp.uiowa.edu/modules/iwp_core/src/Entity/WriterBio.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\iwp_core\Entity;
+
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\RendersAsCardInterface;
+
+/**
+ * Provides an interface for writer bio page entries.
+ */
+class WriterBio extends NodeBundleBase implements RendersAsCardInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    parent::buildCard($build);
+
+    $this->mapFieldsToCardBuild($build, [
+      '#subtitle' => [
+        'field_writer_bio_family_name',
+        'field_writer_bio_given_name',
+      ],
+      '#meta' => [
+        'field_writer_bio_sample',
+        'field_writer_bio_sample_original',
+      ],
+    ]);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultCardStyles(): array {
+    return [
+      ...parent::getDefaultCardStyles(),
+      'media_format' => 'media--square media--border',
+      'media_size' => 'media--medium',
+      'border' => '',
+    ];
+  }
+
+}
