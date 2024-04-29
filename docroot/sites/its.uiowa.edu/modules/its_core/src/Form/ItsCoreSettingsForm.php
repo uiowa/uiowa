63f919b6b087e62d8c6de2f688f374affb1d6adc
Cory Skeers <cory-skeers@uiowa.edu>
2024-04-29 11:54:12 -0500

    Add basic settings form.

---
+++ docroot/sites/its.uiowa.edu/modules/its_core/src/Form/ItsCoreSettingsForm.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\its_core\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure ITS Core settings.
+ */
+class ItsCoreSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'its_core_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['its_core.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['email'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Alert email settings.'),
+      'alert-notification' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Alert notification'),
+        '#description' => $this->t('Notification email that is sent upon alert creation.'),
+        '#default_value' => $this->config('its_core.settings')->get('alert-notification') ?? '',
+      ],
+      'alert-digest' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Alert digest email'),
+        '#description' => $this->t('The daily alert digest email.'),
+        '#default_value' => $this->config('its_core.settings')->get('alert-digest') ?? '',
+      ],
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('its_core.settings')
+      ->set('alert-notification', $form_state->getValue('alert-notification'))
+      ->set('alert-digest', $form_state->getValue('alert-digest'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
