403911b22bd95490de399c04b889c1ba0dee654b
Cory Skeers <cory-skeers@uiowa.edu>
2024-04-29 14:16:25 -0500

    Update to allow for multiple emails with validation. Update help text.

--- docroot/sites/its.uiowa.edu/modules/its_core/src/Form/ItsCoreSettingsForm.php
+++ docroot/sites/its.uiowa.edu/modules/its_core/src/Form/ItsCoreSettingsForm.php
@@ -32,20 +32,47 @@ class ItsCoreSettingsForm extends ConfigFormBase {
       '#type' => 'fieldset',
       '#title' => $this->t('Alert email settings.'),
       'alert-notification' => [
-        '#type' => 'email',
+        // @todo Update to a multiple email field when available.
+        //   https://www.drupal.org/project/drupal/issues/3214029.
+        '#type' => 'textfield',
         '#title' => $this->t('Alert notification'),
-        '#description' => $this->t('Notification email that is sent upon alert creation.'),
+        '#description' => $this->t('Emails to which alert notification emails should be sent when new alerts are created. Multiple emails should be separated by a comma.'),
         '#default_value' => $this->config('its_core.settings')->get('alert-notification') ?? '',
       ],
       'alert-digest' => [
-        '#type' => 'email',
+        // @todo Update to a multiple email field when available.
+        //   https://www.drupal.org/project/drupal/issues/3214029.
+        '#type' => 'textfield',
         '#title' => $this->t('Alert digest email'),
-        '#description' => $this->t('The daily alert digest email.'),
+        '#description' => $this->t('Email to which the daily alert digest email will be sent. Multiple emails should be separated by a comma.'),
         '#default_value' => $this->config('its_core.settings')->get('alert-digest') ?? '',
       ],
     ];
 
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Basic email validation for each email in the comma-delimited string,
+    // based on https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Render%21Element%21Email.php/function/Email%3A%3AvalidateEmail.
+    foreach (['alert-notification', 'alert-digest'] as $fieldname) {
+      $value = trim($form_state->getValue($fieldname));
+      $emails = explode(',', $value);
+      $form_state->setValue($fieldname, $value);
+      foreach ($emails as $email) {
+        if ($email !== '' && !\Drupal::service('email.validator')->isValid($email)) {
+          $form_state
+            ->setError($form, $this->t('The email address %mail is not valid.', [
+              '%mail' => $email,
+            ]));
+          break;
+        }
+      }
+    }
+    parent::validateForm($form, $form_state);
   }
 
   /**
