62fa32c6e0750b0c255943c3fcfb3af0ac1a0e8d
Benjamin Speare <benjamin-speare@uiowa.edu>
2024-05-09 09:06:59 -0500

    Added directions link block and adjusted heading settings for collection item.

---
+++ docroot/sites/commencement.uiowa.edu/modules/commencement_core/src/Plugin/Block/DirectionsLink.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\commencement_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Commencement venue directions link block.
+ *
+ * @Block(
+ *   id = "directionslink_block",
+ *   admin_label = @Translation("Directions Link Block"),
+ *   category = @Translation("Restricted"),
+ *   context_definitions = {
+ *     "node" = @ContextDefinition("entity:node", label = @Translation("Node"))
+ *   }
+ * )
+ */
+class DirectionsLink extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['label_display' => FALSE];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = $this->getContextValue('node');
+    $geo_field = $node->get('field_geolocation');
+
+    $build = [];
+
+    // Check if the $geo_field has a value.
+    if (!$geo_field->isEmpty()) {
+      $geolocation = $geo_field->first()->getValue();
+
+      if (!empty($geolocation['latlon'])) {
+        $latlon_value = $geolocation['latlon'];
+
+        $google_maps_link = 'https://www.google.com/maps?q=' . $latlon_value;
+
+        $link = [
+          '#type' => 'link',
+          '#title' => 'View on Google Maps',
+          '#url' => Url::fromUri($google_maps_link),
+          '#attributes' => [
+            'target' => '_blank',
+          ],
+        ];
+
+        $link_render_array = \Drupal::service('renderer')->render($link);
+
+        $build['container']['link'] = [
+          '#type' => 'markup',
+          '#markup' => $link_render_array,
+        ];
+      }
+    }
+
+    return $build;
+  }
+
+}
