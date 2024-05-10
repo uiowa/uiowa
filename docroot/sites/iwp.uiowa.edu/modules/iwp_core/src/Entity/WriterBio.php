d68bd8f7820513ae866aa98b1b4cedb54f62a266
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2024-05-10 16:50:35 -0500

    add media links to bulleted list

--- docroot/sites/iwp.uiowa.edu/modules/iwp_core/src/Entity/WriterBio.php
+++ docroot/sites/iwp.uiowa.edu/modules/iwp_core/src/Entity/WriterBio.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\iwp_core\Entity;
 
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\uiowa_core\Entity\NodeBundleBase;
 use Drupal\uiowa_core\Entity\RendersAsCardInterface;
 
@@ -22,9 +24,6 @@ class WriterBio extends NodeBundleBase implements RendersAsCardInterface {
         'field_writer_bio_languages',
         'field_writer_bio_countries',
       ],
-      '#content' => [
-        'field_writer_bio_media_link',
-      ],
     ]);
 
     // Combine fields into an unordered list.
@@ -32,10 +31,20 @@ class WriterBio extends NodeBundleBase implements RendersAsCardInterface {
     $list_fields = [
       'field_writer_bio_sample',
       'field_writer_bio_sample_original',
+      'field_writer_bio_media_link',
     ];
     foreach ($list_fields as $field) {
       if ($this->hasField($field) && !$this->$field->isEmpty()) {
-        $items[] = $this->$field->view('teaser');
+        if ($field === 'field_writer_bio_media_link') {
+          $links = $this->get($field)->getValue();
+          foreach ($links as $link) {
+            $items[] = Link::fromTextAndUrl($link['title'], Url::fromUri($link['uri']));
+          }
+        }
+        else {
+          $items[] = $this->$field->view('teaser');
+        }
+
       }
     }
     if (!empty($items)) {
