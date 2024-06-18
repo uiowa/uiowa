c50871de2b5e1a17372c7abf6243ca7b8a4651c0
Cory Skeers <cory-skeers@uiowa.edu>
2024-06-18 14:17:43 -0500

    Add basic bundle class

---
+++ docroot/sites/iwp.uiowa.edu/modules/iwp_core/src/Entity/Resource.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\iwp_core\Entity;
+
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\RendersAsCardInterface;
+
+/**
+ * Provides an interface for resource entities.
+ */
+class Resource extends NodeBundleBase implements RendersAsCardInterface {
+
+}
