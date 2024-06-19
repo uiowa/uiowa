e7e9a506ec3cd2937c077ff2b475b04f34828b19
Cory Skeers <cory-skeers@uiowa.edu>
2024-06-19 10:56:42 -0500

    Include type in teaser. Add it to the card meta. @todo remove commented out reference code.

--- docroot/sites/iwp.uiowa.edu/modules/iwp_core/src/Entity/Resource.php
+++ docroot/sites/iwp.uiowa.edu/modules/iwp_core/src/Entity/Resource.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\iwp_core\Entity;
 
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\uiowa_core\Entity\NodeBundleBase;
 use Drupal\uiowa_core\Entity\RendersAsCardInterface;
 
@@ -9,5 +11,60 @@ use Drupal\uiowa_core\Entity\RendersAsCardInterface;
  * Provides an interface for resource entities.
  */
 class Resource extends NodeBundleBase implements RendersAsCardInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    parent::buildCard($build);
+
+    $this->mapFieldsToCardBuild($build, [
+      '#meta' => [
+        'field_resource_type',
+      ]
+    ]);
+    
+//    // Combine fields into an unordered list.
+//    $items = [];
+//    $list_fields = [
+//      'field_writer_bio_sample',
+//      'field_writer_bio_sample_original',
+//      'field_writer_bio_media_link',
+//    ];
+//    foreach ($list_fields as $field) {
+//      if ($this->hasField($field) && !$this->$field->isEmpty()) {
+//        if ($field === 'field_writer_bio_media_link') {
+//          $links = $this->get($field)->getValue();
+//          foreach ($links as $link) {
+//            $items[] = Link::fromTextAndUrl($link['title'], Url::fromUri($link['uri']));
+//          }
+//        }
+//        else {
+//          $items[] = $this->$field->view('teaser');
+//        }
+//
+//      }
+//    }
+//    if (!empty($items)) {
+//      $build['#content']['related_links'] = [
+//        '#theme' => 'item_list',
+//        '#type' => 'ul',
+//        '#items' => $items,
+//        '#weight' => 3,
+//      ];
+//    }
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultCardStyles(): array {
+    return [
+      ...parent::getDefaultCardStyles(),
+//      'media_format' => 'media--square',
+//      'border' => 'borderless',
+    ];
+  }
 
 }
