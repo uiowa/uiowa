dd8f8166d3f0341d16793a22b163479af8a20631
Cory Skeers <cory-skeers@uiowa.edu>
2024-06-19 14:36:06 -0500

    Remove reference/scaffolding code. Typo in machine name

--- docroot/sites/iwp.uiowa.edu/modules/iwp_core/src/Entity/Resource.php
+++ docroot/sites/iwp.uiowa.edu/modules/iwp_core/src/Entity/Resource.php
@@ -23,36 +23,6 @@ class Resource extends NodeBundleBase implements RendersAsCardInterface {
         'field_resource_type',
       ]
     ]);
-    
-//    // Combine fields into an unordered list.
-//    $items = [];
-//    $list_fields = [
-//      'field_writer_bio_sample',
-//      'field_writer_bio_sample_original',
-//      'field_writer_bio_media_link',
-//    ];
-//    foreach ($list_fields as $field) {
-//      if ($this->hasField($field) && !$this->$field->isEmpty()) {
-//        if ($field === 'field_writer_bio_media_link') {
-//          $links = $this->get($field)->getValue();
-//          foreach ($links as $link) {
-//            $items[] = Link::fromTextAndUrl($link['title'], Url::fromUri($link['uri']));
-//          }
-//        }
-//        else {
-//          $items[] = $this->$field->view('teaser');
-//        }
-//
-//      }
-//    }
-//    if (!empty($items)) {
-//      $build['#content']['related_links'] = [
-//        '#theme' => 'item_list',
-//        '#type' => 'ul',
-//        '#items' => $items,
-//        '#weight' => 3,
-//      ];
-//    }
 
   }
 
@@ -62,8 +32,6 @@ class Resource extends NodeBundleBase implements RendersAsCardInterface {
   public function getDefaultCardStyles(): array {
     return [
       ...parent::getDefaultCardStyles(),
-//      'media_format' => 'media--square',
-//      'border' => 'borderless',
     ];
   }
 
