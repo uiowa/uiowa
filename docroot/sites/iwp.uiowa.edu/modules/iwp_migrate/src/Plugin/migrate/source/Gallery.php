20b25e31e5cc2dfcc380da4c2e3166a0a5a1aac5
Cory Skeers <cory-skeers@uiowa.edu>
2024-07-25 08:56:11 -0500

    Migration structure and mappings

---
+++ docroot/sites/iwp.uiowa.edu/modules/iwp_migrate/src/Plugin/migrate/source/Gallery.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\iwp_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
+use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
+
+/**
+ * Migrate Source plugin.
+ *
+ * @MigrateSource(
+ *   id = "iwp_gallery",
+ *   source_module = "node"
+ * )
+ */
+class Gallery extends BaseNodeSource {
+  use ProcessMediaTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = parent::query();
+    // Make sure our nodes are retrieved in order
+    // for ease of debugging.
+    $query->orderBy('nid');
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    parent::prepareRow($row);
+
+    // Skip over the rest of the preprocessing, as it's not needed
+    // for redirects. Also avoids duplicating the notices.
+    // Return TRUE because the row should be created.
+    if ($this->migration->id() === 'iwp_gallery_redirects') {
+      return TRUE;
+    }
+
+    return TRUE;
+  }
+
+}
