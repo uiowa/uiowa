db6fd85d7f49000997870caa211c20ede3bee2dd
Cory Skeers <cory-skeers@uiowa.edu>
2024-08-21 15:45:27 -0500

    Source plugin class, definitions.

---
+++ docroot/sites/inrc.law.uiowa.edu/modules/inrc_migrate/src/Plugin/migrate/source/Nonprofit.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\inrc_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
+
+/**
+ * Migrate Source plugin.
+ *
+ * @MigrateSource(
+ *   id = "inrc_nonprofit",
+ *   source_module = "node"
+ * )
+ */
+class Nonprofit extends BaseNodeSource {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = parent::fields();
+    $fields['alias'] = $this->t('The URL alias for this node.');
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    // Skip this node if it comes after our last migrated.
+    if ($row->getSourceProperty('nid') < $this->getLastMigrated()) {
+      return FALSE;
+    }
+    parent::prepareRow($row);
+
+    // Skip over the rest of the preprocessing, as it's not needed
+    // for redirects. Also avoids duplicating the notices.
+    // Return TRUE as long as we have an alias so that
+    // the row will be created.
+    if ($this->migration->id() === 'inrc_nonprofit_redirects') {
+      if ($row->getSourceProperty('alias')) {
+        return TRUE;
+      }
+      return FALSE;
+    }
+
+    // Convert the UNIX timestamps into date strings.
+    foreach ([
+      'field_np_board_res_date',
+      'field_np_last_training_date',
+      'field_expiration_date',
+    ] as $field) {
+      if ($timestamp = $row->getSourceProperty($field)) {
+        $row->setSourceProperty($field, date('Y-m-d', $timestamp[0]['value']));
+      }
+    }
+
+    return TRUE;
+  }
+
+}
