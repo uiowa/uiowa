fe7677a80d771ec434a7a59ce08e9a43abd83713
Alan Way <gary-ridgway@uiowa.edu>
2024-09-10 13:42:10 -0500

    Initialize mensicehockeyclub.org.uiowa.edu multisite on uiowa07

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -7564,3 +7564,9 @@ $sites['hancher.uiowa.ddev.site'] = 'hancher.uiowa.edu';
 $sites['hancher.dev.drupal.uiowa.edu'] = 'hancher.uiowa.edu';
 $sites['hancher.stage.drupal.uiowa.edu'] = 'hancher.uiowa.edu';
 $sites['hancher.prod.drupal.uiowa.edu'] = 'hancher.uiowa.edu';
+
+// Directory aliases for mensicehockeyclub.org.uiowa.edu.
+$sites['orgmensicehockeyclub.uiowa.ddev.site'] = 'mensicehockeyclub.org.uiowa.edu';
+$sites['orgmensicehockeyclub.dev.drupal.uiowa.edu'] = 'mensicehockeyclub.org.uiowa.edu';
+$sites['orgmensicehockeyclub.stage.drupal.uiowa.edu'] = 'mensicehockeyclub.org.uiowa.edu';
+$sites['orgmensicehockeyclub.prod.drupal.uiowa.edu'] = 'mensicehockeyclub.org.uiowa.edu';
