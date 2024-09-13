af33db825f52ce3306f74e75604bb1546350a723
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2024-09-13 13:46:41 -0500

    add caching to the session options

--- docroot/sites/registrar.uiowa.edu/modules/registrar_core/src/SessionOptionsTrait.php
+++ docroot/sites/registrar.uiowa.edu/modules/registrar_core/src/SessionOptionsTrait.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\registrar_core;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\uiowa_maui\MauiApi;
 
 /**
@@ -42,7 +43,23 @@ trait SessionOptionsTrait {
     if (!isset($this->maui)) {
       $this->maui = \Drupal::service('uiowa_maui.api');
     }
+    // Define a unique cache ID based on method parameters.
+    $cid = 'registrar_core:session_options:' . $previous . ':' . $future . ':' . ($legacy ? 'legacy' : 'id');
 
+    // Get the cache backend.
+    $cache = \Drupal::cache();
+
+    // Try to load the data from cache.
+    if ($cache_data = $cache->get($cid)) {
+      return $cache_data->data;
+    }
+
+    // Fetch the page cache maximum age from the configuration.
+    $config = \Drupal::config('system.performance');
+    $max_age = $config->get('cache.page.max_age');
+    $request_time = \Drupal::time()->getRequestTime();
+
+    // Fetch session data via the MAUI API.
     $sessions = $this->maui->getSessionsBounded($previous, $future);
     $options = [];
 
@@ -51,6 +68,10 @@ trait SessionOptionsTrait {
     foreach ($sessions as $session) {
       $options[$session->$key] = $session->shortDescription;
     }
+
+    // Cache the result using the page cache maximum age setting.
+    $expiration_time = $max_age > 0 ? $request_time + $max_age : Cache::PERMANENT;
+    $cache->set($cid, $options, $expiration_time, ['maui_api']);
 
     return $options;
   }
