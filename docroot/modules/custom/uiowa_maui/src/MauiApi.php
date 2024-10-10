57840003cb202973b2112ed4cb1b7f8402959ed7
Cory Skeers <cory-skeers@uiowa.edu>
2024-10-10 15:45:55 -0500

    Extend apiclientbase

--- docroot/modules/custom/uiowa_maui/src/MauiApi.php
+++ docroot/modules/custom/uiowa_maui/src/MauiApi.php
@@ -4,7 +4,10 @@ namespace Drupal\uiowa_maui;
 
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\uiowa_core\ApiAuthKeyTrait;
+use Drupal\uiowa_core\ApiClientBase;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
@@ -15,132 +18,22 @@ use Psr\Log\LoggerInterface;
  *
  * @see: https://api.maui.uiowa.edu/maui/pub/webservices/documentation.page
  */
-class MauiApi {
-
+class MauiApi extends ApiClientBase {
+  use ApiAuthKeyTrait;
   use SessionTermTrait;
 
-  const BASE = 'https://api.maui.uiowa.edu/maui/api/';
-
   /**
-   * The uiowa_maui logger channel.
-   *
-   * @var \Psr\Log\LoggerInterface
+   * {@inheritdoc}
    */
-  protected $logger;
-
-  /**
-   * The uiowa_maui cache.
-   *
-   * @var \Drupal\Core\Cache\CacheBackendInterface
-   */
-  protected $cache;
-
-  /**
-   * The HTTP client.
-   *
-   * @var \GuzzleHttp\ClientInterface
-   */
-  protected $client;
-
-  /**
-   * Constructs a Maui object.
-   *
-   * @param \Psr\Log\LoggerInterface $logger
-   *   The uiowa_maui logger channel.
-   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
-   *   The uiowa_maui cache.
-   * @param \GuzzleHttp\ClientInterface $http_client
-   *   The HTTP client.
-   */
-  public function __construct(LoggerInterface $logger, CacheBackendInterface $cache, ClientInterface $http_client) {
-    $this->logger = $logger;
-    $this->cache = $cache;
-    $this->client = $http_client;
+  public function basePath(): string {
+    return 'https://api.maui.uiowa.edu/maui/api/';
   }
 
   /**
-   * Make a MAUI API request and return data.
-   *
-   * @param string $method
-   *   The HTTP method to use.
-   * @param string $path
-   *   The API path to use. Do not include the base URL.
-   * @param array $params
-   *   Optional request parameters.
-   * @param array $options
-   *   Optional request options. All requests expect JSON response data.
-   *
-   * @return mixed
-   *   The API response data.
+   * {@inheritdoc}
    */
-  public function request($method, $path, array $params = [], array $options = []) {
-    // Encode any special characters and trim duplicate slash.
-    $path = UrlHelper::encodePath($path);
-    $uri = self::BASE . ltrim($path, '/');
-
-    // Append any query string parameters.
-    if (!empty($params)) {
-      $query = UrlHelper::buildQuery($params);
-      $uri .= "?{$query}";
-    }
-
-    // Merge additional options with default but allow overriding.
-    $options = array_merge([
-      'headers' => [
-        'Accept' => 'application/json',
-      ],
-    ], $options);
-
-    // Create a hash for the CID. Can always be decoded for debugging purposes.
-    $hash = base64_encode($uri . serialize($options));
-    $cid = "uiowa_maui:request:{$hash}";
-    // Default $data to FALSE in case of API fetch failure.
-    $data = FALSE;
-
-    if ($cache = $this->cache->get($cid)) {
-      $data = $cache->data;
-    }
-    else {
-      try {
-        $response = $this->client->request($method, $uri, $options);
-      }
-      catch (RequestException | GuzzleException $e) {
-        $this->logger->error('Error encountered getting data from @endpoint: @code @error', [
-          '@endpoint' => $uri,
-          '@code' => $e->getCode(),
-          '@error' => $e->getMessage(),
-        ]);
-      }
-
-      if (isset($response)) {
-        $contents = $response->getBody()->getContents();
-
-        if ($options['headers']['Accept'] === 'application/xml') {
-          // Quick and dirty way to use the XML and JSON parsers
-          // to convert from XML to an associative PHP array. XML
-          // allows non-unique names in children unlike JSON, so
-          // we encode to force unique names, then decode to
-          // convert to an array. Based on
-          // https://hakre.wordpress.com/2013/07/09/simplexml-and-json-encode-in-php-part-i/
-          $xml = simplexml_load_string($contents);
-          $data = json_decode(
-            json_encode($xml),
-            TRUE
-          );
-          // Place the contents back into the XML parent's namespace.
-          $data = [$xml->getName() => $data];
-        }
-        else {
-          /** @var object $data */
-          $data = json_decode($contents);
-        }
-
-        // Cache for 15 minutes.
-        $this->cache->set($cid, $data, time() + 900);
-      }
-    }
-
-    return $data;
+  protected function getCacheIdBase(): string {
+    return 'uiowa_maui';
   }
 
   /**
