f57dc7b2d39bfdda28f18b171f8ad4b83c234f09
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2024-10-21 17:05:54 -0500

    Added annotation.

--- docroot/modules/custom/sitenow_advanced_webform/src/Plugin/ProspectorRemotePostWebformHandler.php
+++ docroot/modules/custom/sitenow_advanced_webform/src/Plugin/ProspectorRemotePostWebformHandler.php
@@ -2,10 +2,23 @@
 
 namespace Drupal\sitenow_advanced_webform\Plugin\WebformHandler;
 
-use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\webform\Plugin\WebformHandlerBase;
 
+/**
+ * Webform submission remote post handler.
+ *
+ * @WebformHandler(
+ *   id = "prospector_remote_post",
+ *   label = @Translation("Prospector Remote Post"),
+ *   category = @Translation("External"),
+ *   description = @Translation("Posts webform submissions to a URL."),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_UNLIMITED,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_OPTIONAL,
+ *   tokens = TRUE,
+ * )
+ */
 class ProspectorRemotePostWebformHandler extends WebformHandlerBase {
 
   /**
