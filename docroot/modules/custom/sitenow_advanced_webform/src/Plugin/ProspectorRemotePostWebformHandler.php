a8e1fce5aa61d58ec62752c92e9323ca6437e4b8
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2024-10-22 10:15:28 -0500

    A little bit more cleanup.

--- docroot/modules/custom/sitenow_advanced_webform/src/Plugin/ProspectorRemotePostWebformHandler.php
+++ docroot/modules/custom/sitenow_advanced_webform/src/Plugin/ProspectorRemotePostWebformHandler.php
@@ -24,39 +24,9 @@ class ProspectorRemotePostWebformHandler extends WebformHandlerBase {
   /**
    * {@inheritdoc}
    */
-  public function defaultConfiguration() {
-    return [
-      'endpoint_url' => '',
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    // @todo Add configuration form elements.
-    return $this->setSettingsParents($form);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
     $this->applyFormStateToConfiguration($form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getSummary() {
-    $settings = $this->getSettings();
-
-    return [
-      '#markup' => $this->t('<strong>Endpoint URL:</strong> @endpoint', [
-        '@endpoint' => $settings['endpoint_url'],
-      ]),
-    ];
   }
 
   /**
