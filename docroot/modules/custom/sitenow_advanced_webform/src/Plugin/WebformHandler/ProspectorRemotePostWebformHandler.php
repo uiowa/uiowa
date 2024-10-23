ffaaf4b4d74aa6a1eb903f66fdc6028b1e5f2134
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2024-10-23 12:27:46 -0500

    Config form for site UUID.

--- docroot/modules/custom/sitenow_advanced_webform/src/Plugin/WebformHandler/ProspectorRemotePostWebformHandler.php
+++ docroot/modules/custom/sitenow_advanced_webform/src/Plugin/WebformHandler/ProspectorRemotePostWebformHandler.php
@@ -5,6 +5,7 @@ namespace Drupal\sitenow_advanced_webform\Plugin\WebformHandler;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\webform\Plugin\WebformHandlerBase;
 use Drupal\webform\WebformSubmissionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Webform submission remote post handler.
@@ -23,6 +24,22 @@ use Drupal\webform\WebformSubmissionInterface;
 class ProspectorRemotePostWebformHandler extends WebformHandlerBase {
 
   /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->httpClient = $container->get('http_client');
+    return $instance;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
@@ -38,7 +55,16 @@ class ProspectorRemotePostWebformHandler extends WebformHandlerBase {
     // @todo Load basic auth credentials.
     // @todo Build Guzzle request.
     // @todo Add data from first_name, last_name, email, and phone fields.
+    $elements = ['first_name', 'last_name', 'email', 'phone'];
+    $data = [];
+    foreach ($elements as $element) {
+      if ($value = $webform_submission->getElementData($element)) {
+        $data[$element] = $value;
+      }
+    }
     // @todo Send request.
+    $params = UrlHelper::buildQuery([$data]);
+    $response = $this->httpClient->get($request_url, $request_options);
     // @todo Handle exceptions.
     // @todo Log request and response.
   }
