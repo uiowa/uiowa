bf7eae2edbc00f7314d79f02fdb4023379305d9c
Benjamin Speare <benjamin-speare@uiowa.edu>
2024-11-11 11:07:07 -0600

    Added settings checkbox to uiowa_core to set/unset Google Analytics.

--- docroot/modules/custom/uiowa_core/src/Form/SettingsForm.php
+++ docroot/modules/custom/uiowa_core/src/Form/SettingsForm.php
@@ -58,6 +58,20 @@ class SettingsForm extends ConfigFormBase {
       '#size' => 60,
     ];
 
+    $form['ga'] = [
+      '#type' => 'fieldset',
+      '#title' => 'Google Analytics',
+      '#collapsible' => FALSE,
+    ];
+
+    $form['ga']['uiowa_core_ga'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Include Google Analytics Functionality'),
+      '#default_value' => $config->get('uiowa_core.ga'),
+      '#description' => $this->t('If checked, and Google Analytics are configured, GA snippets will be inserted and loaded on the website.'),
+      '#size' => 60,
+    ];
+
     return $form;
   }
 
@@ -68,6 +82,7 @@ class SettingsForm extends ConfigFormBase {
     $this->config('uiowa_core.settings')
       ->set('uiowa_core.gtag', $form_state->getValue('uiowa_core_gtag'))
       ->set('uiowa_core.campus_gtm', $form_state->getValue('uiowa_core_campus_gtm'))
+      ->set('uiowa_core.ga', $form_state->getValue('uiowa_core_ga'))
       ->save();
     parent::submitForm($form, $form_state);
 
