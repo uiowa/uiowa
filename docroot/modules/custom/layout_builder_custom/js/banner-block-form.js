785811be3025c84ebc4b0848d8e6f48be4079f0f
Alan Way <gary-ridgway@uiowa.edu>
2025-02-04 14:04:04 -0600

    Hey look, its vanilla now!.

--- docroot/modules/custom/layout_builder_custom/js/banner-block-form.js
+++ docroot/modules/custom/layout_builder_custom/js/banner-block-form.js
@@ -9,22 +9,21 @@
       $(once('media-form-attach', '.media-library-widget', context)).each(function () {
 
         // Check that we can access the next field.
-        const checkbox_wrapper = $('div[data-drupal-selector$="autoplay-wrapper"]');
-
-        if (checkbox_wrapper.length) {
+        const checkbox_wrapper = context.querySelector('div[data-drupal-selector$="autoplay-wrapper"]');
+        if (checkbox_wrapper) {
           // Check if the referenced media is a video.
-          const mediaTypeVideo = this.querySelector('.media--video');
-
+          const mediaTypeVideo = context.querySelector('.media--video')
+          //
           if (mediaTypeVideo) {
             // Show the autoplay field.
-            checkbox_wrapper.removeClass('js-hide');
-            checkbox_wrapper.removeAttr('tabindex');
-            checkbox_wrapper.removeAttr('aria-hidden');
+            checkbox_wrapper.classList.remove('js-hide');
+            checkbox_wrapper.removeAttribute('tabindex');
+            checkbox_wrapper.removeAttribute('aria-hidden');
           } else {
             // Hide the autoplay field.
-            checkbox_wrapper.addClass('js-hide');
+            checkbox_wrapper.classList.add('js-hide');
             checkbox_wrapper.tabIndex = -1;
-            checkbox_wrapper.attr('aria-hidden', 'true');
+            checkbox_wrapper.setAttribute('aria-hidden', 'true');
           }
         }
       });
