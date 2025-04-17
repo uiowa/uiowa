d8e2a2f16700b13813b4dac1d6eb0df3db82126b
Benjamin Speare <benjamin-speare@uiowa.edu>
2025-04-17 08:33:37 -0500

    Updated to get the gallery working.

--- docroot/sites/iowaprotocols.medicine.uiowa.edu/modules/iowaprotocols_migrate/src/Plugin/migrate/source/Protocol.php
+++ docroot/sites/iowaprotocols.medicine.uiowa.edu/modules/iowaprotocols_migrate/src/Plugin/migrate/source/Protocol.php
@@ -15,18 +15,8 @@ use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
  * )
  */
 class Protocol extends BaseNodeSource {
-  use ProcessMediaTrait;
 
-  /**
-   * {@inheritdoc}
-   */
-  public function query() {
-    $query = parent::query();
-    // Limit the migration to only those rooms
-    // which are published on the source.
-    $query->condition('n.status', '1', '=');
-    return $query;
-  }
+  use ProcessMediaTrait;
 
   /**
    * {@inheritdoc}
@@ -34,22 +24,37 @@ class Protocol extends BaseNodeSource {
   public function prepareRow(Row $row) {
     parent::prepareRow($row);
 
-    // Process the gallery.
-    $gallery_images = $row->getSourceProperty('field_basic_page_gallery');
-    if (!empty($gallery_images)) {
+    // Process the gallery images from field_article_gallery.
+    $gallery = $row->getSourceProperty('field_basic_page_gallery');
+    if (!empty($gallery)) {
       $new_images = [];
-      foreach ($gallery_images as $gallery_image) {
+      foreach ($gallery as $gallery_image) {
         $new_images[] = $this->processImageField(
           $gallery_image['fid'],
           $gallery_image['alt'],
           $gallery_image['title'],
+          $gallery_image['title']
         );
       }
-      $row->setSourceProperty('field_basic_page_gallery', $new_images);
+      $row->setSourceProperty('gallery', $new_images);
+    }
+
+    // Replace inline files and images in the body,
+    // and set for placement in the body and teaser fields.
+    $body = $row->getSourceProperty('body');
+    if (!empty($body)) {
+      $this->viewMode = 'large';
+      $this->align = 'left';
+      // Search for D7 inline embeds and replace with D8 inline entities.
+      $body[0]['value'] = $this->replaceInlineFiles($body[0]['value']);
+
+      // Set the format to filtered_html while we have it.
+      $body[0]['format'] = 'filtered_html';
+
+      $row->setSourceProperty('body', $body);
     }
 
     return TRUE;
-
   }
 
 }
