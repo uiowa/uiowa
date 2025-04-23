852db147fae36b445153468d761f0f293c739300
James Leone <james-leone@uiowa.edu>
2025-04-23 10:22:05 -0400

    Updating gramatical errors.

--- docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/AccessibilityLinks.php
+++ docroot/sites/facilities.uiowa.edu/modules/facilities_core/src/Plugin/Block/AccessibilityLinks.php
@@ -51,7 +51,7 @@ class AccessibilityLinks extends BlockBase {
         'id' => '7c639bd4000b4d34979a3a3a628d7180',
       ],
       'gender_inclusive_restroom' => [
-        'label' => 'Single use restrooms',
+        'label' => 'SSingle user restrooms',
         'icon' => 'person-half-dress',
         'id' => 'a787689a1da843018156b2f2e97da119',
       ],
