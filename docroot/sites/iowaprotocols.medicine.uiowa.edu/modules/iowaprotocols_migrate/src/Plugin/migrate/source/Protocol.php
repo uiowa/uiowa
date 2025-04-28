62a2bba076907a9169bba0a7d174913eaea7a999
Alan Way <gary-ridgway@uiowa.edu>
2025-04-28 13:38:41 -0500

    It maps!

--- docroot/sites/iowaprotocols.medicine.uiowa.edu/modules/iowaprotocols_migrate/src/Plugin/migrate/source/Protocol.php
+++ docroot/sites/iowaprotocols.medicine.uiowa.edu/modules/iowaprotocols_migrate/src/Plugin/migrate/source/Protocol.php
@@ -5,6 +5,7 @@ namespace Drupal\iowaprotocols_migrate\Plugin\migrate\source;
 use Drupal\migrate\Row;
 use Drupal\sitenow_migrate\Plugin\migrate\source\BaseNodeSource;
 use Drupal\sitenow_migrate\Plugin\migrate\source\ProcessMediaTrait;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Migrate Source plugin.
@@ -19,10 +20,30 @@ class Protocol extends BaseNodeSource {
   use ProcessMediaTrait;
 
   /**
+   * Tag-to-name mapping for keywords.
+   *
+   * @var array
+   */
+  protected $tagMapping;
+
+  /**
    * {@inheritdoc}
    */
   public function prepareRow(Row $row) {
     parent::prepareRow($row);
+
+    // Establish an array to eventually map to field_tags.
+    $tids = [];
+
+    // Set our tagMapping if it's not already.
+    if (empty($this->tagMapping)) {
+      $this->tagMapping = \Drupal::database()
+        ->select('taxonomy_term_field_data', 't')
+        ->fields('t', ['name', 'tid'])
+        ->condition('t.vid', 'tags', '=')
+        ->execute()
+        ->fetchAllKeyed();
+    }
 
     // Process the gallery images from field_article_gallery.
     $gallery = $row->getSourceProperty('field_basic_page_gallery');
@@ -54,7 +75,39 @@ class Protocol extends BaseNodeSource {
       $row->setSourceProperty('body', $body);
     }
 
+    // Process the gallery images from field_article_gallery.
+    $category = $row->getSourceProperty('field_category')[0]["value"];
+    if (!empty($category)) {
+      $tid = $this->createTag($category);
+      $tids[] = $tid;
+    }
+
+    // Send all final tids to field_tags.
+    if (!empty($tids)) {
+      $row->setSourceProperty('tags', $tids);
+    }
+
     return TRUE;
+  }
+
+  /**
+   * Helper function to check for existing tags and create if they don't exist.
+   */
+  private function createTag($tag_name) {
+    // Check if we have a mapping. If we don't yet,
+    // then create a new tag and add it to our map.
+    if (!isset($this->tagMapping[$tag_name])) {
+      $term = Term::create([
+        'name' => $tag_name,
+        'vid' => 'tags',
+      ]);
+      if ($term->save()) {
+        $this->tagMapping[$tag_name] = $term->id();
+      }
+    }
+
+    // Return tid for mapping to field.
+    return $this->tagMapping[$tag_name];
   }
 
 }
