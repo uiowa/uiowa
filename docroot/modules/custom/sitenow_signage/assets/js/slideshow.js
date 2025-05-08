83e1bd0f350ead90dc2a4bb66856e236b746c18e
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2025-05-08 14:22:56 -0500

    Updated to add JS file and get the loading the library working.

---
+++ docroot/modules/custom/sitenow_signage/assets/js/slideshow.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * JavaScript for the date and time block.
+ */
+
+(function ($, Drupal, once) {
+  Drupal.behaviors.signageSlideshow = {
+    attach: function (context, settings) {
+      // Initialize splide for each slideshow.
+    },
+  }
+});
