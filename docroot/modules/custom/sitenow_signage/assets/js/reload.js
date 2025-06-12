336d2694cc01410d1666784158a2d0a64bdc514b
Sean Adams-Hiett <sean-adams-hiett@uiowa.edu>
2025-06-12 10:37:02 -0500

    Adapted D7 JS. Added library definition and loading of the library.

--- docroot/modules/custom/sitenow_signage/assets/js/reload.js
+++ docroot/modules/custom/sitenow_signage/assets/js/reload.js
@@ -4,53 +4,49 @@
  */
 
  // Namespace jQuery to avoid conflicts.
-(function (Drupal, drupalSettings, $) {
-  // Fix to hide AJAX error alert messages.
-  // http://drupal.org/node/1232416#comment-6667014
-  window.alert = function (arg) {
-    if (window.console && console.log) { console.log(arg); }
+(function (Drupal, drupalSettings, once, $) {
+
+  // Attach reload behavior.
+  Drupal.behaviors.reload = {
+    attach: function (context, settings) {
+      once('reload', 'html', context).forEach(() => {
+        Drupal.signageReload();
+      });
+    }
   };
+
   // Define the behavior.
   Drupal.signageReload = function () {
-    // Call updateAvailability every ten minutes.
-    setInterval(Drupal.signageReload.updateWindow, drupalSettings.signageSign.reloadValue);
+    // @todo Set timeout from settings.
+    setInterval(Drupal.signageReload.updateWindow, 5 * 60 * 60);
   };
 
   function hostReachable(location) {
     if (!location) {
       location = window.location.hostname + "/";
     }
-    // Handle IE and more capable browsers
-    const xhr = new(window.ActiveXObject || XMLHttpRequest )( "Microsoft.XMLHTTP");
-
+    // Craft a new `XMLHttpRequest()`.
+    const xhttp = new XMLHttpRequest();
 
     // Open new request as a HEAD to the root hostname with a random param to bust the cache
-    xhr.open("HEAD", "//" + location + "?rand=" + Math.floor((1 + Math.random()) * 0x10000), false);
+    xhttp.open('HEAD', '//' + location + '?rand=' + Math.floor((1 + Math.random()) * 0x10000), false);
 
     // Issue request and handle response
     try {
-      xhr.send();
-      return (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304);
+      xhttp.send();
+      return (xhttp.status >= 200 && xhttp.status < 300 || xhttp.status === 304);
     } catch (error) {
       return false;
     }
 
   }
 
-  // Attach reload behavior.
-  Drupal.behaviors.reload = {
-    attach: function (context, settings) {
-      $('html', context).once('reload', function () {
-        Drupal.signageReload();
-      });
-    }
-  };
-
   // Update all Availability on the page.
   Drupal.signageReload.updateWindow = function () {
-    if (hostReachable(window.location.hostname + drupalSettings.basePath)) {
+    if (hostReachable(window.location.hostname + drupalSettings.path.baseUrl)) {
+      console.log('Digital Signage: Reloading sign.')
       document.location.reload(true);
     }
   };
 
-})(Drupal, drupalSettings, jQuery);
+})(Drupal, drupalSettings, once, jQuery);
