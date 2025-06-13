6c9a9a56537a52c4d6ae030abbf63809fa0bc02d
Benjamin Speare <benjamin-speare@uiowa.edu>
2025-06-13 15:23:29 -0500

    Added view and forecords_core module.

---
+++ docroot/sites/records.fo.uiowa.edu/modules/forecords_core/src/Entity/Record.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\forecords_core\Entity;
+
+use Drupal\uiowa_core\Entity\NodeBundleBase;
+use Drupal\uiowa_core\Entity\RendersAsCardInterface;
+
+/**
+ * Provides an interface for record entities.
+ */
+class Record extends NodeBundleBase implements RendersAsCardInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildCard(array &$build) {
+    parent::buildCard($build);
+
+    // Process additional card mappings.
+    $this->mapFieldsToCardBuild($build, [
+      '#meta' => [
+        'field_record_ad_unit_responsible',
+        'field_record_files_maintained_by',
+        'field_record_category',
+        'field_record_ui_retention_guide',
+      ],
+    ]);
+    $build['#link_indicator'] = TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultCardStyles(): array {
+    return [
+      ...parent::getDefaultCardStyles(),
+    ];
+  }
+
+}
