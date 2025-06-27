7be92347b50b8f2d548503b60f5fdeea27f16ecc
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2025-06-27 11:59:30 -0500

    intial stop prediction output

--- docroot/modules/custom/sitenow_signage/src/ICAreaTransitApiClient.php
+++ docroot/modules/custom/sitenow_signage/src/ICAreaTransitApiClient.php
@@ -31,4 +31,16 @@ class ICAreaTransitApiClient extends ApiClientBase {
     return $response->stops;
   }
 
+  /**
+   * Get bus predictions based on stop id.
+   */
+  public function getPredictions(int $stop_id): array {
+    $response = $this->get('prediction', [
+      'query' => [
+        'stopid' => $stop_id,
+      ],
+    ]);
+    return $response->predictions;
+  }
+
 }
