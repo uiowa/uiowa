a6402d8256f1fd195102ef5aacad4c4b685d3082
Benjamin Speare <benjamin-speare@uiowa.edu>
2025-07-08 15:30:55 -0500

    Added fire log and crime log blocks that now use the logblock.php as the base for each block.

--- docroot/sites/safety.uiowa.edu/modules/safety_core/src/Plugin/Block/CrimeLogBlock.php
+++ docroot/sites/safety.uiowa.edu/modules/safety_core/src/Plugin/Block/CrimeLogBlock.php
@@ -2,14 +2,6 @@
 
 namespace Drupal\safety_core\Plugin\Block;
 
-use Drupal\Core\Url;
-use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Cache\Cache;
-use Drupal\safety_core\Controller\CleryController;
-use Drupal\Core\Datetime\DrupalDateTime;
-
 /**
  * Provides a Crime Log block.
  *
@@ -19,189 +11,48 @@ use Drupal\Core\Datetime\DrupalDateTime;
  *   category = @Translation("Site custom")
  * )
  */
-class CrimeLogBlock extends BlockBase implements ContainerFactoryPluginInterface {
+class CrimeLogBlock extends LogBlock {
 
   /**
-   * The Clery controller service.
-   *
-   * @var \Drupal\safety_core\Controller\CleryController
+   * {@inheritdoc}
    */
-  protected $cleryController;
-
-  /**
-   * Constructs a new CrimeLogBlock instance.
-   */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    CleryController $clery_controller,
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->cleryController = $clery_controller;
+  protected function getLogType() {
+    return 'crime';
   }
 
   /**
-   * Creates an instance of the plugin.
+   * {@inheritdoc}
    */
-  public static function create(
-    ContainerInterface $container,
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-  ) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('safety_core.clery_controller'),
-    );
+  protected function getLibraryName() {
+    return 'clery-log';
   }
 
   /**
-   * Builds the render array for this block.
+   * {@inheritdoc}
    */
-  public function build() {
-    $build = [];
-    $request = \Drupal::request();
-    $start_date = $request->query->get('start_date');
-    $end_date = $request->query->get('end_date');
-
-    // Set defaults if no search performed.
-    if (!$start_date && !$end_date) {
-      $start_date = (new DrupalDateTime('7 days ago'))->format('Y-m-d');
-      $end_date = (new DrupalDateTime('today'))->format('Y-m-d');
-    }
-
-    // Always show all results.
-    $limit = NULL;
-
-    // Build search form.
-    $min_date = (new DrupalDateTime())->modify('-60 days')->format('Y-m-d');
-    $max_date = (new DrupalDateTime())->format('Y-m-d');
-
-    $build['#attached']['library'][] = 'safety_core/clery-log';
-    $build['#attached']['library'][] = 'core/drupal.announce';
-
-    // Check if a search has been done.
-    $has_search = !empty($request->query->get('start_date')) || !empty($request->query->get('end_date'));
-
-    $actions = [
-      'submit' => [
-        '#type' => 'submit',
-        '#value' => $this->t('Search'),
-        '#button_type' => 'primary',
-        '#attributes' => [
-          'class' => ['bg--black'],
-        ],
-      ],
-    ];
-
-    // Only show reset button if a search was done.
-    if ($has_search) {
-      $actions['reset'] = [
-        '#type' => 'link',
-        '#title' => $this->t('Reset'),
-        '#url' => Url::fromRoute('<current>'),
-        '#attributes' => [
-          'class' => ['button', 'bttn--primary'],
-          'role' => 'button',
-        ],
-      ];
-    }
-
-    $build['search_form'] = [
-      '#type' => 'form',
-      '#method' => 'get',
-      '#attributes' => [
-        'class' => [
-          'bg--gray',
-          'uids-content',
-          'element--padding__all--minimal',
-          'block-margin__bottom--extra',
-          'uids-content--horizontal',
-        ],
-      ],
-      'start_date' => [
-        '#type' => 'date',
-        '#title' => $this->t('Start Date'),
-        '#name' => 'start_date',
-        '#value' => $request->query->get('start_date'),
-        '#attributes' => ['min' => $min_date, 'max' => $max_date],
-        '#wrapper_attributes' => ['class' => ['uids-content--horizontal-flex']],
-      ],
-      'end_date' => [
-        '#type' => 'date',
-        '#title' => $this->t('End Date'),
-        '#name' => 'end_date',
-        '#value' => $request->query->get('end_date'),
-        '#attributes' => ['min' => $min_date, 'max' => $max_date],
-        '#wrapper_attributes' => ['class' => ['uids-content--horizontal-flex']],
-      ],
-      'actions' => [
-        '#type' => 'actions',
-        '#attributes' => [
-          'class' => ['form-actions'],
-        ],
-      ] + $actions,
-    ];
-
-    try {
-      $crimes = $this->cleryController->getCrimeData($start_date, $end_date, $limit);
-      $crime_count = count($crimes);
-
-      $build['results'] = [
-        '#theme' => 'crime_log_table',
-        '#crimes' => $crimes,
-        '#start_date' => (new DrupalDateTime($start_date))->format('m/d/Y'),
-        '#end_date' => (new DrupalDateTime($end_date))->format('m/d/Y'),
-        '#cache' => ['max-age' => 3600],
-      ];
-
-      // Pass data to JS for announce functionality.
-      $build['#attached']['drupalSettings']['crimeLog'] = [
-        'crimeCount' => $crime_count,
-        'startDate' => (new DrupalDateTime($start_date))->format('m/d/Y'),
-        'endDate' => (new DrupalDateTime($end_date))->format('m/d/Y'),
-        'isSearch' => !empty($request->query->get('start_date')) || !empty($request->query->get('end_date')),
-      ];
-
-    }
-    catch (\Exception $e) {
-      \Drupal::logger('safety_core')->error('Crime log API error: @message', ['@message' => $e->getMessage()]);
-      $build['error'] = [
-        '#markup' => $this->t('Failed to load crime logs. Please try again later.'),
-      ];
-
-      // Pass errors to JS.
-      $build['#attached']['drupalSettings']['crimeLog'] = [
-        'error' => TRUE,
-        'isSearch' => !empty($request->query->get('start_date')) || !empty($request->query->get('end_date')),
-      ];
-    }
-
-    return $build;
+  protected function getDataKey() {
+    return 'crimes';
   }
 
   /**
-   * Gets cache contexts for this block.
+   * {@inheritdoc}
    */
-  public function getCacheContexts() {
-    return Cache::mergeContexts(parent::getCacheContexts(), ['url.query_args']);
+  protected function getCountKey() {
+    return 'crimeCount';
   }
 
   /**
-   * Gets cache tags for this block.
+   * {@inheritdoc}
    */
-  public function getCacheTags() {
-    return Cache::mergeTags(parent::getCacheTags(), ['crime_log_data']);
+  protected function getCacheTag() {
+    return 'crime_log_data';
   }
 
   /**
-   * Gets the cache max age for this block.
+   * {@inheritdoc}
    */
-  public function getCacheMaxAge() {
-    return 1800;
+  protected function getLogData($start_date, $end_date, $limit = NULL) {
+    return $this->cleryController->getCrimeData($start_date, $end_date, $limit);
   }
 
 }
--- docroot/sites/safety.uiowa.edu/modules/safety_core/src/Plugin/Block/FireLogBlock.php
+++ docroot/sites/safety.uiowa.edu/modules/safety_core/src/Plugin/Block/FireLogBlock.php
@@ -2,11 +2,7 @@
 
 namespace Drupal\safety_core\Plugin\Block;
 
-use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Cache\Cache;
-use Drupal\safety_core\Controller\CleryController;
+use Drupal\Core\Datetime\DrupalDateTime;
 
 /**
  * Provides a Fire Log block.
@@ -17,72 +13,61 @@ use Drupal\safety_core\Controller\CleryController;
  *   category = @Translation("Site custom")
  * )
  */
-class FireLogBlock extends BlockBase implements ContainerFactoryPluginInterface {
+class FireLogBlock extends LogBlock {
 
   /**
-   * The Clery controller service.
+   * {@inheritdoc}
+   */
+  protected function getLogType() {
+    return 'fire';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getLibraryName() {
+    return 'fire-log';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDataKey() {
+    return 'fires';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getCountKey() {
+    return 'fireCount';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getCacheTag() {
+    return 'fire_log_data';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getLogData($start_date, $end_date, $limit = NULL) {
+    return $this->cleryController->getFireData($start_date, $end_date, $limit);
+  }
+
+  /**
+   * Gets the default date range for fire logs.
    *
-   * @var \Drupal\safety_core\Controller\CleryController
+   * @return array
+   *   Array with 'start' and 'end' keys containing formatted dates.
    */
-  protected $cleryController;
-
-  /**
-   * Constructs a new FireLogBlock instance.
-   */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    CleryController $clery_controller,
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->cleryController = $clery_controller;
-  }
-
-  /**
-   * Creates an instance of the plugin.
-   */
-  public static function create(
-    ContainerInterface $container,
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-  ) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('safety_core.clery_controller'),
-    );
-  }
-
-  /**
-   * Builds the render array for this block.
-   */
-  public function build() {
-    $build = [];
-    return $build;
-  }
-
-  /**
-   * Gets cache contexts for this block.
-   */
-  public function getCacheContexts() {
-    return Cache::mergeContexts(parent::getCacheContexts(), ['url.query_args']);
-  }
-
-  /**
-   * Gets cache tags for this block.
-   */
-  public function getCacheTags() {
-    return Cache::mergeTags(parent::getCacheTags(), ['fire_log_data']);
-  }
-
-  /**
-   * Gets the cache max age for this block.
-   */
-  public function getCacheMaxAge() {
-    return 1800;
+  protected function getDefaultDateRange() {
+    return [
+      'start' => (new DrupalDateTime('60 days ago'))->format('Y-m-d'),
+      'end' => (new DrupalDateTime('today'))->format('Y-m-d'),
+    ];
   }
 
 }
