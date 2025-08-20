cadbab020e11ba8ab745f973d71b95ccc6e88654
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2025-08-20 09:10:17 -0500

    switching things back for now

--- docroot/modules/custom/sitenow_signage/js/reload.js
+++ docroot/modules/custom/sitenow_signage/js/reload.js
@@ -2,40 +2,52 @@
  * @file
  * Reload signage signs.
  */
+
+// Namespace jQuery to avoid conflicts.
 (function (Drupal, drupalSettings, once) {
 
   // Attach reload behavior.
   Drupal.behaviors.reload = {
     attach: function (context) {
-      once('signageReload', 'body', context).forEach(() => {
+      once('reload', 'html', context).forEach(() => {
         Drupal.signageReload();
       });
     }
   };
 
+  // Define the behavior.
   Drupal.signageReload = function () {
-    // Prevent stacked timers across attaches.
-    if (Drupal.signageReload.interval) {
-      clearInterval(Drupal.signageReload.interval);
-    }
-
-    const seconds = parseInt(drupalSettings?.signage?.signReloadInterval, 10) || 0;
-    console.log('Digital Signage: Reload interval is ' + seconds + ' seconds');
-
-    if (seconds > 0) {
-      Drupal.signageReload.interval = setInterval(Drupal.signageReload.updateWindow, seconds * 1000);
-    }
+    console.log('Digital Signage: Reload interval is ' + drupalSettings.signage.signReloadInterval + ' seconds');
+    // @todo Set timeout from settings.
+    setInterval(Drupal.signageReload.updateWindow, drupalSettings.signage.signReloadInterval * 1000);
   };
+
+  function hostReachable(location) {
+    if (!location) {
+      location = window.location.hostname + "/";
+    }
+    // Craft a new `XMLHttpRequest()`.
+    const xhttp = new XMLHttpRequest();
+
+    // Open new request as a HEAD to the root hostname with a random param to bust the cache
+    xhttp.open('HEAD', '//' + location + '?rand=' + Math.floor((1 + Math.random()) * 0x10000), false);
+
+    // Issue request and handle response
+    try {
+      xhttp.send();
+      return (xhttp.status >= 200 && xhttp.status < 300 || xhttp.status === 304);
+    } catch (error) {
+      return false;
+    }
+
+  }
 
   // Update all Availability on the page.
   Drupal.signageReload.updateWindow = function () {
-    console.log('Digital Signage: Reloading sign.');
-    const url = new URL(window.location.href);
-    // Remove any existing `_reload` param.
-    url.searchParams.delete('_reload');
-    // Add a fresh one.
-    url.searchParams.set('_reload', Date.now());
-    window.location.replace(url.toString());
+    if (hostReachable(window.location.hostname + drupalSettings.path.baseUrl)) {
+      console.log('Digital Signage: Reloading sign.')
+      document.location.reload(true);
+    }
   };
 
 })(Drupal, drupalSettings, once);
