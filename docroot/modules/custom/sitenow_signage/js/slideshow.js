56e0ced3967406627ff89575332f362e187ed18a
Benjamin Speare <benjamin-speare@uiowa.edu>
2025-08-28 11:39:08 -0500

    Added create/destroy example of how videos could restart within a slideshow.

--- docroot/modules/custom/sitenow_signage/js/slideshow.js
+++ docroot/modules/custom/sitenow_signage/js/slideshow.js
@@ -1,27 +1,41 @@
-/**
- * @file
- * JavaScript for the slideshow block.
- */
-
 (function (Drupal) {
   Drupal.behaviors.signageSlideshow = {
-    attach: function (context, settings) {
+    attach: function (context) {
       context.querySelectorAll('.signage__slideshow').forEach(function (element) {
-        // console.log('adding splide', element);
-        // Initialize Splide with the settings.
-        new Splide(element, {
+        const splide = new Splide(element, {
           autoplay: true,
           interval: 5000,
           type: 'fade',
           pauseOnHover: false,
           drag: false,
           speed: 1500,
-          slideFocus: false,
           arrows: false,
           pagination: false,
           rewind: true,
-        }).mount();
-      })
+        });
+
+        splide.on('move', (newIndex, prevIndex) => {
+          pauseVideo(splide.Components.Slides.getAt(prevIndex).slide);
+          playVideo(splide.Components.Slides.getAt(newIndex).slide);
+        });
+
+        splide.mount();
+      });
+
+      function pauseVideo(slide) {
+        const iframe = slide.querySelector('iframe.media-oembed-content');
+        if (iframe) {
+          iframe.dataset.originalSrc = iframe.src;
+          iframe.src = 'about:blank';
+        }
+      }
+
+      function playVideo(slide) {
+        const iframe = slide.querySelector('iframe.media-oembed-content');
+        if (iframe && iframe.dataset.originalSrc) {
+          iframe.src = iframe.dataset.originalSrc;
+        }
+      }
     },
-  }
+  };
 })(Drupal);
