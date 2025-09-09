116a719181d4071e345320f51bdd29729b7bcdcc
Benjamin Speare <benjamin-speare@uiowa.edu>
2025-09-09 11:09:24 -0500

    Updated JS to hide the other field as well when we are showing/hiding the form elements.

--- docroot/modules/custom/sitenow_signage/js/sign-duration.js
+++ docroot/modules/custom/sitenow_signage/js/sign-duration.js
@@ -1,31 +1,45 @@
 (function (Drupal) {
-  'use strict';
+  "use strict";
 
   Drupal.behaviors.signDurationToggle = {
     attach: function (context) {
-
       function toggleDurationFields() {
-        context.querySelectorAll('[data-drupal-selector*="field-sign-slides"] .ief-form').forEach(function(container) {
-          var hasVideo = container.querySelectorAll('.paragraph-type--slide-video').length > 0;
+        context
+          .querySelectorAll(
+            '[data-drupal-selector*="field-sign-slides"] .ief-form',
+          )
+          .forEach(function (container) {
+            var hasVideo =
+              container.querySelectorAll(".paragraph-type--slide-video")
+                .length > 0;
 
-          container.querySelectorAll('[data-drupal-selector*="field-slide-duration-wrapper"]').forEach(function(field) {
-            if (hasVideo) {
-              // Hide.
-              field.classList.add('js-hide');
-              field.tabIndex = -1;
-              field.setAttribute('aria-hidden', 'true');
-            } else {
-              // Show.
-              field.classList.remove('js-hide');
-              field.removeAttribute('tabindex');
-              field.removeAttribute('aria-hidden');
-            }
+            // Handle both main duration field and duration "other" field.
+            const durationFields = [
+              ...container.querySelectorAll(
+                '[data-drupal-selector*="field-slide-duration-wrapper"]',
+              ),
+              ...container.querySelectorAll(
+                ".field--name-field-slide-duration-other.field--widget-number",
+              ),
+            ];
+
+            durationFields.forEach(function (field) {
+              if (hasVideo) {
+                // Hide.
+                field.classList.add("js-hide");
+                field.tabIndex = -1;
+                field.setAttribute("aria-hidden", "true");
+              } else {
+                // Show.
+                field.classList.remove("js-hide");
+                field.removeAttribute("tabindex");
+                field.removeAttribute("aria-hidden");
+              }
+            });
           });
-        });
       }
 
       setTimeout(toggleDurationFields, 300);
-    }
+    },
   };
-
 })(Drupal);
--- docroot/modules/custom/sitenow_signage/js/slide-duration.js
+++ docroot/modules/custom/sitenow_signage/js/slide-duration.js
@@ -1,29 +1,32 @@
 (function (Drupal) {
-  'use strict';
+  "use strict";
 
   Drupal.behaviors.slideDurationToggle = {
     attach: function (context) {
-      // Check if video elements exist.
-      var hasVideo = context.querySelector('[class*="slide-video"]');
+      const hasVideo = context.querySelector('[class*="slide-video"]');
+      const durationField = context.querySelectorAll(
+        '[data-drupal-selector="edit-field-slide-duration"]',
+      );
+      const durationOtherField = context.querySelectorAll(
+        '[data-drupal-selector="edit-field-slide-duration-other-wrapper"]',
+      );
 
-      // Show/hide duration field.
-      var durationFields = context.querySelectorAll('[data-drupal-selector="edit-field-slide-duration"]');
-      durationFields.forEach(field => {
-        var container = field.closest('.js-form-item') || field;
+      // Merge both duration elements.
+      const allDurationFields = [...durationField, ...durationOtherField];
+
+      allDurationFields.forEach((element) => {
+        const container = element.closest(".js-form-item") || element;
 
         if (hasVideo) {
-          // Hide.
-          container.classList.add('js-hide');
+          container.classList.add("js-hide");
           container.tabIndex = -1;
-          container.setAttribute('aria-hidden', 'true');
+          container.setAttribute("aria-hidden", "true");
         } else {
-          // Show.
-          container.classList.remove('js-hide');
-          container.removeAttribute('tabindex');
-          container.removeAttribute('aria-hidden');
+          container.classList.remove("js-hide");
+          container.removeAttribute("tabindex");
+          container.removeAttribute("aria-hidden");
         }
       });
-    }
+    },
   };
-
 })(Drupal);
