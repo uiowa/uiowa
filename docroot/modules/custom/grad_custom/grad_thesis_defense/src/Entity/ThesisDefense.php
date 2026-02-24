8394bf15b2243134a7b7ad7aaab9eea61fd9bdb0
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2026-02-24 11:43:10 -0600

    adjust full and teaser displays based on issue summary

--- docroot/modules/custom/grad_custom/grad_thesis_defense/src/Entity/ThesisDefense.php
+++ docroot/modules/custom/grad_custom/grad_thesis_defense/src/Entity/ThesisDefense.php
@@ -18,25 +18,6 @@ class ThesisDefense extends NodeBundleBase implements RendersAsCardInterface {
 
     // Add the person library.
     $build['#attached']['library'][] = 'uids_base/person';
-    // Add the media library.
-    $build['#attached']['library'][] = 'uids_base/media';
-
-    // Handle the case when there is no image.
-    if (empty($build['#media'])) {
-      $build['#media']['empty'] = [
-        '#markup' => '<div class="img--empty">&nbsp;</div>',
-      ];
-    }
-
-    // Process additional card mappings.
-    $this->mapFieldsToCardBuild($build, [
-      '#subtitle' => 'field_thesis_defense_title',
-      '#meta' => [
-        'field_thesis_defense_date',
-        'field_thesis_defense_location',
-      ],
-      '#content' => 'field_thesis_defense_chairs',
-    ]);
   }
 
   /**
@@ -49,8 +30,6 @@ class ThesisDefense extends NodeBundleBase implements RendersAsCardInterface {
         'card_media_position' => 'card--layout-left',
         'border' => 'borderless',
         'headline_class' => 'headline--serif',
-        'media_format' => 'media--circle media--border',
-        'media_size' => 'media--small',
       ]
     );
   }
