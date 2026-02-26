2c118bcf9685e3b1cbddbdb0d864e9c3c3fd95f5
Benjamin Speare <benjamin-speare@uiowa.edu>
2026-02-26 16:15:08 -0600

    Added JS to remove the role=tab code from the refinement markup.

--- docroot/modules/custom/uiowa_search/js/cse.results.js
+++ docroot/modules/custom/uiowa_search/js/cse.results.js
@@ -16,11 +16,28 @@
       cseAttributes.as_sitesearch = window.location.hostname + drupalSettings.path.baseUrl;
     }
 
+    // Google injects this markup asynchronously after render.
+    // Observe the results container so role="tab" can be removed when it appears.
+    const removeRefinementTabRole = function () {
+      $('#search-results .gsc-tabHeader[role="tab"]').removeAttr('role');
+    };
+
+    const searchResults = document.getElementById('search-results');
+    if (searchResults) {
+      new MutationObserver(removeRefinementTabRole).observe(searchResults, {
+        childList: true,
+        subtree: true,
+      });
+    }
+
     google.search.cse.element.render({
         div: 'search-results',
         tag: 'search',
         attributes: cseAttributes
     });
+
+    // Handle matching markup inserted during initial render.
+    removeRefinementTabRole();
   };
 
   // Attach uiowaSearchResults behavior.
@@ -31,7 +48,7 @@
           parsetags: 'explicit',
           callback: Drupal.uiowaSearchResults,
         };
-
+        
         let cx = drupalSettings.uiowaSearch.engineId;
         let gcse = document.createElement('script');
         gcse.type = 'text/javascript';
