b3f22947c489034e1e0f3a39adde4609ae2a51dc
Cory Skeers <cory-skeers@uiowa.edu>
2026-03-04 16:55:22 -0600

    Add basic text fields. Update importer to add name/address to other field if there wasn't a mapped NID.

--- docroot/sites/commencement.uiowa.edu/modules/commencement_core/src/EventsProcessor.php
+++ docroot/sites/commencement.uiowa.edu/modules/commencement_core/src/EventsProcessor.php
@@ -104,7 +104,16 @@ class EventsProcessor extends EntityProcessorBase {
     // If the location field exists and is not null, it needs to be converted
     // to an entity ID for an existing venue.
     if (property_exists($record, 'location_name') && !is_null($record->location_name)) {
-      $record->location_name = $this->findVenueNid($record->location_name);
+      $venue_id = $this->findVenueNid($record->location_name);
+      if (!is_null($venue_id)) {
+        $record->location_name = $this->findVenueNid($record->location_name);
+      }
+      else {
+        // If the location venue did not exist, then move the name
+        // and address into separate, plain text fields.
+        $record->venue_name = $record->location_name;
+        $record->venue_address = $record->address;
+      }
     }
 
     // Convert empty strings to null for the room number field.
