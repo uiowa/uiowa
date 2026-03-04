4a7c1a0b716b9322fc7a29584f3e84721397c744
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2026-03-04 10:56:09 -0600

    Delete ukchoralcomposers.lib.uiowa.edu multisite on uiowa08

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -6965,12 +6965,6 @@ $sites['orgmensrugby.dev.drupal.uiowa.edu'] = 'mensrugby.org.uiowa.edu';
 $sites['orgmensrugby.stage.drupal.uiowa.edu'] = 'mensrugby.org.uiowa.edu';
 $sites['orgmensrugby.prod.drupal.uiowa.edu'] = 'mensrugby.org.uiowa.edu';
 
-// Directory aliases for ukchoralcomposers.lib.uiowa.edu.
-$sites['libukchoralcomposers.uiowa.ddev.site'] = 'ukchoralcomposers.lib.uiowa.edu';
-$sites['libukchoralcomposers.dev.drupal.uiowa.edu'] = 'ukchoralcomposers.lib.uiowa.edu';
-$sites['libukchoralcomposers.stage.drupal.uiowa.edu'] = 'ukchoralcomposers.lib.uiowa.edu';
-$sites['libukchoralcomposers.prod.drupal.uiowa.edu'] = 'ukchoralcomposers.lib.uiowa.edu';
-
 // Directory aliases for evedrewelowe.sites.uiowa.edu.
 $sites['sitesevedrewelowe.uiowa.ddev.site'] = 'evedrewelowe.sites.uiowa.edu';
 $sites['sitesevedrewelowe.dev.drupal.uiowa.edu'] = 'evedrewelowe.sites.uiowa.edu';
