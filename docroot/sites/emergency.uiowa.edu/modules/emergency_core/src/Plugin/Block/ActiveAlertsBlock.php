28d9ff557118c10ef4bbfda44e699d539d658142
Alan Way <gary-ridgway@uiowa.edu>
2026-04-30 14:24:15 -0500

    Dependency injection.

--- docroot/sites/emergency.uiowa.edu/modules/emergency_core/src/Plugin/Block/ActiveAlertsBlock.php
+++ docroot/sites/emergency.uiowa.edu/modules/emergency_core/src/Plugin/Block/ActiveAlertsBlock.php
@@ -3,9 +3,11 @@
 namespace Drupal\emergency_core\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\uiowa_core\HeadlineHelper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * An Active Alerts block.
@@ -16,7 +18,43 @@ use Drupal\uiowa_core\HeadlineHelper;
  *   category = @Translation("Site custom"),
  * )
  */
-class ActiveAlertsBlock extends BlockBase {
+class ActiveAlertsBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs an ActiveAlertsBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -66,7 +104,7 @@ class ActiveAlertsBlock extends BlockBase {
    */
   public function build() {
     $config = $this->getConfiguration();
-    $source = \Drupal::config('uiowa_alerts.settings')->get('hawk_alert.source');
+    $source = $this->configFactory->get('uiowa_alerts.settings')->get('hawk_alert.source');
 
     if (empty($config['headline'])) {
       $child_heading_size = $config['child_heading_size'];
