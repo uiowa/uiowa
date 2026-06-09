73970be2a7d300203eb7ef0af19e71f9274f1b0a
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2026-06-09 14:04:58 -0500

    remove delete op if protected, show protected state

--- docroot/modules/custom/sitenow_people/src/PersonTypeListBuilder.php
+++ docroot/modules/custom/sitenow_people/src/PersonTypeListBuilder.php
@@ -4,6 +4,7 @@ namespace Drupal\sitenow_people;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\sitenow_people\PersonTypeInterface;
 
 /**
  * Provides a listing of person types.
@@ -13,11 +14,24 @@ class PersonTypeListBuilder extends ConfigEntityListBuilder {
   /**
    * {@inheritdoc}
    */
+  public function getDefaultOperations(EntityInterface $entity) {
+    $operations = parent::getDefaultOperations($entity);
+    /** @var \Drupal\sitenow_people\PersonTypeInterface $entity */
+    if ($entity->isProtected()) {
+      unset($operations['delete']);
+    }
+    return $operations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function buildHeader() {
     $header['label'] = $this->t('Label');
     $header['id'] = $this->t('Machine name');
     $header['status'] = $this->t('Status');
     $header['allow_former'] = $this->t('Allow former');
+    $header['protected'] = $this->t('Protected');
     return $header + parent::buildHeader();
   }
 
@@ -30,6 +44,7 @@ class PersonTypeListBuilder extends ConfigEntityListBuilder {
     $row['id'] = $entity->id();
     $row['status'] = $entity->status() ? $this->t('Enabled') : $this->t('Disabled');
     $row['allow_former'] = $entity->get('allow_former') ? $this->t('Enabled') : $this->t('Disabled');
+    $row['protected'] = $entity->isProtected() ? $this->t('Yes') : $this->t('No');
     return $row + parent::buildRow($entity);
   }
 
