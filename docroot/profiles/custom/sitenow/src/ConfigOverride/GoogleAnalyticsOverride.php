ed1a37458ee373f111d9863b1ab5e7ba1dee8bfe
Joe Whitsitt <joe-whitsitt@uiowa.edu>
2026-07-10 10:52:19 -0500

    config override transition

--- docroot/profiles/custom/sitenow/src/ConfigOverride/GoogleAnalyticsOverride.php
+++ docroot/profiles/custom/sitenow/src/ConfigOverride/GoogleAnalyticsOverride.php
@@ -2,95 +2,27 @@
 
 namespace Drupal\sitenow\ConfigOverride;
 
-use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Config\ConfigFactoryOverrideInterface;
-use Drupal\Core\Config\StorageInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
- * Google Analytics configuration override.
+ * Deprecated shim so stale cached containers can still instantiate this.
+ *
+ * Delegates everything to GoogleTagOverride. Remove once all sites have
+ * rebuilt their service container.
  */
-class GoogleAnalyticsOverride implements ConfigFactoryOverrideInterface {
+class GoogleAnalyticsOverride extends GoogleTagOverride {
 
   /**
-   * The config factory service.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  private ConfigFactoryInterface $configFactory;
-
-  /**
-   * The request stack service.
-   *
-   * @var \Symfony\Component\HttpFoundation\RequestStack
-   */
-  private RequestStack $requestStack;
-
-  /**
-   * Constructs a new GoogleAnalyticsOverride object.
+   * Constructs a deprecated GoogleAnalyticsOverride shim.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory service.
-   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
-   *   The request stack service.
-   *
-   * @todo Remove $request_stack and the sandbox bypass in loadOverrides()
-   *   before merging to main. Added only to keep data streaming on
-   *   sandbox.dev.drupal.uiowa.edu while this branch is deployed to develop
-   *   ahead of the tmp_google_override sandbox testing branch.
+   * @param \Symfony\Component\HttpFoundation\RequestStack|null $request_stack
+   *   Unused; kept for the old service definition's signature.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, RequestStack $request_stack) {
-    $this->configFactory = $config_factory;
-    $this->requestStack = $request_stack;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function loadOverrides($names) {
-    $overrides = [];
-    if (in_array('google_analytics.settings', $names)) {
-      // @todo Remove this sandbox bypass before merging to main.
-      $request = $this->requestStack->getCurrentRequest();
-      $host = $request ? $request->getHost() : '';
-      $sandbox_bypass = in_array($host, ['sandbox.dev.drupal.uiowa.edu'], TRUE);
-
-      // Remove GA for local development.
-      $env = getenv('AH_SITE_ENVIRONMENT');
-      if ($env !== 'prod' && !$sandbox_bypass) {
-        $overrides['google_analytics.settings']['account'] = '';
-      }
-
-      // Remove GA based on uiowa_core settings.
-      $uiowa_core_config = $this->configFactory->get('uiowa_core.settings');
-      if ($uiowa_core_config && !$uiowa_core_config->get('uiowa_core.ga')) {
-        $overrides['google_analytics.settings']['account'] = '';
-      }
-    }
-
-    return $overrides;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCacheSuffix() {
-    return 'GoogleAnalyticsOverride';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCacheableMetadata($name) {
-    return new CacheableMetadata();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
-    return NULL;
+  public function __construct(ConfigFactoryInterface $config_factory, ?RequestStack $request_stack = NULL) {
+    parent::__construct($config_factory);
   }
 
 }
