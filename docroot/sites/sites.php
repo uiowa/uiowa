9f95c3cfa21a50f180bdd2eb367972327323b3ef
Alan Way <gary-ridgway@uiowa.edu>
2026-07-31 11:19:30 -0500

    Initialize foo.sites.uiowa.edu multisite on uiowa09

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -7888,3 +7888,9 @@ $sites['labtung.uiowa.ddev.site'] = 'tung.lab.uiowa.edu';
 $sites['labtung.dev.drupal.uiowa.edu'] = 'tung.lab.uiowa.edu';
 $sites['labtung.stage.drupal.uiowa.edu'] = 'tung.lab.uiowa.edu';
 $sites['labtung.prod.drupal.uiowa.edu'] = 'tung.lab.uiowa.edu';
+
+// Directory aliases for foo.sites.uiowa.edu.
+$sites['sitesfoo.uiowa.ddev.site'] = 'foo.sites.uiowa.edu';
+$sites['sitesfoo.dev.drupal.uiowa.edu'] = 'foo.sites.uiowa.edu';
+$sites['sitesfoo.stage.drupal.uiowa.edu'] = 'foo.sites.uiowa.edu';
+$sites['sitesfoo.prod.drupal.uiowa.edu'] = 'foo.sites.uiowa.edu';
